489870abc9476fd380c30858b8669d9bc3a721b1
Christopher Chedeau <vjeux@fb.com>
2014-05-05 09:43:56 -0700

    `npm test` in order to build the website!

---
+++ website/server/install.js
@@ -0,0 +1,71 @@
+
+var request = require('request');
+var glob = require('glob');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var server = require('./server.js');
+
+// Sadly, our setup fatals when doing multiple concurrent requests
+// I don't have the time to dig into why, it's easier to just serialize
+// requests.
+var queue = (function() {
+  var is_executing = false;
+  var queue = [];
+  function push(fn) {
+    queue.push(fn);
+    execute();
+  }
+  function execute() {
+    if (is_executing) {
+      return;
+    }
+    if (queue.length === 0) {
+      return;
+    }
+    var fn = queue.shift();
+    is_executing = true;
+    fn(function() {
+      is_executing = false;
+      execute()
+    });
+  }
+  return {push: push};
+})();
+
+glob('src/**/*.*', function (er, files) {
+  var count = files.length;
+
+  function done() {
+    if (--count === 0) {
+      console.log('Check out the build/ folder!');
+      server.close();
+    }
+  }
+
+  files.forEach(function(file) {
+    var targetFile = file.replace(/^src/, 'build');
+
+    if (file.match(/\.js$/)) {
+      targetFile = targetFile.replace(/\.js$/, '.html');
+      queue.push(function(cb) {
+        console.log(targetFile);
+        request('http://localhost:8080/' + targetFile.replace(/^build\//, ''), function(error, response, body) {
+          mkdirp.sync(targetFile.replace(new RegExp('/[^/]*$'), ''));
+          fs.writeFileSync(targetFile, body);
+          done();
+          cb();
+        });
+      });
+    } else {
+      queue.push(function(cb) {
+        console.log(targetFile);
+        fs.readFile(file, function(err, file) {
+          mkdirp.sync(targetFile.replace(new RegExp('/[^/]*$'), ''));
+          fs.writeFileSync(targetFile, file.toString());
+          done();
+          cb();
+        });
+      });
+    }
+  });
+})
--- website/server/server.js
+++ website/server/server.js
@@ -4,7 +4,7 @@ var http = require('http');
 var optimist = require('optimist');
 var path = require('path');
 var reactMiddleware = require('react-page-middleware');
-var build = require('./build.js');
+var convert = require('./convert.js');
 
 var argv = optimist.argv;
 
@@ -26,26 +26,22 @@ var buildOptions = {
   dev: argv.dev !== 'false'
 };
 
-var isServer = !argv.computeForPath;
-if (!isServer) {
-  reactMiddleware.compute(buildOptions)(argv.computeForPath, function(str) {
-    process.stdout.write(str);
-  });
-} else {
-  var app = connect()
-    .use(function(req, res, next) {
-      // build on every single request
-      build();
-      next();
-    })
-    .use(reactMiddleware.provide(buildOptions))
-    .use(connect['static'](FILE_SERVE_ROOT))
-    .use(connect.favicon(path.join(FILE_SERVE_ROOT, 'elements', 'favicon', 'favicon.ico')))
-    .use(connect.logger())
-    .use(connect.compress())
-    .use(connect.errorHandler());
+var app = connect()
+  .use(function(req, res, next) {
+    // convert all the md files on every request. This is not optimal
+    // but fast enough that we don't really need to care right now.
+    convert();
+    next();
+  })
+  .use(reactMiddleware.provide(buildOptions))
+  .use(connect['static'](FILE_SERVE_ROOT))
+  .use(connect.favicon(path.join(FILE_SERVE_ROOT, 'elements', 'favicon', 'favicon.ico')))
+  .use(connect.logger())
+  .use(connect.compress())
+  .use(connect.errorHandler());
 
-  var portToUse = port || 8080;
-  http.createServer(app).listen(portToUse);
-  console.log('Open http://localhost:' + portToUse + '/jest/index.html');
-}
+var portToUse = port || 8080;
+var server = http.createServer(app);
+server.listen(portToUse);
+console.log('Open http://localhost:' + portToUse + '/jest/index.html');
+module.exports = server;
