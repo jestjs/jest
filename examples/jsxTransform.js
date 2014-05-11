e88d341c4f6a26f724101277fd0864b4ac744852
Christopher Chedeau <vjeux@fb.com>
2014-05-10 19:38:02 -0700

    use environment file for __DEV__

--- examples/jsxTransform.js
+++ examples/jsxTransform.js
@@ -2,6 +2,6 @@ var ReactTools = require('react-tools');
 
 module.exports = {
   process: function(src) {
-    return 'var __DEV__ = true;' + ReactTools.transform(src, {harmony: true});
+    return ReactTools.transform(src, {harmony: true});
   }
 };
