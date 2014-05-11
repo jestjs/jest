627b99f613c1b065eb6e7ae6e49c237fa9783617
Christopher Chedeau <vjeux@fb.com>
2014-05-10 18:44:46 -0700

    make the test pass, super hacky though

--- examples/jsxTransform.js
+++ examples/jsxTransform.js
@@ -2,6 +2,6 @@ var ReactTools = require('react-tools');
 
 module.exports = {
   process: function(src) {
-    return ReactTools.transform(src);
+    return 'var __DEV__ = true;' + ReactTools.transform(src, {harmony: true});
   }
 };
