8a16ff6b31248a4e2bb56ce70c3bd789e51eea7d
Christopher Chedeau <vjeux@fb.com>
2014-05-10 18:42:09 -0700

    unmock react. But now, it crashes because __DEV__ is undefined :(

--- examples/__tests__/CheckboxWithLabel-test.js
+++ examples/__tests__/CheckboxWithLabel-test.js
@@ -1,11 +1,8 @@
-jest
-  .dontMock('ReactTestUtils')
-  .dontMock('../CheckboxWithLabel.js');
+jest.dontMock('../CheckboxWithLabel.js');
 
 describe('CheckboxWithLabel', function() {
   it('changes the text after click', function() {
     var CheckboxWithLabel = require('../CheckboxWithLabel.js');
     var ReactTestUtils = require('ReactTestUtils');
-    console.log('fu');
   });
 });
