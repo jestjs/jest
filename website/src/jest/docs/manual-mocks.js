e2fcab791e35024fb4498878fd43cc86434f9603
jeffmo <jeff@anafx.com>
2014-05-12 16:37:52 -0700

    Update ManualMocks.md

--- website/src/jest/docs/manual-mocks.js
+++ website/src/jest/docs/manual-mocks.js
@@ -9,46 +9,104 @@ module.exports = React.createClass({
     return layout({metadata: {"filename":"ManualMocks.js","id":"manual-mocks","title":"Manual mocks","layout":"docs","category":"Reference","permalink":"manual-mocks.html","next":"timer-mocks","href":"/jest/docs/manual-mocks.html"}}, `
 Although autogeneration of mocks is convenient, there are behaviors it misses, such as [fluent interfaces](http://martinfowler.com/bliki/FluentInterface.html). Furthermore, providing useful helpers on mock versions of a module, especially a core module, promotes reuse and can help to hide implementation details.
 
-Manual mocks are defined by writing a module in a \`__mocks__/\` subdirectory.
+Manual mocks are defined by writing a module in a \`__mocks__/\` subdirectory immediately adjacent to the module. When a manual mock exists for a given module, Jest's module system will just use that instead of trying to automatically generating a mock.
 
-TODO: This doesn't work well for relative requires :( We need to make it
-      possible to write manual mocks that only override references to local
-      versions of the module. So algo would be:
-
-      require('foo') -> dir/foo.js --exists?--> dir/__mocks__/foo.js
-
-      I (@jeffmo) stopped editing this doc here until we can get this issue
-      worked out. I think this should block release.
-
-The file name will be interpreted as the mocked module name. Although it's sometimes necessary to write a mock by hand to workaround missing DOM APIs, this should be avoided. If a module can't be mocked, the best fix is to implement the missing functionality in the test framework's DOM simulation.
-
-Assuming that the module can be loaded by the automocker, it's best to build on the automocked API. This makes it harder for mock APIs to get out of sync with real ones. For instance, let's say we're working on an \`Indicator\` that uses a \`Poller\`.
+Here's a contrived example where we have a module that provides a summary of all the files in a given directory.
 
 \`\`\`javascript
-var Poller = require('Poller');
-var Indicator = function() {
-  this.poller = new Poller().setInterval(2000);
+// FileSummarizer.js
+var fs = require('fs');
+
+function summarizeFilesInDirectorySync(directoryPath) {
+  return fs.readdirSync(directoryPath).map(function(fileName) {
+    return {
+      fileName: fileName,
+      directory: directoryPath
+    };
+  });
+  return directoryFileSummary;
+}
+
+exports.summarizeFilesInDirectorySync = summarizeFilesInDirectorySync;
+\`\`\`
+
+Since we'd like our tests to avoid actually hitting the disk (that's pretty slow and fragile), we create a manual mock for the \`fs\` module that implements custom versions of the \`fs\` APIs that we can build on for our tests:
+
+\`\`\`javascript
+// __mocks__/fs.js
+
+// Get the real (not mocked) version of the 'path' module
+var path = require.requireActual('path');
+
+// This is a custom function that our tests can use during setup to specify 
+// what the files on the "mock" filesystem should look like when any of the 
+// \`fs\` APIs are used.
+var _mockFiles = {};
+function __setMockFiles(newMockFiles) {
+  _mockFiles = {};
+  for (var file in newMockFiles) {
+    var dir = path.dirname(file);
+
+    if (!_mockFiles[dir]) {
+      _mockFiles[dir] = [];
+    }
+
+    _mockFiles[dir].push(path.basename(file));
+  }
 };
+
+// A custom version of \`readdirSync\` that reads from the special mocked out 
+// file list set via __setMockFiles
+function readdirSync(directoryPath) {
+  console.log('mock!');
+  return _mockFiles[directoryPath] || [];
+};
+
+exports.__setMockFiles = __setMockFiles;
+exports.readdirSync = readdirSync;
 \`\`\`
 
-In the test for \`Indicator\`, instances will have undefined values for their \`poller\` member unless we carefully \`mockReturnThis\` for \`Poller.prototype.setInterval\`. Let's make it easier for people to use our module by enhancing the mock for \`Poller\`
+Now we write our test:
 
 \`\`\`javascript
-var PollerMock = mockModules.generateMock('Poller');
-PollerMock.prototype.setInterval.mockReturnThis();
-module.exports = PollerMock;
+jest.dontMock('../FileSummarizer');
+
+describe('FileSummarizer', function() {
+  describe('listFilesInDirectorySync', function() {
+    var MOCK_FILE_INFO = {
+      '/path/to/file1.js':
+        'console.log("file1 contents");',
+
+      '/path/to/file2.txt':
+        "file2 contents"
+    };
+
+    beforeEach(function() {
+      // Set up some mocked out file info before each test
+      require('fs').__setMockFiles(MOCK_FILE_INFO);
+    });
+
+    it('includes all files in the directory in the summary', function() {
+      var FileSummarizer = require('../FileSummarizer');
+      var fileSummary = FileSummarizer.summarizeFilesInDirectorySync(
+        '/path/to'
+      );
+
+      expect(fileSummary.length).toBe(2);
+    });
+  });
+});
 \`\`\`
 
-Finally, it's often helpful to include a method that's intended to be called from a test that simulates behavior of mocked class, like \`mockInform\` in an hypothetical \`EventBus\` class. By convention, these helper methods should be prefixed with the word "mock" to make them clearly distinguishable from public APIs.
+As you can see, it's sometimes useful to do more than what the automatic mocker is capable of doing for us. However one downside to manual mocks that you should keep in mind is that manual mocks are manual -- meaning you have to manually update them any time the module they are mocking changes. Because of this, it's best to use the automatic mocker when it will suffice for your needs.
 
-Testing mocks
+
+Testing manual mocks
 -------------
 
-It's generally an anti-pattern to implement an elaborate, stateful mock for a module. Before going down this route, consider covering the original module completely with tests and then whitelisting it, so that requiring it always provides the actual implementation, rather than the mock current list of whitelisted modules.
+It's generally an anti-pattern to implement an elaborate, stateful mock for a module. Before going down this route, consider covering the real module completely with tests and then whitelisting it with [\`config.unmockedModulePathPatterns\`](/jest/docs/api.html#config-unmockedmodulepathpatterns-array-string), so that any tests that \`require()\` it will always get the real implementation (rather than a complicated mock version).
 
-In cases where this kind of mock is unavoidable, it's best to write a test that
-ensures that the mock and the actual implementation are in sync. Luckily, this
-is relatively easy to do with the API provided by \`jest\`, which allows you to explicitly require both the actual and mock implementations of the same module in a single test.
+In cases where this kind of elaborate mock is unavoidable, it's not necessarily a bad idea to write a test that ensures that the mock and the actual implementation are in sync. Luckily, this is relatively easy to do with the API provided by \`jest\`, which allows you to explicitly require both the real module (using \`require.requireActual()\`) and the manually mocked implementation of the module (using \`require()\`) in a single test!
 `);
   }
 });
