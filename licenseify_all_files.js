e19416f993dad601a35c867fed6ab3ad996e7a2b
JeffMo <jeffmo@fb.com>
2014-05-12 20:46:26 -0700

    Run licensify script

--- licenseify_all_files.js
+++ licenseify_all_files.js
@@ -1,4 +1,11 @@
 var FileFinder = require('node-find-files');
+/**
+ * Copyright (c) 2014, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the 
+ * LICENSE file in the root directory of this source tree. An additional grant 
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
 var fs = require('fs');
 var path = require('path');
 
