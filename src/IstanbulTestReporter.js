607127c3ddd8c1c3e1a2a05903a0d9dba2e6534b
Rob Gietema <rob@fourdigits.nl>
2015-03-10 11:38:27 +0100

    Add support for cobertura coverage reporter.

--- src/IstanbulTestReporter.js
+++ src/IstanbulTestReporter.js
@@ -29,7 +29,7 @@ function (config, aggregatedResults) {
   );
 
   if (config.collectCoverage) {
-    reporter.addAll([ 'json', 'text', 'lcov', 'clover' ]);
+    reporter.addAll([ 'json', 'text', 'lcov', 'clover', 'cobertura' ]);
     reporter.write(collector, true, function () {
         console.log('All reports generated');
     });
