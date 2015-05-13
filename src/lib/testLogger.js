5f48519dada7e4dc3faca307f15ca83ae56a9c7f
Charles Arciniega <charlesarcin@gmail.com>
2015-05-13 15:51:48 -0700

    Construct `Test Heirarchy` Object.
    Creates an object that keeps track of an individual test's ancestry.
    This is done in order to maintain the correct relationship between
    `describe` blocks and their child `it` blocks.

--- src/lib/testLogger.js
+++ src/lib/testLogger.js
@@ -6,3 +6,33 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 'use strict';
+
+function _createTestNode(testResult, ancestorTitles, currentNode){
+  currentNode = currentNode || { testTitles: [], childNodes: {} };
+  if (ancestorTitles.length === 0) {
+    currentNode.testTitles.push(testResult);
+  } else {
+    if(!currentNode.childNodes[ancestorTitles[0]]){
+      currentNode.childNodes[ancestorTitles[0]] = {
+        testTitles: [],
+        childNodes: {}
+      };
+    }
+    _createTestNode(
+      testResult,
+      ancestorTitles.slice(1,ancestorTitles.length),
+      currentNode.childNodes[ancestorTitles[0]]
+    );
+  }
+
+  return currentNode;
+}
+
+function _createTestTree(testResults){
+  var tree;
+  for (var i = 0; i < testResults.length; i++){
+    tree = _createTestNode(testResults[i], testResults[i].ancestorTitles, tree);
+  }
+
+  return tree;
+}
