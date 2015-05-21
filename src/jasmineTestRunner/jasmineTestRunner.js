d5c89606db23111230f37efdc8bea3e5dfdfb0e3
Jason Bonta <jbonta@gmail.com>
2015-05-21 12:31:53 -0700

    jasmineTestRunner: eliminate the use of for..in on arrays
    We shouldn't be iterating over arrays with for..in. This cleans it up
    and ensures that we're treating arrays like arrays. It has the side
    effect, for Facebook, of allowing us to use our own Arrray.prototype.es6
    polyfill without warnings.

--- src/jasmineTestRunner/jasmineTestRunner.js
+++ src/jasmineTestRunner/jasmineTestRunner.js
@@ -280,7 +280,9 @@ function jasmineTestRunner(config, environment, moduleLoader, testPath) {
     }
   });
 
-  var jasmineReporter = new JasmineReporter();
+  var jasmineReporter = new JasmineReporter({
+    noHighlight: config.noHighlight,
+  });
   jasmine.getEnv().addReporter(jasmineReporter);
 
   // Run the test by require()ing it
