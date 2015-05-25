5a8b4188bcb05c0e065354748127a684a5514350
Tom van Ommeren <tom.vanommeren@gmail.com>
2015-05-25 10:05:36 +0200

    Refactor jasmine2 JasmineReporter to have private instance methods

--- src/jasmine2TestRunner/JasmineReporter.js
+++ src/jasmine2TestRunner/JasmineReporter.js
@@ -10,23 +10,10 @@
 var colors = require('../lib/colors');
 
 var diff = require('diff');
+var formatMsg = require('../lib/utils').formatMsg;
 var jasmineRequire = require('../../vendor/jasmine/jasmine-2.2.0');
 var jasmine = jasmineRequire.core(jasmineRequire);
 var Q = require('q');
-
-var colorize = colors.colorize;
-
-// Used in colorized mode.
-function _formatMsgColorized(msg, color) {
-  return colors.colorize(msg, color);
-}
-
-// Used in plain mode to output.
-function _formatMsgPlain(msg) {
-  return msg;
-}
-
-var _formatMsg;
 
 var ERROR_TITLE_COLOR = colors.RED + colors.BOLD + colors.UNDERLINE;
 var DIFFABLE_MATCHERS = {
@@ -37,7 +24,106 @@ var DIFFABLE_MATCHERS = {
 };
 var LINEBREAK_REGEX = /[\r\n]/;
 
-function _highlightDifferences(a, b) {
+function JasmineReporter(config) {
+  this._config = config || {};
+  this._resultsDeferred = Q.defer();
+  this._testResults = [];
+  this._currentSuites = [];
+}
+
+JasmineReporter.prototype.specDone = function(result) {
+  this._testResults.push(this._extractSpecResults(result,
+    this._currentSuites.slice(0)));
+};
+
+JasmineReporter.prototype.suiteStarted = function(suite) {
+  this._currentSuites.push(suite);
+};
+
+JasmineReporter.prototype.suiteDone = function() {
+  this._currentSuites.pop();
+};
+
+JasmineReporter.prototype.getResults = function() {
+  var numFailingTests = 0;
+  var numPassingTests = 0;
+
+  this._testResults.forEach(function(testResult) {
+    if (testResult.failureMessages.length > 0) {
+      numFailingTests++;
+    } else {
+      numPassingTests++;
+    }
+  });
+
+  this._resultsDeferred.resolve({
+    numFailingTests: numFailingTests,
+    numPassingTests: numPassingTests,
+    testResults: this._testResults
+  });
+
+  this._testResults = []; // I'm not sure if we really need this
+
+  return this._resultsDeferred.promise;
+};
+
+
+JasmineReporter.prototype._extractSpecResults =
+function(specResult, currentSuites) {
+  var results = {
+    title: 'it ' + specResult.description,
+    ancestorTitles: currentSuites,
+    failureMessages: [],
+    logMessages: [], // Jasmine 2 does not have a logging interface
+    numPassingAsserts: 0 // Jasmine 2 only returns an array of failed asserts.
+  };
+
+  specResult.failedExpectations.forEach(function(failed) {
+    if (!failed.matcherName) {
+      var errorMessage = failed.stack.replace(
+        /(^.*$(?=\n\s*at))/m,
+          this._formatMsg('$1', ERROR_TITLE_COLOR)
+      );
+
+      results.failureMessages.push(errorMessage);
+    } else {
+      var message;
+      if (DIFFABLE_MATCHERS[failed.matcherName]) {
+        var ppActual = this._prettyPrint(failed.actual);
+        var ppExpected = this._prettyPrint(failed.expected);
+        var colorDiff = this._highlightDifferences(ppActual, ppExpected);
+
+        message =
+              this._formatMsg('Expected:', ERROR_TITLE_COLOR) +
+                ' ' + colorDiff.a +
+                ' ' + this._formatMsg(failed.matcherName + ':',
+                                        ERROR_TITLE_COLOR) +
+                ' ' + colorDiff.b;
+      } else {
+        message = this._formatMsg(failed.message, ERROR_TITLE_COLOR);
+      }
+
+      if (failed.stack) {
+        // Replace the error message with a colorized version of the error
+        message = failed.stack.replace(failed.message, message);
+
+        // Remove the 'Error: ' prefix from the stack trace
+        message = message.replace(/^.*Error:\s*/, '');
+
+        // Remove jasmine jonx from the stack trace
+        message = message.split('\n').filter(function(line) {
+          return !/vendor\/jasmine\//.test(line);
+        }).join('\n');
+      }
+
+      results.failureMessages.push(message);
+    }
+  }.bind(this));
+
+  return results;
+};
+
+JasmineReporter.prototype._highlightDifferences = function(a, b) {
   var differ;
   if (a.match(LINEBREAK_REGEX) || b.match(LINEBREAK_REGEX)) {
     // `diff` uses the Myers LCS diff algorithm which runs in O(n+d^2) time
@@ -54,18 +140,18 @@ function _highlightDifferences(a, b) {
   for (var i = 0, il = changes.length; i < il; i++) {
     change = changes[i];
     if (change.added) {
-      ret.b += _formatMsg(change.value, colors.RED_BG);
+      ret.b += this._formatMsg(change.value, colors.RED_BG);
     } else if (change.removed) {
-      ret.a += _formatMsg(change.value, colors.RED_BG);
+      ret.a += this._formatMsg(change.value, colors.RED_BG);
     } else {
       ret.a += change.value;
       ret.b += change.value;
     }
   }
   return ret;
-}
+};
 
-function _prettyPrint(obj, indent, cycleWeakMap) {
+JasmineReporter.prototype._prettyPrint = function(obj, indent, cycleWeakMap) {
   if (!indent) {
     indent = '';
   }
@@ -102,7 +188,7 @@ function _prettyPrint(obj, indent, cycleWeakMap) {
     var orderedKeys = Object.keys(obj).sort();
     var value;
     var keysOutput = [];
-    var keyIndent = _formatMsg('|', colors.GRAY) + ' ';
+    var keyIndent = this._formatMsg('|', colors.GRAY) + ' ';
     for (var i = 0; i < orderedKeys.length; i++) {
       if (orderedKeys[i] === '__jstest_pp_cycle__') {
         continue;
@@ -110,7 +196,7 @@ function _prettyPrint(obj, indent, cycleWeakMap) {
       value = obj[orderedKeys[i]];
       keysOutput.push(
         indent + keyIndent + orderedKeys[i] + ': ' +
-        _prettyPrint(value, indent + keyIndent, cycleWeakMap)
+        this._prettyPrint(value, indent + keyIndent, cycleWeakMap)
       );
     }
     delete obj.__jstest_pp_cycle__;
@@ -118,110 +204,10 @@ function _prettyPrint(obj, indent, cycleWeakMap) {
   } else {
     return jasmine.pp(obj);
   }
-}
-
-function _extractSpecResults(specResult, currentSuites) {
-  var results = {
-    title: 'it ' + specResult.description,
-    ancestorTitles: currentSuites,
-    failureMessages: [],
-    logMessages: [], // Jasmine 2 does not have a logging interface
-    numPassingAsserts: 0 // Jasmine 2 only returns an array of failed asserts.
-  };
-
-  specResult.failedExpectations.forEach(function(failed) {
-    if (!failed.matcherName) {
-      var errorMessage = failed.stack.replace(
-        /(^.*$(?=\n\s*at))/m,
-          _formatMsg('$1', ERROR_TITLE_COLOR)
-      );
-
-      results.failureMessages.push(errorMessage);
-    } else {
-      var message;
-      if (DIFFABLE_MATCHERS[failed.matcherName]) {
-        var ppActual = _prettyPrint(failed.actual);
-        var ppExpected = _prettyPrint(failed.expected);
-        var colorDiff = _highlightDifferences(ppActual, ppExpected);
-
-        message =
-              _formatMsg('Expected:', ERROR_TITLE_COLOR) +
-                ' ' + colorDiff.a +
-                ' ' + _formatMsg(failed.matcherName + ':', ERROR_TITLE_COLOR) +
-                ' ' + colorDiff.b;
-      } else {
-        message = _formatMsg(failed.message, ERROR_TITLE_COLOR);
-      }
-
-      if (failed.stack) {
-        // Replace the error message with a colorized version of the error
-        message = failed.stack.replace(failed.message, message);
-
-        // Remove the 'Error: ' prefix from the stack trace
-        message = message.replace(/^.*Error:\s*/, '');
-
-        // Remove jasmine jonx from the stack trace
-        message = message.split('\n').filter(function(line) {
-          return !/vendor\/jasmine\//.test(line);
-        }).join('\n');
-      }
-
-      results.failureMessages.push(message);
-    }
-  });
-
-  return results;
-}
-
-function JasmineReporter(config) {
-  this._config = config || {};
-  this._resultsDeferred = Q.defer();
-  this._testResults = [];
-  this._currentSuites = [];
-
-  _formatMsg = this._config.noHighlight
-    ? _formatMsgPlain
-    : _formatMsgColorized;
-}
-
-JasmineReporter.prototype.specDone = function(result) {
-  this._testResults.push(_extractSpecResults(result,
-    this._currentSuites.slice(0)));
 };
 
-JasmineReporter.prototype.suiteStarted = function(suite) {
-  this._currentSuites.push(suite);
-};
-
-JasmineReporter.prototype.suiteDone = function() {
-  this._currentSuites.pop();
-};
-
-JasmineReporter.prototype.getResults = function() {
-  var numFailingTests = 0;
-  var numPassingTests = 0;
-
-  this._testResults.forEach(function(testResult) {
-    if (testResult.failureMessages.length > 0) {
-      numFailingTests++;
-    } else {
-      numPassingTests++;
-    }
-  });
-
-  this._resultsDeferred.resolve({
-    numFailingTests: numFailingTests,
-    numPassingTests: numPassingTests,
-    testResults: this._testResults
-  });
-
-  this._testResults = []; // I'm not sure if we really need this
-
-  return this._resultsDeferred.promise;
-};
-
-JasmineReporter.prototype.log = function(str) {
-  console.log('logging: ', str);
+JasmineReporter.prototype._formatMsg = function(msg, color) {
+  return formatMsg(msg, color, this._config);
 };
 
 module.exports = JasmineReporter;
