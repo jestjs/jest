7499d786814797b37c2100b6865f84c177276b37
Blaine Kasten <blainekasten@gmail.com>
2015-07-09 09:36:11 -0500

    Clarify new io.js compatibility message
    ref #425 The notice message isn't as clear to people unaware of io.js and node merger.

--- bin/jest.js
+++ bin/jest.js
@@ -229,7 +229,8 @@ if (fs.existsSync(cwdJestBinPath)) {
   }
 
   console.log('\n  == NOTICE: ==');
-  console.log('    On August 1st, 2015, Jest v0.5.x will work only on io.js');
+  console.log('    Node and io.js will be merging. http://bit.ly/1dOjLVK');
+  console.log('    On August 1st, 2015, Jest v0.5.x will work only on io.js until the merger is complete');
   console.log('    Legacy v0.4.x for Node 0.10.x will still be available on');
   console.log('    on npm and from the "0.4.x" branch on GitHub.\n');
 })();
