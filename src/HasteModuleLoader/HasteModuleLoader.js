c2fbcf7b5a2d259ca47adfa403de2f3ea191733d
cpojer <christoph.pojer@gmail.com>
2015-09-08 12:34:12 -0700

    Set 777 chmod for .haste-cache.

--- src/HasteModuleLoader/HasteModuleLoader.js
+++ src/HasteModuleLoader/HasteModuleLoader.js
@@ -90,6 +90,7 @@ function _constructHasteInst(config, options) {
 
   if (!fs.existsSync(config.cacheDirectory)) {
     fs.mkdirSync(config.cacheDirectory);
+    fs.chmodSync(config.cacheDirectory, '777');
   }
 
   return new NodeHaste(
