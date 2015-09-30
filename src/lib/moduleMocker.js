aee5ee56501bae021d758c51e6e78c10822899e1
cpojer <christoph.pojer@gmail.com>
2015-09-30 15:29:36 +0100

    moduleMocker still can’t use ‘use strict’;

--- src/lib/moduleMocker.js
+++ src/lib/moduleMocker.js
@@ -5,7 +5,8 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
-'use strict';
+
+/* jshint strict:false */
 
 function isA(typeName, value) {
   return Object.prototype.toString.apply(value) === '[object ' + typeName + ']';
