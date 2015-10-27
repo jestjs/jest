73b2bd8441d215ad7abd591a7bbf319e61cc044a
Christoph Pojer <cpojer@fb.com>
2015-10-27 16:18:50 -0700

    Update runner result message
    Summary: This updates the internal and external runner's message to read `1 test failed, 1 test suite failed, 95 tests passed (96 total in 20 test suites, run time 2.808s)` when a test suite fails. It also now uses the correct exit status. This was always broken until I fixed it a few weeks ago, then was broken again when we made the tests/test suites change and was finally fixed again in https://github.com/facebook/jest/pull/575.
    
    public
    
    Reviewed By: zpao
    
    Differential Revision: D2587896
    
    fb-gh-sync-id: dc9f571c317ff550b46e2ef935432df8577477f8

--- src/DefaultTestReporter.js
+++ src/DefaultTestReporter.js
@@ -17,186 +17,188 @@ var FAIL_COLOR = colors.RED_BG + colors.BOLD;
 var PASS_COLOR = colors.GREEN_BG + colors.BOLD;
 var TEST_NAME_COLOR = colors.BOLD;
 
-function DefaultTestReporter(customProcess) {
-  this._process = customProcess || process;
-}
+class DefaultTestReporter {
 
-DefaultTestReporter.prototype.log = function(str) {
-  this._process.stdout.write(str + '\n');
-};
-
-DefaultTestReporter.prototype.onRunStart =
-function(config, aggregatedResults) {
-  this._config = config;
-  this._printWaitingOn(aggregatedResults);
-  if (this._config.verbose) {
-    var verboseLogger = new VerboseLogger(this._config, this._process);
-    this.verboseLog = verboseLogger.verboseLog.bind(verboseLogger);
+  constructor(customProcess) {
+    this._process = customProcess || process;
   }
-};
 
-DefaultTestReporter.prototype.onTestResult =
-function(config, testResult, aggregatedResults) {
-  this._clearWaitingOn();
+  log(str) {
+    this._process.stdout.write(str + '\n');
+  }
 
-  var pathStr =
-    config.rootDir
-    ? path.relative(config.rootDir, testResult.testFilePath)
-    : testResult.testFilePath;
-  var allTestsPassed = testResult.numFailingTests === 0;
-  var testRunTime =
-    testResult.perfStats
-    ? (testResult.perfStats.end - testResult.perfStats.start) / 1000
-    : null;
-
-  var testRunTimeString;
-  if (testRunTime !== null) {
-    testRunTimeString = '(' + testRunTime + 's)';
-    if (testRunTime > 2.5) {
-      testRunTimeString = this._formatMsg(testRunTimeString, FAIL_COLOR);
+  onRunStart(config, aggregatedResults) {
+    this._config = config;
+    this._printWaitingOn(aggregatedResults);
+    if (this._config.verbose) {
+      var verboseLogger = new VerboseLogger(this._config, this._process);
+      this.verboseLog = verboseLogger.verboseLog.bind(verboseLogger);
     }
   }
 
-  var resultHeader = this._getResultHeader(allTestsPassed, pathStr, [
-    testRunTimeString,
-  ]);
+  onTestResult(config, testResult, aggregatedResults) {
+    this._clearWaitingOn();
 
-  /*
-  if (config.collectCoverage) {
-    // TODO: Find a nice pretty way to print this out
-  }
-  */
+    var pathStr =
+      config.rootDir
+      ? path.relative(config.rootDir, testResult.testFilePath)
+      : testResult.testFilePath;
+    var allTestsPassed = testResult.numFailingTests === 0;
+    var testRunTime =
+      testResult.perfStats
+      ? (testResult.perfStats.end - testResult.perfStats.start) / 1000
+      : null;
 
-  this.log(resultHeader);
-  if (config.verbose) {
-    this.verboseLog(testResult.testResults, resultHeader);
-  }
-
-  if (!allTestsPassed) {
-    var failureMessage = formatFailureMessage(testResult, {
-      rootPath: config.rootDir,
-      useColor: !config.noHighlight,
-    });
-    if (config.verbose) {
-      aggregatedResults.postSuiteHeaders.push(
-        resultHeader,
-        failureMessage
-      );
-    } else {
-      // If we write more than one character at a time it is possible that iojs
-      // exits in the middle of printing the result.
-      // If you are reading this and you are from the future, this might not
-      // be true any more.
-      for (var i = 0; i < failureMessage.length; i++) {
-        this._process.stdout.write(failureMessage.charAt(i));
+    var testRunTimeString;
+    if (testRunTime !== null) {
+      testRunTimeString = '(' + testRunTime + 's)';
+      if (testRunTime > 2.5) {
+        testRunTimeString = this._formatMsg(testRunTimeString, FAIL_COLOR);
       }
-      this._process.stdout.write('\n');
     }
 
-    if (config.bail) {
-      this.onRunComplete(config, aggregatedResults);
-      this._process.exit(0);
+    var resultHeader = this._getResultHeader(allTestsPassed, pathStr, [
+      testRunTimeString,
+    ]);
+
+    /*
+    if (config.collectCoverage) {
+      // TODO: Find a nice pretty way to print this out
     }
-  }
+    */
 
-  this._printWaitingOn(aggregatedResults);
-};
-
-DefaultTestReporter.prototype.onRunComplete =
-function(config, aggregatedResults) {
-  var numFailedTests = aggregatedResults.numFailedTests;
-  var numPassedTests = aggregatedResults.numPassedTests;
-  var numTotalTests = aggregatedResults.numTotalTests;
-  var runTime = (Date.now() - aggregatedResults.startTime) / 1000;
-
-  if (numTotalTests === 0) {
-    return;
-  }
-
-  if (config.verbose) {
-    if (aggregatedResults.postSuiteHeaders.length > 0) {
-      this.log(aggregatedResults.postSuiteHeaders.join('\n'));
+    this.log(resultHeader);
+    if (config.verbose) {
+      this.verboseLog(testResult.testResults, resultHeader);
     }
+
+    if (!allTestsPassed) {
+      var failureMessage = formatFailureMessage(testResult, {
+        rootPath: config.rootDir,
+        useColor: !config.noHighlight,
+      });
+      if (config.verbose) {
+        aggregatedResults.postSuiteHeaders.push(
+          resultHeader,
+          failureMessage
+        );
+      } else {
+        // If we write more than one character at a time it is possible that
+        // node exits in the middle of printing the result.
+        // If you are reading this and you are from the future, this might not
+        // be true any more.
+        for (var i = 0; i < failureMessage.length; i++) {
+          this._process.stdout.write(failureMessage.charAt(i));
+        }
+        this._process.stdout.write('\n');
+      }
+
+      if (config.bail) {
+        this.onRunComplete(config, aggregatedResults);
+        this._process.exit(0);
+      }
+    }
+
+    this._printWaitingOn(aggregatedResults);
   }
 
-  var results = '';
-  if (numFailedTests) {
+  onRunComplete(config, aggregatedResults) {
+    var numFailedTests = aggregatedResults.numFailedTests;
+    var numPassedTests = aggregatedResults.numPassedTests;
+    var numTotalTests = aggregatedResults.numTotalTests;
+    var runTime = (Date.now() - aggregatedResults.startTime) / 1000;
+
+    if (numTotalTests === 0) {
+      return;
+    }
+
+    if (config.verbose) {
+      if (aggregatedResults.postSuiteHeaders.length > 0) {
+        this.log(aggregatedResults.postSuiteHeaders.join('\n'));
+      }
+    }
+
+    var results = '';
+    if (numFailedTests) {
+      results += this._formatMsg(
+        numFailedTests + ' test' +
+        (numFailedTests === 1 ? '' : 's') + ' failed',
+        colors.RED + colors.BOLD
+      );
+      results += ', ';
+    }
+
+    if (aggregatedResults.numRuntimeErrorTestSuites) {
+      results += this._formatMsg(
+        aggregatedResults.numRuntimeErrorTestSuites + ' test suite' +
+          (aggregatedResults.numRuntimeErrorTestSuites === 1 ? '' : 's') +
+          ' failed',
+        colors.RED + colors.BOLD
+      );
+      results += ', ';
+    }
+
     results += this._formatMsg(
-      numFailedTests + ' test' + (numFailedTests === 1 ? '' : 's') + ' failed',
-      colors.RED + colors.BOLD
+      numPassedTests + ' test' + (numPassedTests === 1 ? '' : 's') + ' passed',
+      colors.GREEN + colors.BOLD
     );
-    results += ', ';
-  }
-  results += this._formatMsg(
-    numPassedTests + ' test' + (numPassedTests === 1 ? '' : 's') + ' passed',
-    colors.GREEN + colors.BOLD
-  );
 
-  if (aggregatedResults.numRuntimeErrorTestSuites) {
-    results += ', ';
-    results += this._formatMsg(
-      aggregatedResults.numRuntimeErrorTestSuites + ' test suite' +
-        (aggregatedResults.numRuntimeErrorTestSuites === 1 ? '' : 's') +
-        ' failed to run',
-      colors.RED + colors.BOLD
-    );
+    var numTestSuitesExecuted =
+      aggregatedResults.numTotalTestSuites -
+      aggregatedResults.numRuntimeErrorTestSuites;
+
+    results += ' (' + numTotalTests + ' total in ' +
+      numTestSuitesExecuted + ' ' +
+      'test suite' + (numTestSuitesExecuted === 1 ? '' : 's') +
+      ', run time ' + runTime + 's)';
+
+    this.log(results);
   }
 
-  var numTestSuitesExecuted =
-    aggregatedResults.numTotalTestSuites -
-    aggregatedResults.numRuntimeErrorTestSuites;
-
-  results += ' (' + numTotalTests + ' total in ' +
-    numTestSuitesExecuted + ' ' +
-    'test suite' + (numTestSuitesExecuted === 1 ? '' : 's') +
-    ', run time ' + runTime + 's)';
-
-  this.log(results);
-};
-
-DefaultTestReporter.prototype._clearWaitingOn = function() {
-  // Don't write special chars in noHighlight mode
-  // to get clean output for logs.
-  var command = this._config.noHighlight
-    ? '\n'
-    : '\r\x1B[K';
-  this._process.stdout.write(command);
-};
-
-DefaultTestReporter.prototype._formatMsg = function(msg, color) {
-  return formatMsg(msg, color, this._config);
-};
-
-DefaultTestReporter.prototype._getResultHeader =
-function(passed, testName, columns) {
-  var passFailTag = passed
-    ? this._formatMsg(' PASS ', PASS_COLOR)
-    : this._formatMsg(' FAIL ', FAIL_COLOR);
-
-  return [
-    passFailTag,
-    this._formatMsg(testName, TEST_NAME_COLOR),
-  ].concat(columns || []).join(' ');
-};
-
-DefaultTestReporter.prototype._printWaitingOn = function(aggregatedResults) {
-  var completedTestSuites =
-    aggregatedResults.numPassedTestSuites +
-    aggregatedResults.numFailedTestSuites +
-    aggregatedResults.numRuntimeErrorTestSuites;
-  var remainingTestSuites =
-    aggregatedResults.numTotalTestSuites -
-    completedTestSuites;
-  if (remainingTestSuites > 0) {
-    var pluralTestSuites =
-      remainingTestSuites === 1 ? 'test suite' : 'test suites';
-    this._process.stdout.write(
-      this._formatMsg(
-        'Running ' + remainingTestSuites + ' ' + pluralTestSuites + '...',
-        colors.GRAY + colors.BOLD
-      )
-    );
+  _clearWaitingOn() {
+    // Don't write special chars in noHighlight mode
+    // to get clean output for logs.
+    var command = this._config.noHighlight
+      ? '\n'
+      : '\r\x1B[K';
+    this._process.stdout.write(command);
   }
-};
+
+  _formatMsg(msg, color) {
+    return formatMsg(msg, color, this._config);
+  }
+
+  _getResultHeader(passed, testName, columns) {
+    var passFailTag = passed
+      ? this._formatMsg(' PASS ', PASS_COLOR)
+      : this._formatMsg(' FAIL ', FAIL_COLOR);
+
+    return [
+      passFailTag,
+      this._formatMsg(testName, TEST_NAME_COLOR),
+    ].concat(columns || []).join(' ');
+  }
+
+  _printWaitingOn(aggregatedResults) {
+    var completedTestSuites =
+      aggregatedResults.numPassedTestSuites +
+      aggregatedResults.numFailedTestSuites +
+      aggregatedResults.numRuntimeErrorTestSuites;
+    var remainingTestSuites =
+      aggregatedResults.numTotalTestSuites -
+      completedTestSuites;
+    if (remainingTestSuites > 0) {
+      var pluralTestSuites =
+        remainingTestSuites === 1 ? 'test suite' : 'test suites';
+      this._process.stdout.write(
+        this._formatMsg(
+          'Running ' + remainingTestSuites + ' ' + pluralTestSuites + '...',
+          colors.GRAY + colors.BOLD
+        )
+      );
+    }
+  }
+
+}
 
 module.exports = DefaultTestReporter;
