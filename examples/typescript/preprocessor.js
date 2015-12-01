2a20274213909ee53e0ef217f81a9a1c8ce49620
matzjiro <sej.my.love@gmail.com>
2015-12-01 20:05:59 +0900

    typo

--- examples/typescript/preprocessor.js
+++ examples/typescript/preprocessor.js
@@ -6,9 +6,9 @@ module.exports = {
     var result = src;
     if(path.slice(-3) === '.ts' || path.slice(-4) === '.tsx'){
       var d=[];
-      var result = tsc.transpile(src, {module: tsc.ModuleKind.CommonJS, jsx: tsc.JsxEmit.React}, path, d);
+      result = tsc.transpile(src, {module: tsc.ModuleKind.CommonJS, jsx: tsc.JsxEmit.React}, path, d);
       if(d.length !== 0){
-        console.log("transpile info. path=", path, ", d=", d, ", ", JSON.stringify(result));
+        //console.log("transpile info. path=", path, ", d=", d, ", ", JSON.stringify(result));
       }
     }
     return result;
