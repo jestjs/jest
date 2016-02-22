c61c79499910d330b192dad7b5655c7816f0b239
Evan Jacobs <evan.jacobs@enigma.io>
2016-02-22 18:48:47 -0500

    Update license headers

--- src/testRunners/jasmine/__tests__/JasmineFormatter-test.js
+++ src/testRunners/jasmine/__tests__/JasmineFormatter-test.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016, Facebook, Inc. All rights reserved.
+ * Copyright (c) 2016-present, Facebook, Inc. All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
