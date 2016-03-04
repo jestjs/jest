1a847b47bc7cfa0751cd8b152e5e8e0e0e350c92
Christoph Pojer <cpojer@fb.com>
2016-03-04 15:42:24 -0800

    Implement Search
    Reviewed By: voideanvalue
    
    Differential Revision: D3014415
    
    fb-gh-sync-id: 164415792903cccb3e3d4122263aa57340ba46a6
    shipit-source-id: 164415792903cccb3e3d4122263aa57340ba46a6

--- website/core/HeaderLinks.js
+++ website/core/HeaderLinks.js
@@ -9,28 +9,44 @@ var MetadataBlog = require('MetadataBlog');
 var React = require('React');
 
 var HeaderLinks = React.createClass({
-  links: [
+  linksInternal: [
     {section: 'docs', href: '/jest/docs/tutorial.html#content', text: 'docs'},
     {section: 'support', href: '/jest/support.html', text: 'support'},
     MetadataBlog.files.length > 0 && {section: 'blog', href: '/jest/blog/' + MetadataBlog.files[0].path, text: 'blog'},
-    {section: 'github', href: 'http://github.com/facebook/jest', text: 'github'},
   ].filter(function(e) { return e; }),
+  linksExternal: [
+    {section: 'github', href: 'https://github.com/facebook/jest', text: 'GitHub'},
+  ],
+
+  makeLinks: function(links) {
+    return links.map(function(link) {
+      return (
+        <li key={link.section}>
+          <a
+            href={link.href}
+            className={link.section === this.props.section ? 'active' : ''}>
+            {link.text}
+          </a>
+        </li>
+      );
+    }, this);
+  },
 
   render: function() {
     return (
-      <ul className="nav-site">
-        {this.links.map(function(link) {
-          return (
-            <li key={link.section}>
-              <a
-                href={link.href}
-                className={link.section === this.props.section ? 'active' : ''}>
-                {link.text}
-              </a>
-            </li>
-          );
-        }, this)}
-      </ul>
+      <div className="nav-site-wrapper">
+        <ul className="nav-site nav-site-internal">
+          {this.makeLinks(this.linksInternal)}
+        </ul>
+
+        <div className="algolia-search-wrapper">
+          <input id="algolia-doc-search" type="text" placeholder="Search docs..." />
+        </div>
+
+        <ul className="nav-site nav-site-external">
+          {this.makeLinks(this.linksExternal)}
+        </ul>
+      </div>
     );
   }
 });
