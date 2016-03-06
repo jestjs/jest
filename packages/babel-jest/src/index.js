a969d40287d55145a5e9a702dedc9b3ecf58a6f4
cpojer <christoph.pojer@gmail.com>
2016-03-06 11:54:26 -0800

    babel-jest should compile react-native.

--- packages/babel-jest/src/index.js
+++ packages/babel-jest/src/index.js
@@ -6,15 +6,24 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
+'use strict';
+
 const babel = require('babel-core');
 const jestPreset = require('babel-preset-jest');
 const path = require('path');
 
 const NODE_MODULES = path.sep + 'node_modules' + path.sep;
 
+const isNodeModule = filename => {
+  if (filename.includes(NODE_MODULES)) {
+    return !filename.includes(NODE_MODULES + 'react-native' + path.sep);
+  }
+  return false;
+};
+
 module.exports = {
   process(src, filename) {
-    if (!filename.includes(NODE_MODULES) && babel.util.canCompile(filename)) {
+    if (!isNodeModule(filename) && babel.util.canCompile(filename)) {
       return babel.transform(src, {
         filename,
         presets: [jestPreset],
