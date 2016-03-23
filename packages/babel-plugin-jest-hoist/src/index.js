5fbd2cb0bb5e9a45e5b04cc69d24858d43e250e5
Pavel Volokitin <pavelvolokitin@gmail.com>
2016-03-23 23:34:24 +0500

    Allow referencing jest and require in babel-plugin-jest-hoist.

--- packages/babel-plugin-jest-hoist/src/index.js
+++ packages/babel-plugin-jest-hoist/src/index.js
@@ -8,6 +8,12 @@
 
 'use strict';
 
+function invariant(condition, message) {
+  if (!condition) {
+    throw new Error('babel-plugin-jest-hoist: ' + message);
+  }
+}
+
 const JEST_GLOBAL = {name: 'jest'};
 
 const idVisitor = {
@@ -22,17 +28,23 @@ const FUNCTIONS = {
       if (args.length === 1) {
         return args[0].isStringLiteral();
       } else if (args.length === 2) {
+        invariant(
+          args[1].isFunction(),
+          'The second argument of `jest.mock` must be a function.'
+        );
+
         const ids = new Set();
         args[1].traverse(idVisitor, {ids});
 
         const outerScope = args[1].parentPath.scope;
 
         for (let id of ids) {
+          const name = id.node.name;
           let found = false;
           let scope = id.scope;
 
           while (scope !== outerScope) {
-            if (scope.bindings[id.node.name]) {
+            if (scope.bindings[name]) {
               found = true;
               break;
             }
@@ -41,8 +53,9 @@ const FUNCTIONS = {
           }
 
           if (!found) {
-            throw new Error(
-              'babel-plugin-jest-hoist: The second argument of `jest.mock()` ' +
+            invariant(
+              scope.hasGlobal(name) && (name === 'jest' || name === 'require'),
+              'The second argument of `jest.mock()` ' +
               'is not allowed to reference any outside variables.'
             );
           }
