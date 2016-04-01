192c1a0744924e5e768b3b5107120f2cffba7cbf
kentaromiura <kentaromiura@gmail.com>
2016-04-01 10:55:13 +0100

    translate setup.sh to bash

--- postinstall.js
+++ postinstall.js
@@ -6,17 +6,48 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 'use strict';
+const path = require('path');
+const fs = require('fs');
+
+let notNPM = true;
+
+try {
+  fs.accessSync(path.resolve(__dirname, './website/package.json'));
+} catch (e) {
+  notNPM = false;
+}
+
+const spawnSync = require('child_process').spawnSync;
+
+const execute = (cmd, env) => {
+  const options = {
+    stdio: 'inherit',
+    cwd: path.resolve(__dirname, cmd[0]),
+  };
+
+  if (env) {
+    options.env = env;
+  }
+
+  spawnSync(cmd[1], cmd[2].split(' '), options);
+};
 
 if (
-  process.platform !== 'win32' &&
+  notNPM &&
   process.env.NODE_ENV !== 'production'
 ) {
-  const exec = require('child_process').exec;
-  exec('./setup.sh', (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(stdout);
-  });
+  console.log(`Setting up Jest's development environment...`);
+
+  const cmds = [
+    ['.', 'node_modules/.bin/lerna', 'bootstrap'],
+    ['packages/jest-jasmine1', 'npm', 'link'],
+    ['packages/jest-jasmine2', 'npm', 'link'],
+    ['packages/jest-mock', 'npm', 'link'],
+    ['packages/jest-util', 'npm', 'link'],
+    ['.', 'npm', 'link jest-jasmine1'],
+    ['.', 'npm', 'link jest-jasmine2'],
+    ['.', 'npm', 'link jest-mock'],
+    ['.', 'npm', 'link jest-util'],
+  ];
+  cmds.forEach(execute);
 }
