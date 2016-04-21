9d1759529767fe7980c8935d402ac810211c836e
Dmitrii Abramov <dabramov@yahoo-inc.com>
2016-04-20 18:25:06 -0700

    fix jasmine file paths
    Summary:i just noticed that master was failing. These paths got lost :)
    not sure if it was already fixed or not
    Closes https://github.com/facebook/jest/pull/922
    
    Differential Revision: D3206030
    
    fb-gh-sync-id: 9d220f0ccd16833fb2c0eb8f441c4bb866ed22c3
    fbshipit-source-id: 9d220f0ccd16833fb2c0eb8f441c4bb866ed22c3

--- packages/jest-util/lib/__tests__/JasmineFormatter-test.js
+++ packages/jest-util/lib/__tests__/JasmineFormatter-test.js
@@ -12,7 +12,7 @@
 jest.disableAutomock();
 
 const jasmine = require('jest-jasmine1/vendor/jasmine-1.3.0').jasmine;
-const jasmine2Require = require('jest-jasmine2/vendor/jasmine-2.3.4.js');
+const jasmine2Require = require('jest-jasmine2/vendor/jasmine-2.4.1.js');
 const jasmine2 = jasmine2Require.core(jasmine2Require);
 
 const JasmineFormatter = require('../JasmineFormatter');
