28b41d76b3506a62ca7e03324f1baba07705c0e6
Dmitrii Abramov <dmitrii@abramov.io>
2016-05-18 20:44:16 -0700

    Don't throw when returned value is not a promise

--- packages/jest-jasmine2/src/jasmine-pit.js
+++ packages/jest-jasmine2/src/jasmine-pit.js
@@ -21,7 +21,7 @@ function isPromise(obj) {
 function promisifyIt(originalFn, env) {
   return function(specName, fn) {
     if (!fn) {
-      return;
+      return null;
     }
 
     const isAsync = fn.length; // `done` was passed
@@ -39,9 +39,9 @@ function promisifyIt(originalFn, env) {
         } else if (returnValue === undefined) {
           done();
         } else {
-          done.fail(new Error(
-            'Jest: `it` must return either a Promise or undefined.'
-          ));
+          // TODO: 'Jest: `it` must return either a Promise or undefined.'
+          // CoffeeScript has implicit returns, so we can't really throw here
+          done();
         }
       });
     }
