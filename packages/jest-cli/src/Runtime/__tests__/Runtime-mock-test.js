2723b687e3e77f403dc9ccc67a8c3622cce4a802
Valery Bugakov <skymk1@gmail.com>
2016-05-25 00:35:33 -0700

    Added support for virtual mocks
    Summary:
    Trying to fix #701
    cpojer let me know your thoughts about that? Maybe there's a better way for it? :)
    Closes https://github.com/facebook/jest/pull/1040
    
    Differential Revision: D3345541
    
    fbshipit-source-id: 2b12e368089c2e35168aa9c59fb5d7f65af41fed

--- packages/jest-cli/src/Runtime/__tests__/Runtime-mock-test.js
+++ packages/jest-cli/src/Runtime/__tests__/Runtime-mock-test.js
@@ -24,7 +24,7 @@ describe('Runtime', () => {
   });
 
   describe('jest.mock', () => {
-    pit('uses uses explicitly set mocks instead of automocking', () =>
+    it('uses uses explicitly set mocks instead of automocking', () =>
       createRuntime(__filename).then(runtime => {
         const mockReference = {isMock: true};
         const root = runtime.requireModule(runtime.__mockRootPath, './root.js');
@@ -43,10 +43,37 @@ describe('Runtime', () => {
         ).toEqual(mockReference);
       })
     );
+
+    it('sets virtual mock for non-existing module', () =>
+      createRuntime(__filename).then(runtime => {
+        const mockReference = {isVirtualMock: true};
+        const virtual = true;
+        const root = runtime.requireModule(runtime.__mockRootPath, './root.js');
+        // Erase module registry because root.js requires most other modules.
+        root.jest.resetModuleRegistry();
+
+        root.jest.mock('NotInstalledModule', () => mockReference, {virtual});
+        root.jest.mock('../ManuallyMocked', () => mockReference, {virtual});
+
+        expect(
+          runtime.requireModuleOrMock(
+            runtime.__mockRootPath,
+            'NotInstalledModule'
+          )
+        ).toEqual(mockReference);
+
+        expect(
+          runtime.requireModuleOrMock(
+            runtime.__mockRootPath,
+            '../ManuallyMocked'
+          )
+        ).toEqual(mockReference);
+      })
+    );
   });
 
   describe('jest.setMock', () => {
-    pit('uses uses explicitly set mocks instead of automocking', () =>
+    it('uses uses explicitly set mocks instead of automocking', () =>
       createRuntime(__filename).then(runtime => {
         const mockReference = {isMock: true};
         const root = runtime.requireModule(runtime.__mockRootPath, './root.js');
