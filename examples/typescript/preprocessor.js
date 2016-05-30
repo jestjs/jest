e5f4b7220f845404c11d875e2620e6a8882bf63a
Derek Sifford <dereksifford@gmail.com>
2016-05-29 20:28:57 -0400

    Add another replacement case for "__assign"

--- examples/typescript/preprocessor.js
+++ examples/typescript/preprocessor.js
@@ -3,21 +3,20 @@
 const tsc = require('typescript');
 
 module.exports = {
-  process(src, path) {
-    if (path.endsWith('.ts') || path.endsWith('.tsx')) {
-      let code = tsc.transpile(
-        src,
-        {
-          module: tsc.ModuleKind.CommonJS,
-          jsx: tsc.JsxEmit.React,
-        },
-        path,
-        []
-      );
-      code = code.replace(/(}\)\()(.*\|\|.*;)/g, '$1/* istanbul ignore next */$2');
-      code = code.replace(/(var __extends = \(this && this.__extends\))/g, '$1/* istanbul ignore next */');
-      return code;
-    }
-    return src;
-  },
+    process(src, path) {
+        if (path.endsWith('.ts') || path.endsWith('.tsx')) {
+            return tsc.transpile(
+                src, {
+                    module: tsc.ModuleKind.CommonJS,
+                    jsx: tsc.JsxEmit.React,
+                },
+                path, []
+            )
+            .replace(/(}\)\()(.*\|\|.*;)/g, '$1/* istanbul ignore next */$2')
+            .replace(/(var __extends = \(this && this.__extends\))/g, '$1/* istanbul ignore next */')
+            .replace(/(var __assign = \(this && this.__assign\) \|\| Object.assign)/g, '$1 /* istanbul ignore next */');
+        }
+        return src;
+    },
 };
+
