2f609a1490a3c399bb12c5eb439153d07ef20a8c
ColCh <colchgh@gmail.com>
2016-06-12 12:02:40 -0700

    Add backticks parse into jest-haste-map
    Summary:
    Just like mentioned in facebook/node-haste#76
    Thanks for pointing it out!
    Closes https://github.com/facebook/jest/pull/1124
    
    Differential Revision: D3423411
    
    fbshipit-source-id: f5e16505a145eea5aab20d5ab53e49dbb1a02b63

--- packages/jest-haste-map/src/lib/extractRequires.js
+++ packages/jest-haste-map/src/lib/extractRequires.js
@@ -15,8 +15,8 @@ const lineCommentRe = /\/\/.*/g;
 const replacePatterns = {
   EXPORT_RE: /(\bexport\s+(?:[^'"]+\s+from\s+)??)(['"])([^'"]+)(\2)/g,
   IMPORT_RE: /(\bimport\s+(?:[^'"]+\s+from\s+)??)(['"])([^'"]+)(\2)/g,
-  REQUIRE_EXTENSIONS_PATTERN: /(\b(?:require\s*?\.\s*?(?:requireActual|requireMock)|jest\s*?\.\s*?genMockFromModule)\s*?\(\s*?)(['"])([^'"]+)(\2\s*?\))/g,
-  REQUIRE_RE: /(\brequire\s*?\(\s*?)(['"])([^'"]+)(\2\s*?\))/g,
+  REQUIRE_EXTENSIONS_PATTERN: /(\b(?:require\s*?\.\s*?(?:requireActual|requireMock)|jest\s*?\.\s*?genMockFromModule)\s*?\(\s*?)([`'"])([^`'"]+)(\2\s*?\))/g,
+  REQUIRE_RE: /(\brequire\s*?\(\s*?)([`'"])([^`'"]+)(\2\s*?\))/g,
 };
 /* eslint-enable max-len */
 
