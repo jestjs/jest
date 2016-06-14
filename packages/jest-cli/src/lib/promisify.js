d9fc322b5e6a9c5d9c4b5e335cd8f48c092e5a42
David He <dhe@fb.com>
2016-06-14 16:37:31 -0700

    Flowify promisify

--- packages/jest-cli/src/lib/promisify.js
+++ packages/jest-cli/src/lib/promisify.js
@@ -5,21 +5,47 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
+
 'use strict';
 
-module.exports = function promisify(fn) {
-  return function() {
-    const args = Array.prototype.slice.call(arguments);
+type Promiseify0<B> = (f: (cb: (Error, B) => void) => void)
+  => () => Promise<B>;
+type Promiseify1<A1, B> = (f: (a1: A1, cb: (Error, B) => void) => void)
+  => (a1: A1) => Promise<B>;
+type Promiseify2<A1, A2, B> = (f: (a1: A1, a2: A2, cb: (Error, B) => void) => void)
+  => (a1: A1, a2: A2) => Promise<B>;
+type Promiseify3<A1, A2, A3, B> = (f: (a1: A1, a2: A2, a3: A3, cb: (Error, B) => void) => void)
+  => (a1: A1, a2: A2, a3: A3) => Promise<B>;
+type Promiseify4<A1, A2, A3, A4, B> = (f: (a1: A1, a2: A2, a3: A3, a4: A4, cb: (Error, B) => void) => void)
+  => (a1: A1, a2: A2, a3: A3, a4: A4) => Promise<B>;
+type Promiseify5<A1, A2, A3, A4, A5, B> = (f: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, cb: (Error, B) => void) => void)
+  => (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => Promise<B>;
+
+type Promiseify = Promiseify0
+  | Promiseify1
+  | Promiseify2
+  | Promiseify3
+  | Promiseify4
+  | Promiseify5;
+
+/**
+ * Converts a continuation passing function to a promise returning function
+ */
+const promisify: Promiseify = fn => {
+  return function(...args) {
     return new Promise((resolve, reject) => {
-      args.push((err, res) => {
+      const continuation = (err, res) => {
         if (err) {
           reject(err);
         } else {
           resolve(res);
         }
-      });
+      };
 
+      args.push(continuation);
       fn.apply(this, args);
     });
   };
 };
+
+module.exports = promisify;
