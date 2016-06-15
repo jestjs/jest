bd725c5db1aa5ca303c734cb5df73e4036bb64a6
David He <dhe@fb.com>
2016-06-14 17:21:12 -0700

    fixes

--- packages/jest-cli/src/lib/promisify.js
+++ packages/jest-cli/src/lib/promisify.js
@@ -4,34 +4,36 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
  */
 
 'use strict';
 
-type Promiseify0<B> = (f: (cb: (Error, B) => void) => void)
+type Promisify0<B> = (f: (cb: (error: Error, b: B) => void) => void)
   => () => Promise<B>;
-type Promiseify1<A1, B> = (f: (a1: A1, cb: (Error, B) => void) => void)
+type Promisify1<A1, B> = (f: (a1: A1, cb: (error: Error, b: B) => void) => void)
   => (a1: A1) => Promise<B>;
-type Promiseify2<A1, A2, B> = (f: (a1: A1, a2: A2, cb: (Error, B) => void) => void)
+type Promisify2<A1, A2, B> = (f: (a1: A1, a2: A2, cb: (error: Error, b: B) => void) => void)
   => (a1: A1, a2: A2) => Promise<B>;
-type Promiseify3<A1, A2, A3, B> = (f: (a1: A1, a2: A2, a3: A3, cb: (Error, B) => void) => void)
+type Promisify3<A1, A2, A3, B> = (f: (a1: A1, a2: A2, a3: A3, cb: (error: Error, b: B) => void) => void)
   => (a1: A1, a2: A2, a3: A3) => Promise<B>;
-type Promiseify4<A1, A2, A3, A4, B> = (f: (a1: A1, a2: A2, a3: A3, a4: A4, cb: (Error, B) => void) => void)
+type Promisify4<A1, A2, A3, A4, B> = (f: (a1: A1, a2: A2, a3: A3, a4: A4, cb: (error: Error, b: B) => void) => void)
   => (a1: A1, a2: A2, a3: A3, a4: A4) => Promise<B>;
-type Promiseify5<A1, A2, A3, A4, A5, B> = (f: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, cb: (Error, B) => void) => void)
+type Promisify5<A1, A2, A3, A4, A5, B> = (f: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, cb: (error: Error, b: B) => void) => void)
   => (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => Promise<B>;
 
-type Promiseify = Promiseify0
-  | Promiseify1
-  | Promiseify2
-  | Promiseify3
-  | Promiseify4
-  | Promiseify5;
+type Promisify = Promisify0
+  | Promisify1
+  | Promisify2
+  | Promisify3
+  | Promisify4
+  | Promisify5;
 
 /**
  * Converts a continuation passing function to a promise returning function
  */
-const promisify: Promiseify = fn => {
+const promisify: Promisify = fn => {
   return function(...args) {
     return new Promise((resolve, reject) => {
       const continuation = (err, res) => {
