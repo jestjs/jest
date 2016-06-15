ce0bddfeeef9f592dee27f69e3b13cd65019bda2
Marcel Erz <me@marcelerz.com>
2016-06-15 16:32:42 -0700

    Add isMockFunction to the jest runtime (#1166)
    * Add isMockFunction to the jest runtime
    
    * Add documentation
    
    * Revert hoisting code

--- packages/jest-runtime/src/__tests__/Runtime-jest-fn.js
+++ packages/jest-runtime/src/__tests__/Runtime-jest-fn.js
@@ -45,4 +45,15 @@ describe('Runtime', () => {
       })
     );
   });
+
+  describe('jest.isMockFunction', () => {
+    it('recognizes a mocked function', () =>
+      createRuntime(__filename).then(runtime => {
+        const root = runtime.requireModule(runtime.__mockRootPath);
+        const mock = root.jest.fn();
+        expect(root.jest.isMockFunction(() => {})).toBe(false);
+        expect(root.jest.isMockFunction(mock)).toBe(true);
+      })
+    );
+  });
 });
