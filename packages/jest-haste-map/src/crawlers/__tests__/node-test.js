ba8060492fafbb7c1af1b776b3caf422ae704bf5
Kun-Han Lee <kunhanle@gmail.com>
2016-07-04 21:32:19 -0700

    Update node-test.js

--- packages/jest-haste-map/src/crawlers/__tests__/node-test.js
+++ packages/jest-haste-map/src/crawlers/__tests__/node-test.js
@@ -17,7 +17,7 @@ jest.mock('child_process', () => ({
     let closeCallback;
     return {
       stdout: {
-        on: jest.fn().getMockImplementation()((event, callback) => {
+        on: jest.fn().MockImplementation((event, callback) => {
           if (event === 'data') {
             setTimeout(() => {
               callback(mockResponse);
