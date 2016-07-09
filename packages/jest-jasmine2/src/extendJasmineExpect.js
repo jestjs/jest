53ec69c98aaf460abd545147743e9f309be0a048
Dmitrii Abramov <dabramov@yahoo-inc.com>
2016-07-09 14:05:49 -0700

    require `jest-matchers` in test env

---
+++ packages/jest-jasmine2/src/extendJasmineExpect.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+'use strict';
+
+jest.deepUnmock('jest-matchers');
+
+const expect = require('jest-matchers').expect;
+
+const jasmineExpect = global.expect;
+
+// extend jasmine matchers with `jest-matchers`
+global.expect = actual => {
+  const jasmineMatchers = jasmineExpect(actual);
+  const jestMatchers = expect(actual);
+  const not = Object.assign(jasmineMatchers.not, jestMatchers.not);
+  return Object.assign(jasmineMatchers, jestMatchers, {not});
+};
