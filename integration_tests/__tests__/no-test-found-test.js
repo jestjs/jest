f9e4cdf2054e55b34807208939ff27ce05569799
cpojer <christoph.pojer@gmail.com>
2016-07-25 16:29:35 +0900

    Don’t print test summary if no tests were run.

---
+++ integration_tests/__tests__/no-test-found-test.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @emails oncall+jsinfra
+ */
+'use strict';
+
+const runJest = require('../runJest');
+
+describe('Coverage Report', () => {
+  it('outputs coverage report', () => {
+    const result = runJest('coverage_report', ['not-a-valid-test']);
+    const stdout = result.stdout.toString();
+
+    expect(stdout).toMatch('NO TESTS FOUND');
+
+    expect(stdout).not.toMatch('0 tests passed');
+  });
+});
