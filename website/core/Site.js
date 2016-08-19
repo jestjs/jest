b8fcb80d053d5808b7404bc275a0eaa1597bd2df
Kevin Lacker <lacker@gmail.com>
2016-08-19 09:38:29 -0700

    switch google analytics account

--- website/core/Site.js
+++ website/core/Site.js
@@ -37,8 +37,9 @@ var Site = React.createClass({
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-            ga('create', 'UA-387204-10', 'facebook.github.io');
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-44373548-17', 'auto');
             ga('send', 'pageview');
 
             !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)
