c43fb67ba0865b6fd3de6acca31790545fc1fd8f
Jonny Buchanan <jonathan.buchanan@gmail.com>
2016-09-02 12:20:09 +1000

    Don't escape hyphens for Regex

--- packages/jest-util/src/index.js
+++ packages/jest-util/src/index.js
@@ -37,7 +37,7 @@ const escapePathForRegex = (dir: string) => {
 };
 
 const escapeStrForRegex =
-  (string: string) => string.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  (string: string) => string.replace(/[[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 
 const replacePathSepForRegex = (string: string) => {
   if (path.sep === '\\') {
