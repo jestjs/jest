cb37340a9275333aae19a9bbd699325e762eed8f
cpojer <christoph.pojer@gmail.com>
2016-10-03 18:17:14 +0900

    Even prettier website.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,5 +1,6 @@
 var Button = require('Button');
 
+var githubButton = <a className="github-button" href="https://github.com/facebook/jest" data-icon="octicon-star" data-style="mega" data-count-href="/facebook/jest/stargazers" data-count-api="/repos/facebook/jest#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star facebook/jest on GitHub">Star</a>;
 var siteConfig = {
   title: "Jest",
   tagline: "Painless JavaScript Testing",
@@ -7,13 +8,14 @@ var siteConfig = {
   url: "https://facebook.github.io",
   baseUrl: "/jest/",
   repo: "facebook/jest",
+  githubButton: githubButton,
   homepagePromos: [
     <div className="pluginRowBlock">
       <Button href="#getting-started">Get Started</Button>
       <Button href="/jest/docs/tutorial-react.html">React Testing</Button>
       <Button href="/jest/docs/tutorial-react-native.html">React Native Testing</Button>
       <Button href="/jest/docs/api.html">API Reference</Button>
-      <a className="github-button" href="https://github.com/facebook/jest" data-icon="octicon-star" data-style="mega" data-count-href="/facebook/jest/stargazers" data-count-api="/repos/facebook/jest#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star facebook/jest on GitHub">Star</a>
+      githubButton,
     </div>,
   ],
   features: [
