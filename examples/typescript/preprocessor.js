3e5dda918c6e981a61a6b7c2fd469c1c13e6feed
Michel Weststrate <mweststrate@gmail.com>
2016-10-21 13:25:17 +0200

    Use typescript compiler more idiomatically

--- examples/typescript/preprocessor.js
+++ examples/typescript/preprocessor.js
@@ -1,16 +1,14 @@
 // Copyright 2004-present Facebook. All Rights Reserved.
 
 const tsc = require('typescript');
+const tsConfig = require('./tsconfig.json');
 
 module.exports = {
   process(src, path) {
     if (path.endsWith('.ts') || path.endsWith('.tsx')) {
       return tsc.transpile(
         src,
-        {
-          module: tsc.ModuleKind.CommonJS,
-          jsx: tsc.JsxEmit.React,
-        },
+        tsConfig.compilerOptions,
         path,
         []
       );
