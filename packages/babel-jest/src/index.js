5b44d32f087ac7ec657092a8c354a01090be317e
Christoph Pojer <christoph.pojer@gmail.com>
2016-10-29 12:05:08 +0900

    .babelrc is now part of the cache key. (#2023)

--- packages/babel-jest/src/index.js
+++ packages/babel-jest/src/index.js
@@ -14,7 +14,36 @@ import type {Config, Path} from 'types/Config';
 import type {TransformOptions} from 'types/Transform';
 
 const babel = require('babel-core');
+const crypto = require('crypto');
+const fs = require('fs');
 const jestPreset = require('babel-preset-jest');
+const path = require('path');
+
+const BABELRC_FILENAME = '.babelrc';
+
+const cache = Object.create(null);
+
+const getBabelRC = (filename, {useCache}) => {
+  const paths = [];
+  let directory = filename;
+  while (directory !== (directory = path.dirname(directory))) {
+    if (useCache && cache[directory]) {
+      break;
+    }
+
+    paths.push(directory);
+    const configFilePath = path.join(directory, BABELRC_FILENAME);
+    if (fs.existsSync(configFilePath)) {
+      cache[directory] = fs.readFileSync(configFilePath, 'utf8');
+      break;
+    }
+  }
+  paths.forEach(directoryPath => {
+    cache[directoryPath] = cache[directory];
+  });
+
+  return cache[directory] || '';
+};
 
 const createTransformer = (options: any) => {
   options = Object.assign({}, options, {
@@ -45,6 +74,21 @@ const createTransformer = (options: any) => {
         ).code;
       }
       return src;
+    },
+    getCacheKey(
+      fileData: string,
+      filename: Path,
+      configString: string,
+      {instrument, watch}: TransformOptions,
+    ): string {
+      return crypto.createHash('md5')
+        .update(fileData)
+        .update(configString)
+        // Don't use the in-memory cache in watch mode because the .babelrc
+        // file may be modified.
+        .update(getBabelRC(filename, {useCache: !watch}))
+        .update(instrument ? 'instrument' : '')
+        .digest('hex');
     },
   };
 };
