e7dcb0d579198461386096313a0df880c1c2aafe
Dmitrijs Balcers <dbalcers@evolutiongaming.com>
2016-11-15 20:56:21 +0200

    Fix Map comparison #2091
    Fix identation issues #2091
    
    Fix typings #2091
    
    Revert package version
    
    Update spyMatchers snapshot
    
    Compare sets properly, so order of values is irrelevant
    
    Fix syntax
    
    Fix issues on node 4

--- packages/jest-diff/src/__tests__/diff-test.js
+++ packages/jest-diff/src/__tests__/diff-test.js
@@ -63,6 +63,24 @@ describe('no visual difference', () => {
       },
     );
   });
+
+  test('Map key order should be irrelevant', () => {
+    const arg1 = new Map([[1, 'foo'], [2, 'bar']]);
+    const arg2 = new Map([[2, 'bar'], [1, 'foo']]);
+
+    expect(stripAnsi(diff(arg1, arg2))).toBe(
+      'Compared values have no visual difference.',
+    );
+  });
+
+  test('Set value order should be irrelevant', () => {
+    const arg1 = new Set([1, 2]);
+    const arg2 = new Set([2, 1]);
+
+    expect(stripAnsi(diff(arg1, arg2))).toBe(
+      'Compared values have no visual difference.',
+    );
+  });
 });
 
 test('oneline strings', () => {
