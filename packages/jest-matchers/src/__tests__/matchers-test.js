d868e5ec11d8d700a93b3ac0e3df78e366c7beb3
Dmitrii Abramov <dmitrii@abramov.io>
2016-11-15 16:46:45 -0800

    .toHaveLength

--- packages/jest-matchers/src/__tests__/matchers-test.js
+++ packages/jest-matchers/src/__tests__/matchers-test.js
@@ -455,3 +455,42 @@ describe('.toMatch()', () => {
     jestExpect('this?: throws').toMatch('this?: throws');
   });
 });
+
+describe('.toHaveLength', () => {
+  [
+    [[1, 2], 2],
+    [[], 0],
+    [['a', 'b'], 2],
+    ['abc', 3],
+    ['', 0],
+  ].forEach(([received, length]) => {
+    test(`expect(${stringify(received)}).toHaveLength(${length})`, () => {
+      jestExpect(received).toHaveLength(length);
+      expect(() => jestExpect(received).not.toHaveLength(length))
+        .toThrowErrorMatchingSnapshot();
+    });
+  });
+
+  [
+    [[1, 2], 3],
+    [[], 1],
+    [['a', 'b'], 99],
+    ['abc', 66],
+    ['', 1],
+  ].forEach(([received, length]) => {
+    test(`expect(${stringify(received)}).toHaveLength(${length})`, () => {
+      jestExpect(received).not.toHaveLength(length);
+      expect(() => jestExpect(received).toHaveLength(length))
+        .toThrowErrorMatchingSnapshot();
+    });
+  });
+
+  test('error cases', () => {
+    expect(() => jestExpect({a: 9}).toHaveLength(1))
+      .toThrowErrorMatchingSnapshot();
+    expect(() => jestExpect(0).toHaveLength(1))
+      .toThrowErrorMatchingSnapshot();
+    expect(() => jestExpect(undefined).toHaveLength(1))
+      .toThrowErrorMatchingSnapshot();
+  });
+});
