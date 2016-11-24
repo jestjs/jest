841efa1a72c0b3f832cb0b2714b9244b02071f09
Dmitrii Abramov <dmitrii@abramov.io>
2016-11-24 10:38:15 -0800

    reduce maxDepth for very large objects (#2148)

--- packages/jest-matcher-utils/src/__tests__/index-test.js
+++ packages/jest-matcher-utils/src/__tests__/index-test.js
@@ -70,6 +70,21 @@ describe('.stringify()', () => {
     expect(() => expect(evilA).toEqual(evilB))
       .toThrowErrorMatchingSnapshot();
   });
+
+  test('reduces maxDepth if stringifying very large objects', () => {
+    const big = {a: 1, b: {}};
+    const small = {a: 1, b: {}};
+    for (let i = 0; i < 10000; i += 1) {
+      big.b[i] = 'test';
+    }
+
+    for (let i = 0; i < 10; i += 1) {
+      small.b[i] = 'test';
+    }
+
+    expect(stringify(big)).toMatchSnapshot();
+    expect(stringify(small)).toMatchSnapshot();
+  });
 });
 
 
