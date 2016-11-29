a515f4c719f4d78d07b1b20937d9bf3b8522f95b
Michał Pierzchała <thymikee@gmail.com>
2016-11-29 14:58:46 +0100

    Escape regex in snapshots (#2128)
    * Escape regex in snapshots
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json

---
+++ integration_tests/snapshot-escape/__tests__/snapshot-escape-regex.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+'use strict';
+
+const regex = /\dd\ \s+ \w \\\[ \. blahzz.* [xyz]+/;
+
+test('escape regex', () => expect(regex).toMatchSnapshot());
+
+test('escape regex nested in object', () => {
+  const objectContainingRegex = {regex};
+  expect(objectContainingRegex).toMatchSnapshot();
+});
