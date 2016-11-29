9060c18d52752aa635c02b353f1d3e82507db5d3
cpojer <christoph.pojer@gmail.com>
2016-11-29 11:00:42 +0000

    Update jest-react-native preset to forward to react-native.

--- packages/jest-react-native/src/index.js
+++ packages/jest-react-native/src/index.js
@@ -7,19 +7,4 @@
  */
 'use strict';
 
-module.exports = {
-  mockComponent: moduleName => {
-    const RealComponent = require.requireActual(moduleName);
-    const React = require('react');
-    const Component = class extends RealComponent {
-      render() {
-        return React.createElement(
-          RealComponent.displayName || RealComponent.name,
-          this.props,
-          this.props.children,
-        );
-      }
-    };
-    return Component;
-  },
-};
+module.exports = require('react-native/jest/mockComponent');
