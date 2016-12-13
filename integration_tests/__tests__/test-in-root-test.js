a9360bbd5bd4aa4e546f5a0e73c54f8087e2de12
Simen Bekkhus <sbekkhus91@gmail.com>
2016-12-13 12:38:40 +0100

    Allow files called just test.jsx? or spec.jsx? in root (#2211)

---
+++ integration_tests/__tests__/test-in-root-test.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @emails oncall+jsinfra
+ */
+'use strict';
+
+const path = require('path');
+const runJest = require('../runJest');
+
+it('runs tests in only test.js and spec.js', () => {
+  const result = runJest.json('test-in-root').json;
+
+  expect(result.success).toBe(true);
+  expect(result.numTotalTests).toBe(2);
+
+  const testNames = result.testResults
+      .map(res => res.name)
+      .map(name => path.basename(name))
+      .sort();
+
+  expect(testNames.length).toBe(2);
+  expect(testNames[0]).toBe('spec.js');
+  expect(testNames[1]).toBe('test.js');
+});
