0032bf7f5ee1d24fc0602508de04bcf328257319
Rogelio Guzman <rogelioguzmanh@gmail.com>
2016-12-18 15:13:09 -0800

    Add tests to getMaxWorkers and setWatchMode

--- packages/jest-cli/src/lib/setWatchMode.js
+++ packages/jest-cli/src/lib/setWatchMode.js
@@ -1,6 +1,21 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ */
+'use strict';
+
 const buildTestPathPatternInfo = require('./buildTestPathPatternInfo');
 
-const setWatchMode = (argv, mode: 'watch' | 'watchAll', options) => {
+const setWatchMode = (
+  argv: Object,
+  mode: 'watch' | 'watchAll',
+  options: Object,
+) => {
   if (mode === 'watch') {
     argv.watch = true;
     argv.watchAll = false;
