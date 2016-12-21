babd264706d319ea90bf085a098b04a4d6fe91c7
Jonathan Kim <jonathan.kim@db.com>
2016-12-21 14:36:06 +0000

    Add copyright notice and fix up types

--- packages/eslint-plugin-jest/src/index.js
+++ packages/eslint-plugin-jest/src/index.js
@@ -1,7 +1,12 @@
 /* eslint-disable global-require */
 /**
- * @fileoverview Eslint plugin for Jest
- * @author Jonathan Kim
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
  */
 'use strict';
 
--- packages/eslint-plugin-jest/src/rules/types.js
+++ packages/eslint-plugin-jest/src/rules/types.js
@@ -1,16 +1,27 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ */
 export type EslintContext = {
   report: (message: any) => void
 }
 
 export type EslintNode = {
-  callee?: {
+  type: string,
+  arguments: [{ type: string, value: string }],
+  callee: {
     type: string,
     name: string,
-    property?: {
+    property: {
       name: string,
       value: string,
     },
-    object?: {
+    object: {
       name: string,
     },
   }
