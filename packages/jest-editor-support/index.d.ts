b9cb0ace7dbba04fab8b9456f432d43d60e49df0
Blake Embrey <hello@blakeembrey.com>
2016-12-29 20:06:04 -0500

    Update index.d.ts (#2465)

--- packages/jest-editor-support/index.d.ts
+++ packages/jest-editor-support/index.d.ts
@@ -6,114 +6,112 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-declare module 'jest-editor-support' {
-  import {EventEmitter} from 'events';
+import {EventEmitter} from 'events';
 
-  export class Runner extends EventEmitter {
-    constructor(workspace: ProjectWorkspace);
-    start(): void;
-    closeProcess(): void;
-    runJestWithUpdateForSnapshots(completion: any): void;
-  }
+export class Runner extends EventEmitter {
+  constructor(workspace: ProjectWorkspace);
+  start(): void;
+  closeProcess(): void;
+  runJestWithUpdateForSnapshots(completion: any): void;
+}
 
-  export class Settings extends EventEmitter {
-    constructor(workspace: ProjectWorkspace);
-    getConfig(completed: Function): void;
-    jestVersionMajor: number | null;
-    settings: {
-      testRegex: string;
-    };
-  }
-
-  export class ProjectWorkspace {
-    constructor(
-      rootPath: string,
-      pathToJest: string,
-      pathToConfig: string,
-      localJestMajorVersin: number
-    );
-    pathToJest: string;
-    rootPath: string;
-    localJestMajorVersion: number;
-  }
-
-  export interface IParseResults {
-    expects: Expect[];
-    itBlocks: ItBlock[];
-  }
-
-  export function parse(file: string): IParseResults;
-
-  type Location = {
-    column: number,
-    line: number,
+export class Settings extends EventEmitter {
+  constructor(workspace: ProjectWorkspace);
+  getConfig(completed: Function): void;
+  jestVersionMajor: number | null;
+  settings: {
+    testRegex: string;
   };
+}
 
-  class Node {
-    start: Location;
-    end: Location;
-    file: string;
-  }
+export class ProjectWorkspace {
+  constructor(
+    rootPath: string,
+    pathToJest: string,
+    pathToConfig: string,
+    localJestMajorVersin: number
+  );
+  pathToJest: string;
+  rootPath: string;
+  localJestMajorVersion: number;
+}
 
-  export class ItBlock extends Node {
-    name: string;
-  }
+export interface IParseResults {
+  expects: Expect[];
+  itBlocks: ItBlock[];
+}
 
-  export type Expect = Node;
+export function parse(file: string): IParseResults;
 
-  export class TestReconciler {
-    stateForTestFile(file: string): TestReconcilationState;
-    stateForTestAssertion(file: string, name: string): TestFileAssertionStatus | null;
-    failedStatuses(): Array<TestFileAssertionStatus>;
-    updateFileWithJestStatus(data): void;
-  }
+export interface Location {
+  column: number;
+  line: number;
+}
 
-  type TestReconcilationState = "Unknown" |
-    "KnownSuccess" |
-    "KnownFail";
+export class Node {
+  start: Location;
+  end: Location;
+  file: string;
+}
 
-  export type TestFileAssertionStatus = {
-    file: string,
-    message: string,
-    status: TestReconcilationState,
-    assertions: Array<TestAssertionStatus>,
-  }
+export class ItBlock extends Node {
+  name: string;
+}
 
-  export type TestAssertionStatus = {
-    title: string,
-    status: TestReconcilationState,
-    message: string,
-    shortMessage?: string,
-    terseMessage?: string,
-    line?: number,
-  }
+export class Expect extends Node {}
 
-  export type JestFileResults = {
-    name: string,
-    summary: string,
-    message: string,
-    status: "failed" | "passed",
-    startTime: number,
-    endTime: number,
-    assertionResults: Array<JestAssertionResults>,
-  }
+export class TestReconciler {
+  stateForTestFile(file: string): TestReconcilationState;
+  stateForTestAssertion(file: string, name: string): TestFileAssertionStatus | null;
+  failedStatuses(): Array<TestFileAssertionStatus>;
+  updateFileWithJestStatus(data): void;
+}
 
-  export type JestAssertionResults = {
-    name: string,
-    title: string,
-    status: "failed" | "passed",
-    failureMessages: string[],
-  }
+export type TestReconcilationState = "Unknown" |
+  "KnownSuccess" |
+  "KnownFail";
 
-  export type JestTotalResults = {
-    success: boolean,
-    startTime: number,
-    numTotalTests: number,
-    numTotalTestSuites: number,
-    numRuntimeErrorTestSuites: number,
-    numPassedTests: number,
-    numFailedTests: number,
-    numPendingTests: number,
-    testResults: Array<JestFileResults>,
-  }
+export interface TestFileAssertionStatus {
+  file: string;
+  message: string;
+  status: TestReconcilationState;
+  assertions: Array<TestAssertionStatus>;
+}
+
+export interface TestAssertionStatus {
+  title: string;
+  status: TestReconcilationState;
+  message: string;
+  shortMessage?: string;
+  terseMessage?: string;
+  line?: number;
+}
+
+export interface JestFileResults {
+  name: string;
+  summary: string;
+  message: string;
+  status: "failed" | "passed";
+  startTime: number;
+  endTime: number;
+  assertionResults: Array<JestAssertionResults>;
+}
+
+export interface JestAssertionResults {
+  name: string;
+  title: string;
+  status: "failed" | "passed";
+  failureMessages: string[];
+}
+
+export interface JestTotalResults {
+  success: boolean;
+  startTime: number;
+  numTotalTests: number;
+  numTotalTestSuites: number;
+  numRuntimeErrorTestSuites: number;
+  numPassedTests: number;
+  numFailedTests: number;
+  numPendingTests: number;
+  testResults: Array<JestFileResults>;
 }
