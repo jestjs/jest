9769817a57c294b9712541ac12d3a10aed99871f
Héctor Ramos <hector.ramos@gmail.com>
2017-01-06 13:17:39 -0800

    Flex showcase.

--- website/src/jest/users.js
+++ website/src/jest/users.js
@@ -2,152 +2,193 @@
  * @jsx React.DOM
  */
 
-var React = require('React');
-var Site = require('Site');
-var Prism = require('Prism');
-var Marked = require('Marked');
-var unindent = require('unindent');
+const React = require('React');
+const Site = require('Site');
+const Prism = require('Prism');
+const Marked = require('Marked');
+const unindent = require('unindent');
 
-var Container = require('Container');
-var HomeSplash = require('HomeSplash');
-var GridBlock = require('GridBlock');
+const Container = require('Container');
+const HomeSplash = require('HomeSplash');
+const GridBlock = require('GridBlock');
 
-var siteConfig = require('../../siteConfig.js');
-var gettingStartedContent = require('./docs/getting-started.js').content;
-var blog = require('MetadataBlog');
+const siteConfig = require('../../siteConfig.js');
+const gettingStartedContent = require('./docs/getting-started.js').content;
+const blog = require('MetadataBlog');
 
-var index = React.createClass({
+const index = React.createClass({
   render: function() {
-    var users = [
+    const users = [
       {
-        image: 'img/logos/pinterest.png',
-        content: '',
-      },
-      {
-        image: 'img/logos/twitter.png',
-        content: '',
-      },
-      {
-        image: 'img/logos/paypal.png',
-        content: '',
-      },
-      {
+        infoLink: 'https://code.facebook.com',
+        caption: 'Facebook',
         image: 'img/logos/facebook.png',
-        content: '',
       },
       {
+        infoLink: 'https://www.oculus.com/',
+        caption: 'Oculus',
         image: 'img/logos/oculus.png',
-        content: '',
       },
       {
+        infoLink: 'https://www.instagram.com/',
+        caption: 'Instagram',
         image: 'img/logos/instagram.png',
-        content: '',
       },
       {
-        image: 'img/logos/target.png',
-        content: '',
+        infoLink: 'https://www.twitter.com',
+        caption: 'Twitter',
+        image: 'img/logos/twitter.png',
       },
       {
+        infoLink: 'https://www.pinterest.com',
+        caption: 'Pinterest',
+        image: 'img/logos/pinterest.png',
+      },
+      {
+        infoLink: 'http://www.nytimes.com/',
+        caption: 'The New York Times',
         image: 'img/logos/nyt.png',
-        content: '',
       },
       {
-        image: 'img/logos/spotify.png',
-        content: '',
-      },
-      {
-        image: 'img/logos/discord.png',
-        content: '',
-      },
-      {
-        image: 'img/logos/formidablelabs.png',
-        content: '',
-      },
-      {
+        infoLink: 'http://www.ibm.com/',
+        caption: 'IBM',
         image: 'img/logos/ibm.png',
-        content: '',
       },
       {
-        image: 'img/logos/klm.png',
-        content: '',
-      },
-      {
-        image: 'img/logos/xing.png',
-        content: '',
-      },
-      {
-        image: 'img/logos/automattic.png',
-        content: '',
-      },
-      {
-        image: 'img/logos/soundcloud.png',
-        content: '',
-      },
-      {
+        infoLink: 'http://www.ebay.com/',
+        caption: 'ebay',
         image: 'img/logos/ebay.png',
-        content: '',
       },
       {
-        image: 'img/logos/trivago.png',
-        content: '',
+        infoLink: 'https://www.paypal.com',
+        caption: 'PayPal',
+        image: 'img/logos/paypal.png',
       },
       {
-        image: 'img/logos/coinbase.png',
-        content: '',
+        infoLink: 'https://www.spotify.com',
+        caption: 'Spotify',
+        image: 'img/logos/spotify.png',
       },
       {
+        infoLink: 'http://www.target.com',
+        caption: 'Target',
+        image: 'img/logos/target.png',
+      },
+      {
+        infoLink: 'https://www.intuit.com/',
+        caption: 'Intuit',
         image: 'img/logos/intuit.png',
-        content: '',
       },
       {
-        image: 'img/logos/reddit.png',
-        content: '',
-      },
-      {
-        image: 'img/logos/egghead.png',
-        content: '',
-      },
-      {
-        image: 'img/logos/elastic.png',
-        content: '',
-      },
-      {
-        image: 'img/logos/quiqup.png',
-        content: '',
-      },
-      {
+        infoLink: 'http://www.cisco.com/',
+        caption: 'Cisco',
         image: 'img/logos/cisco.png',
-        content: '',
       },
       {
-        image: 'img/logos/globo.png',
-        content: '',
-      },
-      {
+        infoLink: 'https://www.artsy.net/',
+        caption: 'Artsy',
         image: 'img/logos/artsy.png',
-        content: '',
       },
       {
+        infoLink: 'https://automattic.com/',
+        caption: 'Automattic',
+        image: 'img/logos/automattic.png',
+      },
+      {
+        infoLink: 'https://www.coinbase.com/',
+        caption: 'Coinbase',
+        image: 'img/logos/coinbase.png',
+      },
+      {
+        infoLink: 'https://discordapp.com/',
+        caption: 'Discord',
+        image: 'img/logos/discord.png',
+      },
+      {
+        infoLink: 'https://egghead.io/',
+        caption: 'Egghead',
+        image: 'img/logos/egghead.png',
+      },
+      {
+        infoLink: 'https://www.elastic.co/',
+        caption: 'Elastic',
+        image: 'img/logos/elastic.png',
+      },
+      {
+        infoLink: 'http://formidable.com/',
+        caption: 'Formidable',
+        image: 'img/logos/formidablelabs.png',
+      },
+      {
+        infoLink: 'http://www.globo.com/',
+        caption: 'Globo',
+        image: 'img/logos/globo.png',
+      },
+      {
+        infoLink: 'https://www.intercom.com/',
+        caption: 'Intercom',
         image: 'img/logos/intercom.png',
-        content: '',
       },
       {
+        infoLink: 'https://www.klm.com/',
+        caption: 'KLM Royal Dutch Airlines',
+        image: 'img/logos/klm.png',
+      },
+      {
+        infoLink: 'https://www.quiqup.com/',
+        caption: 'Quiqup',
+        image: 'img/logos/quiqup.png',
+      },
+      {
+        infoLink: 'https://www.reddit.com/',
+        caption: 'Reddit',
+        image: 'img/logos/reddit.png',
+      },
+      {
+        infoLink: 'https://seatgeek.com/',
+        caption: 'SeatGeek',
         image: 'img/logos/seatgeek.png',
-        content: '',
       },
       {
+        infoLink: 'https://soundcloud.com/',
+        caption: 'SoundCloud',
+        image: 'img/logos/soundcloud.png',
+      },
+      {
+        infoLink: 'http://www.trivago.com/',
+        caption: 'Trivago',
+        image: 'img/logos/trivago.png',
+      },
+      {
+        infoLink: 'https://www.xing.com/',
+        caption: 'Xing',
+        image: 'img/logos/xing.png',
+      },
+      {
+        infoLink: 'https://wowair.com/',
+        caption: 'WOW air',
         image: 'img/logos/wowair.png',
-        content: '',
       },
     ];
+
+    const showcase = users.map((user) => {
+      return <a href={user.infoLink}><img src={user.image} title={user.caption}/></a>;
+    });
 
     return (
       <Site>
         <div className="mainContainer">
           <Container padding={["bottom","top"]}>
-            <h2>Who's Using Jest?</h2>
-            <GridBlock className="responsiveList threeByGridBlock" contents={users} layout="threeColumn" />
-            <p>Is your company using Jest? Edit this page with a <a href="https://github.com/facebook/jest/edit/website/src/jest/users.js">Pull Request</a> to add your logo.</p>
+            <div className="showcaseSection">
+              <div className="prose">
+                <h1>Who's Using Jest?</h1>
+                <p>Jest is used by teams of all sizes to test websites, mobile apps, and APIs.</p>
+                <p>Is your company using Jest?<br/>Edit this page with a <a href="https://github.com/facebook/jest/edit/website/src/jest/users.js">Pull Request</a> to add your logo.</p>
+              </div>
+              <div className="logos">
+                {showcase}
+              </div>
+            </div>
           </Container>
         </div>
       </Site>
