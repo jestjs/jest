af58d9ae404dda7eb226181c41a69ba7ece391fc
Rogelio Guzman <rogelioguzmanh@gmail.com>
2017-01-08 20:26:03 -0600

    Add spyOn to jest-runtime

--- packages/jest-runtime/src/index.js
+++ packages/jest-runtime/src/index.js
@@ -693,6 +693,7 @@ class Runtime {
       return runtime;
     };
     const fn = this._moduleMocker.fn.bind(this._moduleMocker);
+    const spyOn = this._moduleMocker.spyOn.bind(this._moduleMocker);
 
     const runtime = {
       addMatchers:
@@ -729,6 +730,7 @@ class Runtime {
 
       setMock: (moduleName: string, mock: Object) =>
         setMockFactory(moduleName, () => mock),
+      spyOn,
 
       unmock,
 
