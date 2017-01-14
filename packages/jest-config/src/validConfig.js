9c638f0c2a07167c8d7f942f013528b346afaed8
Michał Pierzchała <thymikee@gmail.com>
2017-01-14 22:45:56 +0100

    Add missing 'testResultsProcessor' to validConfig

--- packages/jest-config/src/validConfig.js
+++ packages/jest-config/src/validConfig.js
@@ -69,6 +69,7 @@ module.exports = ({
   testPathDirs: ['<rootDir>'],
   testPathIgnorePatterns: [NODE_MODULES_REGEXP],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
+  testResultsProcessor: 'processor-node-module',
   testRunner: 'jasmine2',
   testURL: 'about:blank',
   timers: 'real',
