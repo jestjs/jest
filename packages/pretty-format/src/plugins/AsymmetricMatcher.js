07b4bf793531637176fb3ce47f42fd60ce69b58c
Michał Pierzchała <thymikee@gmail.com>
2017-01-15 22:12:21 +0100

    Pretty-print asymmetric matchers (#2476)
    * First attempt to fix this
    
    * Extract asymmetric-matchers from jasmine-utils
    
    * Remove <jasmine.*> adnotations
    
    * Remove obsolete snapshots
    
    * Add AsymmetricMatcher plugin for pretty-format
    
    * Refactor asymmetric-matchers
    
    * Use AsymmetricMatcher plugin in diffs
    
    * Remove unused prettyFormat from asymmetric-matcher
    
    * Check for object presence in AsymmetricMatcher
    
    * Update snapshots
    
    * Tests for asymmetric-matchers
    
    * Update snapshots with patch marks
    
    * Adjust printing of asymmetric matchers
    
    * Use class instances instead of regex to render values

---
+++ packages/pretty-format/src/plugins/AsymmetricMatcher.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ * @flow
+ */
+
+'use strict';
+
+const asymmetricMatcher = Symbol.for('jest.asymmetricMatcher');
+const SPACE = ' ';
+
+class ArrayContaining extends Array {}
+class ObjectContaining extends Object {}
+
+const printAsymmetricMatcher = (
+  val: any,
+  print: Function,
+  indent: Function,
+  opts: Object,
+  colors: Object
+) => {
+  const stringedValue = val.toString();
+
+  if (stringedValue === 'ArrayContaining') {
+    const array = ArrayContaining.from(val.sample);
+    return opts.spacing === SPACE
+      ? stringedValue + SPACE + print(array)
+      : print(array);
+  }
+
+  if (stringedValue === 'ObjectContaining') {
+    const object = Object.assign(new ObjectContaining(), val.sample);
+    return opts.spacing === SPACE
+      ? stringedValue + SPACE + print(object)
+      : print(object);
+  }
+
+  if (stringedValue === 'StringMatching') {
+    return stringedValue + SPACE + print(val.sample);
+  }
+
+  return val.toAsymmetricMatcher();
+};
+
+module.exports = {
+  print: printAsymmetricMatcher,
+  test: (object: any) => object && object.$$typeof === asymmetricMatcher,
+};
