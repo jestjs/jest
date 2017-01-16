8c0340f2f6f8cf215064a526a677451d2965fb79
Michał Pierzchała <thymikee@gmail.com>
2017-01-16 13:57:30 +0100

    Validate config with `jest-validate` package (#2524)
    * WIP validate.js
    
    * Add validate.js to handle config validation
    
    * Validate before normalizing
    
    * Add deprecated config handling
    
    * Fix lint errors
    
    * Fix Runtime-NODE_PATH-test
    
    * Fix linting on validate-test
    
    * Adjust tests
    
    * Move config validation to jest-validate package
    
    * Update readme
    
    * Make 'deprecated' configurable
    
    * Fix normalize and add test for preset
    
    * Move helper functions outside normalize()
    
    * Further simplify normalize.js
    
    * Add copyright to jest-validate types
    
    * Make normalize() a little more declarative
    
    * New Validation Errors for normalize
    
    * Fix extra quotation mark
    
    * Remove scripts from jest-validate's package.json
    
    * Update snapshots
    
    * Simplify condition
    
    * chalk.white -> chalk.reset
    
    * Sort config keys
    
    * Simplify footer condition
    
    * deprecatedWarning -> deprecationWarning
    
    * use getType() from jest-matcher-utils instead of prettyPrintType()
    
    * Adjust README
    
    * Remove preset.preprocessorIgnorePatterns check
    
    * Adjust jest-validate to be more generic
    
    * Update yarn.lock
    
    * Add missing 'testResultsProcessor' to validConfig
    
    * Update types with testResultsProcessor
    
    * Rename runtimeConfigError -> createConfigError
    
    * Rename footer -> comment
    
    * Rename validationError -> createValidationError
    
    * Remove scripts form jest-config package.json
    
    * Add defaultConfig and exampleConfig for self-validation check
    
    * Furhter refine the API of jest-validate
    
    * Adjust validate() usage in normalize.js
    
    * Update README.md
    
    * Update types.js

---
+++ packages/jest-config/src/utils.js
@@ -0,0 +1,134 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ */
+
+'use strict';
+
+import type {Path} from 'types/Config';
+
+const {ValidationError} = require('jest-validate');
+const Resolver = require('jest-resolve');
+const path = require('path');
+const chalk = require('chalk');
+const BULLET: string = chalk.bold('\u25cf ');
+const DOCUMENTATION_NOTE =
+`  ${chalk.bold('Configuration Documentation:')}
+  https://facebook.github.io/jest/docs/configuration.html
+`;
+
+const createValidationError = (message: string) => {
+  return new ValidationError(
+    `${BULLET}Validation Error`,
+    message,
+    DOCUMENTATION_NOTE
+  );
+};
+
+const resolve = (rootDir: string, key: string, filePath: Path) => {
+  const module = Resolver.findNodeModule(
+    _replaceRootDirInPath(rootDir, filePath),
+    {
+      basedir: rootDir,
+    },
+  );
+
+  if (!module) {
+    /* eslint-disable max-len */
+    throw createValidationError(
+      `  Module ${chalk.bold(filePath)} in the ${chalk.bold(key)} option was not found.`
+    );
+    /* eslint-disable max-len */
+  }
+
+  return module;
+};
+
+const _replaceRootDirInPath = (rootDir: string, filePath: Path): string => {
+  if (!/^<rootDir>/.test(filePath)) {
+    return filePath;
+  }
+
+  return path.resolve(
+    rootDir,
+    path.normalize('./' + filePath.substr('<rootDir>'.length)),
+  );
+};
+
+const _replaceRootDirTags = (rootDir: string, config: any) => {
+  switch (typeof config) {
+    case 'object':
+      if (config instanceof RegExp) {
+        return config;
+      }
+
+      if (Array.isArray(config)) {
+        return config.map(item => _replaceRootDirTags(rootDir, item));
+      }
+
+      if (config !== null) {
+        const newConfig = {};
+        for (const configKey in config) {
+          newConfig[configKey] = configKey === 'rootDir'
+            ? config[configKey]
+            : _replaceRootDirTags(rootDir, config[configKey]);
+        }
+        return newConfig;
+      }
+      break;
+    case 'string':
+      return _replaceRootDirInPath(rootDir, config);
+  }
+  return config;
+};
+
+/**
+ * Finds the test environment to use:
+ *
+ * 1. looks for jest-environment-<name> relative to project.
+ * 1. looks for jest-environment-<name> relative to Jest.
+ * 1. looks for <name> relative to project.
+ * 1. looks for <name> relative to Jest.
+ */
+const getTestEnvironment = (config: Object) => {
+  const env = config.testEnvironment;
+  let module = Resolver.findNodeModule(`jest-environment-${env}`, {
+    basedir: config.rootDir,
+  });
+  if (module) {
+    return module;
+  }
+
+  try {
+    return require.resolve(`jest-environment-${env}`);
+  } catch (e) {}
+
+  module = Resolver.findNodeModule(env, {basedir: config.rootDir});
+  if (module) {
+    return module;
+  }
+
+  try {
+    return require.resolve(env);
+  } catch (e) {}
+
+  /* eslint-disable max-len */
+  throw createValidationError(
+    `  Test environment ${chalk.bold(env)} cannot be found. Make sure the ${chalk.bold('testEnvironment')} configuration option points to an existing node module.`
+  );
+  /* eslint-disable max-len */
+};
+
+module.exports = {
+  BULLET,
+  DOCUMENTATION_NOTE,
+  _replaceRootDirInPath,
+  _replaceRootDirTags,
+  getTestEnvironment,
+  resolve,
+};
