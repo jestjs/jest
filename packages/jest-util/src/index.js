eef5cd4f414e391eb62557b35584e3ca43cf0d29
Michał Pierzchała <thymikee@gmail.com>
2017-01-17 16:48:11 +0100

    Introduce 'did you mean' with levenshtein

--- packages/jest-util/src/index.js
+++ packages/jest-util/src/index.js
@@ -76,25 +76,56 @@ const getPackageRoot = () => {
   return root;
 };
 
+const createCLIValidationError = (
+  unrecognizedOptions: Array<string>,
+  allowedOptions: Set<string>,
+) => {
+  let title = `${BULLET} Unrecognized CLI Parameter`;
+  let message;
+  const comment =
+  `  ${chalk.bold('CLI Options Documentation')}:\n` +
+  `  http://facebook.github.io/jest/docs/cli.html\n`;
+
+  if (unrecognizedOptions.length === 1) {
+    const leven = require('leven');
+    const unrecognized = unrecognizedOptions[0];
+    let suggestion;
+
+    allowedOptions.forEach(option => {
+      const steps = leven(option, unrecognized);
+      if (steps < 3) {
+        suggestion = option;
+      }
+    });
+
+    message = `  Unrecognized option ${chalk.bold(format(unrecognized))}.`;
+    if (suggestion) {
+      message += ` Did you mean ${chalk.bold(format(suggestion))}?`;
+    }
+  } else {
+    title += 's';
+    message =
+      `  Following options were not recognized by Jest:\n` +
+      `  ${chalk.bold(format(unrecognizedOptions))}`;
+  }
+
+  return new ValidationError(title, message, comment);
+};
+
 const validateCLIOptions = (argv: Object, options: Object) => {
   const yargsSpecialOptions = ['$0', '_', 'help', 'h'];
-  const allowedOptions = Object.keys(options).reduce((acc, option) => (
+  const allowedOptions = Object.keys(options).reduce((acc, option) =>
     acc
       .add(option)
-      .add(options[option].alias)
-  ), new Set(yargsSpecialOptions));
+      .add(options[option].alias || option),
+    new Set(yargsSpecialOptions)
+  );
   const unrecognizedOptions = Object.keys(argv).filter(arg => (
     !allowedOptions.has(arg)
   ));
+
   if (unrecognizedOptions.length) {
-    const title = `${BULLET} Unrecognized CLI Options Error`;
-    const message =
-      `  These options were not recognized by Jest:\n` +
-      `  ${chalk.bold(format(unrecognizedOptions))}`;
-    const comment =
-      `  ${chalk.bold('CLI Options Documentation')}:\n` +
-      `  http://facebook.github.io/jest/docs/api.html#jest-cli-options\n`;
-    throw new ValidationError(title, message, comment);
+    throw createCLIValidationError(unrecognizedOptions, allowedOptions);
   }
 };
 
