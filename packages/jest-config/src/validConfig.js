780ed25a2b57bdcf802ac00aad679abf1d8df9ce
Max Sysoev <maxim.sysoev@lingvokot.com>
2017-01-25 00:41:39 +0300

    Add globalMocks to valid config

--- packages/jest-config/src/validConfig.js
+++ packages/jest-config/src/validConfig.js
@@ -38,6 +38,7 @@ module.exports = ({
   },
   expand: false,
   forceExit: false,
+  globalMocks: ['<rootDir>/__mocks__'],
   globals: {},
   haste: {
     providesModuleNodeModules: ['react', 'react-native'],
