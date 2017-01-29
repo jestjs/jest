0c611090b8a5cf96e30ebac6f623d6c774f4908a
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2017-01-29 16:51:39 -0700

    Add copyright header.

--- types/Mock.js
+++ types/Mock.js
@@ -1,6 +1,15 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ */
 'use strict';
 
-import type {_MockFunctionMetadata, _ModuleMocker} from 'jest-mock';
+import type {MockFunctionMetadata as _MockFunctionMetadata, ModuleMocker as _ModuleMocker} from 'jest-mock';
 
 export type MockFunctionMetadata = _MockFunctionMetadata;
 export type ModuleMocker = _ModuleMocker;
