64a6b20d6dc2d3fff02e932055220a0c78563a43
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2017-01-29 16:36:09 -0700

    Hoist `jest-mock` types to `types/Mock`.

---
+++ types/Mock.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import type {_MockFunctionMetadata, _ModuleMocker} from 'jest-mock';
+
+export type MockFunctionMetadata = _MockFunctionMetadata;
+export type ModuleMocker = _ModuleMocker;
