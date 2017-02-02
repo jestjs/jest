4f6b7d08759319d4e1211a63ef0ca2f135a1bfbe
Mark Pedrotti <pedrottimark@gmail.com>
2017-02-02 10:46:45 -0500

    Move expect.addSnapshotSerializer to jest-jasmine2

--- packages/jest-jasmine2/src/jest-expect.js
+++ packages/jest-jasmine2/src/jest-expect.js
@@ -15,6 +15,7 @@ import type {RawMatcherFn} from 'types/Matchers';
 const expect = require('jest-matchers');
 
 const {
+  addSerializer,
   toMatchSnapshot,
   toThrowErrorMatchingSnapshot,
 } = require('jest-snapshot');
@@ -32,6 +33,8 @@ module.exports = (config: Config) => {
     expand: config.expand,
   });
   expect.extend({toMatchSnapshot, toThrowErrorMatchingSnapshot});
+
+  expect.addSnapshotSerializer = addSerializer;
 
   const jasmine = global.jasmine;
   jasmine.addMatchers = (jasmineMatchersObject: JasmineMatchersObject) => {
