0bd22cf2273a1d91bd905a9705e091bdafd43fc2
Aren Blondahl <rean1985@gmail.com>
2017-02-03 01:56:01 -0700

    Integration tests for haste retainAllFiles / testPathIgnorePatterns
    Before completing work on the fix we're adding a test-case
    for the problem we're fixing. This allows us to demonstrate
    a scenario where we want to have jest only ignore the files
    in the testPathIgnorePatterns without making assumptions
    about node_modules/ paths.

---
+++ integration_tests/__tests__/haste-retain-all-ignore-paths-test.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+'use strict';
+
+const runJest = require('../runJest');
+
+it('does not run tests in node_modules/', () => {
+  const result = runJest.json('haste-retain-all-ignore-paths').json;
+
+  const testNames = result.testResults
+      .map(res => res.name)
+      .sort();
+
+  expect(testNames.some(
+    x => x.includes('library/__tests__/shouldnt-run.js'))).toBe(false);
+});
+
+it('runs tests in src/node_modules/', () => {
+  const result = runJest.json('haste-retain-all-ignore-paths').json;
+
+  const testNames = result.testResults
+      .map(res => res.name)
+      .sort();
+
+  expect(testNames.some(
+    x => x.includes('src-library/__tests__/should-run.js'))).toBe(true);
+});
+
+
