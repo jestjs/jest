1e75d973f2bd06f014e3decad16439a833a761f3
Nick Farina <nfarina@gmail.com>
2017-02-08 02:31:43 -0800

    Integrate and simplify `toMatchObject`; validate "special cases" (#2798)
    - Hoist methods from `toMatchObject` outside the matchers object to
    better resemble other matchers
      - Simplify “subset matching” while leveraging the expected equality
    and iterable equality tests by creating a customMatcher for `eq`
      - Add test cases to validate jasmine’s special handling of equality
    like `0 == -0`
      - Add test cases to validate `iterableEquality` when used as a custom
    matcher for `toEqual` and `toMatchObject`
      - Cleanup style near changed code.

--- packages/jest-matchers/src/matchers.js
+++ packages/jest-matchers/src/matchers.js
@@ -17,7 +17,10 @@ import type {
 
 const diff = require('jest-diff');
 const {escapeStrForRegex} = require('jest-regex-util');
-const {getPath} = require('./utils');
+const {
+  getObjectSubset,
+  getPath,
+} = require('./utils');
 const {
   EXPECTED_COLOR,
   RECEIVED_COLOR,
@@ -31,8 +34,6 @@ const {
 } = require('jest-matcher-utils');
 const {
   equals,
-  asymmetricMatch,
-  isUndefined,
 } = require('./jasmine-utils');
 
 type ContainIterable = (
@@ -79,6 +80,17 @@ const iterableEquality = (a, b) => {
     return false;
   }
   return true;
+};
+const isObjectWithKeys = a => a !== null && typeof a === 'object' &&
+  !(a instanceof Array) && !(a instanceof Date);
+const subsetEquality = (object, subset) => {
+  if (!isObjectWithKeys(object) || !isObjectWithKeys(subset)) {
+    return undefined;
+  }
+  return Object.keys(subset).every(key =>
+    object.hasOwnProperty(key) &&
+    equals(object[key], subset[key], [iterableEquality, subsetEquality])
+  );
 };
 
 const matchers: MatchersObject = {
@@ -561,114 +573,25 @@ const matchers: MatchersObject = {
       );
     }
 
-    const compare = (expected: any, received: any): boolean => {
-
-      const asymmetricResult = asymmetricMatch(expected, received);
-      if (!isUndefined(asymmetricResult)) {
-        return asymmetricResult;
-      }
-
-      if (typeof received !== typeof expected) {
-        return false;
-      }
-      if (typeof expected !== 'object' || expected === null) {
-        return expected === received;
-      }
-
-      if (Array.isArray(expected)) {
-        if (!Array.isArray(received)) {
-          return false;
-        }
-
-        if (expected.length !== received.length) {
-          return false;
-        }
-
-        return expected.every((exp, i) => compare(exp, received[i]));
-      }
-
-      if (expected instanceof Date && received instanceof Date) {
-        return expected.getTime() === received.getTime();
-      }
-
-      return Object.keys(expected).every(key => {
-        if (!received.hasOwnProperty(key)) {
-          return false;
-        }
-        const exp = expected[key];
-        const act = received[key];
-        if (typeof exp === 'object' && exp !== null && act !== null) {
-          return compare(exp, act);
-        }
-        return act === exp;
-      });
-    };
-
-    // Strip properties form received object that are not present in the expected
-    // object. We need it to print the diff without adding a lot of unrelated noise.
-    const findMatchObject = (expected: Object, received: Object)  => {
-      if (Array.isArray(received)) {
-        if (!Array.isArray(expected)) {
-          return received;
-        }
-
-        if (expected.length !== received.length) {
-          return received;
-        }
-
-        return expected.map((exp, i) => findMatchObject(exp, received[i]));
-
-      } else if (received instanceof Date) {
-        return received;
-      } else if (typeof received === 'object' && received !== null && typeof expected === 'object' && expected !== null) {
-
-        const matchedObject = {};
-
-        let match = false;
-        Object.keys(expected).forEach(key => {
-          if (received.hasOwnProperty(key)) {
-            match = true;
-
-            const exp = expected[key];
-            const act = received[key];
-
-            if (typeof exp === 'object' && exp !== null) {
-              matchedObject[key] = findMatchObject(exp, act);
-            } else {
-              matchedObject[key] = act;
-            }
-          }
-        });
-
-        if (match) {
-          return matchedObject;
-        } else {
-          return received;
-        }
-      } else {
-        return received;
-      }
-    };
-
-    const pass = compare(expectedObject, receivedObject);
+    const pass = equals(receivedObject, expectedObject, [iterableEquality, subsetEquality]);
 
     const message = pass
       ? () => matcherHint('.not.toMatchObject') +
-     `\n\nExpected value not to match object:\n` +
-     `  ${printExpected(expectedObject)}` +
-     `\nReceived:\n` +
-     `  ${printReceived(receivedObject)}`
-       : () => {
-         const diffString = diff(expectedObject, findMatchObject(expectedObject, receivedObject), {
-           expand: this.expand,
-         });
-         return matcherHint('.toMatchObject') +
-         `\n\nExpected value to match object:\n` +
-         `  ${printExpected(expectedObject)}` +
-         `\nReceived:\n` +
-         `  ${printReceived(receivedObject)}` +
-         (diffString ? `\nDifference:\n${diffString}` : '');
-       };
+        `\n\nExpected value not to match object:\n` +
+        `  ${printExpected(expectedObject)}` +
+        `\nReceived:\n` +
+        `  ${printReceived(receivedObject)}`
+      : () => {
+        const diffString = diff(expectedObject, getObjectSubset(receivedObject, expectedObject), {
+          expand: this.expand,
+        });
+        return matcherHint('.toMatchObject') +
+          `\n\nExpected value to match object:\n` +
+          `  ${printExpected(expectedObject)}` +
+          `\nReceived:\n` +
+          `  ${printReceived(receivedObject)}` +
+          (diffString ? `\nDifference:\n${diffString}` : '');
+      };
 
     return {message, pass};
   },
--- packages/jest-matchers/src/utils.js
+++ packages/jest-matchers/src/utils.js
@@ -7,7 +7,6 @@
  *
  * @flow
  */
- /* eslint-disable max-len */
 
 'use strict';
 
@@ -18,7 +17,10 @@ type GetPath = {
   value?: any,
 };
 
-const getPath = (object: Object, propertyPath: string | Array<string>): GetPath => {
+const getPath = (
+  object: Object,
+  propertyPath: string | Array<string>,
+): GetPath => {
   if (!Array.isArray(propertyPath)) {
     propertyPath = propertyPath.split('.');
   }
@@ -29,9 +31,9 @@ const getPath = (object: Object, propertyPath: string | Array<string>): GetPath 
     const prop = propertyPath[0];
     const newObject = object[prop];
     if (!lastProp && (newObject === null || newObject === undefined)) {
-      // This is not the last prop in the chain. If we keep recursing it will hit a
-      // `can't access property X of undefined | null`. At this point we know that the
-      // chain broken and we return right away.
+      // This is not the last prop in the chain. If we keep recursing it will
+      // hit a `can't access property X of undefined | null`. At this point we
+      // know that the chain broken and we return right away.
       return {
         hasEndProp: false,
         lastTraversedObject: object,
@@ -59,7 +61,33 @@ const getPath = (object: Object, propertyPath: string | Array<string>): GetPath 
   }
 };
 
+// Strip properties from object that are not present in the subset. Useful for
+// printing the diff for toMatchObject() without adding unrelated noise.
+const getObjectSubset = (object: Object, subset: Object) => {
+  if (Array.isArray(object)) {
+    if (Array.isArray(subset) && subset.length === object.length) {
+      return subset.map((sub, i) => getObjectSubset(object[i], sub));
+    }
+  } else if (object instanceof Date) {
+    return object;
+  } else if (
+    typeof object === 'object' && object !== null &&
+    typeof subset === 'object' && subset !== null
+  ) {
+    const trimmed = {};
+    Object.keys(subset)
+      .filter(key => object.hasOwnProperty(key))
+      .forEach(key => trimmed[key] = getObjectSubset(object[key], subset[key]));
+
+    if (Object.keys(trimmed).length > 0) {
+      return trimmed;
+    }
+  }
+  return object;
+};
+
 
 module.exports = {
+  getObjectSubset,
   getPath,
 };
