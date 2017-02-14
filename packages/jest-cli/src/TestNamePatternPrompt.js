a3a928b9f70d1c4d427acfdff84f77f04a80bc2d
Osipov Vladimir <zouxuoz@gmail.com>
2017-02-14 14:32:03 +0300

    Refactor and improve watch mode with testPathPattern and testNamePattern (#2568)
    * Separate prompt patterns logic and watch mode logic
    
    * Separate pathname pattern mode logic and watch mode
    
    * Using testPathPattern for path pattern mode instead of _ args
    
    * Add initial testname pattern mode
    
    * Cache testnames for pattern mode
    
    * fix direct exporting
    
    * fix re-bind for on change handler
    
    * fix keys constants in tests
    
    * fix testname whitespace
    
    * rename setWatchMode to setState
    
    * refactor cancel pattern mode function
    
    * fix code style
    
    * rid of default params in functions
    
    * rid of should in tests
    
    * fix code style
    
    * fix comma
    
    * rename prompt files
    
    * fix private fields
    
    * rename prompt to enter
    
    * fix comma
    
    * fix comma and default param
    
    * fix code style
    
    * fix testname whitespace
    
    * refactor cache test names
    
    * Add tests for test name pattern mode
    
    * Add prefix with test file path for test name pattern mode
    
    * Move colorize to separate file
    
    * Update prompt on resize terminal
    
    * Better output for test name pattern mode
    
    * Ignore case for testNamePattern
    
    * Replace all types of the line breaks for test names in watch pattern mode

---
+++ packages/jest-cli/src/TestNamePatternPrompt.js
@@ -0,0 +1,149 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ */
+
+'use strict';
+
+import type {Config} from 'types/Config';
+
+const ansiEscapes = require('ansi-escapes');
+const chalk = require('chalk');
+const {getTerminalWidth} = require('./lib/terminalUtils');
+const stringLength = require('string-length');
+const Prompt = require('./lib/Prompt');
+const formatTestNameByPattern = require('./lib/formatTestNameByPattern');
+
+const pluralizeTest = (total: number) => total === 1 ? 'test' : 'tests';
+
+const usage = (delimiter: string = '\n') => {
+  const messages = [
+    `\n ${chalk.bold('Pattern Mode Usage')}`,
+    ` ${chalk.dim('\u203A Press')} ESC ${chalk.dim('to exit pattern mode.')}\n`,
+  ];
+
+  return messages.filter(message => !!message).join(delimiter) + '\n';
+};
+
+const usageRows = usage().split('\n').length;
+
+module.exports = (
+  config: Config,
+  pipe: stream$Writable | tty$WriteStream,
+  prompt: Prompt,
+) => {
+  class TestNamePatternPrompt {
+    // $FlowFixMe
+    _cachedTestResults;
+
+    constructor() {
+      (this:any).onChange = this.onChange.bind(this);
+    }
+
+    run(
+      onSuccess: Function,
+      onCancel: Function,
+    ) {
+      pipe.write(ansiEscapes.cursorHide);
+      pipe.write(ansiEscapes.clearScreen);
+      pipe.write(usage());
+      pipe.write(ansiEscapes.cursorShow);
+
+      prompt.enter(
+        this.onChange,
+        onSuccess,
+        onCancel,
+      );
+    }
+
+    onChange(
+      pattern: string,
+    ) {
+      pipe.write(ansiEscapes.eraseLine);
+      pipe.write(ansiEscapes.cursorLeft);
+      this.printTypeahead(pattern, 10);
+    }
+
+    printTypeahead(
+      pattern: string,
+      max: number,
+    ) {
+      const matchedTests = this.getMatchedTests(pattern);
+
+      const total = matchedTests.length;
+      const results = matchedTests.slice(0, max);
+      const inputText = `${chalk.dim(' pattern \u203A')} ${pattern}`;
+
+      pipe.write(ansiEscapes.eraseDown);
+      pipe.write(inputText);
+      pipe.write(ansiEscapes.cursorSavePosition);
+
+      if (pattern) {
+        if (total) {
+          pipe.write(`\n\n Pattern matches ${total} ${pluralizeTest(total)}.`);
+        } else {
+          pipe.write(`\n\n Pattern matches no tests.`);
+        }
+
+        const width = getTerminalWidth();
+
+        results.forEach(name => {
+          const testName = formatTestNameByPattern(name, pattern, width - 4);
+
+          pipe.write(`\n ${chalk.dim('\u203A')} ${testName}`);
+        });
+
+        if (total > max) {
+          const more = total - max;
+          pipe.write(
+            // eslint-disable-next-line max-len
+            `\n  ${chalk.dim(`\u203A and ${more} more ${pluralizeTest(more)}`)}`,
+          );
+        }
+      } else {
+        // eslint-disable-next-line max-len
+        pipe.write(`\n\n ${chalk.italic.yellow('Start typing to filter by a test name regex pattern.')}`);
+      }
+
+      pipe.write(ansiEscapes.cursorTo(stringLength(inputText), usageRows - 1));
+      pipe.write(ansiEscapes.cursorRestorePosition);
+    }
+
+    getMatchedTests(pattern: string) {
+      let regex;
+
+      try {
+        regex = new RegExp(pattern, 'i');
+      } catch (e) {
+        return [];
+      }
+
+      const matchedTests = [];
+
+      this._cachedTestResults.forEach(
+        ({testResults}) =>
+          testResults.forEach(
+            ({title}) => {
+              if (regex.test(title)) {
+                matchedTests.push(title);
+              }
+            },
+          )
+      );
+
+      return matchedTests;
+    }
+
+    // $FlowFixMe
+    updateCachedTestResults(testResults) {
+      this._cachedTestResults = testResults || [];
+    }
+  }
+
+  return new TestNamePatternPrompt();
+};
