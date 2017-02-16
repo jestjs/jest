3d8bd214eada81a2b17514fe3930febf340a1ad1
Michał Pierzchała <thymikee@gmail.com>
2017-02-16 14:42:54 +0100

    Add snapshot version support (#2896)
    * Add snapshot version support
    
    * Add missing snapshots
    
    * Add link to snapshot guide, small regexp fixes
    
    * Update examples snapshots
    
    * Add parens for readability
    
    * Remove unnecessary -u flag from transform test
    
    * Update snapshot version messages to be more exhaustive
    
    * Fix error formatting
    
    * Don't write to fs if not needed

--- packages/jest-snapshot/src/__tests__/utils-test.js
+++ packages/jest-snapshot/src/__tests__/utils-test.js
@@ -9,17 +9,30 @@
 'use strict';
 
 const {
+  getSnapshotData,
   getSnapshotPath,
   keyToTestName,
   saveSnapshotFile,
   testNameToKey,
+  SNAPSHOT_GUIDE_LINK,
+  SNAPSHOT_VERSION,
+  SNAPSHOT_VERSION_WARNING,
 } = require('../utils');
 const fs = require('fs');
 const path = require('path');
 
 const writeFileSync = fs.writeFileSync;
-beforeEach(() => fs.writeFileSync = jest.fn());
-afterEach(() => fs.writeFileSync = writeFileSync);
+const readFileSync = fs.readFileSync;
+beforeEach(() => {
+  fs.writeFileSync = jest.fn();
+  fs.readFileSync = jest.fn();
+});
+afterEach(() => {
+  fs.writeFileSync = writeFileSync;
+  fs.readFileSync = readFileSync;
+});
+
+jest.mock('jest-file-exists', () => () => true);
 
 test('keyToTestName()', () => {
   expect(keyToTestName('abc cde 12')).toBe('abc cde');
@@ -49,7 +62,11 @@ test('saveSnapshotFile() works with \r\n', () => {
 
   saveSnapshotFile(data, filename);
   expect(fs.writeFileSync)
-    .toBeCalledWith(filename, 'exports[`myKey`] = `<div>\n</div>`;\n');
+    .toBeCalledWith(
+      filename,
+      `// Jest Snapshot v1, ${SNAPSHOT_GUIDE_LINK}\n\n` +
+        'exports[`myKey`] = `<div>\n</div>`;\n'
+    );
 });
 
 test('saveSnapshotFile() works with \r', () => {
@@ -60,7 +77,73 @@ test('saveSnapshotFile() works with \r', () => {
 
   saveSnapshotFile(data, filename);
   expect(fs.writeFileSync)
-    .toBeCalledWith(filename, 'exports[`myKey`] = `<div>\n</div>`;\n');
+    .toBeCalledWith(
+      filename,
+      `// Jest Snapshot v1, ${SNAPSHOT_GUIDE_LINK}\n\n` +
+        'exports[`myKey`] = `<div>\n</div>`;\n'
+    );
+});
+
+test('getSnapshotData() throws when no snapshot version', () => {
+  const filename = path.join(__dirname, 'old-snapshot.snap');
+  fs.readFileSync = jest.fn(() => 'exports[`myKey`] = `<div>\n</div>`;\n');
+  const update = false;
+
+  expect(() => getSnapshotData(filename, update)).toThrowError(
+    `Outdated snapshot: No snapshot header found. ` +
+    `Jest 19 introduced versioned snapshots to ensure all people on ` +
+    `a project are using the same version of Jest. ` +
+    `Please update all snapshots during this upgrade of Jest.\n\n` +
+    SNAPSHOT_VERSION_WARNING
+  );
+});
+
+test('getSnapshotData() throws for older snapshot version', () => {
+  const filename = path.join(__dirname, 'old-snapshot.snap');
+  fs.readFileSync = jest.fn(() =>
+    `// Jest Snapshot v0.99, ${SNAPSHOT_GUIDE_LINK}\n\n` +
+      'exports[`myKey`] = `<div>\n</div>`;\n'
+  );
+  const update = false;
+
+  expect(() => getSnapshotData(filename, update)).toThrowError(
+    `Outdated snapshot: The version of the snapshot file associated ` +
+    `with this test is outdated. The snapshot file version ensures that ` +
+    `all people on a project are using the same version of Jest. ` +
+    `Please update all snapshots during this upgrade of Jest.\n\n` +
+    `Expected: v${SNAPSHOT_VERSION}\n` +
+    `Received: v0.99\n\n` +
+    SNAPSHOT_VERSION_WARNING
+  );
+});
+
+test('getSnapshotData() throws for newer snapshot version', () => {
+  const filename = path.join(__dirname, 'old-snapshot.snap');
+  fs.readFileSync = jest.fn(() =>
+    `// Jest Snapshot v2, ${SNAPSHOT_GUIDE_LINK}\n\n` +
+      'exports[`myKey`] = `<div>\n</div>`;\n'
+  );
+  const update = false;
+
+  expect(() => getSnapshotData(filename, update)).toThrowError(
+    `Outdated Jest version: the version of this snapshot file indicates ` +
+    `that this project is meant to be used with a newer version of Jest. ` +
+    `The snapshot file version ensures that all people on a project ` +
+    `are using the same version of Jest. ` +
+    `Please update your version of Jest and re-run the tests.\n\n` +
+    `Expected: v${SNAPSHOT_VERSION}\n` +
+    `Received: v2`
+  );
+});
+
+test('getSnapshotData() does not throw for when updating', () => {
+  const filename = path.join(__dirname, 'old-snapshot.snap');
+  fs.readFileSync = jest.fn(() =>
+    'exports[`myKey`] = `<div>\n</div>`;\n'
+  );
+  const update = true;
+
+  expect(() => getSnapshotData(filename, update)).not.toThrow();
 });
 
 test('escaping', () => {
@@ -68,7 +151,11 @@ test('escaping', () => {
   const data = '"\'\\';
   saveSnapshotFile({key: data}, filename);
   const writtenData = fs.writeFileSync.mock.calls[0][1];
-  expect(writtenData).toBe("exports[`key`] = `\"'\\\\`;\n");
+  expect(writtenData)
+    .toBe(
+      `// Jest Snapshot v1, ${SNAPSHOT_GUIDE_LINK}\n\n` +
+        "exports[`key`] = `\"'\\\\`;\n"
+    );
 
   // eslint-disable-next-line no-unused-vars
   const exports = {};
