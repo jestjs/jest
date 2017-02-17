1ec1d640d662e0579c18adcf958b51ee25c6b2eb
Michał Pierzchała <thymikee@gmail.com>
2017-02-17 15:34:52 +0100

    Color snapshot version errors

--- packages/jest-snapshot/src/utils.js
+++ packages/jest-snapshot/src/utils.js
@@ -12,6 +12,7 @@
 
 import type {Path} from 'types/Config';
 
+const chalk = require('chalk');
 const createDirectory = require('jest-util').createDirectory;
 const fileExists = require('jest-file-exists');
 const path = require('path');
@@ -24,10 +25,11 @@ const SNAPSHOT_EXTENSION = 'snap';
 const SNAPSHOT_VERSION = '1';
 const SNAPSHOT_VERSION_REGEXP = /^\/\/ Jest Snapshot v(.+),/;
 const SNAPSHOT_GUIDE_LINK = 'https://goo.gl/fbAQLP';
-const SNAPSHOT_VERSION_WARNING =
-  `Warning: It is advised to revert any local changes to tests or ` +
-  `other code during this upgrade to ensure that no invalid state ` +
-  `is stored as a snapshot.`;
+const SNAPSHOT_VERSION_WARNING = chalk.yellow(
+  `${chalk.bold('Warning')}: It is advised to revert any local changes to ` +
+  `tests or other code during this upgrade to ensure that no invalid state ` +
+  `is stored as a snapshot.`
+);
 
 const writeSnapshotVersion = () =>
   `// Jest Snapshot v${SNAPSHOT_VERSION}, ${SNAPSHOT_GUIDE_LINK}`;
@@ -38,20 +40,25 @@ const validateSnapshotVersion = (snapshotContents: string) => {
 
   if (!version) {
     return new Error(
-      `Outdated snapshot: No snapshot header found. ` +
-      `Jest 19 introduced versioned snapshots to ensure all users of ` +
-      `a project are using the same version of Jest. ` +
-      `Please update all snapshots during this upgrade of Jest.\n\n` +
+      chalk.red(
+        `${chalk.bold('Outdated snapshot')}: No snapshot header found. ` +
+        `Jest 19 introduced versioned snapshots to ensure all users of ` +
+        `a project are using the same version of Jest. ` +
+        `Please update all snapshots during this upgrade of Jest.\n\n`
+      ) +
       SNAPSHOT_VERSION_WARNING
     );
   }
 
   if (version < SNAPSHOT_VERSION) {
     return new Error(
-      `Outdated snapshot: The version of the snapshot file associated ` +
-      `with this test is outdated. The snapshot file version ensures that ` +
-      `all users of a project are using the same version of Jest. ` +
-      `Please update all snapshots during this upgrade of Jest.\n\n` +
+      chalk.red(
+        `${chalk.red.bold('Outdated snapshot')}: The version of the snapshot ` +
+        `file associated with this test is outdated. The snapshot file ` +
+        `version ensures that all users of a project are using ` +
+        `the same version of Jest. ` +
+        `Please update all snapshots during this upgrade of Jest.\n\n`
+      ) +
       `Expected: v${SNAPSHOT_VERSION}\n` +
       `Received: v${version}\n\n` +
       SNAPSHOT_VERSION_WARNING
@@ -60,11 +67,14 @@ const validateSnapshotVersion = (snapshotContents: string) => {
 
   if (version > SNAPSHOT_VERSION) {
     return new Error(
-      `Outdated Jest version: the version of this snapshot file indicates ` +
-      `that this project is meant to be used with a newer version of Jest. ` +
-      `The snapshot file version ensures that all users of a project ` +
-      `are using the same version of Jest. ` +
-      `Please update your version of Jest and re-run the tests.\n\n` +
+      chalk.red(
+        `${chalk.red.bold('Outdated Jest version')}: The version of this ` +
+        `snapshot file indicates that this project is meant to be used ` +
+        `with a newer version of Jest. ` +
+        `The snapshot file version ensures that all users of a project ` +
+        `are using the same version of Jest. ` +
+        `Please update your version of Jest and re-run the tests.\n\n`
+      ) +
       `Expected: v${SNAPSHOT_VERSION}\n` +
       `Received: v${version}`
     );
