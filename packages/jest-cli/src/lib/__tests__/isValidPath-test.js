245a069ff76a14aa265c554fa175884ffd34789c
Rogelio Guzman <rogelioguzmanh@gmail.com>
2017-02-21 13:16:25 -0800

    Fix isValidPath failing Windows test

--- packages/jest-cli/src/lib/__tests__/isValidPath-test.js
+++ packages/jest-cli/src/lib/__tests__/isValidPath-test.js
@@ -10,32 +10,37 @@
 
 'use strict';
 
+/* eslint-disable max-len */
+
+const path = require('path');
 const isValidPath = require('../isValidPath');
 
+const rootDir = path.join(path.sep, 'root');
+
 const config = {
-  rootDir: '/root',
-  roots: ['/root/src', '/root/lib'],
+  rootDir,
+  roots: [path.join(rootDir, 'src'), path.join(rootDir, 'lib')],
 };
 
 it('is valid when it is a file inside roots', () => {
-  expect(isValidPath(config, '/root/src/index.js')).toBe(true);
-  expect(isValidPath(config, '/root/src/components/Link.js')).toBe(true);
-  expect(isValidPath(config, '/root/src/lib/something.js')).toBe(true);
+  expect(isValidPath(config, path.join(rootDir, 'src', 'index.js'))).toBe(true);
+  expect(isValidPath(config, path.join(rootDir, 'src', 'components', 'Link.js'))).toBe(true);
+  expect(isValidPath(config, path.join(rootDir, 'src', 'lib', 'something.js'))).toBe(true);
 });
 
 it('is not valid when it is a snapshot file', () => {
-  expect(isValidPath(config, '/root/src/index.js.snap')).toBe(false);
-  expect(isValidPath(config, '/root/src/components/Link.js.snap')).toBe(false);
-  expect(isValidPath(config, '/root/src/lib/something.js.snap')).toBe(false);
+  expect(isValidPath(config, path.join(rootDir, 'src', 'index.js.snap'))).toBe(false);
+  expect(isValidPath(config, path.join(rootDir, 'src', 'components', 'Link.js.snap'))).toBe(false);
+  expect(isValidPath(config, path.join(rootDir, 'src', 'lib', 'something.js.snap'))).toBe(false);
 });
 
 it('is not valid when it is a file in the coverage dir', () => {
-  expect(isValidPath(config, '/root/coverage/lib/index.js')).toBe(false);
+  expect(isValidPath(config, path.join(rootDir, 'coverage', 'lib', 'index.js'))).toBe(false);
   
   const configWithCoverage = Object.assign({}, config, {
     coverageDirectory: 'cov-dir',
   });
   
-  expect(isValidPath(configWithCoverage, '/root/src/cov-dir/lib/index.js'))
+  expect(isValidPath(configWithCoverage, path.join(rootDir, 'src', 'cov-dir', 'lib', 'index.js')))
     .toBe(false);
 });
