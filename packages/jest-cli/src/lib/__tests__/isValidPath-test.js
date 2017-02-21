621568c871176ef57fbcca286ab334d76b0224c9
Rogelio Guzman <rogelioguzmanh@gmail.com>
2017-02-21 12:11:59 -0800

    Fix infinite loop issue when running watch with --coverage

---
+++ packages/jest-cli/src/lib/__tests__/isValidPath-test.js
@@ -0,0 +1,41 @@
+/**
+* Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*
+* @emails oncall+jsinfra
+*/
+
+'use strict';
+
+const isValidPath = require('../isValidPath');
+
+const config = {
+  rootDir: '/root',
+  roots: ['/root/src', '/root/lib'],
+};
+
+it('is valid when it is a file inside roots', () => {
+  expect(isValidPath(config, '/root/src/index.js')).toBe(true);
+  expect(isValidPath(config, '/root/src/components/Link.js')).toBe(true);
+  expect(isValidPath(config, '/root/src/lib/something.js')).toBe(true);
+});
+
+it('is not valid when it is a file .snap', () => {
+  expect(isValidPath(config, '/root/src/index.js.snap')).toBe(false);
+  expect(isValidPath(config, '/root/src/components/Link.js.snap')).toBe(false);
+  expect(isValidPath(config, '/root/src/lib/something.js.snap')).toBe(false);
+});
+
+it('is not valid when it is a file in the coverage dir', () => {
+  expect(isValidPath(config, '/root/coverage/lib/index.js')).toBe(false);
+  
+  const configWithCoverage = Object.assign({}, config, {
+    coverageDirectory: 'cov-dir',
+  });
+  
+  expect(isValidPath(configWithCoverage, '/root/src/cov-dir/lib/index.js'))
+    .toBe(false);
+});
