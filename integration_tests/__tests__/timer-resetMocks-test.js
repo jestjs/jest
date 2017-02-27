e9f56b7b494fab4db9c1394d2bb355b71509b938
Michał Pierzchała <thymikee@gmail.com>
2017-02-27 11:31:36 +0100

    Restore fake timers after resetting mocks (#2467)

---
+++ integration_tests/__tests__/timer-resetMocks-test.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @emails oncall+jsinfra
+ */
+'use strict';
+
+const runJest = require('../runJest');
+const skipOnWindows = require('skipOnWindows');
+
+skipOnWindows.suite();
+
+test('run timers after resetAllMocks test', () => {
+  const result = runJest('timer-resetMocks/after_resetAllMocks');
+  expect(result.status).toBe(0);
+});
+
+test('run timers with resetMocks in config test', () => {
+  const result = runJest('timer-resetMocks/with_resetMocks');
+  expect(result.status).toBe(0);
+});
