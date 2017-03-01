57e8284da89742401baa31aad67a52f330ac92f6
cpojer <christoph.pojer@gmail.com>
2017-03-01 15:04:57 +0000

    Improve printing of `expect.assertions` error.

--- types/Matchers.js
+++ types/Matchers.js
@@ -25,8 +25,8 @@ export type RawMatcherFn = (
 export type ThrowingMatcherFn = (actual: any) => void;
 export type MatcherContext = {isNot: boolean};
 export type MatcherState = {
+  assertionCalls?: number,
   assertionsExpected?: ?number,
-  assertionsMade?: number,
   currentTestName?: string,
   testPath?: Path,
 };
