439cef509cf6a2f570100eb8a746c649b4498f39
Hannan Ali <abdulhannanali@outlook.com>
2017-03-07 00:16:19 +0500

    correct indentations for react-native example

--- examples/react-native/Intro.js
+++ examples/react-native/Intro.js
@@ -3,45 +3,45 @@
  * https://github.com/facebook/react-native
  * @flow
  */
- 'use strict';
+'use strict';
 
- import React, {Component} from 'react';
- import {
+import React, {Component} from 'react';
+import {
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 
- const styles = StyleSheet.create({
-   container: {
-     alignItems: 'center',
-     backgroundColor: '#F5FCFF',
-     flex: 1,
-     justifyContent: 'center',
-   },
-   instructions: {
-     color: '#333333',
-     marginBottom: 5,
-     textAlign: 'center',
-   },
-   welcome: {
-     fontSize: 20,
-     margin: 10,
-     textAlign: 'center',
-   },
- });
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  instructions: {
+    color: '#333333',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    margin: 10,
+    textAlign: 'center',
+  },
+});
 
- export default class Intro extends Component {
-   render() {
-     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          This is a React Native snapshot test.
-        </Text>
-      </View>
-     );
-   }
+export default class Intro extends Component {
+  render() {
+    return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        Welcome to React Native!
+      </Text>
+      <Text style={styles.instructions}>
+        This is a React Native snapshot test.
+      </Text>
+    </View>
+    );
+  }
 }
