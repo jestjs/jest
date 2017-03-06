b903fe5377ff52d78ff3eb6dfb980eb41e203d2d
Hannan Ali <ali.abdulhannan@gmail.com>
2017-03-07 00:49:04 +0500

    lint all examples using eslint (#3080)
    * lint examples/react-native
    
    * correct indentations for react-native example

--- examples/react-native/Intro.js
+++ examples/react-native/Intro.js
@@ -3,45 +3,45 @@
  * https://github.com/facebook/react-native
  * @flow
  */
- 'use strict';
+'use strict';
 
 import React, {Component} from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  instructions: {
+    color: '#333333',
+    marginBottom: 5,
+    textAlign: 'center',
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
 export default class Intro extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          This is a React Native snapshot test.
-        </Text>
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        Welcome to React Native!
+      </Text>
+      <Text style={styles.instructions}>
+        This is a React Native snapshot test.
+      </Text>
+    </View>
     );
   }
 }
