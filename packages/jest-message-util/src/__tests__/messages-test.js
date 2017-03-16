2dc4952dbfb55173c4d8e4feb0752b4b43ff1e65
Christoph Pojer <cpojer@fb.com>
2017-03-16 11:25:53 +0900

    Fork Jasmine (#3147)
    * Remove unused and unsupported Jasmine features.
    
    * Move jasmine to src.
    
    * Jasmine is now a beautiful piece of pretty printed code.
    
    * Fix up stack trace filtering.
    
    * Lint fixes.
    
    * More dead code removal.
    
    * Call it jasmine-light.

--- packages/jest-message-util/src/__tests__/messages-test.js
+++ packages/jest-message-util/src/__tests__/messages-test.js
@@ -12,16 +12,16 @@
 const {formatResultsErrors} = require('../messages');
 
 const unixStackTrace = `  ` +
-  `at stack (../jest-jasmine2/vendor/jasmine-2.4.1.js:1580:17)
-  at Object.addResult (../jest-jasmine2/vendor/jasmine-2.4.1.js:1550:14)
+  `at stack (../jest-jasmine2/build/jasmine-2.4.1.js:1580:17)
+  at Object.addResult (../jest-jasmine2/build/jasmine-2.4.1.js:1550:14)
   at jasmine.addResult (../jest-jasmine2/build/index.js:82:44)
-  at Spec.Env.factory (../jest-jasmine2/vendor/jasmine-2.4.1.js:641:18)
-  at Spec.addResult (../jest-jasmine2/vendor/jasmine-2.4.1.js:333:34)
-  at Expectation.addResult (../jest-jasmine2/vendor/jasmine-2.4.1.js:591:21)
-  at Expectation.toBe (../jest-jasmine2/vendor/jasmine-2.4.1.js:1504:12)
+  at Spec.Env.factory (../jest-jasmine2/build/jasmine-2.4.1.js:641:18)
+  at Spec.addResult (../jest-jasmine2/build/jasmine-2.4.1.js:333:34)
+  at Expectation.addResult (../jest-jasmine2/build/jasmine-2.4.1.js:591:21)
+  at Expectation.toBe (../jest-jasmine2/build/jasmine-2.4.1.js:1504:12)
   at Object.it (build/__tests__/messages-test.js:45:41)
   at Object.<anonymous> (../jest-jasmine2/build/jasmine-pit.js:35:32)
-  at attemptAsync (../jest-jasmine2/vendor/jasmine-2.4.1.js:1919:24)`;
+  at attemptAsync (../jest-jasmine2/build/jasmine-2.4.1.js:1919:24)`;
 
 it('should exclude jasmine from stack trace for Unix paths.', () => {
   const messages = formatResultsErrors(
