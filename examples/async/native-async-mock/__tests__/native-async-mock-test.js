7287dea26d1717d245feec757f0bf9d152693d33
Michał Pierzchała <thymikee@gmail.com>
2017-03-27 20:15:22 +0200

    Return a promise from mock

--- examples/async/native-async-mock/__tests__/native-async-mock-test.js
+++ examples/async/native-async-mock/__tests__/native-async-mock-test.js
@@ -2,10 +2,15 @@
 
 'use strict';
 
-jest.mock('../native');
+jest.mock('../native', () => ({
+  asyncMethod: () => Promise.resolve('test'),
+  syncMethod: () => 'test',
+}));
+
 const native = require('../native');
 
-test('mock works with native async', () => {
-  expect(native.asyncMethod).toBeDefined();
+test('mock works with native async', async () => {
   expect(native.syncMethod).toBeDefined();
+  expect(native.asyncMethod).toBeDefined();
+  await expect(native.asyncMethod()).resolves.toBe('test');
 });
