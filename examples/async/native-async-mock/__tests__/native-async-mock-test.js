399cf15b96c94f8eaa64108e923c79fd015f48a6
Michał Pierzchała <thymikee@gmail.com>
2017-03-27 20:18:36 +0200

    Only test async

--- examples/async/native-async-mock/__tests__/native-async-mock-test.js
+++ examples/async/native-async-mock/__tests__/native-async-mock-test.js
@@ -4,13 +4,11 @@
 
 jest.mock('../native', () => ({
   asyncMethod: () => Promise.resolve('test'),
-  syncMethod: () => 'test',
 }));
 
 const native = require('../native');
 
 test('mock works with native async', async () => {
-  expect(native.syncMethod).toBeDefined();
   expect(native.asyncMethod).toBeDefined();
   await expect(native.asyncMethod()).resolves.toBe('test');
 });
