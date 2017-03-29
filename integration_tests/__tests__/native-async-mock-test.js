510951465a755fec35b59f229b73797963be3259
Dmitrii Abramov <dmitrii@abramov.io>
2017-03-28 18:43:18 -0700

    native-async-module integration test (#3224)

---
+++ integration_tests/__tests__/native-async-mock-test.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+'use strict';
+
+const {linkJestPackage, run} = require('../utils');
+const {extractSummary} = require('../utils');
+const path = require('path');
+const runJest = require('../runJest');
+const skipOnWindows = require('skipOnWindows');
+
+skipOnWindows.suite();
+const dir = path.resolve(__dirname, '..', 'native-async-mock');
+
+test('mocks async functions', () => {
+  if (process.platform !== 'win32') {
+    run('npm install', dir);
+    linkJestPackage('babel-jest', dir);
+  }
+  // --no-cache because babel can cache stuff and result in false green
+  const {stderr} = runJest(dir, ['--no-cache']);
+  expect(extractSummary(stderr)).toMatchSnapshot();
+});
