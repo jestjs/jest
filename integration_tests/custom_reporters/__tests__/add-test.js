e36d4427e9a7ca1eb156bf6fc7e78c3f97285d64
Hannan Ali <abdulhannanali@outlook.com>
2017-04-04 23:29:09 +0500

    prettier integration_tests

--- integration_tests/custom_reporters/__tests__/add-test.js
+++ integration_tests/custom_reporters/__tests__/add-test.js
@@ -1,9 +1,9 @@
 const add = require('../add');
 
 describe('Custom Reporters', () => {
-    test('adds ok', () => {
-        expect(add(1, 2)).toBe(3);
-        expect(add(3, 4)).toBe(7);
-        expect(add(12, 24)).toBe(36);
-    });
+  test('adds ok', () => {
+    expect(add(1, 2)).toBe(3);
+    expect(add(3, 4)).toBe(7);
+    expect(add(12, 24)).toBe(36);
+  });
 });
