1dd225d9e714c1ec2786b0ed7ffc9c64bcd6ccc3
Andrew Imm <andrewi@fb.com>
2017-04-13 22:50:30 -0700

    Support custom platforms on jest-haste-map

--- packages/jest-haste-map/src/lib/getPlatformExtension.js
+++ packages/jest-haste-map/src/lib/getPlatformExtension.js
@@ -17,13 +17,20 @@ const SUPPORTED_PLATFORM_EXTS = {
 };
 
 // Extract platform extension: index.ios.js -> ios
-function getPlatformExtension(file: string): ?string {
+function getPlatformExtension(
+  file: string,
+  platforms?: Array<string>,
+): ?string {
   const last = file.lastIndexOf('.');
   const secondToLast = file.lastIndexOf('.', last - 1);
   if (secondToLast === -1) {
     return null;
   }
   const platform = file.substring(secondToLast + 1, last);
+  // If an overriding platform array is passed, check that first
+  if (platforms && platforms.indexOf(platform) !== -1) {
+    return platform;
+  }
   return SUPPORTED_PLATFORM_EXTS[platform] ? platform : null;
 }
 
