336c2afba6842e17ed82822da9d18f50f0c9c57e
Ffloriel <florielfedry@gmail.com>
2017-05-05 23:18:17 +0100

    Add favicon
    Add favicon for jest website.

--- website/core/Head.js
+++ website/core/Head.js
@@ -20,6 +20,16 @@ const Head = React.createClass({
         <meta property="og:image" content="https://facebook.github.io/jest/img/opengraph.png" />
         <meta property="og:description" content={this.props.description} />
 
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/jest/img/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/jest/img/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/jest/img/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/jest/img/favicon/manifest.json" />
+        <link rel="mask-icon" href="/jest/img/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/jest/img/favicon/favicon.ico" />
+        <meta name="msapplication-config" content="/jest/img/favicon/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+
         <link rel="alternate" type="application/atom+xml" href="/jest/blog/atom.xml" title="Jest Blog ATOM Feed" />
         <link rel="alternate" type="application/rss+xml" href="/jest/blog/feed.xml" title="Jest Blog RSS Feed" />
         <link rel="shortcut icon" href="/jest/img/favicon.png" />
