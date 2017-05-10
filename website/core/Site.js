6b5eb813876043df318deae70a5ea9479e62bc46
Eric Nakagawa <ericnakagawa@gmail.com>
2017-05-10 14:41:30 -0700

    Exposing the localized tagline as the page's title (#3544)
    Manually merging the single file change.

--- website/core/Site.js
+++ website/core/Site.js
@@ -16,8 +16,9 @@ const Site = React.createClass({
   render() {
     const title = this.props.title
       ? this.props.title + ' · ' + siteConfig.title
-      : siteConfig.title + ' · ' + siteConfig['en'].tagline;
-    const description = this.props.description || siteConfig['en'].tagline;
+      : siteConfig.title + ' · ' + siteConfig[this.props.language].tagline;
+    const description =
+      this.props.description || siteConfig[this.props.language].tagline;
     const url =
       siteConfig.url + siteConfig.baseUrl + (this.props.url || 'index.html');
     return (
@@ -35,8 +36,13 @@ const Site = React.createClass({
             <Footer language={this.props.language} />
           </div>
           <div id="fb-root" />
-          <script type="text/javascript" src="https://cdn.jsdelivr.net/docsearch.js/1/docsearch.min.js"></script>
-          <script dangerouslySetInnerHTML={{__html: `
+          <script
+            type="text/javascript"
+            src="https://cdn.jsdelivr.net/docsearch.js/1/docsearch.min.js"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -54,12 +60,13 @@ const Site = React.createClass({
               indexName: 'jest',
               inputSelector: '#search_input_react'
             });
-          `}} />
-          <script async defer src="https://buttons.github.io/buttons.js"></script>
+          `,
+            }}
+          />
+          <script async defer src="https://buttons.github.io/buttons.js" />
         </body>
       </html>
     );
   },
 });
-
 module.exports = Site;
