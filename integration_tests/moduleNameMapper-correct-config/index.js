e42de450586f028e5e7a0b05b567ce13996fa825
Johan Lindgren <johan@lindgr3n.com>
2017-05-16 20:59:23 +0200

    Refactored test to use snapshot

--- integration_tests/moduleNameMapper-correct-config/index.js
+++ integration_tests/moduleNameMapper-correct-config/index.js
@@ -5,10 +5,9 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
- * @flow
  */
 
 'use strict';
 // Inlcude style so moduleNameMapping triggers
 const style = require('./style.css');
-module.exports = () => {};
+module.exports = () => 'test';
