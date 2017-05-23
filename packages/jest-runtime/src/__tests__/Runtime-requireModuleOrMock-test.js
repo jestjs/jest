692d02994764551ca3298f15fda5adfcaa4d662f
Jean Lauliac <lauliacj@gmail.com>
2017-05-23 16:33:53 +0100

    jest-haste-map: add test to check duplicate modules are broken (#3632)

--- packages/jest-runtime/src/__tests__/Runtime-requireModuleOrMock-test.js
+++ packages/jest-runtime/src/__tests__/Runtime-requireModuleOrMock-test.js
@@ -169,9 +169,10 @@ it('unmocks modules in config.unmockedModulePathPatterns for tests with automock
   }).then(runtime => {
     const root = runtime.requireModule(runtime.__mockRootPath);
     root.jest.enableAutomock();
-    const nodeModule = runtime.requireModuleOrMock(runtime.__mockRootPath, 'npm3-main-dep');
+    const nodeModule = runtime.requireModuleOrMock(
+      runtime.__mockRootPath,
+      'npm3-main-dep',
+    );
     const moduleData = nodeModule();
     expect(moduleData.isUnmocked()).toBe(true);
-  })
-);
-
+  }));
