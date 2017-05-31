769e1b6d6edb064e2d76652faee89c623dc46aaa
Dmitrii Abramov <dmitrii@abramov.io>
2017-05-31 00:57:48 -0700

    cleanupStackTrace (#3696)

--- integration_tests/__tests__/failures-test.js
+++ integration_tests/__tests__/failures-test.js
@@ -15,32 +15,19 @@ const dir = path.resolve(__dirname, '../failures');
 
 skipOnWindows.suite();
 
-// Some node versions add an extra line to the error stack trace. This makes
-// snapshot tests fail on different machines. This function makes sure
-// this extra line is always removed.
-const stripInconsistentStackLines = summary => {
-  summary.rest = summary.rest
-    .replace(/\n^.*process\._tickCallback.*$/gm, '')
-    .replace(/\n^.*_throws.*$/gm, '')
-    .replace(/\n^.*Function\..*(throws|doesNotThrow).*$/gm, '')
-    .replace(/\n^.*setup-jest-globals\.js.*$/gm, '')
-    .replace(/(\n^.*Object.<anonymous>)\.test(.*$)/gm, '$1$2');
-  return summary;
-};
-
 test('not throwing Error objects', () => {
   let stderr;
   stderr = runJest(dir, ['throw-number-test.js']).stderr;
-  expect(stripInconsistentStackLines(extractSummary(stderr))).toMatchSnapshot();
+  expect(extractSummary(stderr)).toMatchSnapshot();
   stderr = runJest(dir, ['throw-string-test.js']).stderr;
-  expect(stripInconsistentStackLines(extractSummary(stderr))).toMatchSnapshot();
+  expect(extractSummary(stderr)).toMatchSnapshot();
   stderr = runJest(dir, ['throw-object-test.js']).stderr;
-  expect(stripInconsistentStackLines(extractSummary(stderr))).toMatchSnapshot();
+  expect(extractSummary(stderr)).toMatchSnapshot();
   stderr = runJest(dir, ['assertion-count-test.js']).stderr;
-  expect(stripInconsistentStackLines(extractSummary(stderr))).toMatchSnapshot();
+  expect(extractSummary(stderr)).toMatchSnapshot();
 });
 
 test('works with node assert', () => {
   const {stderr} = runJest(dir, ['node-assertion-error-test.js']);
-  expect(stripInconsistentStackLines(extractSummary(stderr))).toMatchSnapshot();
+  expect(extractSummary(stderr)).toMatchSnapshot();
 });
