9d17ea9297ab6e4c697899a99ee35ae97a4a1c7d
Michał Pierzchała <thymikee@gmail.com>
2017-06-26 22:58:18 +0200

    Make sure runAllTimers also clears all ticks

--- packages/jest-util/src/__tests__/fake_timers.test.js
+++ packages/jest-util/src/__tests__/fake_timers.test.js
@@ -406,6 +406,21 @@ describe('FakeTimers', () => {
         ),
       );
     });
+
+    it('also clears ticks', () => {
+      const global = {process};
+      const timers = new FakeTimers(global, moduleMocker);
+      timers.useFakeTimers();
+
+      const fn = jest.genMockFn();
+      global.setTimeout(() => {
+        process.nextTick(fn);
+      }, 0);
+      expect(fn.mock.calls.length).toBe(0);
+
+      timers.runAllTimers();
+      expect(fn.mock.calls.length).toBe(1);
+    });
   });
 
   describe('runTimersToTime', () => {
