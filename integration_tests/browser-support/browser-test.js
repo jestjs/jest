54462cd8869072375d087dc440073eb8ca87f5ab
skovhus <kenneth.skovhus@gmail.com>
2017-06-27 14:12:39 +0200

    Add karma browser tests for es5 builds

---
+++ integration_tests/browser-support/browser-test.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+/* eslint-disable */
+var expect = require('../../packages/jest-matchers/build-es5/index.js');
+var mock = require('../../packages/jest-mock/build-es5/index.js');
+
+describe('es5 builds in browser', function() {
+  it('runs assertions', function() {
+    expect(1).toBe(1);
+  });
+
+  it('runs mocks', function() {
+    var someMockFunction = mock.fn();
+    expect(someMockFunction.mock.calls.length).toBe(0);
+    someMockFunction();
+    expect(someMockFunction.mock.calls.length).toBe(1);
+  });
+});
