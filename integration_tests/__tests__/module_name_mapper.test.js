518c6ad53720077c6b3274abc1a7bb1978b9147d
lindgr3n <johan@lindgr3n.com>
2017-06-29 09:57:24 +0200

    Throw when moduleNameMapper points to inexistent module (#3567)
    * Fixes issue-1888
    
    * Prettier fix and chalk to package.json dependencies
    
    * Added integration test
    
    * Added integration test
    
    * Refactored test to use snapshot
    
    * Refactored test to use snapshot
    
    * Rename to camelCase
    
    * Fixed lint warnings
    
    * Fixed lint warnings
    
    * Minor fixes
    
    * Use strict on integration tests
    
    * Added integration test
    
    * Refactored test to use snapshot
    
    * Fixed lint warnings
    
    * Added integration test
    
    * Refactored test to use snapshot
    
    * Minor fixes
    
    * Use strict on integration tests
    
    * Use Error instead of ValidationError to avoid duplicate message
    
    * Add fb copyright back to auto_reset_mocks test
    
    * Removed jest-validate dep from jest-resolve
    
    * Run prettier
    
    * Revert auto_clear_mocks change
    
    * Revert auto_reset_mocks change
    
    * Normalize test paths on windows
    
    * Don't use slash to normalize summary
    
    * Skip test on windows

---
+++ integration_tests/__tests__/module_name_mapper.test.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+'use strict';
+
+const runJest = require('../runJest');
+const {extractSummary} = require('../utils');
+const skipOnWindows = require('skipOnWindows');
+
+// Works on windows, we just need to adjust snapshot test output
+skipOnWindows.suite();
+
+test('moduleNameMapper wrong configuration', () => {
+  const {stderr, status} = runJest('module_name_mapper_wrong_config');
+  const {rest} = extractSummary(stderr);
+
+  expect(status).toBe(1);
+  expect(rest).toMatchSnapshot();
+});
+
+test('moduleNameMapper correct configuration', () => {
+  const {stderr, status} = runJest('module_name_mapper_correct_config');
+  const {rest} = extractSummary(stderr);
+
+  expect(status).toBe(0);
+  expect(rest).toMatchSnapshot();
+});
