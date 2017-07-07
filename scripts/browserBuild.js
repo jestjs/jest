3d63117a629e7acda7ed95f2a3a351be1c7f4290
Konstantin Raev <bestander@gmail.com>
2017-07-07 12:07:33 -0700

    merge and fix es5 build

--- scripts/browserBuild.js
+++ scripts/browserBuild.js
@@ -20,7 +20,7 @@ const babelEs5Options = Object.assign(
   {},
   {
     babelrc: false,
-    exclude: 'node_modules/!(ansi-styles)/**',
+    exclude: 'node_modules/!(ansi-styles|chalk|ansi-regex)/**',
     plugins: [
       'syntax-trailing-function-commas',
       'transform-flow-strip-types',
