1cc00a24cc074ff9c38565f6811979d85e76e7b5
Dmitrii Abramov <dmitrii@abramov.io>
2017-07-19 15:24:50 -0700

    Stricter config resolution logic

---
+++ packages/jest-config/src/__tests__/resolve_config_path.test.js
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+import {cleanup, writeFiles} from '../../../../integration_tests/utils';
+import os from 'os';
+import path from 'path';
+import resolveConfigPath from '../resolve_config_path';
+
+const DIR = path.resolve(os.tmpdir(), 'resolve_config_path_test');
+const ERROR_PATTERN = /Could not find a config file based on provided values/;
+const NO_ROOT_DIR_ERROR_PATTERN = /Can\'t find a root directory/;
+
+beforeEach(() => cleanup(DIR));
+afterEach(() => cleanup(DIR));
+
+test('file path', () => {
+  const relativeConfigPath = 'a/b/c/my_config.js';
+  const absoluteConfigPath = path.resolve(DIR, relativeConfigPath);
+
+  writeFiles(DIR, {[relativeConfigPath]: ''});
+
+  // absolute
+  expect(resolveConfigPath(absoluteConfigPath, DIR)).toBe(absoluteConfigPath);
+  expect(() => resolveConfigPath('/does_not_exist', DIR)).toThrowError(
+    NO_ROOT_DIR_ERROR_PATTERN,
+  );
+
+  // relative
+  expect(resolveConfigPath(relativeConfigPath, DIR)).toBe(absoluteConfigPath);
+  expect(() => resolveConfigPath('does_not_exist', DIR)).toThrowError(
+    NO_ROOT_DIR_ERROR_PATTERN,
+  );
+});
+
+test('directory path', () => {
+  const relativePackageJsonPath = 'a/b/c/package.json';
+  const absolutePackageJsonPath = path.resolve(DIR, relativePackageJsonPath);
+  const relativeJestConfigPath = 'a/b/c/jest.config.js';
+  const absoluteJestConfigPath = path.resolve(DIR, relativeJestConfigPath);
+
+  writeFiles(DIR, {'a/b/c/some_random_file.js': ''});
+
+  // no configs yet. should throw
+  expect(() =>
+    // absolute
+    resolveConfigPath(path.dirname(absoluteJestConfigPath), DIR),
+  ).toThrowError(ERROR_PATTERN);
+
+  expect(() =>
+    // relative
+    resolveConfigPath(path.dirname(relativeJestConfigPath), DIR),
+  ).toThrowError(ERROR_PATTERN);
+
+  writeFiles(DIR, {[relativePackageJsonPath]: ''});
+
+  // absolute
+  expect(resolveConfigPath(path.dirname(absolutePackageJsonPath), DIR)).toBe(
+    absolutePackageJsonPath,
+  );
+
+  // relative
+  expect(resolveConfigPath(path.dirname(relativePackageJsonPath), DIR)).toBe(
+    absolutePackageJsonPath,
+  );
+
+  writeFiles(DIR, {[relativeJestConfigPath]: ''});
+
+  // jest.config.js takes presedence
+
+  // absolute
+  expect(resolveConfigPath(path.dirname(absolutePackageJsonPath), DIR)).toBe(
+    absoluteJestConfigPath,
+  );
+
+  // relative
+  expect(resolveConfigPath(path.dirname(relativePackageJsonPath), DIR)).toBe(
+    absoluteJestConfigPath,
+  );
+
+  expect(() => {
+    resolveConfigPath(
+      path.join(path.dirname(relativePackageJsonPath), 'j/x/b/m/'),
+      DIR,
+    );
+  }).toThrowError(NO_ROOT_DIR_ERROR_PATTERN);
+});
---
+++ packages/jest-config/src/read_config_file_and_set_root_dir.js
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ */
+
+import type {InitialOptions, Path} from 'types/Config';
+
+import path from 'path';
+import fs from 'fs';
+import jsonlint from './vendor/jsonlint';
+import {PACKAGE_JSON} from './constants';
+
+// Read the configuration and set its `rootDir`
+// 1. If it's a `package.json` file, we look into its "jest" property
+// 2. For any other file, we just require it.
+module.exports = (configPath: Path): InitialOptions => {
+  const isJSON = configPath.endsWith('.json');
+  let configObject;
+
+  try {
+    // $FlowFixMe dynamic require
+    configObject = require(configPath);
+  } catch (error) {
+    if (isJSON) {
+      throw new Error(
+        `Jest: Failed to parse config file ${configPath}\n` +
+          `  ${jsonlint.errors(fs.readFileSync(configPath, 'utf8'))}`,
+      );
+    } else {
+      throw error;
+    }
+  }
+
+  if (configPath.endsWith(PACKAGE_JSON)) {
+    // Event if there's no "jest" property in package.json we will still use
+    // an empty object.
+    configObject = configObject.jest || {};
+  }
+
+  if (configObject.rootDir) {
+    // We don't touch it if it has an absolute path specified
+    if (!path.isAbsolute(configObject.rootDir)) {
+      // otherwise, we'll resolve it relative to the file's __dirname
+      configObject.rootDir = path.resolve(
+        path.dirname(configPath),
+        configObject.rootDir,
+      );
+    }
+  } else {
+    // If rootDir is not there, we'll set it to this file's __dirname
+    configObject.rootDir = path.dirname(configPath);
+  }
+
+  return configObject;
+};
