382b3a11b66bd6f3f446415d0508d3cd23ca675d
Dmitrii Abramov <dmitrii@abramov.io>
2017-07-27 11:43:40 -0700

    fix merge conflict

--- test_utils.js
+++ test_utils.js
@@ -22,6 +22,7 @@ const DEFAULT_GLOBAL_CONFIG: GlobalConfig = {
   coverageReporters: [],
   coverageThreshold: {global: {}},
   expand: false,
+  findRelatedTests: false,
   forceExit: false,
   json: false,
   lastCommit: false,
