cb38a712647500dbd89c4cf4ddd8ab5a82db3b8a
Min Idzelis <midzelis@salesforce.com>
2017-08-01 17:03:19 -0400

    Add 2nd test, to ensure custom resolver works without moduleNameMapper.

--- packages/jest-resolve/src/__tests__/resolve.test.js
+++ packages/jest-resolve/src/__tests__/resolve.test.js
@@ -81,7 +81,7 @@ describe('findNodeModule', () => {
 });
 
 describe('getMockModule', () => {
-  it('is possible to use custom resolver to resolve deps inside mock modules', () => {
+  it('is possible to use custom resolver to resolve deps inside mock modules with moduleNameMapper', () => {
     userResolver.mockImplementation(() => 'module');
 
     const moduleMap = new ModuleMap({
@@ -96,6 +96,27 @@ describe('getMockModule', () => {
           regex: /(.*)/,
         },
       ],
+      resolver: require.resolve('../__mocks__/userResolver'),
+    });
+    const src = require.resolve('../');
+    resolver.getMockModule(src, 'dependentModule');
+
+    expect(userResolver).toHaveBeenCalled();
+    expect(userResolver.mock.calls[0][0]).toBe('dependentModule');
+    expect(userResolver.mock.calls[0][1]).toHaveProperty(
+      'basedir',
+      path.dirname(src),
+    );
+  });
+  it('is possible to use custom resolver to resolve deps inside mock modules without moduleNameMapper', () => {
+    userResolver.mockImplementation(() => 'module');
+
+    const moduleMap = new ModuleMap({
+      duplicates: [],
+      map: [],
+      mocks: [],
+    });
+    const resolver = new Resolver(moduleMap, {
       resolver: require.resolve('../__mocks__/userResolver'),
     });
     const src = require.resolve('../');
