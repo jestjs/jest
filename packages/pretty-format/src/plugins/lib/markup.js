f47d7de8cec6dc33fcb83c7df80a9253890ffc9c
Mark Pedrotti <pedrottimark@gmail.com>
2017-08-01 17:18:31 -0400

    Factor out common code for markup in React plugins (#4171)
    * Factor out common code for markup in React plugins
    
    * Add mistakenly deleted export
    
    * Put getType after getChildren to improve diff
    
    * Make the 2 new tests parallel
    
    * Fix pretty lint error
    
    * Replace tag with tagColor

---
+++ packages/pretty-format/src/plugins/lib/markup.js
@@ -0,0 +1,127 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ */
+
+import type {Config, Printer, Refs} from 'types/PrettyFormat';
+
+import escapeHTML from './escape_html';
+
+// Return spacing and indentation that precedes the property.
+const printProp = (
+  key: string,
+  value: any,
+  config: Config,
+  printer: Printer,
+  indentation: string,
+  depth: number,
+  refs: Refs,
+): string => {
+  const indentationNext = indentation + config.indent;
+  let printed = printer(value, indentationNext, depth, refs);
+
+  if (typeof value !== 'string') {
+    if (printed.indexOf('\n') !== -1) {
+      printed =
+        config.spacingOuter +
+        indentationNext +
+        printed +
+        config.spacingOuter +
+        indentation;
+    }
+    printed = '{' + printed + '}';
+  }
+
+  const colors = config.colors;
+  return (
+    config.spacingInner +
+    indentation +
+    colors.prop.open +
+    key +
+    colors.prop.close +
+    '=' +
+    colors.value.open +
+    printed +
+    colors.value.close
+  );
+};
+
+// Return empty string if keys is empty.
+export const printProps = (
+  keys: Array<string>,
+  props: Object,
+  config: Config,
+  printer: Printer,
+  indentation: string,
+  depth: number,
+  refs: Refs,
+): string =>
+  keys
+    .map(key =>
+      printProp(key, props[key], config, printer, indentation, depth, refs),
+    )
+    .join('');
+
+// Return empty string if children is empty.
+export const printChildren = (
+  children: Array<any>,
+  config: Config,
+  printer: Printer,
+  indentation: string,
+  depth: number,
+  refs: Refs,
+): string => {
+  const colors = config.colors;
+  return children
+    .map(
+      child =>
+        config.spacingOuter +
+        indentation +
+        (typeof child === 'string'
+          ? colors.content.open + escapeHTML(child) + colors.content.close
+          : printer(child, indentation, depth, refs)),
+    )
+    .join('');
+};
+
+// Separate the functions to format props, children, and element,
+// so a plugin could override a particular function, if needed.
+// Too bad, so sad: the traditional (but unnecessary) space
+// in a self-closing tagColor requires a second test of printedProps.
+export const printElement = (
+  type: string,
+  printedProps: string,
+  printedChildren: string,
+  config: Config,
+  indentation: string,
+): string => {
+  const tagColor = config.colors.tag;
+  return (
+    tagColor.open +
+    '<' +
+    type +
+    (printedProps &&
+      tagColor.close +
+        printedProps +
+        config.spacingOuter +
+        indentation +
+        tagColor.open) +
+    (printedChildren
+      ? '>' +
+        tagColor.close +
+        printedChildren +
+        config.spacingOuter +
+        indentation +
+        tagColor.open +
+        '</' +
+        type
+      : (printedProps && !config.min ? '' : ' ') + '/') +
+    '>' +
+    tagColor.close
+  );
+};
--- packages/pretty-format/src/plugins/react_test_component.js
+++ packages/pretty-format/src/plugins/react_test_component.js
@@ -13,137 +13,50 @@ import type {
   Printer,
   NewPlugin,
   ReactTestObject,
-  ReactTestChild,
   Refs,
 } from 'types/PrettyFormat';
 
-import escapeHTML from './lib/escape_html';
+import {printChildren, printElement, printProps} from './lib/markup';
 
 const testSymbol = Symbol.for('react.test.json');
 
-function printChildren(
-  children: Array<ReactTestChild>,
-  config: Config,
-  printer: Printer,
-  indentation: string,
-  depth: number,
-  refs: Refs,
-): string {
-  const colors = config.colors;
-  return children
-    .map(
-      child =>
-        config.spacingOuter +
-        indentation +
-        (typeof child === 'string'
-          ? colors.content.open + escapeHTML(child) + colors.content.close
-          : printer(child, indentation, depth, refs)),
-    )
-    .join('');
-}
-
-function printProps(
-  keys: Array<string>,
-  props: Object,
-  config: Config,
-  printer: Printer,
-  indentation: string,
-  depth: number,
-  refs: Refs,
-): string {
-  const indentationNext = indentation + config.indent;
-  const colors = config.colors;
-  return keys
-    .sort()
-    .map(key => {
-      const value = props[key];
-      let printed = printer(value, indentationNext, depth, refs);
-
-      if (typeof value !== 'string') {
-        if (printed.indexOf('\n') !== -1) {
-          printed =
-            config.spacingOuter +
-            indentationNext +
-            printed +
-            config.spacingOuter +
-            indentation;
-        }
-        printed = '{' + printed + '}';
-      }
-
-      return (
-        config.spacingInner +
-        indentation +
-        colors.prop.open +
-        key +
-        colors.prop.close +
-        '=' +
-        colors.value.open +
-        printed +
-        colors.value.close
-      );
-    })
-    .join('');
-}
-
 export const serialize = (
-  instance: ReactTestObject,
+  object: ReactTestObject,
   config: Config,
   printer: Printer,
   indentation: string,
   depth: number,
   refs: Refs,
-): string => {
-  const tag = config.colors.tag;
-  let result = tag.open + '<' + instance.type;
-
-  let hasProps = false;
-  if (instance.props) {
-    const keys = Object.keys(instance.props);
-    hasProps = keys.length !== 0;
-    if (hasProps) {
-      result +=
-        tag.close +
-        printProps(
-          keys,
-          instance.props,
+): string =>
+  printElement(
+    object.type,
+    object.props
+      ? printProps(
+          Object.keys(object.props).sort(),
+          // Despite ternary expression, Flow 0.51.0 found incorrect error:
+          // undefined is incompatible with the expected param type of Object
+          // $FlowFixMe
+          object.props,
           config,
           printer,
           indentation + config.indent,
           depth,
           refs,
-        ) +
-        config.spacingOuter +
-        indentation +
-        tag.open;
-    }
-  }
-
-  if (instance.children) {
-    result +=
-      '>' +
-      tag.close +
-      printChildren(
-        instance.children,
-        config,
-        printer,
-        indentation + config.indent,
-        depth,
-        refs,
-      ) +
-      config.spacingOuter +
-      indentation +
-      tag.open +
-      '</' +
-      instance.type +
-      '>' +
-      tag.close;
-  } else {
-    result += (hasProps && !config.min ? '' : ' ') + '/>' + tag.close;
-  }
-
-  return result;
-};
+        )
+      : '',
+    object.children
+      ? printChildren(
+          object.children,
+          config,
+          printer,
+          indentation + config.indent,
+          depth,
+          refs,
+        )
+      : '',
+    config,
+    indentation,
+  );
 
 export const test = (val: any) => val && val.$$typeof === testSymbol;
 
