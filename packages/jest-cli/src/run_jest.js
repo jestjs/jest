8d0b6d7fd46db3abd2e767dd6efc839e4c62ecb6
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-08-03 15:39:16 +0100

    Add extra info to no tests for changed files message (#4188)

--- packages/jest-cli/src/run_jest.js
+++ packages/jest-cli/src/run_jest.js
@@ -40,7 +40,7 @@ const getNoTestsFoundMessage = (testRunData, globalConfig) => {
       chalk.dim(
         globalConfig.watch
           ? 'Press `a` to run all tests, or run Jest with `--watchAll`.'
-          : 'Run Jest without `-o` to run all tests.',
+          : 'Run Jest without `-o` or with `--all` to run all tests.',
       )
     );
   }
