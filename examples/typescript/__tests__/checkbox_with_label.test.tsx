d57e55358ae79115690664908da66d9e41e6bfab
Groo Gang <kidkkr@icloud.com>
2017-08-07 15:44:17 +0900

    Update the reference from `react-addons-test-utils` to `react-dom/test-utils` Fixes #4202 (#4204)
    * Update checkbox_with_label.test.tsx
    
    ReactTestUtils has been moved to react-dom/test-utils
    
    * Add react, react-dom dependencies #4202

--- examples/typescript/__tests__/checkbox_with_label.test.tsx
+++ examples/typescript/__tests__/checkbox_with_label.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as TestUtils from 'react-addons-test-utils';
+import * as TestUtils from 'react-dom/test-utils';
 
 const CheckboxWithLabel = require('../CheckboxWithLabel');
 
