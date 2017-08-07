4d4f0d9a7850a8d1c90bc0b8171689b570f4c3e4
Michał Pierzchała <thymikee@gmail.com>
2017-08-07 22:20:37 +0200

    Remove windows logging

--- packages/jest-snapshot/src/utils.js
+++ packages/jest-snapshot/src/utils.js
@@ -125,7 +125,6 @@ const getSnapshotData = (snapshotPath: Path, update: SnapshotUpdateState) => {
 };
 
 const removeWrappingBackticks = (str: string) => {
-  console.log(str); // debug windows
   return str.replace(/(^\s*)`|`(\s*)$/g, '$1$2');
 };
 
