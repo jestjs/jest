655130e63bbe65ad01b0b14d64749fb8875a3ce4
Dmitrii Abramov <dmitrii@abramov.io>
2017-08-13 16:19:12 -0700

    remove @emails directive using jest-docblock

--- packages/jest-runtime/src/__tests__/runtime_require_module_or_mock.test.js
+++ packages/jest-runtime/src/__tests__/runtime_require_module_or_mock.test.js
@@ -5,8 +5,8 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
- * @emails oncall+jsinfra
  */
+
 'use strict';
 
 const moduleNameMapper = {
