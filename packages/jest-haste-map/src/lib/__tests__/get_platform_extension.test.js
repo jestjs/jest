f8352893cf3effe06949a27a1fae6ac18bdfa054
Aaron Abramov <dmitrii@abramov.io>
2017-08-14 00:05:15 -0700

    remove @emails directive using jest-docblock (#4268)

--- packages/jest-haste-map/src/lib/__tests__/get_platform_extension.test.js
+++ packages/jest-haste-map/src/lib/__tests__/get_platform_extension.test.js
@@ -6,8 +6,8 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
- * @emails oncall+jsinfra
  */
+
 'use strict';
 
 let getPlatformExtension;
