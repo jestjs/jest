977b3e171d262d42453b8d374c9659dc1c365647
Mark Pedrotti <pedrottimark@gmail.com>
2017-08-21 13:28:13 -0400

    Join lines with newline in jest-diff

--- packages/jest-diff/src/diff_strings.js
+++ packages/jest-diff/src/diff_strings.js
@@ -71,11 +71,11 @@ const diffLines = (a: string, b: string): Diff => {
           .map(line => {
             const highlightedLine = highlightTrailingWhitespace(line, bgColor);
             const mark = color(part.added ? '+' : part.removed ? '-' : ' ');
-            return mark + ' ' + color(highlightedLine) + '\n';
+            return mark + ' ' + color(highlightedLine);
           })
-          .join('');
+          .join('\n');
       })
-      .join('')
+      .join('\n')
       .trim(),
     isDifferent,
   };
@@ -125,16 +125,16 @@ const structuredPatch = (a: string, b: string, contextLines?: number): Diff => {
             const bgColor = getBgColor(added, removed);
 
             const highlightedLine = highlightTrailingWhitespace(line, bgColor);
-            return color(highlightedLine) + '\n';
+            return color(highlightedLine);
           })
-          .join('');
+          .join('\n');
 
         isDifferent = true;
         return shouldShowPatchMarks(hunk, oldLinesCount)
           ? createPatchMark(hunk) + lines
           : lines;
       })
-      .join('')
+      .join('\n')
       .trim(),
     isDifferent,
   };
