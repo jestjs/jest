e2783ce0cb4e884ce5cf56f337ebb860ff4eee2d
Mark Pedrotti <pedrottimark@gmail.com>
2017-08-23 11:45:17 -0400

    Support SVG elements in HTMLElement plugin

--- packages/pretty-format/src/plugins/html_element.js
+++ packages/pretty-format/src/plugins/html_element.js
@@ -42,7 +42,7 @@ const ELEMENT_NODE = 1;
 const TEXT_NODE = 3;
 const COMMENT_NODE = 8;
 
-const ELEMENT_REGEXP = /^HTML\w*?Element$/;
+const ELEMENT_REGEXP = /^(HTML|SVG)\w*?Element$/;
 
 const testNode = (nodeType: any, name: any) =>
   (nodeType === ELEMENT_NODE && ELEMENT_REGEXP.test(name)) ||
