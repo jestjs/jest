ab3fe2d4d936b76347c30b8aaf25f24d751ba6fc
chitchu <chitchu@gmail.com>
2017-08-25 12:54:41 +1000

    Unified 'no tests found' message for non-verbose MPR

---
+++ packages/jest-cli/src/get_no_test_found_verbose.js
@@ -0,0 +1,42 @@
+import chalk from 'chalk';
+import pluralize from './pluralize';
+
+const getNoTestFoundVerbose = (testRunData, globalConfig): string => {
+  const individualResults = testRunData.map(testRun => {
+    const stats = testRun.matches.stats || {};
+    const config = testRun.context.config;
+    const statsMessage = Object.keys(stats)
+      .map(key => {
+        if (key === 'roots' && config.roots.length === 1) {
+          return null;
+        }
+        const value = config[key];
+        if (value) {
+          const matches = pluralize('match', stats[key], 'es');
+          return `  ${key}: ${chalk.yellow(value)} - ${matches}`;
+        }
+        return null;
+      })
+      .filter(line => line)
+      .join('\n');
+
+    return testRun.matches.total
+      ? `In ${chalk.bold(config.rootDir)}\n` +
+        `  ${pluralize('file', testRun.matches.total || 0, 's')} checked.\n` +
+        statsMessage
+      : `No files found in ${config.rootDir}.\n` +
+        `Make sure Jest's configuration does not exclude this directory.` +
+        `\nTo set up Jest, make sure a package.json file exists.\n` +
+        `Jest Documentation: ` +
+        `facebook.github.io/jest/docs/configuration.html`;
+  });
+  return (
+    chalk.bold('No tests found') +
+    '\n' +
+    individualResults.join('\n') +
+    '\n' +
+    `Pattern: ${chalk.yellow(globalConfig.testPathPattern)} - 0 matches`
+  );
+};
+
+module.exports = getNoTestFoundVerbose;
