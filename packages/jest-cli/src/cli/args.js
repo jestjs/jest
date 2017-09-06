d63c815af36fafef0cd5f84035130d106cc4ab67
Trevor Brindle <tabrindle@gmail.com>
2017-09-06 10:54:10 -0400

    improve wording in arg description

--- packages/jest-cli/src/cli/args.js
+++ packages/jest-cli/src/cli/args.js
@@ -120,7 +120,7 @@ export const options = {
   clearCache: {
     default: undefined,
     description:
-      'Clear default directory Jest uses for cache and exits.' +
+      'Clears the configured Jest cache directory and then exits.' +
       'Default directory can be found by calling jest --showConfig',
     type: 'boolean',
   },
