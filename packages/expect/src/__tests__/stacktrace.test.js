7e485a01280a22d313ec22aac2ca6e11a9f7b838
Dennis Keil <dennis.keil10@gmail.com>
2017-09-20 13:27:58 +0200

    Added test for stack trace of nested matchers.

---
+++ packages/expect/src/__tests__/stacktrace.test.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+const jestExpect = require('../');
+
+jestExpect.extend({
+  toMatchPredicate(received, argument) {
+    argument(received);
+    return {
+      message: () => '',
+      pass: true,
+    };
+  },
+});
+
+it('stack trace points to correct location when using nested matchers', () => {
+  try {
+    jestExpect(true).toMatchPredicate(value => {
+      jestExpect(value).toBe(false);
+    });
+  } catch (error) {
+    expect(error.stack).toContain('jestExpect.toMatchPredicate.value');
+    expect(error.stack).toContain('stacktrace.test.js:25');
+  }
+});
