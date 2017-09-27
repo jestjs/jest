d948aa1d173f8001d727a35c4cfae067bd464f3b
Sheel Patel <hello@sheel.me>
2017-09-27 16:23:58 -0600

    Fixed a minor spelling error

--- packages/jest-config/src/resolve_config_path.js
+++ packages/jest-config/src/resolve_config_path.js
@@ -83,7 +83,7 @@ const makeResolutionErrorMessage = (initialPath: Path, cwd: Path) => {
     'Could not find a config file based on provided values:\n' +
     `path: "${initialPath}"\n` +
     `cwd: "${cwd}"\n` +
-    'Configh paths must be specified by either a direct path to a config\n' +
+    'Config paths must be specified by either a direct path to a config\n' +
     'file, or a path to a directory. If directory is given, Jest will try to\n' +
     `traverse directory tree up, until it finds either "${JEST_CONFIG}" or\n` +
     `"${PACKAGE_JSON}".`
