852e35950ec49af3fea5d65d81766ee81284c074
recca0120 <recca0120@gmail.com>
2017-10-01 12:07:25 +0800

    fix tests

--- packages/jest-editor-support/src/__tests__/settings.test.js
+++ packages/jest-editor-support/src/__tests__/settings.test.js
@@ -1,9 +1,8 @@
 /**
  * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  *
  * @flow
  */
@@ -12,7 +11,8 @@ import EventEmitter from 'events';
 import ProjectWorkspace from '../project_workspace';
 import Settings from '../Settings';
 
-('use strict');
+
+'use strict';
 
 describe('Settings', () => {
   it('sets itself up fom the constructor', () => {
@@ -27,35 +27,6 @@ describe('Settings', () => {
     expect(settings.settings).toEqual(expect.any(Object));
   });
 
-  it('reads and parses the configs', () => {
-    const workspace = new ProjectWorkspace(
-      'root_path',
-      'path_to_jest',
-      'test',
-      1000,
-    );
-    const completed = jest.fn();
-    const configs = [{cacheDirectory: '/tmp/jest', name: '[md5 hash]'}];
-    const json = {
-      configs,
-      version: '19.0.0',
-    };
-
-    const mockProcess: any = new EventEmitter();
-    mockProcess.stdout = new EventEmitter();
-    const createProcess = () => mockProcess;
-    const buffer = makeBuffer(JSON.stringify(json));
-    const settings = new Settings(workspace, {createProcess});
-
-    settings.getConfigs(completed);
-    settings.getConfigProcess.stdout.emit('data', buffer);
-    settings.getConfigProcess.emit('close');
-
-    expect(completed).toHaveBeenCalled();
-    expect(settings.jestVersionMajor).toBe(19);
-    expect(settings.settings).toEqual(configs);
-  });
-
   it('reads and parses the config', () => {
     const workspace = new ProjectWorkspace(
       'root_path',
@@ -66,7 +37,7 @@ describe('Settings', () => {
     const completed = jest.fn();
     const config = {cacheDirectory: '/tmp/jest', name: '[md5 hash]'};
     const json = {
-      configs,
+      config,
       version: '19.0.0',
     };
 
@@ -83,6 +54,35 @@ describe('Settings', () => {
     expect(completed).toHaveBeenCalled();
     expect(settings.jestVersionMajor).toBe(19);
     expect(settings.settings).toEqual(config);
+  });
+
+  it('reads and parses the configs', () => {
+    const workspace = new ProjectWorkspace(
+      'root_path',
+      'path_to_jest',
+      'test',
+      1000,
+    );
+    const completed = jest.fn();
+    const configs = [{cacheDirectory: '/tmp/jest', name: '[md5 hash]'}];
+    const json = {
+      configs,
+      version: '21.0.0',
+    };
+
+    const mockProcess: any = new EventEmitter();
+    mockProcess.stdout = new EventEmitter();
+    const createProcess = () => mockProcess;
+    const buffer = makeBuffer(JSON.stringify(json));
+    const settings = new Settings(workspace, {createProcess});
+
+    settings.getConfigs(completed);
+    settings.getConfigProcess.stdout.emit('data', buffer);
+    settings.getConfigProcess.emit('close');
+
+    expect(completed).toHaveBeenCalled();
+    expect(settings.jestVersionMajor).toBe(19);
+    expect(settings.settings).toEqual(configs);
   });
 
   it('calls callback even if no data is sent', () => {
