07c06743fc69fbb99fcb56eedc8febdba5a455cd
Nícolas Iensen <nicolas.iensen@gmail.com>
2017-10-03 11:47:44 +0200

    Add expected exception message in the check method tests

--- packages/jest-cli/src/__tests__/cli/args.test.js
+++ packages/jest-cli/src/__tests__/cli/args.test.js
@@ -19,26 +19,36 @@ describe('check', () => {
 
   it('raises an exception if runInBand and maxWorkers are both specified', () => {
     const argv: Argv = {maxWorkers: 2, runInBand: true};
-    expect(() => check(argv)).toThrow();
+    expect(() => check(argv)).toThrow(
+      'Both --runInBand and --maxWorkers were specified',
+    );
   });
 
   it('raises an exception if onlyChanged and watchAll are both specified', () => {
     const argv: Argv = {onlyChanged: true, watchAll: true};
-    expect(() => check(argv)).toThrow();
+    expect(() => check(argv)).toThrow(
+      'Both --onlyChanged and --watchAll were specified',
+    );
   });
 
   it('raises an exception if findRelatedTests is specified with no file paths', () => {
-    const argv: Argv = {findRelatedTests: true};
-    expect(() => check(argv)).toThrow();
+    const argv: Argv = {_: [], findRelatedTests: true};
+    expect(() => check(argv)).toThrow(
+      'The --findRelatedTests option requires file paths to be specified',
+    );
   });
 
   it('raises an exception if maxWorkers is specified with no number', () => {
     const argv: Argv = {maxWorkers: undefined};
-    expect(() => check(argv)).toThrow();
+    expect(() => check(argv)).toThrow(
+      'The --maxWorkers option requires a number to be specified',
+    );
   });
 
   it('raises an exception if config is not a valid JSON string', () => {
     const argv: Argv = {config: 'x:1'};
-    expect(() => check(argv)).toThrow();
+    expect(() => check(argv)).toThrow(
+      'The --config option requires a JSON string literal, or a file path with a .js or .json extension',
+    );
   });
 });
