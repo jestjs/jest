ab6d017372e1fa82c84ad3c3e211e10ab585ccb4
Miguel Jiménez Esún <mjesun@users.noreply.github.com>
2017-10-04 11:14:22 +0100

    Initial version of jest-worker (#4497)
    * Initial version of jest-parallel
    
    * Use "export default". Change BSD to MIT
    
    * Fix flow types
    
    * Add the ability of using custom properties / non-error objects
    
    * Move missing require to import
    
    * Modifying a bunch of types and adding $FlowFixMe because Flow.... 🤦🏻‍♂️
    
    * Fix lint errors
    
    * Increase coverage to 100%

---
+++ packages/jest-worker/src/__performance_tests__/workers/jest_worker.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const pi = require('./pi');
+
+module.exports.loadTest = function() {
+  return pi();
+};
---
+++ packages/jest-worker/src/__performance_tests__/workers/pi.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function() {
+  const points = 10000;
+  let inside = 0;
+
+  for (let i = 0; i < points; i++) {
+    if (Math.pow(Math.random(), 2) + Math.pow(Math.random(), 2) <= 1) {
+      inside++;
+    }
+  }
+
+  return 4 * inside / points;
+};
