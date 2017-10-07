29623ee8769b539be60371d3c780fc6c4e9a42e2
Gabriel Ricard <4550801+gricard@users.noreply.github.com>
2017-10-06 21:25:57 -0400

    Switched from snapshot matching to regex matching because the snapshots vary by platform, apparently

--- integration_tests/__tests__/mock_names.test.js
+++ integration_tests/__tests__/mock_names.test.js
@@ -13,66 +13,58 @@ const {extractSummary} = require('../utils');
 
 test('suite without mock name, mock called', () => {
   const {stderr, status} = runJest('mock-names/without-mock-name');
-  const {rest} = extractSummary(stderr);
 
   expect(status).toBe(0);
-  expect(rest).toMatchSnapshot();
+  expect(stderr).toMatch(/PASS/);
 });
 
 test('suite without mock name, mock not called', () => {
   const {stderr, status} = runJest('mock-names/without-mock-name-not-called');
-  const {rest} = extractSummary(stderr);
 
   expect(status).toBe(1);
-  expect(rest).toMatchSnapshot();
+  expect(stderr).toMatch(/expect\(jest\.fn\(\)\)\.toHaveBeenCalled/);
 });
 
 test('suite with mock name, mock called', () => {
   const {stderr, status} = runJest('mock-names/with-mock-name');
-  const {rest} = extractSummary(stderr);
 
   expect(status).toBe(0);
-  expect(rest).toMatchSnapshot();
+  expect(stderr).toMatch(/PASS/);
 });
 
 test('suite with mock name, mock not called', () => {
   const {stderr, status} = runJest('mock-names/with-mock-name-not-called');
-  const {rest} = extractSummary(stderr);
 
   expect(status).toBe(1);
-  expect(rest).toMatchSnapshot();
+  expect(stderr).toMatch(/expect\(myMockedFunction\)\.toHaveBeenCalled/);
 });
 
 test('suite with mock name, long form, mock called', () => {
   const {stderr, status} = runJest('mock-names/with-mock-name-long');
-  const {rest} = extractSummary(stderr);
 
   expect(status).toBe(0);
-  expect(rest).toMatchSnapshot();
+  expect(stderr).toMatch(/PASS/);
 });
 
 test('suite with mock name, long form, mock not called', () => {
   const {stderr, status} = runJest('mock-names/with-mock-name-long-not-called');
-  const {rest} = extractSummary(stderr);
 
   expect(status).toBe(1);
-  expect(rest).toMatchSnapshot();
+  expect(stderr).toMatch(/expect\(myMockedFunctionLong\)\.toHaveBeenCalled/);
 });
 
 test('suite with mock name, short form, mock called', () => {
   const {stderr, status} = runJest('mock-names/with-mock-name-short');
-  const {rest} = extractSummary(stderr);
 
   expect(status).toBe(0);
-  expect(rest).toMatchSnapshot();
+  expect(stderr).toMatch(/PASS/);
 });
 
 test('suite with mock name, short form, mock not called', () => {
   const {stderr, status} = runJest(
     'mock-names/with-mock-name-short-not-called',
   );
-  const {rest} = extractSummary(stderr);
 
   expect(status).toBe(1);
-  expect(rest).toMatchSnapshot();
+  expect(stderr).toMatch(/expect\(myMockedFunctionShort\)\.toHaveBeenCalled/);
 });
