184461599ae31025bac81218f85282c9f018eb1b
Simen Bekkhus <sbekkhus91@gmail.com>
2017-10-26 16:00:15 +0200

    revert change for stackTraceLimit

--- packages/jest-environment-jsdom/src/index.js
+++ packages/jest-environment-jsdom/src/index.js
@@ -32,7 +32,7 @@ class JSDOMEnvironment {
     const global = (this.global = this.document.window.document.defaultView);
     // Node's error-message stack size is limited at 10, but it's pretty useful
     // to see more than that when a test fails.
-    Error.stackTraceLimit = 100;
+    this.global.Error.stackTraceLimit = 100;
     installCommonGlobals(global, config.globals);
 
     if (!global.requestAnimationFrame) {
