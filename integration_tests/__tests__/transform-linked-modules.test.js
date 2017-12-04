36aa76ac6e840f5a7bac3bb813de19c75ade6d93
Konstantin Pschera <me@kons.ch>
2017-12-04 14:40:10 +0100

    Skip test on windows

--- integration_tests/__tests__/transform-linked-modules.test.js
+++ integration_tests/__tests__/transform-linked-modules.test.js
@@ -2,7 +2,10 @@
 
 'use strict';
 
+const skipOnWindows = require('../../scripts/skip_on_windows');
 const runJest = require('../runJest');
+
+skipOnWindows.suite();
 
 it('should transform linked modules', () => {
   const result = runJest.json('transform-linked-modules', ['--no-cache']).json;
