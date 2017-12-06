6ade256b1583e458577145fef2a7bb566605bcf9
Miguel Jiménez Esún <mjesun@users.noreply.github.com>
2017-12-06 13:21:06 +0000

    Revert "Add new "setupTestFramework" option (#4976)" (#5025)
    This reverts commit dbfd7877c450058cd7485cbc2530e8f9b1fa6dba.

--- test_setup_file.js
+++ test_setup_file.js
@@ -7,17 +7,11 @@
 
 const jasmineReporters = require('jasmine-reporters');
 
-module.exports = function(global) {
-  const {jasmine} = global;
+// Some of the `jest-runtime` tests are very slow and cause
+// timeouts on travis
+jest.setTimeout(70000);
 
-  if (jasmine) {
-    // Some of the `jest-runtime` tests are very slow and cause timeouts on
-    // Travis CI.
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 70000;
-
-    // Running on AppVeyor, add the custom reporter.
-    if (process.env.APPVEYOR_API_URL) {
-      jasmine.getEnv().addReporter(new jasmineReporters.AppVeyorReporter());
-    }
-  }
-};
+if (global.jasmine && process.env.APPVEYOR_API_URL) {
+  // Running on AppVeyor, add the custom reporter.
+  jasmine.getEnv().addReporter(new jasmineReporters.AppVeyorReporter());
+}
