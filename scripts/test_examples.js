f172841fdc24a5fdeb2ad794124220e27772439c
Simen Bekkhus <sbekkhus91@gmail.com>
2017-12-11 16:45:18 +0100

    add react-native example as a workspace

--- scripts/test_examples.js
+++ scripts/test_examples.js
@@ -14,27 +14,11 @@ const ROOT = path.resolve(__dirname, '..');
 const EXAMPLES_DIR = path.resolve(ROOT, 'examples');
 const JEST_CLI_PATH = path.resolve(ROOT, 'packages/jest-cli');
 const JEST_BIN_PATH = path.resolve(JEST_CLI_PATH, 'bin/jest.js');
-const NODE_VERSION = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
-const SKIP_ON_OLD_NODE = ['react-native'];
-const INSTALL = ['react-native'];
 
 const examples = fs
   .readdirSync(EXAMPLES_DIR)
   .map(file => path.resolve(EXAMPLES_DIR, file))
-  .filter(f => fs.lstatSync(path.resolve(f)).isDirectory())
-  .filter(exampleDirectory => {
-    const exampleName = path.basename(exampleDirectory);
-    if (NODE_VERSION < 6 && SKIP_ON_OLD_NODE.indexOf(exampleName) !== -1) {
-      console.log(`Skipping ${exampleName} on node ${process.version}.`);
-      return false;
-    }
-
-    if (INSTALL.indexOf(exampleName) !== -1) {
-      runCommand('yarn', ['--production'], exampleDirectory);
-    }
-
-    return true;
-  });
+  .filter(f => fs.lstatSync(path.resolve(f)).isDirectory());
 
 runCommand(
   JEST_BIN_PATH,
