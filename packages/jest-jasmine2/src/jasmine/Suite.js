2ec0c8b02f5f23fb9ea832077f2525a0ef32a3ef
Josh Goldberg <josh@fullscreenmario.com>
2017-12-21 11:25:17 -0500

    Allowed named classes and functions as BlockNames
    describe can now take in classes and functions as the names.

--- packages/jest-jasmine2/src/jasmine/Suite.js
+++ packages/jest-jasmine2/src/jasmine/Suite.js
@@ -38,7 +38,7 @@ import expectationResultFactory from '../expectation_result_factory';
 export default function Suite(attrs: Object) {
   this.id = attrs.id;
   this.parentSuite = attrs.parentSuite;
-  this.description = attrs.description;
+  this.description = convertDescriptorToString(attrs.description);
   this.throwOnExpectationFailure = !!attrs.throwOnExpectationFailure;
 
   this.beforeFns = [];
@@ -180,6 +180,25 @@ Suite.prototype.addExpectationResult = function() {
     }
   }
 };
+
+function convertDescriptorToString(descriptor) {
+  if (typeof descriptor === 'string' || descriptor === undefined) {
+    return descriptor;
+  }
+
+  if (descriptor.name !== undefined) {
+    return descriptor.name;
+  }
+
+  const stringified = descriptor.toString();
+  const typeDescriptorMatch = stringified.match(/class|function/);
+  const indexOfNameSpace =
+    typeDescriptorMatch.index + typeDescriptorMatch[0].length;
+  const indexOfNameAfterSpace = stringified.search(/\(|\{/, indexOfNameSpace);
+  const name = stringified.substring(indexOfNameSpace, indexOfNameAfterSpace);
+
+  return name.trim();
+}
 
 function isAfterAll(children) {
   return children && children[0].result.status;
