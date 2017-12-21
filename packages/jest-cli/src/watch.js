5c41af3b833a7e5a452ab8304a0e9b5ba001e7ba
Lorenzo Stanco <lorenzo.stanco@gmail.com>
2017-12-21 22:17:11 +0100

    New line before quitting watch mode (eslint fix)

--- packages/jest-cli/src/watch.js
+++ packages/jest-cli/src/watch.js
@@ -161,7 +161,7 @@ export default function watch(
   let activePlugin: ?WatchPlugin;
   const onKeypress = (key: string) => {
     if (key === KEYS.CONTROL_C || key === KEYS.CONTROL_D) {
-      outputStream.write("\n");
+      outputStream.write('\n');
       process.exit(0);
       return;
     }
@@ -205,7 +205,7 @@ export default function watch(
 
     switch (key) {
       case KEYS.Q:
-        outputStream.write("\n");
+        outputStream.write('\n');
         process.exit(0);
         return;
       case KEYS.ENTER:
