9cbc26bab656fa9e9952242b2d733a86c6a61d43
Nick McCurdy <nick@nickmccurdy.com>
2018-01-05 06:42:01 -0500

    Treat dumb terminals as noninteractive (#5237)
    * Treat dumb terminals as noninteractive
    
    * Add basic changelog entry

--- packages/jest-util/src/__tests__/is_interactive.test.js
+++ packages/jest-util/src/__tests__/is_interactive.test.js
@@ -1,17 +1,21 @@
 let oldIsTTY;
+let oldTERM;
 
 beforeEach(() => {
   oldIsTTY = process.stdout.isTTY;
+  oldTERM = process.env.TERM;
 });
 
 afterEach(() => {
   process.stdout.isTTY = oldIsTTY;
+  process.env.TERM = oldTERM;
   jest.resetModules();
 });
 
 it('Returns true when running on interactive environment', () => {
   jest.doMock('is-ci', () => false);
   process.stdout.isTTY = true;
+  process.env.TERM = 'xterm-256color';
 
   const isInteractive = require('../is_interative').default;
   expect(isInteractive).toBe(true);
@@ -24,6 +28,7 @@ it('Returns false when running on a non-interactive environment', () => {
   // Test with is-ci being true and isTTY false
   jest.doMock('is-ci', () => true);
   process.stdout.isTTY = false;
+  process.env.TERM = 'xterm-256color';
   isInteractive = require('../is_interative').default;
   expect(isInteractive).toBe(expectedResult);
 
@@ -31,6 +36,7 @@ it('Returns false when running on a non-interactive environment', () => {
   jest.resetModules();
   jest.doMock('is-ci', () => false);
   process.stdout.isTTY = false;
+  process.env.TERM = 'xterm-256color';
   isInteractive = require('../is_interative').default;
   expect(isInteractive).toBe(expectedResult);
 
@@ -38,6 +44,15 @@ it('Returns false when running on a non-interactive environment', () => {
   jest.resetModules();
   jest.doMock('is-ci', () => true);
   process.stdout.isTTY = true;
+  process.env.TERM = 'xterm-256color';
+  isInteractive = require('../is_interative').default;
+  expect(isInteractive).toBe(expectedResult);
+
+  // Test with dumb terminal
+  jest.resetModules();
+  jest.doMock('is-ci', () => false);
+  process.stdout.isTTY = false;
+  process.env.TERM = 'dumb';
   isInteractive = require('../is_interative').default;
   expect(isInteractive).toBe(expectedResult);
 });
