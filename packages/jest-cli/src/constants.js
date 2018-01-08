dd7a8c3b239c98c804de9b32410b72518bf13691
Dan Abramov <dan.abramov@gmail.com>
2018-01-08 19:18:33 +0000

    Use a better console-clearing string on Windows

--- packages/jest-cli/src/constants.js
+++ packages/jest-cli/src/constants.js
@@ -9,7 +9,7 @@
 
 const isWindows = process.platform === 'win32';
 
-export const CLEAR = isWindows ? '\x1Bc' : '\x1B[2J\x1B[3J\x1B[H';
+export const CLEAR = isWindows ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H';
 
 export const KEYS = {
   A: '61',
