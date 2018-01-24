80cd6960efc2a56f4248f710fd1b55150e146bb6
kdnakt <a.kid.1985@gmail.com>
2018-01-25 05:12:53 +0900

    add testcase for yarn

--- packages/jest-cli/src/reporters/__tests__/summary_reporter.test.js
+++ packages/jest-cli/src/reporters/__tests__/summary_reporter.test.js
@@ -8,40 +8,56 @@
 
 const env = Object.assign({}, process.env);
 const write = process.stderr.write;
+const globalConfig = {
+  watch: false,
+};
+const aggregatedResults = {
+  numTotalTestSuites: 1,
+  snapshot: {
+    filesUnmatched: 1,
+    unmatched: 2,
+  },
+  testResults: {},
+};
+
+let results = [];
 let SummaryReporter;
 
 beforeEach(() => {
+  process.env.npm_lifecycle_event = 'test';
+  process.env.npm_lifecycle_script = 'jest';
+  process.stderr.write = result => results.push(result);
   SummaryReporter = require('../summary_reporter').default;
 });
 
 afterEach(() => {
+  results = [];
   process.env = env;
   process.stderr.write = write;
 });
 
 describe('onRunComplete', () => {
-  test('myTest', () => {
+  test('npm test', () => {
     process.env.npm_config_user_agent = 'npm';
-    process.env.npm_lifecycle_event = 'test';
-    process.env.npm_lifecycle_script = 'jest';
     const results = [];
     process.stderr.write = result => results.push(result);
-    const testReporter = new SummaryReporter({
-      watch: false,
-    });
-    const aggregatedResults = {
-      numTotalTestSuites: 1,
-      snapshot: {
-        filesUnmatched: 1,
-        unmatched: 2,
-      },
-      testResults: {},
-    };
+    const testReporter = new SummaryReporter(globalConfig);
     testReporter.onRunComplete(new Set(), aggregatedResults);
     expect(results[0]).toMatch('\u001b[1mSnapshot Summary\u001b[22m');
     expect(results[1]).toMatch(
       '\u001b[1m\u001b[31m › 2 snapshot tests\u001b[39m\u001b[22m failed in 1 test suite. ' +
         '\u001b[2mInspect your code changes or run `npm test -- -u` to update them.\u001b[22m',
+    );
+  });
+
+  test('yarn test', () => {
+    process.env.npm_config_user_agent = 'yarn';
+    const testReporter = new SummaryReporter(globalConfig);
+    testReporter.onRunComplete(new Set(), aggregatedResults);
+    expect(results[0]).toMatch('\u001b[1mSnapshot Summary\u001b[22m');
+    expect(results[1]).toMatch(
+      '\u001b[1m\u001b[31m › 2 snapshot tests\u001b[39m\u001b[22m failed in 1 test suite. ' +
+        '\u001b[2mInspect your code changes or run `yarn test -u` to update them.\u001b[22m',
     );
   });
 });
