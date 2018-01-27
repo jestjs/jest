480269f36389a9e53622795f4e6abfcfdac35c86
kdnakt <a.kid.1985@gmail.com>
2018-01-27 19:29:09 +0900

    add missing attributes for test results

--- packages/jest-cli/src/reporters/__tests__/summary_reporter.test.js
+++ packages/jest-cli/src/reporters/__tests__/summary_reporter.test.js
@@ -9,16 +9,23 @@
 import SummaryReporter from '../summary_reporter';
 
 const env = Object.assign({}, process.env);
+const now = Date.now;
 const write = process.stderr.write;
 const globalConfig = {
   watch: false,
 };
 const aggregatedResults = {
+  numFailedTestSuites: 1,
+  numFailedTests: 1,
+  numPassedTestSuites: 0,
   numTotalTestSuites: 1,
+  numTotalTests: 1,
   snapshot: {
     filesUnmatched: 1,
+    total: 2,
     unmatched: 2,
   },
+  startTime: 0,
   testResults: {},
 };
 
@@ -28,24 +35,26 @@ beforeEach(() => {
   process.env.npm_lifecycle_event = 'test';
   process.env.npm_lifecycle_script = 'jest';
   process.stderr.write = result => results.push(result);
+  Date.now = () => 10;
 });
 
 afterEach(() => {
   results = [];
   process.env = env;
   process.stderr.write = write;
+  Date.now = now;
 });
 
 test('snapshots needs update with npm test', () => {
   process.env.npm_config_user_agent = 'npm';
   const testReporter = new SummaryReporter(globalConfig);
   testReporter.onRunComplete(new Set(), aggregatedResults);
-  expect(results[0] + results[1]).toMatchSnapshot();
+  expect(results.join('')).toMatchSnapshot();
 });
 
 test('snapshots needs update with yarn test', () => {
   process.env.npm_config_user_agent = 'yarn';
   const testReporter = new SummaryReporter(globalConfig);
   testReporter.onRunComplete(new Set(), aggregatedResults);
-  expect(results[0] + results[1]).toMatchSnapshot();
+  expect(results.join('')).toMatchSnapshot();
 });
