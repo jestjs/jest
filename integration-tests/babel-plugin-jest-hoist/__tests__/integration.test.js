12a12f1b9a1f550b28de856a2aea9a34237d6294
Rafael Ramalho <rafaelramalho@blip.pt>
2018-01-31 10:14:07 +0000

    Fixed the unit test related to the backticks issue and updated the changelog with the PR link

--- integration-tests/babel-plugin-jest-hoist/__tests__/integration.test.js
+++ integration-tests/babel-plugin-jest-hoist/__tests__/integration.test.js
@@ -45,7 +45,7 @@ jest.mock('../__test_modules__/e', () => {
     },
   };
 });
-jest.mock('../__test_modules__/jest-backticks');
+jest.mock(`../__test_modules__/jest-backticks`);
 jest.mock('virtual-module', () => 'kiwi', {virtual: true});
 // This has types that should be ignored by the out-of-scope variables check.
 jest.mock('has-flow-types', () => (props: {children: mixed}) => 3, {
