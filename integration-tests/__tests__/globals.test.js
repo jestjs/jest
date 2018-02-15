d8eac51810bf56b2106948a63fd1dbe06e02107b
Brian Macdonald <brian.lmacdonald@gmail.com>
2018-02-15 13:02:56 -0500

    fix: cleans up stack using cleanUpStackTrace

--- integration-tests/__tests__/globals.test.js
+++ integration-tests/__tests__/globals.test.js
@@ -123,8 +123,8 @@ test('cannot test with no implementation', () => {
   expect(status).toBe(1);
 
   const {summary, rest} = extractSummary(stderr);
-  const restWithoutStack = rest.slice(0, 343);
-  expect(restWithoutStack).toMatchSnapshot();
+
+  expect(rest).toMatchSnapshot();
   expect(summary).toMatchSnapshot();
 });
 
@@ -202,8 +202,7 @@ test('cannot test with no implementation with expand arg', () => {
   expect(status).toBe(1);
 
   const {summary, rest} = extractSummary(stderr);
-  const restWithoutStack = rest.slice(0, 343);
-  expect(restWithoutStack).toMatchSnapshot();
+  expect(rest).toMatchSnapshot();
   expect(summary).toMatchSnapshot();
 });
 
