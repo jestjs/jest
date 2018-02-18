b2532103047d39e42e139a061b9eadcf7ce82a5e
Stian Didriksen <stipsan@gmail.com>
2018-02-18 01:34:20 +0100

    add test case for --onlyChanged and --coverage

--- integration-tests/__tests__/only_changed.test.js
+++ integration-tests/__tests__/only_changed.test.js
@@ -12,7 +12,7 @@
 import runJest from '../runJest';
 import os from 'os';
 import path from 'path';
-const {cleanup, run, writeFiles} = require('../Utils');
+const {cleanup, run, writeFiles, extractSummary} = require('../Utils');
 
 const DIR = path.resolve(os.tmpdir(), 'jest_only_changed');
 const GIT = 'git -c user.name=jest_test -c user.email=jest_test@test.com';
@@ -71,6 +71,64 @@ test('run only changed files', () => {
   expect(stderr).not.toMatch(/PASS __tests__(\/|\\)file1.test.j/);
   expect(stderr).toMatch(/PASS __tests__(\/|\\)file2.test.js/);
   expect(stderr).toMatch(/PASS __tests__(\/|\\)file3.test.js/);
+});
+
+test('report test coverage for only changed files', () => {
+  writeFiles(DIR, {
+    '__tests__/a.test.js': `
+    require('../a');
+    require('../b');
+    test('a', () => expect(1).toBe(1));
+  `,
+    '__tests__/b.test.js': `
+    require('../b');
+    test('b', () => expect(1).toBe(1));
+  `,
+    'a.js': 'module.exports = {}',
+    'b.js': 'module.exports = {}',
+    'package.json': JSON.stringify({
+      jest: {collectCoverage: true, testEnvironment: 'node'},
+    }),
+  });
+
+  run(`${GIT} init`, DIR);
+  run(`${GIT} add .`, DIR);
+  run(`${GIT} commit -m "first"`, DIR);
+
+  writeFiles(DIR, {
+    'a.js': 'module.exports = {modified: true}',
+  });
+
+  let stdout;
+  let stderr;
+
+  ({stdout, stderr} = runJest(DIR));
+  let summary;
+  let rest;
+  ({summary, rest} = extractSummary(stderr));
+  expect(summary).toMatchSnapshot();
+  expect(
+    rest
+      .split('\n')
+      .map(s => s.trim())
+      .sort()
+      .join('\n'),
+  ).toMatchSnapshot();
+
+  // both a.js and b.js should be in the coverage
+  expect(stdout).toMatchSnapshot();
+
+  ({stdout, stderr} = runJest(DIR, ['-o']));
+
+  ({summary, rest} = extractSummary(stderr));
+
+  expect(summary).toMatchSnapshot();
+  // should only run a.js
+  expect(rest).toMatchSnapshot();
+  // coverage should be collected only for a.js
+  expect(stdout).toMatchSnapshot();
+  // coverage for b.js should not be in the report
+  expect(stdout).not.toMatch('b.js');
 });
 
 test('onlyChanged in config is overwritten by --all or testPathPattern', () => {
