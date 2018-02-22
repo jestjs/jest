616772704df035395e2012e8826b3c7740672293
Rogelio Guzman <rogelioguzmanh@gmail.com>
2018-02-22 08:45:51 -0800

    Add fsChange hook for plugins

--- packages/jest-cli/src/jest_hooks.js
+++ packages/jest-cli/src/jest_hooks.js
@@ -8,35 +8,52 @@
  */
 
 import type {AggregatedResult} from 'types/TestResult';
+import type {ProjectConfig, Path} from 'types/Config';
 
+type JestHookExposedFS = {
+  projects: Array<{config: ProjectConfig, testPaths: Array<Path>}>,
+};
+
+type FsChange = (fs: JestHookExposedFS) => void;
 type ShouldRunTestSuite = (testPath: string) => Promise<boolean>;
 type TestRunComplete = (results: AggregatedResult) => void;
 
 export type JestHookSubscriber = {
+  fsChange: (fn: FsChange) => void,
   shouldRunTestSuite: (fn: ShouldRunTestSuite) => void,
   testRunComplete: (fn: TestRunComplete) => void,
 };
 
 export type JestHookEmitter = {
+  fsChange: (fs: JestHookExposedFS) => void,
   shouldRunTestSuite: (testPath: string) => Promise<boolean>,
   testRunComplete: (results: AggregatedResult) => void,
 };
 
 class JestHooks {
   _listeners: {
+    fsChange: Array<FsChange>,
     shouldRunTestSuite: Array<ShouldRunTestSuite>,
     testRunComplete: Array<TestRunComplete>,
   };
 
   constructor() {
     this._listeners = {
+      fsChange: [],
       shouldRunTestSuite: [],
       testRunComplete: [],
     };
   }
 
+  isUsed(hook: string) {
+    return this._listeners[hook] && this._listeners[hook].length;
+  }
+
   getSubscriber(): JestHookSubscriber {
     return {
+      fsChange: fn => {
+        this._listeners.fsChange.push(fn);
+      },
       shouldRunTestSuite: fn => {
         this._listeners.shouldRunTestSuite.push(fn);
       },
@@ -48,6 +65,8 @@ class JestHooks {
 
   getEmitter(): JestHookEmitter {
     return {
+      fsChange: fs =>
+        this._listeners.fsChange.forEach(listener => listener(fs)),
       shouldRunTestSuite: async testPath =>
         Promise.all(
           this._listeners.shouldRunTestSuite.map(listener =>
