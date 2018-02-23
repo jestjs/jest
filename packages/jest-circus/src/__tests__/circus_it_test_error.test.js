f3f12135bc98e80f3bc4fefa2aa8b47f0f5c847e
Michał Pierzchała <thymikee@gmail.com>
2018-02-23 16:06:54 +0100

    fix: handle flow ignores in circus tests

--- packages/jest-circus/src/__tests__/circus_it_test_error.test.js
+++ packages/jest-circus/src/__tests__/circus_it_test_error.test.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * *@flow
+ * @flow
  */
 
 'use strict';
@@ -35,16 +35,19 @@ describe('test/it error throwing', () => {
   });
   it(`it throws error with missing callback function`, () => {
     expect(() => {
+      // $FlowFixMe: Easy, we're testing runitme errors here
       circusIt('test2');
     }).toThrowError('Missing second argument. It must be a callback function.');
   });
   it(`it throws an error when first argument isn't a string`, () => {
     expect(() => {
+      // $FlowFixMe: Easy, we're testing runitme errors here
       circusIt(() => {});
     }).toThrowError(`Invalid first argument, () => {}. It must be a string.`);
   });
   it('it throws an error when callback function is not a function', () => {
     expect(() => {
+      // $FlowFixMe: Easy, we're testing runitme errors here
       circusIt('test4', 'test4b');
     }).toThrowError(
       `Invalid second argument, test4b. It must be a callback function.`,
@@ -57,16 +60,19 @@ describe('test/it error throwing', () => {
   });
   it(`test throws error with missing callback function`, () => {
     expect(() => {
+      // $FlowFixMe: Easy, we're testing runitme errors here
       circusTest('test6');
     }).toThrowError('Missing second argument. It must be a callback function.');
   });
   it(`test throws an error when first argument isn't a string`, () => {
     expect(() => {
+      // $FlowFixMe: Easy, we're testing runitme errors here
       circusTest(() => {});
     }).toThrowError(`Invalid first argument, () => {}. It must be a string.`);
   });
   it('test throws an error when callback function is not a function', () => {
     expect(() => {
+      // $FlowFixMe: Easy, we're testing runitme errors here
       circusTest('test8', 'test8b');
     }).toThrowError(
       `Invalid second argument, test8b. It must be a callback function.`,
