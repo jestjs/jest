cdf325c3cc9d7a3e3ccca6bd3177057203ae9c12
Matt Phillips <matt@mattphillips.io>
2018-05-01 01:29:03 +0100

    Add test for nested each property on test.only.each

---
+++ packages/jest-jasmine2/src/__tests__/each-only.test.js
@@ -0,0 +1,8 @@
+describe('.test.only.each', () => {
+  test.each([[0, 0, 0], [1, 1, 2], [5, 10, 15]])(
+    'returns result of adding %s to %s',
+    (a, b, expected) => {
+      expect(a + b).toBe(expected);
+    },
+  );
+});
