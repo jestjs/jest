652737bd006cba36d7deb19e35d4abb109a5c4a9
Matt Phillips <matt@mattphillips.io>
2018-05-01 11:45:58 +0100

    Add license to new files

--- packages/jest-jasmine2/src/__tests__/integration/each-skip.test.js
+++ packages/jest-jasmine2/src/__tests__/integration/each-skip.test.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2018-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 describe('.test.skip.each', () => {
   test.skip.each([[0, 0, 0], [1, 1, 2], [5, 10, 15]])(
     'returns result of adding %s to %s',
--- packages/jest-jasmine2/src/__tests__/integration/each.test.js
+++ packages/jest-jasmine2/src/__tests__/integration/each.test.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2018-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 describe('Data driven tests', () => {
   describe('array table', () => {
     describe('.test.each', () => {
