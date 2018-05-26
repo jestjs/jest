7277201cb39af14c5597d0c772c01fd44f718d3e
Aaron Abramov <aaron@abramov.io>
2018-05-26 11:27:04 +0100

    [jest-circus] --testNamePattern support

--- integration-tests/testNamePattern/__tests__/test_name_pattern.test.js
+++ integration-tests/testNamePattern/__tests__/test_name_pattern.test.js
@@ -9,4 +9,5 @@
 test('should match 1', () => expect(1).toBe(1));
 test('should match 2', () => expect(1).toBe(1));
 test('should not match 1', () => expect(1).toBe(1));
+test('wisconsin', () => expect(1).toBe(1));
 test.concurrent('should not match 2', () => expect(1).toBe(1));
