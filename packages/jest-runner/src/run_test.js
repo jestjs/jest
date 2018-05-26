a31a80966a6b971871e3f9a8ce1966c622a3ee1b
Jordan Eldredge <jordan@jordaneldredge.com>
2018-05-26 16:37:20 +0100

    Fix flow/lint again

--- packages/jest-runner/src/run_test.js
+++ packages/jest-runner/src/run_test.js
@@ -66,12 +66,14 @@ async function runTestInternal(
     );
   }
 
+  /* $FlowFixMe */
   const TestEnvironment = (require(testEnvironment): EnvironmentClass);
   const testFramework = ((process.env.JEST_CIRCUS === '1'
     ? /* $FlowFixMe */
       require('jest-circus/build/legacy_code_todo_rewrite/jest_adapter.js') // eslint-disable-line import/no-extraneous-dependencies
         .default
-    : require(config.testRunner)): TestFramework);
+    : /* $FlowFixMe */
+      require(config.testRunner)): TestFramework);
   /* $FlowFixMe */
   const Runtime = (require(config.moduleLoader || 'jest-runtime'): Class<
     RuntimeClass,
