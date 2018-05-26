44182628bdedd0273517510a9d10ce6a32251490
Michał Pierzchała <thymikee@gmail.com>
2018-05-26 15:30:54 +0100

    Update message based on feedback

--- packages/jest-runtime/src/helpers.js
+++ packages/jest-runtime/src/helpers.js
@@ -1,23 +1,25 @@
 // @flow
 import chalk from 'chalk';
+const DOT = ' \u2022 ';
 
 export const enhanceUnexpectedTokenMessage = (e: Error) => {
   e.stack =
     `${chalk.bold.red('Jest encountered an unexpected token')}
 
-This usually means that you are trying to import a file which Jest cannot parse, e.g. it's not JavaScript.
+This usually means that you are trying to import a file which Jest cannot parse, e.g. it's not plain JavaScript.
 
-Jest runs on source files, so it happens that it needs to process them with a transformer (${chalk.bold(
-      '"transform"',
-    )} option).
-By default a transformer is not processing "node_modules" for performance reasons.
-If you need to process some of your node modules please adjust the ${chalk.bold(
+By default, if Jest sees a Babel config, it will use that to transform your files, ignoring "node_modules".
+
+Here's what you can do:
+${DOT}To have some of your "node_modules" files transformed, you can specify a custom ${chalk.bold(
       '"transformIgnorePatterns"',
-    )} option.
-
-If you need to read non-JS files (e.g. binary assets), stubbing them out with ${chalk.bold(
+    )} in your config.
+${DOT}If you need a custom transformation specify a ${chalk.bold(
+      '"transform"',
+    )} option in your config.
+${DOT}If you simply want to mock your non-JS modules (e.g. binary assets) you can stub them out with the ${chalk.bold(
       '"moduleNameMapper"',
-    )} should help.
+    )} config option.
 
 You'll find more details and examples of these config options in the docs:
 ${chalk.cyan('https://facebook.github.io/jest/docs/en/configuration.html')}
