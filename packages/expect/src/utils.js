0b90e2cde9f203625949ad508e647c38f617c747
Will Brock <wpbrock@gmail.com>
2018-05-27 12:16:19 -0400

    Update null check

--- packages/expect/src/utils.js
+++ packages/expect/src/utils.js
@@ -206,8 +206,7 @@ export const subsetEquality = (object: Object, subset: Object) => {
 
   return Object.keys(subset).every(
     key =>
-      object !== null &&
-      object !== undefined &&
+      object != null &&
       hasOwnProperty(object, key) &&
       equals(object[key], subset[key], [iterableEquality, subsetEquality]),
   );
