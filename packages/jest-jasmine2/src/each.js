fdc272a3ea02d4daa03d1f94352a3ae0487c9ac2
Matt Phillips <matt@mattphillips.io>
2018-05-27 11:38:32 +0100

    Move jest-jasemine2 each code to jest-each (#6308)
    * Add chalk and pretty-format dependenecies to jest-each
    
    * Add bind logic to jest-each
    
    * Add jest-each dependency to jest-jasmeine2
    
    * Replace jest-jasmine2 each logic with jest-each
    
    * Fix broken snapshot
    
    * Update changelog

--- packages/jest-jasmine2/src/each.js
+++ packages/jest-jasmine2/src/each.js
@@ -9,14 +9,7 @@
 
 import type {Environment} from 'types/Environment';
 
-import util from 'util';
-import chalk from 'chalk';
-import pretty from 'pretty-format';
-
-type Table = Array<Array<any>>;
-
-const EXPECTED_COLOR = chalk.green;
-const RECEIVED_COLOR = chalk.red;
+import {bind as bindEach} from 'jest-each';
 
 export default (environment: Environment): void => {
   environment.global.it.each = bindEach(environment.global.it);
@@ -25,75 +18,4 @@ export default (environment: Environment): void => {
   environment.global.describe.each = bindEach(environment.global.describe);
   environment.global.xdescribe.each = bindEach(environment.global.xdescribe);
   environment.global.fdescribe.each = bindEach(environment.global.fdescribe);
-};
-
-const bindEach = (cb: Function) => (...args: any) => (
-  title: string,
-  test: Function,
-): void => {
-  if (args.length === 1) {
-    const table: Table = args[0];
-    return table.forEach(row =>
-      cb(util.format(title, ...row), applyRestParams(row, test)),
-    );
-  }
-
-  const templateStrings = args[0];
-  const data = args.slice(1);
-
-  const keys = getHeadingKeys(templateStrings[0]);
-  const table = buildTable(data, keys.length, keys);
-
-  if (data.length % keys.length !== 0) {
-    return cb(title, () => {
-      throw new Error(
-        'Not enough arguments supplied for given headings:\n' +
-          EXPECTED_COLOR(keys.join(' | ')) +
-          '\n\n' +
-          'Received:\n' +
-          RECEIVED_COLOR(pretty(data)) +
-          '\n\n' +
-          `Missing ${RECEIVED_COLOR(`${data.length % keys.length}`)} arguments`,
-      );
-    });
-  }
-
-  return table.forEach(row =>
-    cb(interpolate(title, row), applyObjectParams(row, test)),
-  );
-};
-
-const applyRestParams = (params: Array<any>, test: Function) => {
-  if (params.length < test.length) return done => test(...params, done);
-
-  return () => test(...params);
-};
-
-const getHeadingKeys = (headings: string): Array<string> =>
-  headings.replace(/\s/g, '').split('|');
-
-const buildTable = (
-  data: Array<any>,
-  rowSize: number,
-  keys: Array<string>,
-): Array<any> =>
-  Array.from({length: data.length / rowSize})
-    .map((_, index) => data.slice(index * rowSize, index * rowSize + rowSize))
-    .map(row =>
-      row.reduce(
-        (acc, value, index) => Object.assign({}, acc, {[keys[index]]: value}),
-        {},
-      ),
-    );
-
-const interpolate = (title: string, data: any) =>
-  Object.keys(data).reduce(
-    (acc, key) => acc.replace('$' + key, data[key]),
-    title,
-  );
-
-const applyObjectParams = (obj: any, test: Function) => {
-  if (test.length > 1) return done => test(obj, done);
-
-  return () => test(obj);
 };
