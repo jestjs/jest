0db8e8e3f5103045e65051b395053ce62751c058
Matt Phillips <matt@mattphillips.io>
2018-05-30 15:30:26 +0100

    [jest-each] Feature: handle 1d array (#6351)
    * Add logic to deal with 1D array of args
    
    * Add 1d e2e tests
    
    * Update changelog
    
    * Move test above describe to fix circus

--- packages/jest-each/src/bind.js
+++ packages/jest-each/src/bind.js
@@ -19,7 +19,9 @@ const RECEIVED_COLOR = chalk.red;
 export default (cb: Function) => (...args: any) =>
   function eachBind(title: string, test: Function): void {
     if (args.length === 1) {
-      const table: Table = args[0];
+      const table: Table = args[0].every(Array.isArray)
+        ? args[0]
+        : args[0].map(entry => [entry]);
       return table.forEach(row =>
         cb(util.format(title, ...row), applyRestParams(row, test)),
       );
