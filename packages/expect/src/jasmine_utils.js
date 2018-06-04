6538f68764d3a909b196e0cacea384ac40fa189f
Michel Weststrate <mweststrate@gmail.com>
2018-06-04 15:32:06 +0200

    Supress incorrect flow error

--- packages/expect/src/jasmine_utils.js
+++ packages/expect/src/jasmine_utils.js
@@ -222,6 +222,7 @@ function keys(obj, isArray, hasKey) {
   }
 
   for (var x = 0; x < allKeys.length; x++) {
+    //$FlowFixMe
     if (typeof allKeys[x] === 'symbol' || !allKeys[x].match(/^[0-9]+$/)) {
       extraKeys.push(allKeys[x]);
     }
