7934ca3df04cbb742571ef4bf85969e8e694a551
Sven Liebig <liebigsv@gmail.com>
2018-06-17 16:36:48 +0200

    revert it to the old test with no describe

--- e2e/__tests__/location_in_results.test.js
+++ e2e/__tests__/location_in_results.test.js
@@ -67,95 +67,138 @@ describe(`default run, with no '--testLocationInResults' flag`, () => {
   });
 });
 
-describe(`run, with '--testLocationInResults' flag`, () => {
-  let result;
-  let assertions;
+it('adds correct location info when provided with flag', () => {
+  const result = runJest.json('location-in-results', [
+    '--testLocationInResults',
+  ]).json;
 
-  beforeEach(() => {
-    result = runJest.json('location-in-results', ['--testLocationInResults'])
-      .json;
-    assertions = result.testResults[0].assertionResults;
+  const assertions = result.testResults[0].assertionResults;
+  expect(result.success).toBe(true);
+  expect(result.numTotalTests).toBe(6);
+  expect(assertions[0].location).toEqual({
+    column: 1,
+    line: 12,
   });
 
-  it('result should success, with 6 total tests', () => {
-    result = runJest.json('location-in-results', ['--testLocationInResults'])
-      .json;
-    assertions = result.testResults[0].assertionResults;
-    expect(result.success).toBe(true);
-    expect(result.numTotalTests).toBe(6);
+  expect(assertions[1].location).toEqual({
+    column: 1,
+    line: 16,
   });
 
-  describe('assertion at index', () => {
-    describe('0', () => {
-      it('should have property location equal to { column: 1, line: 10 }', () => {
-        expect(assertions[0]).toHaveProperty(
-          'location',
-          expect.objectContaining({
-            column: 1,
-            line: 12,
-          }),
-        );
-      });
-    });
+  expect(assertions[2].location).toEqual({
+    column: 1,
+    line: 20,
+  });
 
-    describe('1', () => {
-      it('should have property location equal to { column: 1, line: 14 }', () => {
-        expect(assertions[1]).toHaveProperty(
-          'location',
-          expect.objectContaining({
-            column: 1,
-            line: 16,
-          }),
-        );
-      });
-    });
+  // Technically the column should be 3, but callsites is not correct.
+  // jest-circus uses stack-utils + asyncErrors which resolves this.
+  expect(assertions[3].location).toEqual({
+    column: ConditionalTest.isJestCircusRun() ? 3 : 2,
+    line: 25,
+  });
 
-    describe('2', () => {
-      it('should have property location equal to { column: 1, line: 18 }', () => {
-        expect(assertions[2]).toHaveProperty(
-          'location',
-          expect.objectContaining({
-            column: 1,
-            line: 20,
-          }),
-        );
-      });
-    });
+  expect(assertions[4].location).toEqual({
+    column: ConditionalTest.isJestCircusRun() ? 3 : 2,
+    line: 29,
+  });
 
-    describe('3', () => {
-      it('should have property location equal to { column: 3, line: 23 }', () => {
-        expect(assertions[3]).toHaveProperty(
-          'location',
-          expect.objectContaining({
-            column: ConditionalTest.isJestCircusRun() ? 3 : 2,
-            line: 25,
-          }),
-        );
-      });
-    });
-
-    describe('4', () => {
-      it('should have property location equal to { column: 3, line: 27 }', () => {
-        expect(assertions[4]).toHaveProperty(
-          'location',
-          expect.objectContaining({
-            column: ConditionalTest.isJestCircusRun() ? 3 : 2,
-            line: 29,
-          }),
-        );
-      });
-    });
-
-    describe('5', () => {
-      it('should have property location equal to { column: 3, line: 31 }', () => {
-        expect(assertions[5]).toHaveProperty(
-          'location',
-          expect.objectContaining({
-            column: ConditionalTest.isJestCircusRun() ? 3 : 2,
-            line: 33,
-          }),
-        );
-      });
-    });
+  expect(assertions[5].location).toEqual({
+    column: ConditionalTest.isJestCircusRun() ? 3 : 2,
+    line: 33,
   });
 });
+
+// ci is failing somehow
+
+// describe(`run, with '--testLocationInResults' flag`, () => {
+//   let result;
+//   let assertions;
+
+//   beforeAll(() => {
+//     result = runJest.json('location-in-results', ['--testLocationInResults'])
+//       .json;
+//     assertions = result.testResults[0].assertionResults;
+//   });
+
+//   it('result should success', () => {
+//     expect(result.success).toBe(true);
+//   });
+
+//   it('result should have 6 total tests', () => {
+//     expect(result.numTotalTests).toBe(6);
+//   });
+
+//   describe('assertion at index', () => {
+//     describe('0', () => {
+//       it('should have property location equal to { column: 1, line: 10 }', () => {
+//         expect(assertions[0]).toHaveProperty(
+//           'location',
+//           expect.objectContaining({
+//             column: 1,
+//             line: 12,
+//           }),
+//         );
+//       });
+//     });
+
+//     describe('1', () => {
+//       it('should have property location equal to { column: 1, line: 14 }', () => {
+//         expect(assertions[1]).toHaveProperty(
+//           'location',
+//           expect.objectContaining({
+//             column: 1,
+//             line: 16,
+//           }),
+//         );
+//       });
+//     });
+
+//     describe('2', () => {
+//       it('should have property location equal to { column: 1, line: 18 }', () => {
+//         expect(assertions[2]).toHaveProperty(
+//           'location',
+//           expect.objectContaining({
+//             column: 1,
+//             line: 20,
+//           }),
+//         );
+//       });
+//     });
+
+//     describe('3', () => {
+//       it('should have property location equal to { column: 3, line: 23 }', () => {
+//         expect(assertions[3]).toHaveProperty(
+//           'location',
+//           expect.objectContaining({
+//             column: ConditionalTest.isJestCircusRun() ? 3 : 2,
+//             line: 25,
+//           }),
+//         );
+//       });
+//     });
+
+//     describe('4', () => {
+//       it('should have property location equal to { column: 3, line: 27 }', () => {
+//         expect(assertions[4]).toHaveProperty(
+//           'location',
+//           expect.objectContaining({
+//             column: ConditionalTest.isJestCircusRun() ? 3 : 2,
+//             line: 29,
+//           }),
+//         );
+//       });
+//     });
+
+//     describe('5', () => {
+//       it('should have property location equal to { column: 3, line: 31 }', () => {
+//         expect(assertions[5]).toHaveProperty(
+//           'location',
+//           expect.objectContaining({
+//             column: ConditionalTest.isJestCircusRun() ? 3 : 2,
+//             line: 33,
+//           }),
+//         );
+//       });
+//     });
+//   });
+// });
