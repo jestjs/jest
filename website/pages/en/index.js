404da0f53bd9c6204823e4ce96c1b6742e7470e3
Yangshun Tay <tay.yang.shun@gmail.com>
2018-06-22 14:39:24 -0700

    Update Docusaurus to 1.3.0 (#6510)
    * Update Docusaurus to 1.3.0
    
    * Improve index.js
    
    * Prettier
    
    * Prettier
    
    * Revert theme change
    
    * Use clean URLs
    
    * Update yarn.lock

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -40,10 +40,7 @@ class HomeSplash extends React.Component {
         <div className="homeSplashFade">
           <div className="wrapper homeWrapper">
             <div className="projectLogo">
-              <img
-                src={siteConfig.baseUrl + 'img/jest-outline.svg'}
-                alt="Jest"
-              />
+              <img src={siteConfig.baseUrl + 'img/jest.svg'} alt="Jest" />
             </div>
             <div className="inner">
               <h2 className="projectTitle">
@@ -120,7 +117,7 @@ class Index extends React.Component {
       <div>
         <HomeSplash language={this.props.language} config={siteConfig} />
         <div className="mainContainer">
-          <Container padding={['bottom', 'top']}>
+          <Container padding={['bottom', 'top']} background="light">
             <GridBlock
               align="center"
               contents={[
@@ -162,26 +159,26 @@ class Index extends React.Component {
               layout="fourColumn"
             />
           </Container>
-
-          <div
-            className="productShowcaseSection paddingBottom"
-            style={{textAlign: 'center'}}
-          >
-            <h2>
-              <translate>Zero configuration testing platform</translate>
-            </h2>
-            <MarkdownBlock>
-              <translate>
-                Jest is used by Facebook to test all JavaScript code including
-                React applications. One of Jest's philosophies is to provide an
-                integrated \"zero-configuration\" experience. We observed that
-                when engineers are provided with ready-to-use tools, they end up
-                writing more tests, which in turn results in more stable and
-                healthy code bases.
-              </translate>
-            </MarkdownBlock>
-          </div>
-
+          <Container padding={['bottom', 'top']}>
+            <div
+              className="productShowcaseSection paddingBottom"
+              style={{textAlign: 'center'}}
+            >
+              <h2>
+                <translate>Zero configuration testing platform</translate>
+              </h2>
+              <MarkdownBlock>
+                <translate>
+                  Jest is used by Facebook to test all JavaScript code including
+                  React applications. One of Jest's philosophies is to provide
+                  an integrated \"zero-configuration\" experience. We observed
+                  that when engineers are provided with ready-to-use tools, they
+                  end up writing more tests, which in turn results in more
+                  stable and healthy code bases.
+                </translate>
+              </MarkdownBlock>
+            </div>
+          </Container>
           <Container padding={['bottom', 'top']} background="light">
             <GridBlock
               contents={[
@@ -245,7 +242,6 @@ class Index extends React.Component {
               ]}
             />
           </Container>
-
           <Container background="dark" padding={['bottom', 'top']}>
             <a className="anchor" name="use" />
             <a className="hash-link" href="#use" />
