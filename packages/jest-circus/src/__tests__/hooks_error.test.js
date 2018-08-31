d0c28bd03fb685e27675668aedcd3beee86126a4
Alcedo Nathaniel De Guzman Jr <natealcedo@gmail.com>
2018-08-31 16:45:35 +0800

    Standardise error message for validating hook functions in circus similar to how jasmine_light does it

--- packages/jest-circus/src/__tests__/hooks_error.test.js
+++ packages/jest-circus/src/__tests__/hooks_error.test.js
@@ -15,10 +15,12 @@ describe('hooks error throwing', () => {
   test.each([['beforeEach'], ['beforeAll'], ['afterEach'], ['afterAll']])(
     '%s throws an error when the first argument is not a function',
     fn => {
-      expect(() => {
-        circus[fn]('param');
-      }).toThrowError(
-        'Invalid first argument, param. It must be a callback function.',
+      ['String', 1, {}, Symbol('hello'), true, null, undefined].forEach(el =>
+        expect(() => {
+          circus[fn](el);
+        }).toThrowError(
+          'Invalid first argument. It must be a callback function.',
+        ),
       );
     },
   );
