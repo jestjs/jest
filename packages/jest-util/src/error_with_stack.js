ad25ed3a44d55a942a563f0f8ebbfa7daaa71a77
Matt Phillips <matt@mattphillips.io>
2018-09-29 12:30:57 +0100

    Update message to an optional type

--- packages/jest-util/src/error_with_stack.js
+++ packages/jest-util/src/error_with_stack.js
@@ -8,7 +8,7 @@
  */
 
 export default class ErrorWithStack extends Error {
-  constructor(message: string, callsite: Function) {
+  constructor(message: ?string, callsite: Function) {
     super(message);
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, callsite);
