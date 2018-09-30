93bb48e693ff7c105a1144c7725ad855687943a6
Matt Phillips <matt@mattphillips.io>
2018-09-30 14:21:44 +0100

    Add flow support to test file

--- packages/jest-util/src/__tests__/error_with_stack.test.js
+++ packages/jest-util/src/__tests__/error_with_stack.test.js
@@ -3,6 +3,8 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
  */
 
 import ErrorWithStack from '../error_with_stack';
@@ -12,7 +14,7 @@ describe('ErrorWithStack', () => {
   const callsite = () => {};
 
   it('calls Error.captureStackTrace with given callsite when capture exists', () => {
-    Error.captureStackTrace = jest.fn();
+    jest.spyOn(Error, 'captureStackTrace');
 
     const actual = new ErrorWithStack(message, callsite);
 
