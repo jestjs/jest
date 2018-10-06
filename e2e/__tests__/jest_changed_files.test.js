18aabce9c1dbb90ff345946627e0d838e77b7f5b
Kevin Huynh <khuyn003@gmail.com>
2018-10-06 11:47:23 -0700

    Add integration tests for handling bad revision values for `changedSince`

--- e2e/__tests__/jest_changed_files.test.js
+++ e2e/__tests__/jest_changed_files.test.js
@@ -17,6 +17,7 @@ import {
 } from '../../packages/jest-changed-files/src';
 import {skipSuiteOnWindows} from '../../scripts/ConditionalTest';
 import {cleanup, run, writeFiles} from '../Utils';
+import runJest from '../runJest';
 
 skipSuiteOnWindows();
 
@@ -243,6 +244,25 @@ test('monitors only root paths for git', async () => {
   ).toEqual(['file2.txt', 'file3.txt']);
 });
 
+test('handles a bad revision for "changedSince", for git', async () => {
+  writeFiles(DIR, {
+    '.watchmanconfig': '',
+    '__tests__/file1.test.js': `require('../file1'); test('file1', () => {});`,
+    'file1.js': 'module.exports = {}',
+    'package.json': '{}',
+  });
+
+  run(`${GIT} init`, DIR);
+  run(`${GIT} add .`, DIR);
+  run(`${GIT} commit -m "first"`, DIR);
+
+  const stderr = runJest(DIR, ['--changedSince=blablabla']).stderr;
+
+  expect(stderr).toMatch(
+    `\n\n  ● Test suite failed to run\n\n    fatal: bad revision '^blablabla'\n`,
+  );
+});
+
 test('gets changed files for hg', async () => {
   if (process.env.CI) {
     // Circle and Travis have very old version of hg (v2, and current
@@ -370,4 +390,23 @@ test('monitors only root paths for hg', async () => {
       .map(filePath => path.basename(filePath))
       .sort(),
   ).toEqual(['file2.txt', 'file3.txt']);
+});
+
+test('handles a bad revision for "changedSince", for hg', async () => {
+  writeFiles(DIR, {
+    '.watchmanconfig': '',
+    '__tests__/file1.test.js': `require('../file1'); test('file1', () => {});`,
+    'file1.js': 'module.exports = {}',
+    'package.json': '{}',
+  });
+
+  run(`${HG} init`, DIR);
+  run(`${HG} add .`, DIR);
+  run(`${HG} commit -m "first"`, DIR);
+
+  const stderr = runJest(DIR, ['--changedSince=blablabla']).stderr;
+
+  expect(stderr).toMatch(
+    `\n\n  ● Test suite failed to run\n\n    abort: unknown revision 'blablabla'!\n`,
+  );
 });
