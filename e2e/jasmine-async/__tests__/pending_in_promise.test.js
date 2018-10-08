f44002056a47e3ef924462710caf3849092cc143
Baris Gümüstas <baris.guemuestas@bonial.com>
2018-10-08 13:54:42 +0200

    Skipping the e2e "jasmine pending during test" test for circus.

--- e2e/jasmine-async/__tests__/pending_in_promise.test.js
+++ e2e/jasmine-async/__tests__/pending_in_promise.test.js
@@ -1,12 +1,3 @@
-/**
- * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @jest-environment jsdom
- */
-
 'use strict';
 
 it('skips a test inside a promise', () =>
