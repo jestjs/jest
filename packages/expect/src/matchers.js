565eae7cc780f74a1bdb5089bbf4b5bce6722da2
Jeffrey Priebe <jeffreypriebe@gmail.com>
2018-10-13 14:50:28 -0700

    Fix: Change toBeGreaterThan messages to be correct for `.not`

--- packages/expect/src/matchers.js
+++ packages/expect/src/matchers.js
@@ -131,7 +131,8 @@ const matchers: MatchersObject = {
         isNot: this.isNot,
       }) +
       '\n\n' +
-      `Expected value to be grear than: ${printExpected(expected)}\n` +
+      `Expected value${pass ? ' not' : ''} to be greater than: ` +
+      `${printExpected(expected)}\n` +
       `Received: ${printReceived(actual)}`;
     return {message, pass};
   },
@@ -144,7 +145,7 @@ const matchers: MatchersObject = {
         isNot: this.isNot,
       }) +
       '\n\n' +
-      `Expected value to be greater than or equal: ` +
+      `Expected value${pass ? ' not' : ''} to be greater than or equal: ` +
       `${printExpected(expected)}\n` +
       `Received: ${printReceived(actual)}`;
     return {message, pass};
@@ -201,7 +202,8 @@ const matchers: MatchersObject = {
         isNot: this.isNot,
       }) +
       '\n\n' +
-      `Expected value to be less than: ${printExpected(expected)}\n` +
+      `Expected value${pass ? ' not' : ''} to be less than: ` +
+      `${printExpected(expected)}\n` +
       `Received: ${printReceived(actual)}`;
     return {message, pass};
   },
@@ -214,7 +216,8 @@ const matchers: MatchersObject = {
         isNot: this.isNot,
       }) +
       '\n\n' +
-      `Expected value to be less than or equal: ${printExpected(expected)}\n` +
+      `Expected value${pass ? ' not' : ''} to be less than or equal: ` +
+      `${printExpected(expected)}\n` +
       `Received: ${printReceived(actual)}`;
     return {message, pass};
   },
