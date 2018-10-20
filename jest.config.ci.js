9ea535448abf9d6326c114bc646831c017f7e5f8
Simen Bekkhus <sbekkhus91@gmail.com>
2018-10-20 14:53:04 +0200

    use normal reporters

--- jest.config.ci.js
+++ jest.config.ci.js
@@ -2,9 +2,8 @@
 
 // Object spread is just node 8
 module.exports = Object.assign({}, require('./jest.config'), {
-  coverageReporters: ['json'],
   reporters: [
     ['jest-junit', {output: 'reports/junit/js-test-results.xml'}],
-    ['jest-silent-reporter', {useDots: true}],
+    'default',
   ],
 });
