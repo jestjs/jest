2a50d67dc86f198e514fbff0516b247a882bbb9a
Christian Delahousse <cdelahousse@fb.com>
2018-10-25 14:54:43 -0700

    Adding copyright headers to e2e/override-globals/setup.js
    'nuff said.

--- e2e/override-globals/setup.js
+++ e2e/override-globals/setup.js
@@ -1,2 +1,11 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
 Date.now = () => 0;
 process.hrtime = () => [0, 5000];
