9b853baf2489386d6b30f4c68a91a1955a96776c
Dan Minshew <ofenixculpa@gmail.com>
2018-10-26 15:53:50 -0500

    website: add an additional filter predicate to backers

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -86,25 +86,29 @@ class Contributors extends React.Component {
           </translate>
         </p>
         <div>
-          {backers.filter(b => b.tier === 'backer').map(b => (
-            <a
-              key={b.id}
-              className="backer-item"
-              title={`$${b.totalDonations / 100} by ${b.name || b.slug}`}
-              target="_blank"
-              href={b.website || `https://opencollective.com/${b.slug}`}
-            >
-              {
-                <img
-                  className="backer-avatar"
-                  src={b.avatar}
-                  alt={
-                    b.name || b.slug ? `${b.name || b.slug}'s avatar` : 'avatar'
-                  }
-                />
-              }
-            </a>
-          ))}
+          {backers
+            .filter(b => b.tier === 'backer' && !b.slug.includes('adult'))
+            .map(b => (
+              <a
+                key={b.id}
+                className="backer-item"
+                title={`$${b.totalDonations / 100} by ${b.name || b.slug}`}
+                target="_blank"
+                href={b.website || `https://opencollective.com/${b.slug}`}
+              >
+                {
+                  <img
+                    className="backer-avatar"
+                    src={b.avatar}
+                    alt={
+                      b.name || b.slug
+                        ? `${b.name || b.slug}'s avatar`
+                        : 'avatar'
+                    }
+                  />
+                }
+              </a>
+            ))}
           <div>
             <a
               className="support-button"
