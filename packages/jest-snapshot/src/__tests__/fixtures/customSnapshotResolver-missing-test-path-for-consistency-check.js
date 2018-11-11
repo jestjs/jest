894e962be4a57ccedd44c22379d575e5c987085b
Nicklas Gummesson <nicklas@dropbox.com>
2018-11-11 11:17:58 -0800

    Improve consistency check for snapshot resolver

---
+++ packages/jest-snapshot/src/__tests__/fixtures/customSnapshotResolver-missing-test-path-for-consistency-check.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+
+module.exports = {
+  resolveSnapshotPath: (testPath, snapshotExtension) => {},
+  resolveTestPath: (snapshotFilePath, snapshotExtension) => {},
+};
