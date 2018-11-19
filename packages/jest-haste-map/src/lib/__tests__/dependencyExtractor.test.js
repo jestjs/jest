c16ea91f62af18c8fb934646a6eca966eabecea8
Rubén Norte <rubennorte@fb.com>
2018-11-19 10:31:48 +0000

    Fix headers and naming

--- packages/jest-haste-map/src/lib/__tests__/dependencyExtractor.test.js
+++ packages/jest-haste-map/src/lib/__tests__/dependencyExtractor.test.js
@@ -1,7 +1,9 @@
 /**
- * Copyright 2004-present Facebook. All Rights Reserved.
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
  *
- * @emails oncall+javascript_foundation
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
  * @flow
  */
 
