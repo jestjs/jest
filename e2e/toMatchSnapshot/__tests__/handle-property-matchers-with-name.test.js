ac4511ca563952ab78c0bdcc5c12100997cf16df
João Conde <up201503256@fe.up.pt>
2018-12-01 22:23:15 +0000

    Snapshot updates

--- e2e/toMatchSnapshot/__tests__/handle-property-matchers-with-name.test.js
+++
@@ -1,5 +0,0 @@
-test('handles property matchers with deep properties', () => {
-  expect({user: {createdAt: new Date(), name: 'CHANGED'}}).toMatchSnapshot({
-    user: {createdAt: expect.any(Date), name: 'CHANGED'},
-  });
-});
