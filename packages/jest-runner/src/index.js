aa116bd777f48aebb2f6c5f38cb12c1cb7b4e237
Lantz Warrick <betalantz@gmail.com>
2018-12-03 18:40:31 -0800

    renamed 'run_test.js' to 'runTest.js' and updated imports

--- packages/jest-runner/src/index.js
+++ packages/jest-runner/src/index.js
@@ -20,7 +20,7 @@ import type {
 import typeof {worker} from './test_worker';
 
 import exit from 'exit';
-import runTest from './run_test';
+import runTest from './runTest';
 import throat from 'throat';
 import Worker from 'jest-worker';
 
