047222c623cdbd94cbf4ee0865ddd4ef5cfe418b
Miguel Campos <amodestduck@gmail.com>
2018-12-28 12:39:53 -0800

    changing references to match file name

--- e2e/__tests__/emptyDescribeWithHooks.test.js
+++ e2e/__tests__/emptyDescribeWithHooks.test.js
@@ -19,25 +19,25 @@ const dir = path.resolve(__dirname, '../empty-describe-with-hooks');
 skipSuiteOnJasmine();
 
 test('hook in empty describe', () => {
-  const result = runJest(dir, ['hook-in-empty-describe.test.js']);
+  const result = runJest(dir, ['hookInEmptyDescribe.test.js']);
   expect(result.status).toBe(1);
   expect(extractSummary(result.stderr)).toMatchSnapshot();
 });
 
 test('hook in describe with skipped test', () => {
-  const result = runJest(dir, ['hook-in-describe-with-skipped-test.test.js']);
+  const result = runJest(dir, ['hookInDescribeWithSkippedTest.test.js']);
   expect(result.status).toBe(0);
   expect(extractSummary(result.stderr)).toMatchSnapshot();
 });
 
 test('hook in empty nested describe', () => {
-  const result = runJest(dir, ['hook-in-empty-nested-describe.test.js']);
+  const result = runJest(dir, ['hookInEmptyNestedDescribe.test.js']);
   expect(result.status).toBe(1);
   expect(extractSummary(result.stderr)).toMatchSnapshot();
 });
 
 test('multiple hooks in empty describe', () => {
-  const result = runJest(dir, ['multiple-hooks-in-empty-describe.test.js']);
+  const result = runJest(dir, ['multipleHooksInEmptyDescribe.test.js']);
   expect(result.status).toBe(1);
   expect(extractSummary(result.stderr)).toMatchSnapshot();
 });
