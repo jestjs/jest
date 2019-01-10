3c727f1897c65a85836b700c2da9850a80964c09
Mark Pedrotti <pedrottimark@gmail.com>
2019-01-10 12:50:32 -0500

    Add Facebook copyright header to index.js file

--- packages/diff-sequences/perf/index.js
+++ packages/diff-sequences/perf/index.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // Make sure to run node with --expose-gc option!
 
 // The times are reliable if about 1% relative mean error if you run it:
