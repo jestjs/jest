066f0e3cbf1c684587d4a0c60b020eb1a6b6f3e1
Simen Bekkhus <sbekkhus91@gmail.com>
2019-01-13 17:05:13 +0100

    chore: update deps (#7618)

--- packages/pretty-format/src/__tests__/react.test.js
+++ packages/pretty-format/src/__tests__/react.test.js
@@ -719,7 +719,6 @@ test('supports forwardRef with a child', () => {
   }
 
   expect(
-    // $FlowFixMe - https://github.com/facebook/flow/issues/6103
     formatElement(React.createElement(React.forwardRef(Cat), null, 'mouse')),
   ).toEqual('<ForwardRef(Cat)>\n  mouse\n</ForwardRef(Cat)>');
 });
