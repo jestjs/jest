34197abef8882de3cc1da69f544dd1e727c08074
Simen Bekkhus <sbekkhus91@gmail.com>
2019-01-18 15:02:50 +0100

    Run code transforms over `global{Setup,Teardown}` (#7562)

--- e2e/global-setup/setup.js
+++ e2e/global-setup/setup.js
@@ -13,7 +13,8 @@ const path = require('path');
 const DIR = path.join(os.tmpdir(), 'jest-global-setup');
 
 module.exports = function() {
-  return new Promise((resolve, reject) => {
+  // This uses a flow annotation to show it can be transpiled
+  return new Promise((resolve, reject: any) => {
     createDirectory(DIR);
     const fileId = crypto.randomBytes(20).toString('hex');
     fs.writeFileSync(path.join(DIR, fileId), 'setup');
