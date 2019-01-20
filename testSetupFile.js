6ff89bb736d6f775175de8d43a5a7fe231055254
Michał Pierzchała <thymikee@gmail.com>
2019-01-20 17:57:03 +0100

    remove jasmine-reporters

--- testSetupFile.js
+++ testSetupFile.js
@@ -5,8 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const jasmineReporters = require('jasmine-reporters');
-
 // Some of the `jest-runtime` tests are very slow and cause
 // timeouts on travis
 jest.setTimeout(70000);
