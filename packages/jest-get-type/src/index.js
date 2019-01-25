a9622654d5db05f5d1f2a0d6d8222cc1dbe7070f
Matt Phillips <matt@mattphillips.io>
2019-01-25 17:48:38 +0000

    Move getType to named file with ES6 import/export

--- packages/jest-get-type/src/index.js
+++ packages/jest-get-type/src/index.js
@@ -7,57 +7,6 @@
  * @flow
  */
 
-'use strict';
+import getType from './getType';
 
-export type ValueType =
-  | 'array'
-  | 'boolean'
-  | 'function'
-  | 'null'
-  | 'number'
-  | 'object'
-  | 'regexp'
-  | 'map'
-  | 'set'
-  | 'date'
-  | 'string'
-  | 'symbol'
-  | 'undefined';
-
-// get the type of a value with handling the edge cases like `typeof []`
-// and `typeof null`
-const getType = (value: any): ValueType => {
-  if (value === undefined) {
-    return 'undefined';
-  } else if (value === null) {
-    return 'null';
-  } else if (Array.isArray(value)) {
-    return 'array';
-  } else if (typeof value === 'boolean') {
-    return 'boolean';
-  } else if (typeof value === 'function') {
-    return 'function';
-  } else if (typeof value === 'number') {
-    return 'number';
-  } else if (typeof value === 'string') {
-    return 'string';
-  } else if (typeof value === 'object') {
-    if (value.constructor === RegExp) {
-      return 'regexp';
-    } else if (value.constructor === Map) {
-      return 'map';
-    } else if (value.constructor === Set) {
-      return 'set';
-    } else if (value.constructor === Date) {
-      return 'date';
-    }
-    return 'object';
-    // $FlowFixMe https://github.com/facebook/flow/issues/1015
-  } else if (typeof value === 'symbol') {
-    return 'symbol';
-  }
-
-  throw new Error(`value of unknown type: ${value}`);
-};
-
-module.exports = getType;
+export default getType;
