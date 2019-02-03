18503b92b5f0e1ef7215fad257c941190357e8c0
rostog <rostogiorgi@gmail.com>
2019-02-03 14:14:06 +0100

    added copyright headers

--- e2e/module-name-mapper-mock/__tests__/storage/track/TrackExpected.test.js
+++ e2e/module-name-mapper-mock/__tests__/storage/track/TrackExpected.test.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const {Track} = require('@@storage/track/Track');
 jest.mock('@@storage/track/Track');
 
