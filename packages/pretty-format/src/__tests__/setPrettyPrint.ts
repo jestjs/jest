080998edd46f1c75e8864dae1e6b6cad99265a8e
Simen Bekkhus <sbekkhus91@gmail.com>
2019-02-08 10:39:43 +0100

    chore: remove cyclic dev dep on jest-diff from pretty-format

--- packages/pretty-format/src/__tests__/setPrettyPrint.ts
+++ packages/pretty-format/src/__tests__/setPrettyPrint.ts
@@ -5,9 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// @ts-ignore
-import diff from 'jest-diff';
-
 import prettyFormat from '../';
 import {OptionsReceived, Plugins} from '../types';
 
@@ -34,7 +31,8 @@ const setPrettyPrint = (plugins: Plugins) => {
             `Received:\n` +
             `  ${this.utils.printReceived(prettyFormatted)}`
         : () => {
-            const diffString = diff(expected, prettyFormatted, {
+            // @ts-ignore: https://github.com/DefinitelyTyped/DefinitelyTyped/pull/32891
+            const diffString = this.utils.diff(expected, prettyFormatted, {
               expand: this.expand,
             });
             return (
