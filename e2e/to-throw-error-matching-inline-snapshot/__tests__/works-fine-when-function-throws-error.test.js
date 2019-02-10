87234f6f229762a6ee6e97222130db834b8ee3c5
Sascha Englert <sascha@englerts.de>
2019-02-10 15:16:15 +0100

    Removed test file that was accedently added

--- e2e/to-throw-error-matching-inline-snapshot/__tests__/works-fine-when-function-throws-error.test.js
+++
@@ -1,8 +0,0 @@
-
-    test('works fine when function throws error', () => {
-      expect(() => {
-        throw new Error('apple');
-      })
-        .toThrowErrorMatchingInlineSnapshot();
-    });
-  
