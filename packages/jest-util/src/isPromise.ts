408cb5334b4d4814a67f42bc7868c0e212039956
Tim Seckinger <seckinger.tim@gmail.com>
2019-02-10 15:01:02 +0100

    copyright header

--- packages/jest-util/src/isPromise.ts
+++ packages/jest-util/src/isPromise.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // see ES2015 spec 25.4.4.5, https://stackoverflow.com/a/38339199
 const isPromise = (candidate: unknown): candidate is Promise<unknown> =>
   Promise.resolve(candidate) === candidate;
