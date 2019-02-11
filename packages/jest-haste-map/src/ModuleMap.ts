e7476abf2b246722476825cf07793cb5f7801cd9
Simen Bekkhus <sbekkhus91@gmail.com>
2019-02-11 23:37:33 +0100

    never!!

--- packages/jest-haste-map/src/ModuleMap.ts
+++ packages/jest-haste-map/src/ModuleMap.ts
@@ -22,7 +22,7 @@ import H from './constants';
 const EMPTY_OBJ = {} as {[key: string]: any};
 const EMPTY_MAP = new Map();
 
-type ValueType<T> = T extends Map<string, infer V> ? V : T;
+type ValueType<T> = T extends Map<string, infer V> ? V : never;
 
 export type SerializableModuleMap = {
   // There is no easier way to extract the type of the entries of a Map
