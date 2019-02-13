9e31173216d026b2190d94e1fda2bdaf917623ba
Simen Bekkhus <sbekkhus91@gmail.com>
2019-02-13 19:15:10 +0100

    chore: migrate jest-resolve to TypeScript (#7871)

--- packages/jest-resolve/src/isBuiltinModule.ts
+++ packages/jest-resolve/src/isBuiltinModule.ts
@@ -3,23 +3,20 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- *
- * @flow
  */
 
-// $FlowFixMe: Flow doesn't know about the `module` module
-import {builtinModules} from 'module';
+import module from 'module';
 
-// https://github.com/facebook/flow/pull/5160
-declare var process: {
-  binding(type: string): {},
+// @ts-ignore: "private" api
+declare const process: {
+  binding(type: string): {};
 };
 
 const EXPERIMENTAL_MODULES = ['worker_threads'];
 
 const BUILTIN_MODULES = new Set(
-  builtinModules
-    ? builtinModules.concat(EXPERIMENTAL_MODULES)
+  module.builtinModules
+    ? module.builtinModules.concat(EXPERIMENTAL_MODULES)
     : Object.keys(process.binding('natives'))
         .filter((module: string) => !/^internal\//.test(module))
         .concat(EXPERIMENTAL_MODULES),
