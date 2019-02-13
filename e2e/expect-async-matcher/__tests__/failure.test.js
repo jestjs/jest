7ac2a3c5a83eae4b13710b63fe7f7d1d343f3af0
Simen Bekkhus <sbekkhus91@gmail.com>
2019-02-13 09:36:40 +0100

    normalize tests

--- e2e/expect-async-matcher/__tests__/failure.test.js
+++ e2e/expect-async-matcher/__tests__/failure.test.js
@@ -4,13 +4,10 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-'use strict';
 
 import {toHaveLengthAsync} from '../matchers';
 
-expect.extend({
-  toHaveLengthAsync,
-});
+expect.extend({toHaveLengthAsync});
 
 it('fail with expected non promise values', () =>
   expect([1]).toHaveLengthAsync(Promise.resolve(2)));
@@ -21,7 +18,7 @@ it('fail with expected non promise values and not', () =>
 it('fail with expected promise values', () =>
   expect(Promise.resolve([1])).resolves.toHaveLengthAsync(Promise.resolve(2)));
 
-it('fail with expected promise values and not', async () =>
+it('fail with expected promise values and not', () =>
   expect(Promise.resolve([1, 2])).resolves.not.toHaveLengthAsync(
     Promise.resolve(2)
   ));
