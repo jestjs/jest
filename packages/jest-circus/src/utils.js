38698cbd65e6db2be0b5651abdb8429057a8b81f
doniyor2109 <doniyor2109@gmail.com>
2019-02-14 22:32:47 +0500

    Inherit "only" mode unless there is tests with "only" mode already (#7888)

--- packages/jest-circus/src/utils.js
+++ packages/jest-circus/src/utils.js
@@ -65,12 +65,6 @@ export const makeTest = (
   timeout: ?number,
   asyncError: Exception,
 ): TestEntry => {
-  let _mode = mode;
-  if (!mode) {
-    // if not set explicitly, inherit from its parent describe
-    _mode = parent.mode;
-  }
-
   const errors: Array<[?Exception, Exception]> = [];
 
   return {
@@ -79,7 +73,7 @@ export const makeTest = (
     errors,
     fn,
     invocations: 0,
-    mode: _mode,
+    mode,
     name: convertDescriptorToString(name),
     parent,
     startedAt: null,
