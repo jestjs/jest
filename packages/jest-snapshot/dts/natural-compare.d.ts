3de88c4ce8f4ecca4b68a577f2e7d65c6069dd55
doniyor2109 <doniyor2109@gmail.com>
2019-02-16 11:59:38 +0500

    Added @types/natural-comapre

--- packages/jest-snapshot/dts/natural-compare.d.ts
+++
@@ -1,5 +0,0 @@
-// TODO Replace with @types/natural-compare when it is merged https://github.com/DefinitelyTyped/DefinitelyTyped/pull/33084
-declare module 'natural-compare' {
-  const NaturalCompare: (a: string, b: string) => number;
-  export default NaturalCompare;
-}
