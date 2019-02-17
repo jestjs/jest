30efca32b9deb8be478146cfc73a909f979d01e6
Alcedo Nathaniel De Guzman Jr <natealcedo@gmail.com>
2019-02-17 20:09:01 +0800

    Migrate extractExpectedAssertionsErrors

--- packages/expect/src/extractExpectedAssertionsErrors.ts
+++ packages/expect/src/extractExpectedAssertionsErrors.ts
@@ -4,7 +4,6 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow
  */
 
 import {
@@ -14,7 +13,7 @@ import {
   pluralize,
 } from 'jest-matcher-utils';
 
-import {getState, setState} from './jestMatchersObject';
+import { getState, setState } from './jestMatchersObject';
 
 const resetAssertionsLocalState = () => {
   setState({
