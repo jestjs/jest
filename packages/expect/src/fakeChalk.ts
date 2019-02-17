73042ba55424fb1170f66132b175c48e7363d848
Alcedo Nathaniel De Guzman Jr <natealcedo@gmail.com>
2019-02-17 20:08:24 +0800

    migrate fakechalk

--- packages/expect/src/fakeChalk.ts
+++ packages/expect/src/fakeChalk.ts
@@ -3,15 +3,14 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- * @flow
  */
 
 import ansiStyles from 'ansi-styles';
 
-const returnInput = str => str;
+const returnInput = (str: any) => str;
 
 const allColorsAsFunc = Object.keys(ansiStyles)
-  .map(style => ({[style]: returnInput}))
+  .map(style => ({ [style]: returnInput }))
   .reduce((acc, cur) => Object.assign(acc, cur));
 
 Object.keys(allColorsAsFunc)
