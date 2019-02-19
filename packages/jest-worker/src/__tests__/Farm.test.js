0297b72542b638b91d4e3a5404a7e472d1ecfdd0
Miguel Jimenez Esun <mjimenez@fb.com>
2019-02-19 12:10:58 +0000

    Fix queue management in jest-worker

--- packages/jest-worker/src/__tests__/Farm.test.js
+++ packages/jest-worker/src/__tests__/Farm.test.js
@@ -200,18 +200,19 @@ describe('Farm', () => {
     workerReply(0, null, 17);
     await p0;
 
-    // Note that the stickiness is not created by the method name or the arguments
-    // it is solely controlled by the provided "computeWorkerKey" method, which in
-    // the test example always returns the same key, so all calls should be
-    // redirected to worker 1 (which is the one that resolved the first call).
+    // Note that the stickiness is not created by the method name or the
+    // arguments it is solely controlled by the provided "computeWorkerKey"
+    // method, which in the test example always returns the same key, so all
+    // calls should be redirected to worker 1 (which is the one that resolved
+    // the first call).
     const p1 = farm.doWork('foo', 'bar');
     workerReply(1, null, 17);
     await p1;
 
-    // The first time, a call with a "1234567890abcdef" hash had never been done
-    // earlier ("foo" call), so it got queued to all workers. Later, since the one
-    // that resolved the call was the one in position 1, all subsequent calls are
-    // only redirected to that worker.
+    // The first time, a call with a "1234567890abcdef" hash had never been
+    // done earlier ("foo" call), so it got queued to all workers. Later, since
+    // the one that resolved the call was the one in position 1, all subsequent
+    // calls are only redirected to that worker.
     expect(callback).toHaveBeenCalledTimes(2); // Only "foo".
     expect(callback).toHaveBeenNthCalledWith(
       1,
@@ -248,11 +249,11 @@ describe('Farm', () => {
     workerReply(1, null, 17);
     await p1;
 
-    // Both requests are send to the same worker
-    // The first time, a call with a "1234567890abcdef" hash had never been done
-    // earlier ("foo" call), so it got queued to all workers. Later, since the one
-    // that resolved the call was the one in position 1, all subsequent calls are
-    // only redirected to that worker.
+    // Both requests are send to the same worker.  The first time, a call with
+    // a "1234567890abcdef" hash had never been done earlier ("foo" call), so
+    // it got queued to all workers. Later, since the one that resolved the
+    // call was the one in position 1, all subsequent calls are only redirected
+    // to that worker.
     expect(callback).toHaveBeenCalledTimes(2);
     expect(callback).toHaveBeenNthCalledWith(
       1,
@@ -268,5 +269,68 @@ describe('Farm', () => {
       expect.any(Function),
       expect.any(Function),
     );
+  });
+
+  it('checks that locking works, and jobs are never lost', async () => {
+    const hash = jest
+      .fn()
+      // This will go to both queues, but picked by the first worker.
+      .mockReturnValueOnce(0)
+      // This will go to both queues too, but picked by the second worker.
+      .mockReturnValueOnce(1)
+      // This will go to worker 0, now only assigned to it.
+      .mockReturnValueOnce(0)
+      // This will go to worker 1, now only assigned to it.
+      .mockReturnValueOnce(1)
+      // This will go to both queues too, but will wait, since workers are busy.
+      .mockReturnValueOnce(2)
+      // This will only go to the first queue.
+      .mockReturnValueOnce(0)
+      // This will be gone if the queue implementation is wrong.
+      .mockReturnValueOnce(0)
+      // Push onto the second queue; potentially wiping the earlier job.
+      .mockReturnValueOnce(1);
+
+    const farm = new Farm(2, callback, hash);
+
+    // First and second jobs get resolved, so that their hash is sticked to
+    // the right worker: worker assignment is performed when workers reply, not
+    // when the call is made.
+    const p0 = farm.doWork('work-0');
+    const p1 = farm.doWork('work-1');
+    workerReply(0, null, 'response-0');
+    await p0;
+    workerReply(1, null, 'response-1');
+    await p1;
+
+    // Now we perform the rest of the calls (7 resolves before 5 and 6, since 4
+    // is in both queues, and as soon as you resolve 4, 7 will be picked).
+    const p2 = farm.doWork('work-2');
+    const p3 = farm.doWork('work-3');
+    const p4 = farm.doWork('work-4');
+    const p5 = farm.doWork('work-5');
+    const p6 = farm.doWork('work-6');
+    const p7 = farm.doWork('work-7');
+    workerReply(2, null, 'response-2');
+    await p2;
+    workerReply(3, null, 'response-3');
+    await p3;
+    workerReply(4, null, 'response-4');
+    await p4;
+    workerReply(5, null, 'response-7');
+    await p7;
+    workerReply(6, null, 'response-5');
+    await p5;
+    workerReply(7, null, 'response-6');
+    await p6;
+
+    await expect(p0).resolves.toBe('response-0');
+    await expect(p1).resolves.toBe('response-1');
+    await expect(p2).resolves.toBe('response-2');
+    await expect(p3).resolves.toBe('response-3');
+    await expect(p4).resolves.toBe('response-4');
+    await expect(p5).resolves.toBe('response-5');
+    await expect(p6).resolves.toBe('response-6');
+    await expect(p7).resolves.toBe('response-7');
   });
 });
