e5469ce6031b16562dab41988cbd43a8fb9cf1a1
Matt Phillips <matt@mattphillips.io>
2019-02-27 22:42:02 +0000

    Migrate js src to ts

--- packages/jest-each/src/index.ts
+++ packages/jest-each/src/index.ts
@@ -4,29 +4,19 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow
  */
+
+type Global = NodeJS.Global;
 
 import bind from './bind';
 
-type GlobalCallbacks = {
-  test(title: string, test: Function): void,
-  xtest(title: string, test: Function): void,
-  it(title: string, test: Function): void,
-  fit(title: string, test: Function): void,
-  xit(title: string, test: Function): void,
-  describe(title: string, test: Function): void,
-  fdescribe(title: string, test: Function): void,
-  xdescribe(title: string, test: Function): void,
-};
-
-const install = (g: GlobalCallbacks, ...args: Array<mixed>) => {
-  const test = (title: string, test: Function, timeout: number) =>
+const install = (g: Global, ...args: Array<any>) => {
+  const test = (title: string, test: Function, timeout?: number) =>
     bind(g.test)(...args)(title, test, timeout);
   test.skip = bind(g.test.skip)(...args);
   test.only = bind(g.test.only)(...args);
 
-  const it = (title: string, test: Function, timeout: number) =>
+  const it = (title: string, test: Function, timeout?: number) =>
     bind(g.it)(...args)(title, test, timeout);
   it.skip = bind(g.it.skip)(...args);
   it.only = bind(g.it.only)(...args);
@@ -35,7 +25,7 @@ const install = (g: GlobalCallbacks, ...args: Array<mixed>) => {
   const fit = bind(g.fit)(...args);
   const xtest = bind(g.xtest)(...args);
 
-  const describe = (title: string, suite: Function, timeout: number) =>
+  const describe = (title: string, suite: Function, timeout?: number) =>
     bind(g.describe, false)(...args)(title, suite, timeout);
   describe.skip = bind(g.describe.skip, false)(...args);
   describe.only = bind(g.describe.only, false)(...args);
@@ -45,10 +35,9 @@ const install = (g: GlobalCallbacks, ...args: Array<mixed>) => {
   return {describe, fdescribe, fit, it, test, xdescribe, xit, xtest};
 };
 
-const each = (...args: Array<mixed>) => install(global, ...args);
+const each = (...args: Array<any>) => install(global, ...args);
 
-each.withGlobal = (g: GlobalCallbacks) => (...args: Array<mixed>) =>
-  install(g, ...args);
+each.withGlobal = (g: Global) => (...args: Array<any>) => install(g, ...args);
 
 export {bind};
 
