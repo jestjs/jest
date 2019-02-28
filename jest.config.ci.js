6b43534524dc37af2d31234ad7fa7daa16953d84
Simen Bekkhus <sbekkhus91@gmail.com>
2019-03-01 00:04:38 +0100

    normal reporter?

--- jest.config.ci.js
+++ jest.config.ci.js
@@ -4,9 +4,5 @@
 
 // Object spread is just node 8
 module.exports = Object.assign({}, require('./jest.config'), {
-  coverageReporters: ['json'],
-  reporters: [
-    ['jest-junit', {output: 'reports/junit/js-test-results.xml'}],
-    ['jest-silent-reporter', {useDots: true}],
-  ],
+  coverageReporters: ['json']
 });
