5b5c32697e060bcbfd3421bcb7670cd7fb33f9bf
Simen Bekkhus <sbekkhus91@gmail.com>
2019-03-01 00:40:51 +0100

    no cache?

--- jest.config.ci.js
+++ jest.config.ci.js
@@ -4,5 +4,9 @@
 
 // Object spread is just node 8
 module.exports = Object.assign({}, require('./jest.config'), {
-  coverageReporters: ['json']
+  coverageReporters: ['json'],
+  reporters: [
+    ['jest-junit', {output: 'reports/junit/js-test-results.xml'}],
+    ['jest-silent-reporter', {useDots: true}],
+  ],
 });
