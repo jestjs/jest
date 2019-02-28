0a6480c00f0ccf47b68df0e24c13f7669f61d33c
Nicolas DUBIEN <github@dubien.org>
2019-02-28 02:32:20 +0100

    Switch to built-in fc.anything()

--- packages/expect/src/__tests__/matchers.property.test.js
+++ packages/expect/src/__tests__/matchers.property.test.js
@@ -17,21 +17,8 @@ const anythingSettings = {
   withSet: false,
 };
 
-// generate anything: from primitives to objects
-const customAnything = () =>
-  fc.oneof(
-    // generate anything: from primitives to objects
-    fc.anything(anythingSettings),
-    // generate object - increase the share of object in generated values
-    fc.object(anythingSettings),
-    // generate array - increase the share of array in generated values
-    fc.array(fc.anything(anythingSettings)),
-    // generate array - increase the share of array in generated values
-    fc.array(fc.object(anythingSettings)),
-  );
-
 // assertion settings
-const assertSettings = {};
+const assertSettings = {}; // eg.: {numRuns: 10000}
 
 beforeAll(() => {
   chalk.enabled = true;
@@ -42,36 +29,33 @@ afterAll(() => {
 });
 
 describe('.toStrictEqual()', () => {
-  const safeExpectStrictEqual = (a, b) => {
-    try {
-      expect(a).toEqual(b);
-      return true;
-    } catch (err) {
-      return false;
-    }
-  };
-  test('should distinguish distinct values', () => {
+  it('should distinguish distinct values', () => {
     fc.assert(
-      fc.property(customAnything(), customAnything(), (a, b) => {
-        // Given:  a and b values such as a different from b
-        // Assert: We expect `expect(a).not.toStrictEqual(b)`
-        const replacer = (k, v) => {
-          if (v === undefined) return '<undefined>';
-          if (v instanceof Set)
-            return `new Set([${JSON.stringify(Array.from(v), replacer)}])`;
-          if (v instanceof Map)
-            return `new Map([${JSON.stringify(Array.from(v), replacer)}])`;
-          return v;
-        };
-        fc.pre(JSON.stringify(a, replacer) !== JSON.stringify(b, replacer));
-        expect(a).not.toStrictEqual(b);
-      }),
+      fc.property(
+        fc.anything(anythingSettings),
+        fc.anything(anythingSettings),
+        (a, b) => {
+          // Given:  a and b values such as a different from b
+          // Assert: We expect `expect(a).not.toStrictEqual(b)`
+          const replacer = (k, v) => {
+            if (v === undefined) return '<undefined>';
+            if (v instanceof Set)
+              return `new Set([${JSON.stringify(Array.from(v), replacer)}])`;
+            if (v instanceof Map)
+              return `new Map([${JSON.stringify(Array.from(v), replacer)}])`;
+            return v;
+          };
+          fc.pre(JSON.stringify(a, replacer) !== JSON.stringify(b, replacer));
+          expect(a).not.toStrictEqual(b);
+        },
+      ),
       assertSettings,
     );
   });
-  test('should be reflexive', () => {
+
+  it('should be reflexive', () => {
     fc.assert(
-      fc.property(fc.dedup(customAnything(), 2), ([a, b]) => {
+      fc.property(fc.dedup(fc.anything(anythingSettings), 2), ([a, b]) => {
         // Given:  a and b identical values
         // Assert: We expect `expect(a).toStrictEqual(b)`
         expect(a).toStrictEqual(b);
@@ -79,31 +63,36 @@ describe('.toStrictEqual()', () => {
       assertSettings,
     );
   });
-  test('should be symmetric', () => {
+
+  it('should be symmetric', () => {
+    const safeExpectStrictEqual = (a, b) => {
+      try {
+        expect(a).toEqual(b);
+        return true;
+      } catch (err) {
+        return false;
+      }
+    };
     fc.assert(
-      fc.property(customAnything(), customAnything(), (a, b) => {
-        // Given:  a and b values
-        // Assert: We expect `expect(a).toStrictEqual(b)`
-        //         to be equivalent to `expect(b).toStrictEqual(a)`
-        expect(safeExpectStrictEqual(a, b)).toBe(safeExpectStrictEqual(b, a));
-      }),
+      fc.property(
+        fc.anything(anythingSettings),
+        fc.anything(anythingSettings),
+        (a, b) => {
+          // Given:  a and b values
+          // Assert: We expect `expect(a).toStrictEqual(b)`
+          //         to be equivalent to `expect(b).toStrictEqual(a)`
+          expect(safeExpectStrictEqual(a, b)).toBe(safeExpectStrictEqual(b, a));
+        },
+      ),
       assertSettings,
     );
   });
 });
 
 describe('.toEqual()', () => {
-  const safeExpectEqual = (a, b) => {
-    try {
-      expect(a).toEqual(b);
-      return true;
-    } catch (err) {
-      return false;
-    }
-  };
-  test('should be reflexive', () => {
+  it('should be reflexive', () => {
     fc.assert(
-      fc.property(fc.dedup(customAnything(), 2), ([a, b]) => {
+      fc.property(fc.dedup(fc.anything(anythingSettings), 2), ([a, b]) => {
         // Given:  a and b identical values
         // Assert: We expect `expect(a).toEqual(b)`
         expect(a).toEqual(b);
@@ -111,26 +100,39 @@ describe('.toEqual()', () => {
       assertSettings,
     );
   });
-  test('should be symmetric', () => {
+
+  it('should be symmetric', () => {
+    const safeExpectEqual = (a, b) => {
+      try {
+        expect(a).toEqual(b);
+        return true;
+      } catch (err) {
+        return false;
+      }
+    };
     fc.assert(
-      fc.property(customAnything(), customAnything(), (a, b) => {
-        // Given: a and b values
-        // Assert: We expect `expect(a).toEqual(b)`
-        //         to be equivalent to `expect(b).toEqual(a)`
-        expect(safeExpectEqual(a, b)).toBe(safeExpectEqual(b, a));
-      }),
+      fc.property(
+        fc.anything(anythingSettings),
+        fc.anything(anythingSettings),
+        (a, b) => {
+          // Given: a and b values
+          // Assert: We expect `expect(a).toEqual(b)`
+          //         to be equivalent to `expect(b).toEqual(a)`
+          expect(safeExpectEqual(a, b)).toBe(safeExpectEqual(b, a));
+        },
+      ),
       assertSettings,
     );
   });
 });
 
 describe('.toContain()', () => {
-  test('should always find the value when inside the array', () => {
+  it('should always find the value when inside the array', () => {
     fc.assert(
       fc.property(
-        fc.array(customAnything()),
-        fc.array(customAnything()),
-        customAnything().filter(v => !Number.isNaN(v)),
+        fc.array(fc.anything(anythingSettings)),
+        fc.array(fc.anything(anythingSettings)),
+        fc.anything(anythingSettings).filter(v => !Number.isNaN(v)),
         (startValues, endValues, v) => {
           // Given:  startValues, endValues arrays and v value (not NaN)
           // Assert: We expect `expect([...startValues, v, ...endValues]).toContainEqual(v)`
@@ -140,12 +142,13 @@ describe('.toContain()', () => {
       assertSettings,
     );
   });
-  test('should not find the value if it has been cloned into the array', () => {
+
+  it('should not find the value if it has been cloned into the array', () => {
     fc.assert(
       fc.property(
-        fc.array(customAnything()),
-        fc.array(customAnything()),
-        fc.dedup(customAnything(), 2),
+        fc.array(fc.anything(anythingSettings)),
+        fc.array(fc.anything(anythingSettings)),
+        fc.dedup(fc.anything(anythingSettings), 2),
         (startValues, endValues, [a, b]) => {
           // Given:  startValues, endValues arrays
           //         and [a, b] identical values
@@ -161,12 +164,12 @@ describe('.toContain()', () => {
 });
 
 describe('.toContainEqual()', () => {
-  test('should always find the value when inside the array', () => {
+  it('should always find the value when inside the array', () => {
     fc.assert(
       fc.property(
-        fc.array(customAnything()),
-        fc.array(customAnything()),
-        customAnything(),
+        fc.array(fc.anything(anythingSettings)),
+        fc.array(fc.anything(anythingSettings)),
+        fc.anything(anythingSettings),
         (startValues, endValues, v) => {
           // Given:  startValues, endValues arrays and v any value
           // Assert: We expect `expect([...startValues, v, ...endValues]).toContainEqual(v)`
@@ -176,12 +179,13 @@ describe('.toContainEqual()', () => {
       assertSettings,
     );
   });
-  test('should always find the value when cloned inside the array', () => {
+
+  it('should always find the value when cloned inside the array', () => {
     fc.assert(
       fc.property(
-        fc.array(customAnything()),
-        fc.array(customAnything()),
-        fc.dedup(customAnything(), 2),
+        fc.array(fc.anything(anythingSettings)),
+        fc.array(fc.anything(anythingSettings)),
+        fc.dedup(fc.anything(anythingSettings), 2),
         (startValues, endValues, [a, b]) => {
           // Given:  startValues, endValues arrays
           //         and [a, b] identical values
