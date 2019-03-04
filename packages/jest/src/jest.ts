fe83d6416c2f49123417b9bdbc7898025f2fdd01
Simen Bekkhus <sbekkhus91@gmail.com>
2019-03-05 00:34:12 +0100

    chore: do jest package as well

--- packages/jest/src/jest.ts
+++ packages/jest/src/jest.ts
@@ -3,10 +3,8 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- *
- * @flow
  */
 
 import * as cli from 'jest-cli';
 
-module.exports = cli;
+export = cli;
