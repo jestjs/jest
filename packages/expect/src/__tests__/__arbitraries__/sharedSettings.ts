b63f1c8acd2af12bd60cb5f3d941cf9be226d227
Nicolas DUBIEN <github@dubien.org>
2019-03-19 21:52:06 +0100

    Bump to the latest version of fast-check
    It should provide a better memory footprint.
    
    Previously the default value of maxKeys was 10 (not accessible by the end-user in previous version).
    The value has been reduced to 5 as it should already be enough to detect most of problems related to objects.

--- packages/expect/src/__tests__/__arbitraries__/sharedSettings.ts
+++ packages/expect/src/__tests__/__arbitraries__/sharedSettings.ts
@@ -11,6 +11,8 @@ import fc from 'fast-check';
 // settings for anything arbitrary
 export const anythingSettings = {
   key: fc.oneof(fc.string(), fc.constantFrom('k1', 'k2', 'k3')),
+  maxDepth: 2, // Limit object depth (default: 2)
+  maxKeys: 5, // Limit number of keys per object (default: 5)
   withBoxedValues: true,
   // Issue #7975 have to be fixed before enabling the generation of Map
   withMap: false,
