b602e2db427e098d71e05dc8220278a2df72967c
Nicolas DUBIEN <github@dubien.org>
2019-03-19 21:40:16 +0100

    Revert "Revert "Add fuzzing based tests in Jest (#8012)""
    This reverts commit 13b44127d5e5088b72e913c9236f0b7b474a5eff.

---
+++ packages/expect/src/__tests__/__arbitraries__/sharedSettings.ts
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import fc from 'fast-check';
+
+// settings for anything arbitrary
+export const anythingSettings = {
+  key: fc.oneof(fc.string(), fc.constantFrom('k1', 'k2', 'k3')),
+  withBoxedValues: true,
+  // Issue #7975 have to be fixed before enabling the generation of Map
+  withMap: false,
+  // Issue #7975 have to be fixed before enabling the generation of Set
+  withSet: false,
+};
+
+// assertion settings
+export const assertSettings = {}; // eg.: {numRuns: 10000}
