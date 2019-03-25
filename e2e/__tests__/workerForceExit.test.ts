a696764b75ebc658ce7bf009603dc3f45b2e3af2
Tim Seckinger <seckinger.tim@gmail.com>
2019-03-25 23:38:21 +0100

    workerForceExit e2e test

---
+++ e2e/__tests__/workerForceExit.test.ts
@@ -0,0 +1,62 @@
+import {tmpdir} from 'os';
+import {resolve} from 'path';
+import findProcess from 'find-process';
+
+import {
+  cleanup,
+  generateTestFilesToForceUsingWorkers,
+  writeFiles,
+} from '../Utils';
+import runJest from '../runJest';
+
+const DIR = resolve(tmpdir(), 'worker-force-exit');
+
+beforeEach(() => cleanup(DIR));
+const testFiles = {
+  ...generateTestFilesToForceUsingWorkers(),
+  'package.json': `{
+      "testEnvironment": "node"
+  }`,
+};
+afterEach(() => cleanup(DIR));
+
+const verifyNumPassed = stderr => {
+  const numberOfTestsPassed = (stderr.match(/\bPASS\b/g) || []).length;
+  // assuming -1 because of package.json, but +1 because of the individual test file
+  expect(numberOfTestsPassed).toBe(Object.keys(testFiles).length);
+};
+
+test('prints a warning if a worker is force exited', () => {
+  writeFiles(DIR, {
+    ...testFiles,
+    '__tests__/simple.test.js': `
+      test('t', () => {
+        require('http').createServer().listen(0);
+      });
+    `,
+  });
+  const {status, stderr, stdout} = runJest(DIR, ['--maxWorkers=2']);
+
+  expect(status).toBe(0);
+  verifyNumPassed(stderr);
+  expect(stdout).toContain('A worker process has failed to exit gracefully');
+});
+
+test('force exits a worker that fails to exit gracefully', async () => {
+  writeFiles(DIR, {
+    ...testFiles,
+    '__tests__/timeoutKilled.test.js': `
+      test('t', () => {
+        require('http').createServer().listen(0);
+        console.error('pid: ' + process.pid);
+      });
+    `,
+  });
+  const {status, stderr} = runJest(DIR, ['--maxWorkers=2']);
+
+  expect(status).toBe(0);
+  verifyNumPassed(stderr);
+
+  const [pid] = /pid: \d+/.exec(stderr);
+  expect(await findProcess('pid', pid)).toHaveLength(0);
+});
