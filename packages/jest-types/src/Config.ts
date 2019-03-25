041a81d869ede7488e9214b2ebcd84f2f84ee5a4
Alcedo Nathaniel De Guzman Jr <natealcedo@gmail.com>
2019-03-25 19:36:22 +0800

    Update ProjectConfig

--- packages/jest-types/src/Config.ts
+++ packages/jest-types/src/Config.ts
@@ -367,7 +367,7 @@ export type ProjectConfig = {
   dependencyExtractor?: string;
   detectLeaks: boolean;
   detectOpenHandles: boolean;
-  displayName?: DisplayName;
+  displayName?: DisplayName | null | undefined;
   errorOnDeprecated: boolean;
   extraGlobals: Array<keyof NodeJS.Global>;
   filter: Path | null | undefined;
