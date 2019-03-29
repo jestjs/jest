8413503a821bfc7b123a09374182534652459353
Scott Hovestadt <scotthovestadt@fb.com>
2019-03-28 18:35:54 -0700

    Restore accidental deletion.

--- packages/jest-runner/src/runTest.ts
+++ packages/jest-runner/src/runTest.ts
@@ -253,6 +253,7 @@ async function runTestInternal(
 
     result.perfStats = {end: Date.now(), start};
     result.testFilePath = path;
+    result.console = testConsole.getBuffer();
     result.skipped = testCount === result.numPendingTests;
     result.displayName = config.displayName;
 
