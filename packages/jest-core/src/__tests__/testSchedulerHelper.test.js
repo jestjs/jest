194535618abb5fc17deac34288efdaa23c8be597
Tim Seckinger <seckinger.tim@gmail.com>
2019-04-07 17:22:37 +0200

    pass config into shouldRunInBand

--- packages/jest-core/src/__tests__/testSchedulerHelper.test.js
+++ packages/jest-core/src/__tests__/testSchedulerHelper.test.js
@@ -23,22 +23,22 @@ const getTestMock = () => ({
 const getTestsMock = () => [getTestMock(), getTestMock()];
 
 test.each`
-  tests              | watch    | maxWorkers   | timings        | expectedResult
-  ${[getTestMock()]} | ${true}  | ${undefined} | ${[500, 500]}  | ${true}
-  ${getTestsMock()}  | ${true}  | ${1}         | ${[2000, 500]} | ${true}
-  ${getTestsMock()}  | ${true}  | ${2}         | ${[2000, 500]} | ${false}
-  ${[getTestMock()]} | ${true}  | ${undefined} | ${[2000, 500]} | ${false}
-  ${getTestMock()}   | ${true}  | ${undefined} | ${[500, 500]}  | ${false}
-  ${getTestsMock()}  | ${false} | ${1}         | ${[2000, 500]} | ${true}
-  ${getTestMock()}   | ${false} | ${2}         | ${[2000, 500]} | ${false}
-  ${[getTestMock()]} | ${false} | ${undefined} | ${[2000]}      | ${true}
-  ${getTestsMock()}  | ${false} | ${undefined} | ${[500, 500]}  | ${true}
-  ${new Array(45)}   | ${false} | ${undefined} | ${[500]}       | ${false}
-  ${getTestsMock()}  | ${false} | ${undefined} | ${[2000, 500]} | ${false}
+  tests              | timings        | maxWorkers   | watch    | expectedResult
+  ${[getTestMock()]} | ${[500, 500]}  | ${undefined} | ${true}  | ${true}
+  ${getTestsMock()}  | ${[2000, 500]} | ${1}         | ${true}  | ${true}
+  ${getTestsMock()}  | ${[2000, 500]} | ${2}         | ${true}  | ${false}
+  ${[getTestMock()]} | ${[2000, 500]} | ${undefined} | ${true}  | ${false}
+  ${getTestMock()}   | ${[500, 500]}  | ${undefined} | ${true}  | ${false}
+  ${getTestsMock()}  | ${[2000, 500]} | ${1}         | ${false} | ${true}
+  ${getTestMock()}   | ${[2000, 500]} | ${2}         | ${false} | ${false}
+  ${[getTestMock()]} | ${[2000]}      | ${undefined} | ${false} | ${true}
+  ${getTestsMock()}  | ${[500, 500]}  | ${undefined} | ${false} | ${true}
+  ${new Array(45)}   | ${[500]}       | ${undefined} | ${false} | ${false}
+  ${getTestsMock()}  | ${[2000, 500]} | ${undefined} | ${false} | ${false}
 `(
   'shouldRunInBand() - should return $expectedResult for runInBand mode',
-  ({tests, watch, maxWorkers, timings, expectedResult}) => {
-    expect(shouldRunInBand(tests, watch, maxWorkers, timings)).toBe(
+  ({tests, timings, maxWorkers, watch, expectedResult}) => {
+    expect(shouldRunInBand(tests, timings, {maxWorkers, watch})).toBe(
       expectedResult,
     );
   },
--- packages/jest-core/src/testSchedulerHelper.ts
+++ packages/jest-core/src/testSchedulerHelper.ts
@@ -5,17 +5,17 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import {Config} from '@jest/types';
 import {Test} from 'jest-runner';
 
 const SLOW_TEST_TIME = 1000;
 
 export function shouldRunInBand(
   tests: Array<Test>,
-  isWatchMode: boolean,
-  maxWorkers: number,
   timings: Array<number>,
+  {maxWorkers, watch, watchAll}: Config.GlobalConfig,
 ) {
-  /**
+  /*
    * Run in band if we only have one test or one worker available, unless we
    * are using the watch mode, in which case the TTY has to be responsive and
    * we cannot schedule anything in the main thread. Same logic applies to
@@ -26,6 +26,7 @@ export function shouldRunInBand(
    * force running in band.
    * https://github.com/facebook/jest/blob/700e0dadb85f5dc8ff5dac6c7e98956690049734/packages/jest-config/src/getMaxWorkers.js#L14-L17
    */
+  const isWatchMode = watch || watchAll;
   const areFastTests = timings.every(timing => timing < SLOW_TEST_TIME);
   const oneWorkerOrLess = maxWorkers <= 1;
   const oneTestOrLess = tests.length <= 1;
