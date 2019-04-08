2fc468c5fb236b081c081b90735af89f410bb79f
Mark Pedrotti <pedrottimark@gmail.com>
2019-04-08 15:23:30 -0400

    expect: Improve report when matcher fails, part 15 (#8281)
    * expect: Improve report when matcher fail, part 15
    
    * Update CHANGELOG.md
    
    * Rename isShallowInequality, refactor deepEqualityName, edit and move message
    
    * Rename toStrictEqualTesters
    
    * Separate deepEqualityName from hasConciseReport
    
    * Add 2 snapshot tests for date
    
    * Update packages/expect/src/matchers.ts
    
    Co-Authored-By: pedrottimark <pedrottimark@gmail.com>
    
    * Update 6 snapshots for improved deep equality message

--- packages/expect/src/matchers.ts
+++ packages/expect/src/matchers.ts
@@ -8,9 +8,9 @@
 
 import getType, {isPrimitive} from 'jest-get-type';
 import {
+  DIM_COLOR,
   EXPECTED_COLOR,
   RECEIVED_COLOR,
-  SUGGEST_TO_EQUAL,
   SUGGEST_TO_CONTAIN_EQUAL,
   diff,
   ensureExpectedIsNonNegativeInteger,
@@ -22,6 +22,7 @@ import {
   printReceived,
   printExpected,
   printWithType,
+  stringify,
   MatcherHintOptions,
 } from 'jest-matcher-utils';
 import {MatchersObject, MatcherState} from './types';
@@ -41,6 +42,12 @@ import {
 } from './utils';
 import {equals} from './jasmineUtils';
 
+const toStrictEqualTesters = [
+  iterableEquality,
+  typeEquality,
+  sparseArrayEquality,
+];
+
 type ContainIterable =
   | Array<unknown>
   | Set<unknown>
@@ -50,10 +57,11 @@ type ContainIterable =
 
 const matchers: MatchersObject = {
   toBe(this: MatcherState, received: unknown, expected: unknown) {
-    const matcherName = '.toBe';
+    const matcherName = 'toBe';
     const options: MatcherHintOptions = {
       comment: 'Object.is equality',
       isNot: this.isNot,
+      promise: this.promise,
     };
 
     const pass = Object.is(received, expected);
@@ -62,32 +70,57 @@ const matchers: MatchersObject = {
       ? () =>
           matcherHint(matcherName, undefined, undefined, options) +
           '\n\n' +
-          `Expected: ${printExpected(expected)}\n` +
-          `Received: ${printReceived(received)}`
+          `Expected: not ${printExpected(expected)}`
       : () => {
           const receivedType = getType(received);
           const expectedType = getType(expected);
-          const suggestToEqual =
-            receivedType === expectedType &&
-            (receivedType === 'object' || expectedType === 'array') &&
-            equals(received, expected, [iterableEquality]);
-          const oneline = isOneline(expected, received);
-          const diffString = diff(expected, received, {expand: this.expand});
+
+          let deepEqualityName = null;
+          if (expectedType !== 'map' && expectedType !== 'set') {
+            // If deep equality passes when referential identity fails,
+            // but exclude map and set until review of their equality logic.
+            if (equals(received, expected, toStrictEqualTesters, true)) {
+              deepEqualityName = 'toStrictEqual';
+            } else if (equals(received, expected, [iterableEquality])) {
+              deepEqualityName = 'toEqual';
+            }
+          }
+
+          const hasConciseReport =
+            receivedType !== expectedType ||
+            (isPrimitive(expected) &&
+              (expectedType !== 'string' || isOneline(expected, received))) ||
+            expectedType === 'date' ||
+            expectedType === 'function' ||
+            expectedType === 'regexp' ||
+            (received instanceof Error && expected instanceof Error);
+          const hasDifference = stringify(expected) !== stringify(received);
+          const difference =
+            !hasConciseReport && hasDifference
+              ? diff(expected, received, {expand: this.expand}) // string | null
+              : null;
 
           return (
             matcherHint(matcherName, undefined, undefined, options) +
             '\n\n' +
-            `Expected: ${printExpected(expected)}\n` +
-            `Received: ${printReceived(received)}` +
-            (diffString && !oneline ? `\n\nDifference:\n\n${diffString}` : '') +
-            (suggestToEqual ? ` ${SUGGEST_TO_EQUAL}` : '')
+            (deepEqualityName !== null
+              ? DIM_COLOR(
+                  `If it should pass with deep equality, replace "${matcherName}" with "${deepEqualityName}"`,
+                ) + '\n\n'
+              : '') +
+            (difference !== null
+              ? difference
+              : `Expected: ${printExpected(expected)}\n` +
+                (hasDifference
+                  ? `Received: ${printReceived(received)}`
+                  : 'Received value has no visual difference'))
           );
         };
 
     // Passing the actual and expected objects so that a custom reporter
     // could access them, for example in order to display a custom visual diff,
     // or create a different error message
-    return {actual: received, expected, message, name: 'toBe', pass};
+    return {actual: received, expected, message, name: matcherName, pass};
   },
 
   toBeCloseTo(
@@ -856,12 +889,7 @@ const matchers: MatchersObject = {
       isNot: this.isNot,
     };
 
-    const pass = equals(
-      received,
-      expected,
-      [iterableEquality, typeEquality, sparseArrayEquality],
-      true,
-    );
+    const pass = equals(received, expected, toStrictEqualTesters, true);
 
     const message = pass
       ? () =>
