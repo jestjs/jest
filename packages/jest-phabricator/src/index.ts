fa6c9ee69dcec4a86e04f0b136eb84ada1ecd345
Scott Hovestadt <scotthovestadt@fb.com>
2019-04-21 12:44:46 -0700

    Resolve lint errors on jest-phabricator example code.

--- packages/jest-phabricator/src/index.ts
+++ packages/jest-phabricator/src/index.ts
@@ -1,9 +1,4 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+// Copyright 2004-present Facebook. All Rights Reserved.
 
 import {AggregatedResult} from '@jest/test-result';
 
