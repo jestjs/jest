ebc9fe80c36449692f84155c6c4f73910bf987ec
Simen Bekkhus <sbekkhus91@gmail.com>
2019-04-22 13:19:38 +0200

    add script to remove postinstall

---
+++ scripts/remove-postinstall.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const {writeFileSync} = require('fs');
+
+const pkgPath = require.resolve('../package.json');
+
+const pkg = require('../package.json');
+
+delete pkg.scripts.postinstall;
+
+writeFileSync(pkgPath, `${JSON.stringify(pkg, null, 2)}\n`);
