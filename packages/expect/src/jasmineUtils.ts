043092c631272f233259d2a1f4367f5d7ab903d6
Mihail Bodrov <bodrovmiha@yandex.ru>
2019-04-24 01:25:52 +0300

    [expect] optimize compare nodes :art:

--- packages/expect/src/jasmineUtils.ts
+++ packages/expect/src/jasmineUtils.ts
@@ -127,9 +127,8 @@ function eq(
   }
 
   var aIsDomNode = isDomNode(a);
-  var bIsDomNode = isDomNode(b);
   // Use DOM3 method isEqualNode (IE>=9)
-  if (aIsDomNode && typeof a.isEqualNode === 'function' && bIsDomNode) {
+  if (aIsDomNode && isDomNode(b)) {
     return a.isEqualNode(b);
   }
 
