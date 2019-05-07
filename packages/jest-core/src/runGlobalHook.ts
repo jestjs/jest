dd4e8958ebfae6902f9f78c1b47aa2ed29fc6db3
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-05-06 22:41:57 -0400

    [jest-core](runGlobalHook.ts)-Fix p-each-series types, and fix usage.

--- packages/jest-core/src/runGlobalHook.ts
+++ packages/jest-core/src/runGlobalHook.ts
@@ -6,12 +6,17 @@
  */
 
 import {extname} from 'path';
-import pEachSeries from 'p-each-series';
 import {addHook} from 'pirates';
 import {Config} from '@jest/types';
 import {Test} from 'jest-runner';
 import {ScriptTransformer} from '@jest/transform';
 import {interopRequireDefault} from 'jest-util';
+
+// Needed , revert to normal import when drop node 6 and upgrade p-each-series 2.1.0
+const pEachSeries = require('p-each-series') as <ValueType>(
+  input: Iterable<PromiseLike<ValueType> | ValueType>,
+  iterator: (element: ValueType, index: number) => unknown,
+) => Promise<Array<ValueType>>;
 
 export default async ({
   allTests,
@@ -31,63 +36,66 @@ export default async ({
   }
 
   if (globalModulePaths.size > 0) {
-    await pEachSeries(Array.from(globalModulePaths), async modulePath => {
-      if (!modulePath) {
-        return;
-      }
+    await pEachSeries(
+      Array.from(globalModulePaths),
+      async (modulePath: string | null | undefined) => {
+        if (!modulePath) {
+          return;
+        }
 
-      const correctConfig = allTests.find(
-        t => t.context.config[moduleName] === modulePath,
-      );
-
-      const projectConfig = correctConfig
-        ? correctConfig.context.config
-        : // Fallback to first config
-          allTests[0].context.config;
-
-      const transformer = new ScriptTransformer(projectConfig);
-
-      // Load the transformer to avoid a cycle where we need to load a
-      // transformer in order to transform it in the require hooks
-      transformer.preloadTransformer(modulePath);
-
-      let transforming = false;
-      const revertHook = addHook(
-        (code, filename) => {
-          try {
-            transforming = true;
-            return (
-              transformer.transformSource(filename, code, false).code || code
-            );
-          } finally {
-            transforming = false;
-          }
-        },
-        {
-          exts: [extname(modulePath)],
-          ignoreNodeModules: false,
-          matcher: (...args) => {
-            if (transforming) {
-              // Don't transform any dependency required by the transformer itself
-              return false;
-            }
-            return transformer.shouldTransform(...args);
-          },
-        },
-      );
-
-      const globalModule = interopRequireDefault(require(modulePath)).default;
-
-      if (typeof globalModule !== 'function') {
-        throw new TypeError(
-          `${moduleName} file must export a function at ${modulePath}`,
+        const correctConfig = allTests.find(
+          t => t.context.config[moduleName] === modulePath,
         );
-      }
 
-      await globalModule(globalConfig);
+        const projectConfig = correctConfig
+          ? correctConfig.context.config
+          : // Fallback to first config
+            allTests[0].context.config;
 
-      revertHook();
-    });
+        const transformer = new ScriptTransformer(projectConfig);
+
+        // Load the transformer to avoid a cycle where we need to load a
+        // transformer in order to transform it in the require hooks
+        transformer.preloadTransformer(modulePath);
+
+        let transforming = false;
+        const revertHook = addHook(
+          (code, filename) => {
+            try {
+              transforming = true;
+              return (
+                transformer.transformSource(filename, code, false).code || code
+              );
+            } finally {
+              transforming = false;
+            }
+          },
+          {
+            exts: [extname(modulePath)],
+            ignoreNodeModules: false,
+            matcher: (...args) => {
+              if (transforming) {
+                // Don't transform any dependency required by the transformer itself
+                return false;
+              }
+              return transformer.shouldTransform(...args);
+            },
+          },
+        );
+
+        const globalModule = interopRequireDefault(require(modulePath)).default;
+
+        if (typeof globalModule !== 'function') {
+          throw new TypeError(
+            `${moduleName} file must export a function at ${modulePath}`,
+          );
+        }
+
+        await globalModule(globalConfig);
+
+        revertHook();
+      },
+    );
   }
 
   return Promise.resolve();
