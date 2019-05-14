6d9ed2c0a9d26cf84cc8a3ed67bb13c4e631beba
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-05-14 11:48:34 -0400

    update getPath with overloads

--- packages/jest-each/src/table/template.ts
+++ packages/jest-each/src/table/template.ts
@@ -70,8 +70,46 @@ const replaceKeyPathWithValue = (template: Template) => (
   return title.replace(match, pretty(value, {maxDepth: 1, min: true}));
 };
 
-const getPath = (template: Template, [head, ...tail]: Array<string>): any => {
+/* eslint import/export: 0*/
+export function getPath<
+  Obj extends Template,
+  A extends keyof Obj,
+  B extends keyof Obj[A],
+  C extends keyof Obj[A][B],
+  D extends keyof Obj[A][B][C],
+  E extends keyof Obj[A][B][C][D]
+>(obj: Obj, path: [A, B, C, D, E]): Obj[A][B][C][D][E];
+export function getPath<
+  Obj extends Template,
+  A extends keyof Obj,
+  B extends keyof Obj[A],
+  C extends keyof Obj[A][B],
+  D extends keyof Obj[A][B][C]
+>(obj: Obj, path: [A, B, C, D]): Obj[A][B][C][D];
+export function getPath<
+  Obj extends Template,
+  A extends keyof Obj,
+  B extends keyof Obj[A],
+  C extends keyof Obj[A][B]
+>(obj: Obj, path: [A, B, C]): Obj[A][B][C];
+export function getPath<
+  Obj extends Template,
+  A extends keyof Obj,
+  B extends keyof Obj[A]
+>(obj: Obj, path: [A, B]): Obj[A][B];
+export function getPath<Obj extends Template, A extends keyof Obj>(
+  obj: Obj,
+  path: [A],
+): Obj[A];
+export function getPath<Obj extends Template>(
+  obj: Obj,
+  path: Array<string>,
+): unknown;
+export function getPath(
+  template: Template,
+  [head, ...tail]: Array<string>,
+): unknown {
   if (!head || !template.hasOwnProperty || !template.hasOwnProperty(head))
     return template;
   return getPath(template[head] as Template, tail);
-};
+}
