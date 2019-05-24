95d6d5593c5f88027759ce3ce9e247211878a0da
Mark Pedrotti <pedrottimark@gmail.com>
2019-05-24 16:15:45 -0400

    Add copyright header to 2 added files

--- packages/jest-matcher-utils/src/printDiff.ts
+++ packages/jest-matcher-utils/src/printDiff.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import {Diff, DIFF_EQUAL, DIFF_DELETE, DIFF_INSERT} from './cleanupSemantic';
 import {INVERTED_COLOR} from './index';
 
