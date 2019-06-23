606ab64217e652383aa1bddb59da8cb0091d9ae7
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-06-23 17:52:27 +0300

    Fix lint issue

--- packages/jest-core/src/__tests__/getNoTestsFoundMessage.test.js
+++ packages/jest-core/src/__tests__/getNoTestsFoundMessage.test.js
@@ -1,3 +1,5 @@
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
 import getNoTestsFoundMessage from '../getNoTestsFoundMessage';
 
 describe('getNoTestsFoundMessage', () => {
