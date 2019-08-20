6eeff97f4cf36d33e1cdaa0e59b4f7d79f3a7d96
Simen Bekkhus <sbekkhus91@gmail.com>
2019-08-20 13:46:54 +0200

    chore: update snapshots to remove trailing newline

--- e2e/__tests__/supportsDashedArgs.ts
+++ e2e/__tests__/supportsDashedArgs.ts
@@ -45,15 +45,14 @@ test('throws error for unknown dashed & camelcase args', () => {
     '--useStderr',
   ]);
   expect(result.stderr).toMatchInlineSnapshot(`
-● Unrecognized CLI Parameters:
+    ● Unrecognized CLI Parameters:
 
-  Following options were not recognized:
-  ["doesNotExist", "also-does-not-exist"]
+      Following options were not recognized:
+      ["doesNotExist", "also-does-not-exist"]
 
-  CLI Options Documentation:
-  https://jestjs.io/docs/en/cli.html
+      CLI Options Documentation:
+      https://jestjs.io/docs/en/cli.html
 
-
-`);
+  `);
   expect(result.status).toBe(1);
 });
