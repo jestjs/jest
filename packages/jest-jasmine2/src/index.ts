b8a4a8559977a60b24b92036ab7eabf50c6f89ac
Simen Bekkhus <sbekkhus91@gmail.com>
2019-08-22 15:47:08 +0200

    chore: remove deprecated exports from jest-util (#8863)

--- packages/jest-jasmine2/src/index.ts
+++ packages/jest-jasmine2/src/index.ts
@@ -12,7 +12,7 @@ import {JestEnvironment} from '@jest/environment';
 import {SnapshotStateType} from 'jest-snapshot';
 import Runtime = require('jest-runtime');
 
-import {getCallsite} from 'jest-util';
+import {getCallsite} from '@jest/source-map';
 import installEach from './each';
 import {installErrorOnPrivate} from './errorOnPrivate';
 import JasmineReporter from './reporter';
