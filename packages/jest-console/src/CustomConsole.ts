aecc0aabd7b873ad5de488746a1eb2f11ba24077
Simen Bekkhus <sbekkhus91@gmail.com>
2019-08-26 08:36:56 +0200

    chore: import clearline rather than duplicating it

--- packages/jest-console/src/CustomConsole.ts
+++ packages/jest-console/src/CustomConsole.ts
@@ -9,14 +9,8 @@ import assert = require('assert');
 import {format} from 'util';
 import {Console} from 'console';
 import chalk from 'chalk';
+import {clearLine} from 'jest-util';
 import {LogCounters, LogMessage, LogTimers, LogType} from './types';
-
-// TODO: Copied from `jest-util`. Import from it in Jest 25
-function clearLine(stream: NodeJS.WritableStream & {isTTY?: boolean}) {
-  if (stream.isTTY) {
-    stream.write('\x1b[999D\x1b[K');
-  }
-}
 
 type Formatter = (type: LogType, message: LogMessage) => string;
 
