c6799c5da753f05f0c573840f0fbca0b67048306
Catherine Chanse <cchanse@frbsf.org>
2019-09-03 15:45:11 -0700

    fix: Allow --testURL as a valid CLI option

--- packages/jest-validate/src/validateCLIOptions.ts
+++ packages/jest-validate/src/validateCLIOptions.ts
@@ -80,6 +80,7 @@ export default function validateCLIOptions(
   const unrecognizedOptions = Object.keys(argv).filter(
     arg =>
       !allowedOptions.has(camelcase(arg)) &&
+      !allowedOptions.has(arg) &&
       (!rawArgv.length || rawArgv.includes(arg)),
     [],
   );
