428430ee8763f59946f486ac1fefe8b63b4ab5c4
Artem Rudenko <artemr2009@gmail.com>
2019-09-05 17:31:08 +0300

    fix comments

--- examples/angular/app.component.spec.ts
+++ examples/angular/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 import { DataService } from './shared/data.service';
@@ -12,6 +12,9 @@ const dataServiceSpy = jest.fn()
   }))
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
@@ -19,26 +22,24 @@ describe('AppComponent', () => {
         { provide: DataService, useClass: dataServiceSpy }
       ]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app)
+      .toBeTruthy();
   });
 
   it(`should have as title 'angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual(title);
+    expect(app.title)
+      .toEqual(title);
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      `Welcome to ${title}!`
-    );
+    expect(compiled.querySelector('h1').textContent)
+      .toContain(`Welcome to ${title}!`);
   });
 });
--- examples/angular/setupJest.js
+++ examples/angular/setupJest.js
@@ -1,37 +1,17 @@
 'use strict';
 
-try {
-  require('core-js/es6/reflect');
-} catch (e) {
-  try {
-    require('core-js/es/reflect');
-  } catch (e) {
-    throw new Error('core-js es6-reflect not found!');
-  }
-}
-
-try {
-  require('core-js/es7/reflect');
-} catch (e) {
-  try {
-    require('core-js/proposals/reflect-metadata');
-  } catch (e) {
-    throw new Error('core-js es7-reflect not found!');
-  }
-}
-
+require('core-js/proposals/reflect-metadata');
 require('zone.js/dist/zone.js');
 require('zone.js/dist/proxy.js');
 require('zone.js/dist/sync-test');
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
-require('./zone-patch');
+require('jest-zone-patch');
 
 const getTestBed = require('@angular/core/testing').getTestBed;
-const BrowserDynamicTestingModule = require('@angular/platform-browser-dynamic/testing').BrowserDynamicTestingModule;
-const platformBrowserDynamicTesting = require('@angular/platform-browser-dynamic/testing').platformBrowserDynamicTesting;
+const testingModule = require('@angular/platform-browser-dynamic/testing');
 
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  testingModule.BrowserDynamicTestingModule,
+  testingModule.platformBrowserDynamicTesting()
 );
