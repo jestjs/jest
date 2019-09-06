dd8937b6812c44ed07f602d8543a5d84ca2a6f1d
Artem Rudenko <artemr2009@gmail.com>
2019-09-06 10:42:42 +0300

    fix lint errors

--- babel.config.js
+++ babel.config.js
@@ -22,7 +22,7 @@ module.exports = {
   ],
   plugins: [
     'babel-plugin-transform-typescript-metadata',
-    ["@babel/plugin-proposal-decorators", {legacy: true}],
+    ['@babel/plugin-proposal-decorators', {legacy: true}],
     ['@babel/plugin-transform-modules-commonjs', {allowTopLevelThis: true}],
     '@babel/plugin-transform-strict-mode',
     '@babel/plugin-proposal-class-properties',
