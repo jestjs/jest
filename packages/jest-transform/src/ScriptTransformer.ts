71ee338121b1b8c8498c29154ee3761579807cf1
Simen Bekkhus <sbekkhus91@gmail.com>
2019-11-22 08:55:30 +0100

    Revert "fix lint error"
    This reverts commit 9c7827219100f0fe3cd669d04b8f4781900904e8.

--- packages/jest-transform/src/ScriptTransformer.ts
+++ packages/jest-transform/src/ScriptTransformer.ts
@@ -366,7 +366,7 @@ export default class ScriptTransformer {
         const transformedSource = this.transformSource(
           filename,
           content,
-          instrument,
+          instrument
         );
 
         wrappedCode = wrap(transformedSource.code, ...extraGlobals);
