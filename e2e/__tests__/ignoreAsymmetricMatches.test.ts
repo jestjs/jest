29154cda886b9cd6c7f0ff94a1fb49ebb7f59076
WeiAnAn <j22461615@gmail.com>
2019-12-07 22:30:40 +0800

    add e2e test

---
+++ e2e/__tests__/ignoreAsymmetricMatches.test.ts
@@ -0,0 +1,36 @@
+import * as path from 'path';
+import runJest from '../runJest';
+import {wrap} from 'jest-snapshot-serializer-raw';
+import {extractSummary} from '../Utils';
+
+const dir = path.resolve(__dirname, '../ignore-asymmetric-matches');
+
+test('should not ignore in default', () => {
+  const {stderr} = runJest(dir);
+  const {rest} = extractSummary(stderr);
+  expect(rest).toMatchSnapshot();
+});
+
+test('should ignore with --ignoreAsymmetricMatches cli option', () => {
+  const {stderr} = runJest(dir, ['--ignoreAsymmetricMatches']);
+  const {rest} = extractSummary(stderr);
+  expect(rest).toMatchSnapshot();
+});
+
+test('should ignore with ignoreAsymmetricMatches=true in config', () => {
+  const {stderr} = runJest(dir, [
+    '--config',
+    JSON.stringify({ignoreAsymmetricMatches: true}),
+  ]);
+  const {rest} = extractSummary(stderr);
+  expect(rest).toMatchSnapshot();
+});
+
+test('should not ignore with ignoreAsymmetricMatches=false in config', () => {
+  const {stderr} = runJest(dir, [
+    '--config',
+    JSON.stringify({ignoreAsymmetricMatches: false}),
+  ]);
+  const {rest} = extractSummary(stderr);
+  expect(rest).toMatchSnapshot();
+});
