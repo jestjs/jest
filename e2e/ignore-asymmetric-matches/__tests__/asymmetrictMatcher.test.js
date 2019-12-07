d7c9a55172e785a6ccc5bad90993cdab3ca2701f
WeiAnAn <j22461615@gmail.com>
2019-12-08 03:08:35 +0800

    add copyright headers

--- e2e/ignore-asymmetric-matches/__tests__/asymmetrictMatcher.test.js
+++ e2e/ignore-asymmetric-matches/__tests__/asymmetrictMatcher.test.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 test('minimal test', () => {
   const expected = {a: expect.any(Number), b: 2};
   const received = {a: 1, b: 1};
