40f34db818bc2722f4dd4b5351ac464bb45ae14c
Kai Hao <kevin830726@gmail.com>
2019-12-07 13:29:25 +0800

    Delete matcher mark file

--- packages/expect/src/externalMatcherMark.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This file is intentionally created for marking external matchers with
- * an explicit function call in the stack trace, benefiting the inline snapshot
- * system to correctly strip out user defined code when trying to locate the
- * top frame. This is required for custom inline snapshot matchers to work.
- */
-export default function noop<T>(any: T): T {
-  return any;
-}
