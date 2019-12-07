32f4c5ec0e43571ccfa3eb92c6658e68c14dba28
Kai Hao <kevin830726@gmail.com>
2019-12-07 18:11:25 +0800

    Fix typo

--- packages/jest-snapshot/src/inline_snapshots.ts
+++ packages/jest-snapshot/src/inline_snapshots.ts
@@ -133,7 +133,7 @@ const groupSnapshotsByFile = groupSnapshotsBy(({frame: {file}}) => file);
 
 const indent = (snapshot: string, numIndents: number, indentation: string) => {
   const lines = snapshot.split('\n');
-  // Prevent re-identation of inline snapshots.
+  // Prevent re-indentation of inline snapshots.
   if (
     lines.length >= 2 &&
     lines[1].startsWith(indentation.repeat(numIndents + 1))
