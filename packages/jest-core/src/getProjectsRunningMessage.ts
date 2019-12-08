54362dc396beaa0caecfd3b99077f9d227920be5
Yacine Hmito <yacine.hmito@gmail.com>
2019-12-08 12:56:42 +0100

    copyright notice on top

--- packages/jest-core/src/getProjectsRunningMessage.ts
+++ packages/jest-core/src/getProjectsRunningMessage.ts
@@ -1,12 +1,12 @@
-import {Config} from '@jest/types';
-import getProjectDisplayName from './getProjectDisplayName';
-
 /**
  * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+import {Config} from '@jest/types';
+import getProjectDisplayName from './getProjectDisplayName';
 
 export default function getProjectsRunningMessage(
   projectConfigs: Array<Config.ProjectConfig>,
