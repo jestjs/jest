c80ef46093b1b93409609a26b73e68e421805541
Simen Bekkhus <sbekkhus91@gmail.com>
2019-12-08 10:55:52 +0100

    skip test on old nodes

--- e2e/__tests__/v8Coverage.test.ts
+++ e2e/__tests__/v8Coverage.test.ts
@@ -7,16 +7,19 @@
 
 import * as path from 'path';
 import {wrap} from 'jest-snapshot-serializer-raw';
+import {onNodeVersions} from '@jest/test-utils';
 import runJest from '../runJest';
 
 const DIR = path.resolve(__dirname, '../v8-coverage');
 
-test('does not explode on missing sourcemap', () => {
-  const sourcemapDir = path.join(DIR, 'no-sourcemap');
-  const {stdout, exitCode} = runJest(sourcemapDir, ['--v8-coverage'], {
-    stripAnsi: true,
-  });
+onNodeVersions('>=10', () => {
+  test('prints coverage', () => {
+    const sourcemapDir = path.join(DIR, 'no-sourcemap');
+    const {stdout, exitCode} = runJest(sourcemapDir, ['--v8-coverage'], {
+      stripAnsi: true,
+    });
 
-  expect(exitCode).toBe(0);
-  expect(wrap(stdout)).toMatchSnapshot();
+    expect(exitCode).toBe(0);
+    expect(wrap(stdout)).toMatchSnapshot();
+  });
 });
