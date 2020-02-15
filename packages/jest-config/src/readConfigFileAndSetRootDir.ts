e275a9f85378270f524a009f9940b2530046f37d
Lucas Azzola <lucas@azzola.dev>
2020-02-15 13:49:43 +1100

    Handle ERR_REQUIRE_ESM when reading configuration

--- packages/jest-config/src/readConfigFileAndSetRootDir.ts
+++ packages/jest-config/src/readConfigFileAndSetRootDir.ts
@@ -10,55 +10,49 @@ import * as fs from 'fs';
 import {Config} from '@jest/types';
 // @ts-ignore: vendored
 import jsonlint from './vendor/jsonlint';
-import {
-  JEST_CONFIG_EXT_JSON,
-  JEST_CONFIG_EXT_MJS,
-  PACKAGE_JSON,
-} from './constants';
+import {JEST_CONFIG_EXT_JSON, PACKAGE_JSON} from './constants';
 import importMjs from './importMjs';
 
 // Read the configuration and set its `rootDir`
 // 1. If it's a `package.json` file, we look into its "jest" property
-// 2. For any other file, we just require it.
+// 2. For any other file, we just require it. If we receive an 'ERR_REQUIRE_ESM'
+//    from node, perform a dynamic import instead.
 export default async function readConfigFileAndSetRootDir(
   configPath: Config.Path,
 ): Promise<Config.InitialOptions> {
   const isJSON = configPath.endsWith(JEST_CONFIG_EXT_JSON);
-  const isMjs = configPath.endsWith(JEST_CONFIG_EXT_MJS);
   let configObject;
 
-  if (isMjs) {
-    try {
-      const importedConfig = await importMjs(configPath);
+  try {
+    configObject = require(configPath);
+  } catch (error) {
+    if (error.code === 'ERR_REQUIRE_ESM') {
+      try {
+        const importedConfig = await importMjs(configPath);
 
-      if (!importedConfig.default) {
-        throw new Error(
-          `Jest: Failed to load mjs config file ${configPath} - did you use a default export?`,
-        );
-      }
+        if (!importedConfig.default) {
+          throw new Error(
+            `Jest: Failed to load mjs config file ${configPath} - did you use a default export?`,
+          );
+        }
 
-      configObject = importedConfig.default;
-    } catch (error) {
-      if (error.message === 'Not supported') {
-        throw new Error(
-          `Jest: Your version of Node does not support dynamic import - please enable it or use a .cjs file extension for file ${configPath}`,
-        );
-      }
+        configObject = importedConfig.default;
+      } catch (error) {
+        if (error.message === 'Not supported') {
+          throw new Error(
+            `Jest: Your version of Node does not support dynamic import - please enable it or use a .cjs file extension for file ${configPath}`,
+          );
+        }
 
-      throw error;
-    }
-  } else {
-    try {
-      configObject = require(configPath);
-    } catch (error) {
-      if (isJSON) {
-        throw new Error(
-          `Jest: Failed to parse config file ${configPath}\n` +
-            `  ${jsonlint.errors(fs.readFileSync(configPath, 'utf8'))}`,
-        );
-      } else {
         throw error;
       }
+    } else if (isJSON) {
+      throw new Error(
+        `Jest: Failed to parse config file ${configPath}\n` +
+          `  ${jsonlint.errors(fs.readFileSync(configPath, 'utf8'))}`,
+      );
+    } else {
+      throw error;
     }
   }
 
