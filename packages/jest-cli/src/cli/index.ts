ea3131dec7d192a07d402d36e6a0df390711a6f5
Simen Bekkhus <sbekkhus91@gmail.com>
2020-02-26 12:37:06 +0100

    Revert "chore: upgrade and provide option"
    This reverts commit 883c81efa80b01a9e3557429581c4cdaa121ff12.

--- packages/jest-cli/src/cli/index.ts
+++ packages/jest-cli/src/cli/index.ts
@@ -57,7 +57,6 @@ export const buildArgv = (maybeArgv?: Array<string>): Config.Argv => {
   const rawArgv: Config.Argv | Array<string> =
     maybeArgv || process.argv.slice(2);
   const argv: Config.Argv = yargs(rawArgv)
-    .parserConfiguration({'duplicate-arguments-array': false})
     .usage(args.usage)
     .version(version)
     .alias('help', 'h')
