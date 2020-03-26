ab707b0f5240ef36aadfb915d6f59050fe445d33
Simen Bekkhus <sbekkhus91@gmail.com>
2020-03-26 21:23:00 +0100

    fix: ensure all packages correctly downlevel TS files (#9715)

--- scripts/buildTs.js
+++ scripts/buildTs.js
@@ -44,11 +44,30 @@ try {
   );
   console.error(e.stack);
   process.exitCode = 1;
+  return;
 }
 
 const downlevelArgs = ['--silent', 'downlevel-dts', 'build', 'build/ts3.4'];
 
 console.log(chalk.inverse(' Downleveling TypeScript definition files '));
+
+packagesWithTs.forEach(pkgDir => {
+  const pkg = require(pkgDir + '/package.json');
+
+  if (!pkg.types) {
+    throw new Error(`Package ${pkg.name} is missing \`types\` field`);
+  }
+
+  if (!pkg.typesVersions) {
+    throw new Error(`Package ${pkg.name} is missing \`typesVersions\` field`);
+  }
+
+  if (pkg.main.replace(/\.js$/, '.d.ts') !== pkg.types) {
+    throw new Error(
+      `\`main\` and \`types\` field of ${pkg.name} does not match`
+    );
+  }
+});
 
 // we want to limit the number of processes we spawn
 const cpus = Math.max(1, os.cpus().length - 1);
