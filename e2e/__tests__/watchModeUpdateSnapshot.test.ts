b7cd4328b71e3191718c85294408b6c0ed3ef242
Simen Bekkhus <sbekkhus91@gmail.com>
2020-03-28 10:44:52 +0100

    chore: fix some type errors in e2e tests (#9720)

--- e2e/__tests__/watchModeUpdateSnapshot.test.ts
+++ e2e/__tests__/watchModeUpdateSnapshot.test.ts
@@ -22,7 +22,7 @@ expect.addSnapshotSerializer({
   test: val => typeof val === 'string' && val.includes('[s[u'),
 });
 
-const setupFiles = input => {
+const setupFiles = (input: Array<{keys: Array<string>}>) => {
   writeFiles(DIR, {
     '__tests__/__snapshots__/bar.spec.js.snap': `// Jest Snapshot v1, https://goo.gl/fbAQLP
 
