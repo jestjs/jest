ed5e84ec36ba440ae372570a6f6506a7afc7069d
Simen Bekkhus <sbekkhus91@gmail.com>
2020-04-02 13:08:12 +0200

    lint ignore

--- website/static/js/code-block-buttons.js
+++ website/static/js/code-block-buttons.js
@@ -1,5 +1,3 @@
-// Turn off ESLint for this file because it's sent down to users as-is.
-/* eslint-disable */
 window.addEventListener('load', function() {
   function button(label, ariaLabel, icon, className) {
     const btn = document.createElement('button');
