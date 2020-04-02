37656ec591cf83bdede01b4f22692c17aa4d5967
Simen Bekkhus <sbekkhus91@gmail.com>
2020-04-02 12:20:03 +0200

    chore: extract `AssertionResult` from `@jest/test-result` (#9749)

--- packages/jest-types/src/index.ts
+++ packages/jest-types/src/index.ts
@@ -8,6 +8,7 @@
 import type * as Circus from './Circus';
 import type * as Config from './Config';
 import type * as Global from './Global';
+import type * as TestResult from './TestResult';
 import type * as TransformTypes from './Transform';
 
-export type {Circus, Config, Global, TransformTypes};
+export type {Circus, Config, Global, TestResult, TransformTypes};
