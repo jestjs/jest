accb699c5920acb5334cb67bd0c0cf1ffec900b8
Simen Bekkhus <sbekkhus91@gmail.com>
2020-04-06 08:58:24 +0200

    feat: enable syntax plugins for syntax that works in node

--- packages/babel-preset-jest/index.js
+++ packages/babel-preset-jest/index.js
@@ -5,10 +5,14 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-module.exports = () => ({
+module.exports = {
   plugins: [
     require.resolve('babel-plugin-jest-hoist'),
+    // TODO: replace all the below with a single preset in Jest 26: https://github.com/facebook/jest/pull/9774
     require.resolve('@babel/plugin-syntax-object-rest-spread'),
     require.resolve('@babel/plugin-syntax-bigint'),
+    require.resolve('@babel/plugin-syntax-class-properties'),
+    require.resolve('@babel/plugin-syntax-logical-assignment-operators'),
+    require.resolve('@babel/plugin-syntax-numeric-separator'),
   ],
-});
+};
