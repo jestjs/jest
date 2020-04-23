3a30ae1602d7663cff972bcbe164125c29ed8e10
Matthew Preble <mpreble@indeed.com>
2020-04-23 14:43:09 -0500

    Lint fix - import order.

--- e2e/__tests__/stackTraceSourceMapsWithCoverage.test.ts
+++ e2e/__tests__/stackTraceSourceMapsWithCoverage.test.ts
@@ -5,9 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 import * as path from 'path';
+import wrap from 'jest-snapshot-serializer-raw';
 import {extractSummary, run} from '../Utils';
 import runJest from '../runJest';
-import wrap from 'jest-snapshot-serializer-raw';
 
 it('processes stack traces and code frames with source maps with coverage', () => {
   const dir = path.resolve(
