cd059ecf7b77b582ba05890bdc025621e09aaec0
Tim Seckinger <seckinger.tim@gmail.com>
2020-04-24 00:32:05 +0200

    test aliased import

--- e2e/babel-plugin-jest-hoist/__tests__/importJest.test.js
+++ e2e/babel-plugin-jest-hoist/__tests__/importJest.test.js
@@ -7,22 +7,25 @@
  */
 
 import {jest} from '@jest/globals';
+import {jest as aliasedJest} from '@jest/globals';
 import * as JestGlobals from '@jest/globals';
 
 import a from '../__test_modules__/a';
 import b from '../__test_modules__/b';
 import c from '../__test_modules__/c';
+import d from '../__test_modules__/d';
 
 // These will be hoisted above imports
 
 jest.unmock('../__test_modules__/a');
-JestGlobals.jest.unmock('../__test_modules__/b');
+aliasedJest.unmock('../__test_modules__/b');
+JestGlobals.jest.unmock('../__test_modules__/c');
 
 // These will not be hoisted above imports
 
 {
   const jest = {unmock: () => {}};
-  jest.unmock('../__test_modules__/c');
+  jest.unmock('../__test_modules__/d');
 }
 
 // tests
@@ -37,7 +40,12 @@ test('namespace import', () => {
   expect(b()).toBe('unmocked');
 });
 
+test('namespace import', () => {
+  expect(c._isMockFunction).toBe(undefined);
+  expect(c()).toBe('unmocked');
+});
+
 test('fake jest, shadowed import', () => {
-  expect(c._isMockFunction).toBe(true);
-  expect(c()).toBe(undefined);
+  expect(d._isMockFunction).toBe(true);
+  expect(d()).toBe(undefined);
 });
