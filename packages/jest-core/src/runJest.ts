73271f0bd24384525cc4026b9140f6cd00dfeb93
陳乙山 <pipio1994@gmail.com>
2020-04-26 15:20:20 +0800

    Create searchSources at first for reuse in runJest

--- packages/jest-core/src/runJest.ts
+++ packages/jest-core/src/runJest.ts
@@ -34,13 +34,12 @@ import type {Filter, TestRunData} from './types';
 
 const getTestPaths = async (
   globalConfig: Config.GlobalConfig,
-  context: Context,
+  source: SearchSource,
   outputStream: NodeJS.WriteStream,
   changedFiles: ChangedFiles | undefined,
   jestHooks: JestHookEmitter,
   filter?: Filter,
 ) => {
-  const source = new SearchSource(context);
   const data = await source.getTestPaths(globalConfig, changedFiles, filter);
 
   if (!data.tests.length && globalConfig.onlyChanged && data.noSCM) {
@@ -167,11 +166,14 @@ export default async function runJest({
     }
   }
 
+  const searchSources = contexts.map(context => new SearchSource(context));
+
   const testRunData: TestRunData = await Promise.all(
-    contexts.map(async context => {
+    contexts.map(async (context, index) => {
+      const searchSource = searchSources[index];
       const matches = await getTestPaths(
         globalConfig,
-        context,
+        searchSource,
         outputStream,
         changedFilesPromise && (await changedFilesPromise),
         jestHooks,
@@ -246,8 +248,8 @@ export default async function runJest({
     if (changedFilesInfo.changedFiles) {
       testSchedulerContext.changedFiles = changedFilesInfo.changedFiles;
       const sourcesRelatedToTestsInChangedFilesArray = contexts
-        .map(context => {
-          const searchSource = new SearchSource(context);
+        .map((_, index) => {
+          const searchSource = searchSources[index];
           const relatedSourceFromTestsInChangedFiles = searchSource.findRelatedSourcesFromTestsInChangedFiles(
             changedFilesInfo,
           );
