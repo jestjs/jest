5d352b4ec96745e801efcc3e8d79c814eaf29ec9
Simen Bekkhus <sbekkhus91@gmail.com>
2020-04-26 22:01:14 +0200

    Revert "snapshot updates, idk..."
    This reverts commit e4778f882c9cff1c44d11dcd3cb2371cbc7ee48e.

--- e2e/__tests__/resolveBrowserField.test.ts
+++ e2e/__tests__/resolveBrowserField.test.ts
@@ -66,7 +66,7 @@ test('preserves module identity for symlinks when using browser field resolution
     console.log
         needs-preserved-id executed
 
-          at Object.log (packages/needs-preserved-id/index.js:1:9)
+          at Object.<anonymous> (packages/needs-preserved-id/index.js:1:13)
   `);
   expect(exitCode).toEqual(0);
 });
