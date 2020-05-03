24c4b5461b0e0adcd915e989c0d46b23b188280d
Michał Pierzchała <thymikee@gmail.com>
2020-05-03 22:35:25 +0200

    fix copyright

--- e2e/__tests__/watchModeNoAccess.test.ts
+++ e2e/__tests__/watchModeNoAccess.test.ts
@@ -1,13 +1,10 @@
 /**
- * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow
  */
-
-'use strict';
 
 import * as os from 'os';
 import * as path from 'path';
