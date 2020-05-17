524597220f2872391122ac29a3e587656d45a62d
Kenrick <kenrick95@gmail.com>
2020-05-17 14:36:32 +0800

    Update coverageHandlebars

--- e2e/__tests__/coverageHandlebars.test.ts
+++ e2e/__tests__/coverageHandlebars.test.ts
@@ -8,7 +8,7 @@
 import * as path from 'path';
 import {readFileSync} from 'graceful-fs';
 import wrap from 'jest-snapshot-serializer-raw';
-import {cleanup, run} from '../Utils';
+import {cleanup, extractSummary, run} from '../Utils';
 import runJest from '../runJest';
 
 const dir = path.resolve(__dirname, '../coverage-handlebars');
@@ -20,10 +20,13 @@ beforeAll(() => {
 
 it('code coverage for Handlebars', () => {
   run('yarn', dir);
-  const result = runJest(dir, ['--coverage', '--no-cache']);
+  const {exitCode, stdout, stderr} = runJest(dir, ['--coverage', '--no-cache']);
 
-  expect(result.exitCode).toBe(0);
-  expect(wrap(result.stdout)).toMatchSnapshot();
+  expect(exitCode).toBe(0);
+  const {rest, summary} = extractSummary(stdout);
+  expect(wrap(summary)).toMatchSnapshot();
+  expect(wrap(rest)).toMatchSnapshot();
+  expect(wrap(stderr)).toMatchSnapshot();
 
   const coverageMapFile = path.join(coverageDir, 'coverage-final.json');
   const coverageMap = JSON.parse(readFileSync(coverageMapFile, 'utf-8'));
