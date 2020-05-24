ce2ae464c98b1cdd071c892ae680980132747081
Yen-Chi Chen <gruenen@gmail.com>
2020-05-24 20:30:17 +0200

    linting

--- packages/jest-console/src/__tests__/getConsoleOutput.test.ts
+++ packages/jest-console/src/__tests__/getConsoleOutput.test.ts
@@ -5,42 +5,53 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import {formatStackTrace} from 'jest-message-util';
-import getConsoleOutput from '../getConsoleOutput';
 import BufferedConsole from '../BufferedConsole';
-import { LogType } from '../types';
+import getConsoleOutput from '../getConsoleOutput';
+import {formatStackTrace} from 'jest-message-util';
+import {LogType} from '../types';
 
 jest.mock('jest-message-util', () => ({
-    formatStackTrace: jest.fn(),
+  formatStackTrace: jest.fn(),
 }));
 
-describe('getConsoleOutput', ()=>{
-    formatStackTrace.mockImplementation(()=>'throw new Error("Whoops!");');
-    let cases = ['assert', 'count', 'debug', 'dir', 'dirxml', 'error', 'group', 'groupCollapsed', 'info', 'log', 'time', 'warn'];
-   
-    cases.forEach((logType)=>{
-        it(`takes noStackTrace and pass it on for ${logType}`, ()=>{
-            getConsoleOutput(
-                'someRootPath',
-                true,
-                BufferedConsole.write([], logType as LogType, 'message', 4),
-                {
-                    rootDir: 'root',
-                    testMatch: [],
-                },
-                true
-            );
-            expect(formatStackTrace).toHaveBeenCalled();
-            expect(formatStackTrace).toHaveBeenCalledWith(
-                expect.anything(),
-                expect.anything(),
-                expect.objectContaining(
-                    {
-                        noStackTrace: true,
-                        noCodeFrame: expect.anything()
-                    }
-                )
-            );
-        });
+describe('getConsoleOutput', () => {
+  formatStackTrace.mockImplementation(() => 'throw new Error("Whoops!");');
+  const cases = [
+    'assert',
+    'count',
+    'debug',
+    'dir',
+    'dirxml',
+    'error',
+    'group',
+    'groupCollapsed',
+    'info',
+    'log',
+    'time',
+    'warn',
+  ];
+
+  cases.forEach(logType => {
+    it(`takes noStackTrace and pass it on for ${logType}`, () => {
+      getConsoleOutput(
+        'someRootPath',
+        true,
+        BufferedConsole.write([], logType as LogType, 'message', 4),
+        {
+          rootDir: 'root',
+          testMatch: [],
+        },
+        true,
+      );
+      expect(formatStackTrace).toHaveBeenCalled();
+      expect(formatStackTrace).toHaveBeenCalledWith(
+        expect.anything(),
+        expect.anything(),
+        expect.objectContaining({
+          noCodeFrame: expect.anything(),
+          noStackTrace: true,
+        }),
+      );
     });
+  });
 });
