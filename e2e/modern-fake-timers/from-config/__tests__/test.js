104a0f2fb3e51d3ff8828ef4da82bbbc1a10fabe
Liam Doran <liamfd@gmail.com>
2020-06-15 10:46:55 -0400

    feat: update setSystemTime types to match FakeTimers._clock

--- e2e/modern-fake-timers/from-config/__tests__/test.js
+++ e2e/modern-fake-timers/from-config/__tests__/test.js
@@ -7,8 +7,18 @@
 
 'use strict';
 
-test('fake timers', () => {
+test('fake timers with number argument', () => {
   jest.setSystemTime(0);
+
+  expect(Date.now()).toBe(0);
+
+  jest.setSystemTime(1000);
+
+  expect(Date.now()).toBe(1000);
+});
+
+test('fake timers with Date argument', () => {
+  jest.setSystemTime(new Date(0));
 
   expect(Date.now()).toBe(0);
 
