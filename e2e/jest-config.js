15bcd10025a1b6b5674e478b036ced7ac8b29852
Mansehej Singh <mansehej@gmail.com>
2020-07-06 22:04:18 +0530

    Add copyright header to jest-config in tests

--- e2e/jest-config.js
+++ e2e/jest-config.js
@@ -1,1 +1,11 @@
-// Empty file just for testing purposes.
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+/* -------------------------------------------------------------------------- */
+/*                    Empty file just for testing purposes.                   */
+/* -------------------------------------------------------------------------- */
