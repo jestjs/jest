17fda863b94e78f0a9191cf8718426229387bea2
Chris Shepherd <chris@chrisshepherd.me>
2020-08-12 22:22:50 +0100

    Revert change to TypeScript sum test

--- examples/typescript/__tests__/sum-test.ts
+++ examples/typescript/__tests__/sum-test.ts
@@ -1,12 +1,13 @@
 // Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
 
-import sum from '../sum';
-
 it('adds 1 + 2 to equal 3 in TScript', () => {
+  // Generally, `import` should be used for TypeScript
+  // as using `require` will not return any type information.
+  const sum = require('../sum.ts').default;
   expect(sum(1, 2)).toBe(3);
 });
 
 it('adds 1 + 2 to equal 3 in JavaScript', () => {
-  const sumJs = require('../sum.js');
-  expect(sumJs(1, 2)).toBe(3);
+  const sum = require('../sum.js');
+  expect(sum(1, 2)).toBe(3);
 });
