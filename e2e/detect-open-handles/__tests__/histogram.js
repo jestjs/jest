5f86e41b00a19e682ba4a4f4d031965b4c3e968a
C L <c>
2020-08-19 00:11:50 +0100

    Add histogram object

--- e2e/detect-open-handles/__tests__/histogram.js
+++ e2e/detect-open-handles/__tests__/histogram.js
@@ -4,7 +4,13 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+const perf_hooks = require('perf_hooks');
 
 test('something', () => {
+  try {
+    perf_hooks.monitorEventLoopDelay({}).enable();
+  } catch {
+    // node version <11.10.0
+  }
   expect(true).toBe(true);
 });
