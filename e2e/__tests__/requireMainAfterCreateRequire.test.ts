5bae7dae08f5cf7dc4c0cddd48b1039bd6c1c8ed
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-10-09 19:11:52 +0530

    Add integration test

---
+++ e2e/__tests__/requireMainAfterCreateRequire.test.ts
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import * as path from 'path';
+
+import runJest from '../runJest';
+
+test('`require.main` not undefined after createRequire', () => {
+  const {stdout} = runJest('require-main-after-create-require');
+
+  expect(stdout).toMatch(
+    path.resolve(
+      path.join(
+        __dirname,
+        '../require-main-after-create-require/__tests__/parent.test.js',
+      ),
+    ),
+  );
+});
---
+++ e2e/require-main-after-create-require/__tests__/parent.test.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const Module = require('module');
+const path = require('path');
+
+test('require child from parent', () => {
+  // createRequire with a different file
+  require = Module.createRequire(path.resolve('./empty.js'));
+
+  require('./child');
+
+  expect(1).toBe(1);
+});
