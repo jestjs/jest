4ca6caf144e908ac9e81c12e71582913e47243bd
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-10-09 20:56:29 +0530

    Skip test on node v10

--- e2e/__tests__/requireMainAfterCreateRequire.test.ts
+++ e2e/__tests__/requireMainAfterCreateRequire.test.ts
@@ -5,18 +5,20 @@
  * LICENSE file in the root directory of this source tree.
  */
 import * as path from 'path';
-
+import {onNodeVersions} from '@jest/test-utils';
 import runJest from '../runJest';
 
-test('`require.main` not undefined after createRequire', () => {
-  const {stdout} = runJest('require-main-after-create-require');
+onNodeVersions('^12.16.0 || >=13.7.0', () => {
+  test('`require.main` not undefined after createRequire', () => {
+    const {stdout} = runJest('require-main-after-create-require');
 
-  expect(stdout).toMatch(
-    path.resolve(
-      path.join(
-        __dirname,
-        '../require-main-after-create-require/__tests__/parent.test.js',
+    expect(stdout).toMatch(
+      path.resolve(
+        path.join(
+          __dirname,
+          '../require-main-after-create-require/__tests__/parent.test.js',
+        ),
       ),
-    ),
-  );
+    );
+  });
 });
