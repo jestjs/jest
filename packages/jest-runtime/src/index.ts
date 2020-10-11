dd435fcedb4ecbc28ef4e21e28bcd353b827e020
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-10-11 19:25:07 +0530

    Add mainModule instance variable

--- packages/jest-runtime/src/index.ts
+++ packages/jest-runtime/src/index.ts
@@ -150,6 +150,7 @@ class Runtime {
     | null;
   private _internalModuleRegistry: ModuleRegistry;
   private _isCurrentlyExecutingManualMock: string | null;
+  private _mainModule: Module | null;
   private _mockFactories: Map<string, () => unknown>;
   private _mockMetaDataCache: Map<
     string,
@@ -202,6 +203,7 @@ class Runtime {
     this._explicitShouldMock = new Map();
     this._internalModuleRegistry = new Map();
     this._isCurrentlyExecutingManualMock = null;
+    this._mainModule = null;
     this._mockFactories = new Map();
     this._mockRegistry = new Map();
     // during setup, this cannot be null (and it's fine to explode if it is)
@@ -1052,6 +1054,15 @@ class Runtime {
         );
       }),
     ];
+
+    if (!this._mainModule) {
+      this._mainModule = module;
+    }
+
+    Object.defineProperty(module, 'main', {
+      enumerable: true,
+      value: this._mainModule,
+    });
 
     try {
       compiledFunction.call(
