6fccad97401ffab7d55d459c85c19fd3c323df93
Jason Palmer <jason@jason-palmer.com>
2020-11-14 11:56:31 -0500

    Support default exports for runners

--- packages/jest-core/src/TestScheduler.ts
+++ packages/jest-core/src/TestScheduler.ts
@@ -193,7 +193,11 @@ export default class TestScheduler {
     contexts.forEach(context => {
       const {config} = context;
       if (!testRunners[config.runner]) {
-        const Runner: typeof TestRunner = require(config.runner);
+        let Runner: typeof TestRunner = require(config.runner);
+        if (Runner.default) {
+          Runner = Runner.default;
+        }
+
         const runner = new Runner(this._globalConfig, {
           changedFiles: this._context?.changedFiles,
           sourcesRelatedToTestsInChangedFiles: this._context
