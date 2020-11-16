29156d1e4c99f242fab44fd2f444a35a77ed404f
Simen Bekkhus <sbekkhus91@gmail.com>
2020-11-16 15:21:30 +0100

    chore: attempt to retry flaky test

--- packages/jest-repl/src/__tests__/runtime_cli.test.js
+++ packages/jest-repl/src/__tests__/runtime_cli.test.js
@@ -5,19 +5,26 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import path from 'path';
 import {sync as spawnSync} from 'execa';
 
-const JEST_RUNTIME = path.resolve(__dirname, '../../bin/jest-runtime-cli.js');
+const JEST_RUNTIME = require.resolve('../../bin/jest-runtime-cli.js');
+
+const timeout = 10_000;
 
 const run = args =>
   spawnSync(JEST_RUNTIME, args, {
     cwd: process.cwd(),
     env: process.env,
     reject: false,
+    timeout: timeout - 500,
   });
 
 describe('Runtime CLI', () => {
+  beforeAll(() => {
+    jest.retryTimes(3);
+    jest.setTimeout(timeout);
+  });
+
   it('fails with no path', () => {
     const expectedOutput =
       'Please provide a path to a script. (See --help for details)';
@@ -25,25 +32,22 @@ describe('Runtime CLI', () => {
   });
 
   it('displays script output', () => {
-    const scriptPath = path.resolve(__dirname, './test_root/logging.js');
+    const scriptPath = require.resolve('./test_root/logging.js');
     expect(run([scriptPath, '--no-cache']).stdout).toMatch('Hello, world!');
   });
 
   it('always disables automocking', () => {
-    const scriptPath = path.resolve(__dirname, './test_root/logging.js');
+    const scriptPath = require.resolve('./test_root/logging.js');
     const output = run([
       scriptPath,
       '--no-cache',
-      '--config=' +
-        JSON.stringify({
-          automock: true,
-        }),
+      '--config=' + JSON.stringify({automock: true}),
     ]);
     expect(output.stdout).toMatch('Hello, world!');
   });
 
   it('throws script errors', () => {
-    const scriptPath = path.resolve(__dirname, './test_root/throwing.js');
+    const scriptPath = require.resolve('./test_root/throwing.js');
     expect(run([scriptPath, '--no-cache']).stderr).toMatch('Error: throwing');
   });
 });
