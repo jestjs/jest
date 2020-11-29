daf4d02903f659fa3d1c04012e391c3e754429ae
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2020-11-29 13:58:02 +0100

    fix(jest‑environment‑jsdom): Use inner realm’s ArrayBuffer constructor (#10885)

--- e2e/env-test/__tests__/equivalent.test.js
+++ e2e/env-test/__tests__/equivalent.test.js
@@ -6,9 +6,31 @@
  */
 'use strict';
 
+const {isArrayBuffer} = require('util').types;
+const isJSDOM =
+  typeof window !== 'undefined' && typeof document !== 'undefined';
+
 test('Buffer', () => {
   const bufFromArray = Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]);
-  expect(bufFromArray.buffer instanceof ArrayBuffer).toBeTruthy();
+  expect(isArrayBuffer(bufFromArray.buffer)).toBeTruthy();
   const bufFromArrayBuffer = Buffer.from(new ArrayBuffer(6));
   expect(bufFromArrayBuffer.buffer instanceof ArrayBuffer).toBeTruthy();
+});
+
+test.each([
+  ['Int8Array', Int8Array, isJSDOM],
+  ['Int16Array', Int16Array, isJSDOM],
+  ['Int32Array', Int32Array, isJSDOM],
+  ['Uint8Array', Uint8Array],
+  ['Uint8ClampedArray', Uint8ClampedArray, isJSDOM],
+  ['Uint16Array', Uint16Array, isJSDOM],
+  ['Uint32Array', Uint32Array, isJSDOM],
+  ['Float32Array', Float32Array, isJSDOM],
+  ['Float64Array', Float64Array, isJSDOM],
+])('%s', (name, ctor, testInstanceof = true) => {
+  const typedArray = ctor.of();
+  expect(isArrayBuffer(typedArray.buffer)).toBeTruthy();
+  if (testInstanceof) {
+    expect(typedArray.buffer instanceof ArrayBuffer).toBeTruthy();
+  }
 });
