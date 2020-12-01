ebec391cef73a7fbb8c29886d80dad80dc6e7ecd
Takeru <takeru.moe@gmail.com>
2020-12-02 00:55:13 +0800

    chore: update copyright header

--- e2e/native-esm/circularDependentB.mjs
+++ e2e/native-esm/circularDependentB.mjs
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import circularDependentA from './circularDependentA.mjs';
 
 export default {
