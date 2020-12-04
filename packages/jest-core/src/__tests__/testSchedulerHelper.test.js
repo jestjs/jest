09d38764962ff8a530a1855774250364cc733404
Oliver Bell <freshollie@gmail.com>
2020-12-04 00:25:01 +0000

    chore: update snapshots

--- packages/jest-core/src/__tests__/testSchedulerHelper.test.js
+++ packages/jest-core/src/__tests__/testSchedulerHelper.test.js
@@ -23,19 +23,19 @@ const getTestMock = () => ({
 const getTestsMock = () => [getTestMock(), getTestMock()];
 
 test.each`
-  tests              | timings        | detectOpenHandles | maxWorkers   | watch    | expectedResult
-  ${[getTestMock()]} | ${[500, 500]}  | ${false}          | ${undefined} | ${true}  | ${true}
-  ${getTestsMock()}  | ${[2000, 500]} | ${false}          | ${1}         | ${true}  | ${true}
-  ${getTestsMock()}  | ${[2000, 500]} | ${false}          | ${2}         | ${true}  | ${false}
-  ${[getTestMock()]} | ${[2000, 500]} | ${false}          | ${undefined} | ${true}  | ${false}
-  ${getTestMock()}   | ${[500, 500]}  | ${false}          | ${undefined} | ${true}  | ${false}
-  ${getTestsMock()}  | ${[2000, 500]} | ${false}          | ${1}         | ${false} | ${true}
-  ${getTestMock()}   | ${[2000, 500]} | ${false}          | ${2}         | ${false} | ${false}
-  ${[getTestMock()]} | ${[2000]}      | ${false}          | ${undefined} | ${false} | ${true}
-  ${getTestsMock()}  | ${[500, 500]}  | ${false}          | ${undefined} | ${false} | ${true}
-  ${new Array(45)}   | ${[500]}       | ${false}          | ${undefined} | ${false} | ${false}
-  ${getTestsMock()}  | ${[2000, 500]} | ${false}          | ${undefined} | ${false} | ${false}
-  ${getTestsMock()}  | ${[2000, 500]} | ${true}           | ${undefined} | ${false} | ${true}
+  tests                                     | timings        | detectOpenHandles | maxWorkers   | watch    | expectedResult
+  ${[getTestMock()]}                        | ${[500, 500]}  | ${false}          | ${undefined} | ${true}  | ${true}
+  ${getTestsMock()}                         | ${[2000, 500]} | ${false}          | ${1}         | ${true}  | ${true}
+  ${getTestsMock()}                         | ${[2000, 500]} | ${false}          | ${2}         | ${true}  | ${false}
+  ${[getTestMock()]}                        | ${[2000, 500]} | ${false}          | ${undefined} | ${true}  | ${false}
+  ${getTestsMock()}                         | ${[500, 500]}  | ${false}          | ${undefined} | ${true}  | ${false}
+  ${getTestsMock()}                         | ${[2000, 500]} | ${false}          | ${1}         | ${false} | ${true}
+  ${getTestsMock()}                         | ${[2000, 500]} | ${false}          | ${2}         | ${false} | ${false}
+  ${[getTestMock()]}                        | ${[2000]}      | ${false}          | ${undefined} | ${false} | ${true}
+  ${getTestsMock()}                         | ${[500, 500]}  | ${false}          | ${undefined} | ${false} | ${true}
+  ${new Array(45).fill(1).map(getTestMock)} | ${[500]}       | ${false}          | ${undefined} | ${false} | ${false}
+  ${getTestsMock()}                         | ${[2000, 500]} | ${false}          | ${undefined} | ${false} | ${false}
+  ${getTestsMock()}                         | ${[2000, 500]} | ${true}           | ${undefined} | ${false} | ${true}
 `(
   'shouldRunInBand() - should return $expectedResult for runInBand mode',
   ({tests, timings, detectOpenHandles, maxWorkers, watch, expectedResult}) => {
