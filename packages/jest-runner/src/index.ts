ff90e0842ce38e141a174842c1a3013f84000b84
Simen Bekkhus <sbekkhus91@gmail.com>
2020-12-28 20:30:22 +0100

    fix: use advanced serialization when spawning jest workers

--- packages/jest-runner/src/index.ts
+++ packages/jest-runner/src/index.ts
@@ -166,7 +166,12 @@ export default class TestRunner {
 
     const worker = new Worker(TEST_WORKER_PATH, {
       exposedMethods: ['worker'],
-      forkOptions: {stdio: 'pipe'},
+      forkOptions: {
+        // default to advanced serialization in order to match worker threads
+        // @ts-expect-error: option does not exist on the node 10 types
+        serialization: 'advanced',
+        stdio: 'pipe',
+      },
       maxRetries: 3,
       numWorkers: this._globalConfig.maxWorkers,
       setupArgs: [
