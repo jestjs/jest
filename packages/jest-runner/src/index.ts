049042d8528e9a66fc5e8b95220ec836e7f7213b
Simen Bekkhus <sbekkhus91@gmail.com>
2020-12-30 17:32:47 +0100

    Update index.ts

--- packages/jest-runner/src/index.ts
+++ packages/jest-runner/src/index.ts
@@ -167,7 +167,7 @@ export default class TestRunner {
     const worker = new Worker(TEST_WORKER_PATH, {
       exposedMethods: ['worker'],
       forkOptions: {
-        // default to advanced serialization in order to match worker threads
+        // use advanced serialization in order to transfer objects with circular references
         // @ts-expect-error: option does not exist on the node 10 types
         serialization: 'advanced',
         stdio: 'pipe',
