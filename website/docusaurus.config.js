75df92ea48e600a24248b43f701c6d10febebda3
slorber <lorber.sebastien@gmail.com>
2021-01-21 16:37:43 +0100

    fix jest blog post layout

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -108,10 +108,10 @@ module.exports = {
         theme: {
           customCss: [
             path.resolve('src/components/v1/legacyCSS.css'),
-            path.resolve('src/css/docusaurusTheme.css'),
-            path.resolve('src/css/algoliaDocSearchTheme.css'),
-            path.resolve('static/css/custom.css'),
-            path.resolve('static/css/jest.css'),
+            path.resolve('src/css/docusaurusTheme.scss'),
+            path.resolve('src/css/algoliaDocSearchTheme.scss'),
+            path.resolve('static/css/custom.scss'),
+            path.resolve('static/css/jest.scss'),
           ],
         },
       },
