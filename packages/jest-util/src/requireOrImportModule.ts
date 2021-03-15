2884d0395741de6e95b3818aab4c489b5fc0e61c
Wei-An, Yen <j22461615@gmail.com>
2021-03-16 00:58:47 +0800

    add copyright header

--- packages/jest-util/src/requireOrImportModule.ts
+++ packages/jest-util/src/requireOrImportModule.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import {isAbsolute} from 'path';
 import {pathToFileURL} from 'url';
 import type {Config} from '@jest/types';
