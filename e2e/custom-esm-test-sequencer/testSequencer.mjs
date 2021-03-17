6016b682ab4bf855df5c5631dee77feb4d8326ce
Wei-An, Yen <j22461615@gmail.com>
2021-03-18 00:16:55 +0800

    feat(jest-core): Add support for `testSequencer` written in ESM

---
+++ e2e/custom-esm-test-sequencer/testSequencer.mjs
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import Sequencer from '@jest/test-sequencer';
+
+export default class CustomSequencer extends Sequencer.default {
+  sort(tests) {
+    const copyTests = Array.from(tests);
+    return copyTests.sort((testA, testB) => (testA.path > testB.path ? 1 : -1));
+  }
+}
