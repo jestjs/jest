f908b1798f87079087a46bc3becbeed516f64dd4
Simen Bekkhus <sbekkhus91@gmail.com>
2021-03-20 09:10:07 +0100

    chore: bump @types/babel__core to get rid of custom types

--- packages/babel-jest/src/loadBabelConfig.ts
+++ packages/babel-jest/src/loadBabelConfig.ts
@@ -6,15 +6,4 @@
  */
 
 // this is a separate file so it can be mocked in tests
-export {loadPartialConfig} from '@babel/core';
-
-import {
-  PartialConfig,
-  TransformOptions,
-  // @ts-expect-error: https://github.com/DefinitelyTyped/DefinitelyTyped/pull/51741
-  loadPartialConfigAsync as asyncVersion,
-} from '@babel/core';
-
-export const loadPartialConfigAsync: (
-  options?: TransformOptions,
-) => Promise<Readonly<PartialConfig> | null> = asyncVersion;
+export {loadPartialConfig, loadPartialConfigAsync} from '@babel/core';
