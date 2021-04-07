08dfd90b3c14bf7f885d31042ffbff203ccfc9f6
Gilles Yvetot <gilles@omniinc.com>
2021-04-07 10:29:15 -0700

    test: dummy test using dummy ESM test env

---
+++ e2e/test-environment-esm/EnvESM.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import NodeEnvironment from 'jest-environment-node';
+
+export default class Env extends NodeEnvironment {
+  constructor() {
+    super();
+    this.global = global;
+    this.moduleMocker = {};
+  }
+}
