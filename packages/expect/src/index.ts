f9ad1bcc656005739221e69c3cee1a68c5045dad
Matt Hargett <plaztiksyke@gmail.com>
2021-05-03 21:46:49 -0700

    getMessage() uses a call operator on the message field, so it can't ever be a string. The type annotation on it was already correct, just fix up these runtime checks and messages.

--- packages/expect/src/index.ts
+++ packages/expect/src/index.ts
@@ -373,14 +373,13 @@ const _validateResult = (result: any) => {
     typeof result !== 'object' ||
     typeof result.pass !== 'boolean' ||
     (result.message &&
-      typeof result.message !== 'string' &&
       typeof result.message !== 'function')
   ) {
     throw new Error(
       'Unexpected return from a matcher function.\n' +
         'Matcher functions should ' +
         'return an object in the following format:\n' +
-        '  {message?: string | function, pass: boolean}\n' +
+        '  {message?: () => string, pass: boolean}\n' +
         `'${matcherUtils.stringify(result)}' was returned`,
     );
   }
