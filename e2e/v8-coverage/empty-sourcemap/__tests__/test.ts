c7a46989eb907b19a6873ecc516a8a3edab72994
Simen Bekkhus <sbekkhus91@gmail.com>
2021-06-04 10:38:08 +0200

    move test of e2e so it's not executed as part of test run

--- e2e/v8-coverage/empty-sourcemap/__tests__/test.ts
+++ e2e/v8-coverage/empty-sourcemap/__tests__/test.ts
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import * as types from './types';
+import * as types from '../types';
 
 test('dummy-test', () => {
   expect(types).toEqual({});
