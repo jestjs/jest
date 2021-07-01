385be9f3b3d556efba02bcf6d0a3720866968774
Michael Glenn <mbg198618@gmail.com>
2021-07-01 09:50:40 -0500

    Create badge-status.java

---
+++ badge-status.java
@@ -0,0 +1,2 @@
+[tested with jest](https://img.shields.io/badge/tested_with-jest-99424f.svg)](https://github.com/facebook/jest)
+[jest](https://jestjs.io/img/jest-badge.svg)](https://github.com/facebook/jest)
