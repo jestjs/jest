feb621cc38fa53c0fd4cbc05c3625aad95b92e36
George Batalinski <george.batalinski@gmail.com>
2021-08-02 11:41:27 -0400

    testFailureExitCode.test.ts (17.552 s) -> testFailureExitCode.test.ts (11.895 s)

--- e2e/__tests__/testFailureExitCodeWhenFail.test.ts
+++ e2e/__tests__/testFailureExitCodeWhenFail.test.ts
@@ -15,24 +15,22 @@ const DIR = path.resolve(tmpdir(), 'test-failure-exit-code-test');
 beforeEach(() => cleanup(DIR));
 afterAll(() => cleanup(DIR));
 
-test('exits with a specified code when bailing from a failed test', () => {
+test('exits with a specified code when test fail', () => {
   writeFiles(DIR, {
     '__tests__/test.test.js': `test('test', () => { expect(1).toBe(2); });`,
-    '__tests__/test2.test.js': `test('test2', () => { expect(1).toBe(2); });`,
     'package.json': JSON.stringify({
       jest: {testEnvironment: 'node', testFailureExitCode: 99},
     }),
   });
 
-  let {exitCode} = runJest(DIR, ['--bail']);
+  let {exitCode} = runJest(DIR);
   expect(exitCode).toBe(99);
 
-  ({exitCode} = runJest(DIR, ['--bail', '--testFailureExitCode', '77']));
+  ({exitCode} = runJest(DIR, ['--testFailureExitCode', '77']));
   expect(exitCode).toBe(77);
 
   writeFiles(DIR, {
     '__tests__/test.test.js': `test('test', () => { expect(1).toBe(2); });`,
-    '__tests__/test2.test.js': `test('test2', () => { expect(1).toBe(2); });`,
     'package.json': JSON.stringify({
       jest: {testEnvironment: 'node'},
     }),
