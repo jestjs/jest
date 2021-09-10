9afb6efbda61a46a571cc5f37991dbe8d90e8b15
Simen Bekkhus <sbekkhus91@gmail.com>
2021-09-10 10:38:39 +0200

    snaps again

--- e2e/__tests__/resolveConditions.test.ts
+++ e2e/__tests__/resolveConditions.test.ts
@@ -19,7 +19,7 @@ beforeAll(() => {
 // The versions where vm.Module exists and commonjs with "exports" is not broken
 onNodeVersions('^12.16.0 || >=13.7.0', () => {
   test('resolves package exports correctly with custom resolver', () => {
-    // run multiple times to ensure there are noe caching errors
+    // run multiple times to ensure there are no caching errors
     for (let i = 0; i < 5; i++) {
       const {exitCode} = runJest(dir, [], {
         nodeOptions: '--experimental-vm-modules',
