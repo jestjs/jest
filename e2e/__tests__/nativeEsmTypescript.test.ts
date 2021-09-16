2f135d3b443d3790f7d7f1253e63df38d5a1caaf
Simen Bekkhus <sbekkhus91@gmail.com>
2021-09-16 10:56:33 +0200

    chore: remove node 13 as condition in some tests (#11880)

--- e2e/__tests__/nativeEsmTypescript.test.ts
+++ e2e/__tests__/nativeEsmTypescript.test.ts
@@ -12,7 +12,7 @@ import {json as runJest} from '../runJest';
 const DIR = resolve(__dirname, '../native-esm-typescript');
 
 // The versions where vm.Module exists and commonjs with "exports" is not broken
-onNodeVersions('^12.16.0 || >=13.7.0', () => {
+onNodeVersions('>=12.16.0', () => {
   test('runs TS test with native ESM', () => {
     const {exitCode, json} = runJest(DIR, [], {
       nodeOptions: '--experimental-vm-modules --no-warnings',
