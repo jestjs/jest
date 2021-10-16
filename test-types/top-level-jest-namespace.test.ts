05e0433a03a1a6954140c5ac530d98cab68427f8
mrazauskas <72159681+mrazauskas@users.noreply.github.com>
2021-10-16 13:48:00 +0300

    chore: remove unnecessary `empty.d.ts`

--- test-types/top-level-jest-namespace.test.ts
+++ test-types/top-level-jest-namespace.test.ts
@@ -3,8 +3,6 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- *
- * @type ./empty.d.ts
  */
 
 import {expectError, expectType} from 'mlh-tsd';
