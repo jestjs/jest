31e8198dd324ff819d75f53adc4fa563085902bc
mrazauskas <72159681+mrazauskas@users.noreply.github.com>
2021-11-10 19:59:41 +0200

    add copyright header

--- packages/expect/src/types-patch.ts
+++ packages/expect/src/types-patch.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import type * as jestMatcherUtils from 'jest-matcher-utils';
 
 // TODO remove in Jest 28, the utils must be imported from 'jest-matcher-utils'
