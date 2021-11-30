d8e4cbf1180381400383ae072b51ae3bdaa80e0c
Jon Wallsten <Jon.wallsten@gmail.com>
2021-11-30 11:15:50 +0100

    jest-worker: Avoid crash when "--max-old-space-size" inside process.execArgv

--- packages/jest-worker/src/workers/NodeThreadsWorker.ts
+++ packages/jest-worker/src/workers/NodeThreadsWorker.ts
@@ -67,7 +67,7 @@ export default class ExperimentalWorker implements WorkerInterface {
       eval: false,
       // Suppress --max_old_space_size flags while preserving others (like --harmony). See https://nodejs.org/api/worker_threads.html#new-workerfilename-options
       execArgv: process.execArgv.filter(
-        v => !/^--(max_old_space_size)/.test(v),
+        v => !/^--(max_old_space_size|max-old-space-size)/.test(v),
       ),
       // @ts-expect-error: added in newer versions
       resourceLimits: this._options.resourceLimits,
