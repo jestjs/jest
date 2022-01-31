105817882878a3ad8209cc9eac9309e389bba3c2
Biki-das <bikid475@gmail.com>
2022-01-31 20:16:09 +0530

    Added mockFn.mock.lastCall

--- packages/jest-mock/src/index.ts
+++ packages/jest-mock/src/index.ts
@@ -167,6 +167,10 @@ type MockFunctionState<T, Y extends Array<unknown>> = {
   instances: Array<T>;
   invocationCallOrder: Array<number>;
   /**
+   * Getter for retrieving the last call arguments
+   */
+  lastCall?: Y;
+  /**
    * List of results of calls to the mock function.
    */
   results: Array<MockFunctionResult>;
@@ -516,6 +520,9 @@ export class ModuleMocker {
       state = this._defaultMockState();
       this._mockState.set(f, state);
     }
+
+    state.lastCall = state.calls[state.calls.length - 1];
+
     return state;
   }
 
@@ -536,6 +543,7 @@ export class ModuleMocker {
       calls: [],
       instances: [],
       invocationCallOrder: [],
+      lastCall: undefined,
       results: [],
     };
   }
