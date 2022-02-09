5f1d31cc472b3c9e75f5d7e2e34285ca70ef4d5e
Simen Bekkhus <sbekkhus91@gmail.com>
2022-02-09 14:26:26 +0100

    chore: remove another `export = ` from `jest-haste-map`

--- packages/jest-haste-map/src/index.ts
+++ packages/jest-haste-map/src/index.ts
@@ -42,7 +42,7 @@ import type {
   SerializableModuleMap,
   WorkerMetadata,
 } from './types';
-import FSEventsWatcher = require('./watchers/FSEventsWatcher');
+import {FSEventsWatcher} from './watchers/FSEventsWatcher';
 // @ts-expect-error: not converted to TypeScript - it's a fork: https://github.com/facebook/jest/pull/10919
 import NodeWatcher from './watchers/NodeWatcher';
 // @ts-expect-error: not converted to TypeScript - it's a fork: https://github.com/facebook/jest/pull/5387
