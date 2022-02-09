b89a1bbe3652a3f5a8969ce768f3b74298ca021d
Simen Bekkhus <sbekkhus91@gmail.com>
2022-02-09 15:04:22 +0100

    chore: remove `export =` from e2e tests

--- e2e/coverage-remapping/covered.ts
+++ e2e/coverage-remapping/covered.ts
@@ -7,11 +7,11 @@
 
 /* eslint-disable local/ban-types-eventually */
 
-export = function difference(a: number, b: number): number {
+export function difference(a: number, b: number): number {
   const branch1: boolean = true ? 1 : 0;
   const branch2: boolean = true ? 1 : 0;
   const branch3: boolean = true || true || false;
   const fn: Function = true ? () => null : () => null;
 
   return a - b;
-};
+}
