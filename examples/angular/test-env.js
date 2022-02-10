c7ed620192c0f74f7839d3ac46a7a2feb9861d79
Simen Bekkhus <sbekkhus91@gmail.com>
2022-02-10 15:27:38 +0100

    link PR

--- examples/angular/test-env.js
+++ examples/angular/test-env.js
@@ -7,6 +7,7 @@ const {
 module.exports = class AngularEnv extends JSDOMTestEnvironment {
   exportConditions() {
     // we need to include `node` as `rxjs` defines `node`, `es2015`, `default`, not `browser` or `require`
+    // https://github.com/ReactiveX/rxjs/pull/6821
     return super.exportConditions().concat('node');
   }
 };
