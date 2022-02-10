cbc41c22d67d4cc353be1f88f489a46ed1ec787d
Simen Bekkhus <sbekkhus91@gmail.com>
2022-02-10 11:21:17 +0100

    chore: update to `eslint@8` (#12356)

--- packages/jest-snapshot/src/index.ts
+++ packages/jest-snapshot/src/index.ts
@@ -5,8 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-/* eslint-disable local/ban-types-eventually */
-
 import * as fs from 'graceful-fs';
 import type {Config} from '@jest/types';
 import type {FS as HasteFS} from 'jest-haste-map';
