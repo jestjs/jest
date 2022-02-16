1e230f01975310f99052f7f7242cbba7fb44824c
mrazauskas <72159681+mrazauskas@users.noreply.github.com>
2022-02-16 13:25:03 +0200

    move SnapshotMatchers

--- packages/jest-expect/src/types.ts
+++ packages/jest-expect/src/types.ts
@@ -6,7 +6,11 @@
  */
 
 import type {AsymmetricMatchers, BaseExpect, Matchers} from 'expect';
-import type {SnapshotState, addSerializer} from 'jest-snapshot';
+import type {
+  SnapshotMatchers,
+  SnapshotState,
+  addSerializer,
+} from 'jest-snapshot';
 
 export type JestExpect = {
   <T = unknown>(actual: T): JestMatchers<void, T> &
@@ -41,46 +45,6 @@ type PromiseMatchers<T = unknown> = {
   resolves: JestMatchers<Promise<void>, T> &
     Inverse<JestMatchers<Promise<void>, T>>;
 };
-
-interface SnapshotMatchers<R extends void | Promise<void>, T> {
-  /**
-   * This ensures that a value matches the most recent snapshot with property matchers.
-   * Check out [the Snapshot Testing guide](https://jestjs.io/docs/snapshot-testing) for more information.
-   */
-  toMatchSnapshot(hint?: string): R;
-  /**
-   * This ensures that a value matches the most recent snapshot.
-   * Check out [the Snapshot Testing guide](https://jestjs.io/docs/snapshot-testing) for more information.
-   */
-  toMatchSnapshot<U extends Record<keyof T, unknown>>(
-    propertyMatchers: Partial<U>,
-    hint?: string,
-  ): R;
-  /**
-   * This ensures that a value matches the most recent snapshot with property matchers.
-   * Instead of writing the snapshot value to a .snap file, it will be written into the source code automatically.
-   * Check out [the Snapshot Testing guide](https://jestjs.io/docs/snapshot-testing) for more information.
-   */
-  toMatchInlineSnapshot(snapshot?: string): R;
-  /**
-   * This ensures that a value matches the most recent snapshot with property matchers.
-   * Instead of writing the snapshot value to a .snap file, it will be written into the source code automatically.
-   * Check out [the Snapshot Testing guide](https://jestjs.io/docs/snapshot-testing) for more information.
-   */
-  toMatchInlineSnapshot<U extends Record<keyof T, unknown>>(
-    propertyMatchers: Partial<U>,
-    snapshot?: string,
-  ): R;
-  /**
-   * Used to test that a function throws a error matching the most recent snapshot when it is called.
-   */
-  toThrowErrorMatchingSnapshot(hint?: string): R;
-  /**
-   * Used to test that a function throws a error matching the most recent snapshot when it is called.
-   * Instead of writing the snapshot value to a .snap file, it will be written into the source code automatically.
-   */
-  toThrowErrorMatchingInlineSnapshot(snapshot?: string): R;
-}
 
 declare module 'expect' {
   interface MatcherState {
