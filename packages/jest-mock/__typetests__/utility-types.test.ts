af1038d530602e8999662f1001e23a2bfc8a8c2c
mrazauskas <72159681+mrazauskas@users.noreply.github.com>
2022-02-19 15:01:29 +0200

    more type tests

--- packages/jest-mock/__typetests__/utility-types.test.ts
+++ packages/jest-mock/__typetests__/utility-types.test.ts
@@ -5,21 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import {expectType} from 'tsd-lite';
-import type {MethodKeys, PropertyKeys} from 'jest-mock';
-
-interface SomeObject {
-  methodA(): void;
-  methodB(a: string): number;
-  propertyA: number;
-  propertyB: string;
-}
-
-declare const objectMethods: MethodKeys<SomeObject>;
-declare const objectProperties: PropertyKeys<SomeObject>;
-
-expectType<'methodA' | 'methodB'>(objectMethods);
-expectType<'propertyA' | 'propertyB'>(objectProperties);
+import {expectAssignable, expectNotAssignable, expectType} from 'tsd-lite';
+import type {
+  ClassLike,
+  FunctionLike,
+  MethodKeys,
+  PropertyKeys,
+} from 'jest-mock';
 
 class SomeClass {
   propertyB = 123;
@@ -44,8 +36,49 @@ class SomeClass {
   }
 }
 
+interface SomeObject {
+  methodA(): void;
+  methodB(a: string): number;
+  propertyA: number;
+  propertyB: string;
+}
+
+// ClassLike
+
+expectAssignable<ClassLike>(SomeClass);
+expectNotAssignable<ClassLike>(() => {});
+expectNotAssignable<ClassLike>(function abc() {
+  return;
+});
+expectNotAssignable<ClassLike>({} as SomeObject);
+expectNotAssignable<ClassLike>('abc');
+expectNotAssignable<ClassLike>(123);
+expectNotAssignable<ClassLike>(false);
+
+// FunctionLike
+
+expectAssignable<FunctionLike>(() => {});
+expectAssignable<FunctionLike>(function abc() {
+  return;
+});
+expectNotAssignable<FunctionLike>({} as SomeObject);
+expectNotAssignable<FunctionLike>(SomeClass);
+expectNotAssignable<FunctionLike>('abc');
+expectNotAssignable<FunctionLike>(123);
+expectNotAssignable<FunctionLike>(false);
+
+// MethodKeys
+
 declare const classMethods: MethodKeys<SomeClass>;
-declare const classProperties: PropertyKeys<SomeClass>;
+declare const objectMethods: MethodKeys<SomeObject>;
 
 expectType<'methodA' | 'methodB'>(classMethods);
+expectType<'methodA' | 'methodB'>(objectMethods);
+
+// PropertyKeys
+
+declare const classProperties: PropertyKeys<SomeClass>;
+declare const objectProperties: PropertyKeys<SomeObject>;
+
 expectType<'propertyA' | 'propertyB' | 'propertyC'>(classProperties);
+expectType<'propertyA' | 'propertyB'>(objectProperties);
