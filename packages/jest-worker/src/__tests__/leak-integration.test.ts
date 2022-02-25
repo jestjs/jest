fc3d0341a0a5d9b1b4b7ecb506f2698e6b15ccd2
IIIEII <al.iiieii@gmail.com>
2022-02-25 16:25:59 +0300

    Eliminate Farm work results leakage by clearing callback reference (#12497)

--- packages/jest-worker/src/__tests__/leak-integration.test.ts
+++ packages/jest-worker/src/__tests__/leak-integration.test.ts
@@ -11,30 +11,71 @@ import {writeFileSync} from 'graceful-fs';
 import LeakDetector from 'jest-leak-detector';
 import {Worker} from '../../build/index';
 
-let workerFile!: string;
-beforeAll(() => {
-  workerFile = join(tmpdir(), 'baz.js');
-  writeFileSync(workerFile, 'module.exports.fn = () => {};');
-});
-
-let worker!: Worker;
-beforeEach(() => {
-  worker = new Worker(workerFile, {
-    enableWorkerThreads: true,
-    exposedMethods: ['fn'],
-  });
-});
-afterEach(async () => {
-  await worker.end();
-});
-
-it('does not retain arguments after a task finished', async () => {
-  let leakDetector!: LeakDetector;
-  await new Promise((resolve, reject) => {
-    const obj = {};
-    leakDetector = new LeakDetector(obj);
-    (worker as any).fn(obj).then(resolve, reject);
+describe('WorkerThreads leaks', () => {
+  let workerFile!: string;
+  beforeAll(() => {
+    workerFile = join(tmpdir(), 'baz.js');
+    writeFileSync(workerFile, 'module.exports.fn = () => {};');
   });
 
-  expect(await leakDetector.isLeaking()).toBe(false);
+  let worker!: Worker;
+  beforeEach(() => {
+    worker = new Worker(workerFile, {
+      enableWorkerThreads: true,
+      exposedMethods: ['fn'],
+    });
+  });
+  afterEach(async () => {
+    await worker.end();
+  });
+
+  it('does not retain arguments after a task finished', async () => {
+    let leakDetector!: LeakDetector;
+    await new Promise((resolve, reject) => {
+      const obj = {};
+      leakDetector = new LeakDetector(obj);
+      (worker as any).fn(obj).then(resolve, reject);
+    });
+
+    expect(await leakDetector.isLeaking()).toBe(false);
+  });
+});
+
+describe('Worker leaks', () => {
+  let workerFile!: string;
+  beforeAll(() => {
+    workerFile = join(tmpdir(), 'baz.js');
+    writeFileSync(workerFile, 'module.exports.fn = (obj) => [obj];');
+  });
+
+  let worker!: Worker;
+  beforeEach(() => {
+    worker = new Worker(workerFile, {
+      enableWorkerThreads: false,
+      exposedMethods: ['fn'],
+    });
+  });
+  afterEach(async () => {
+    await worker.end();
+  });
+
+  it('does not retain result after next task call', async () => {
+    let leakDetector!: LeakDetector;
+    await new Promise((resolve, reject) => {
+      const obj = {};
+      (worker as any)
+        .fn(obj)
+        .then((result: unknown) => {
+          leakDetector = new LeakDetector(result);
+          return result;
+        })
+        .then(resolve, reject);
+    });
+    await new Promise((resolve, reject) => {
+      const obj = {};
+      (worker as any).fn(obj).then(resolve, reject);
+    });
+
+    expect(await leakDetector.isLeaking()).toBe(false);
+  });
 });
