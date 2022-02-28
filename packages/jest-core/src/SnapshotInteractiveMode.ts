96bc87f8f68eb34bc2c572153d0268a28b21bd59
Biki-das <bikid475@gmail.com>
2022-02-28 13:11:24 +0530

    Disabled rule

--- packages/jest-core/src/SnapshotInteractiveMode.ts
+++ packages/jest-core/src/SnapshotInteractiveMode.ts
@@ -204,7 +204,8 @@ export default class SnapshotInteractiveMode {
   }
 
   updateWithResults(results: AggregatedResult): void {
-    const hasSnapshotFailure = results.snapshot.failure != null;
+    // eslint-disable-next-line no-implicit-coercion
+    const hasSnapshotFailure = !!results.snapshot.failure;
     if (hasSnapshotFailure) {
       this._drawUIOverlay();
       return;
