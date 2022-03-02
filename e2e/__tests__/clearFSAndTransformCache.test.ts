e203476cd25b5672456e0c6f723f53b4335803e7
Simen Bekkhus <sbekkhus91@gmail.com>
2022-03-02 09:55:01 +0100

    tweaks

--- e2e/__tests__/clearFSAndTransformCache.test.ts
+++ e2e/__tests__/clearFSAndTransformCache.test.ts
@@ -1,14 +1,16 @@
+import {tmpdir} from 'os';
 import * as path from 'path';
 import {cleanup, writeFiles} from '../Utils';
 import runJest from '../runJest';
 
-const dirName = 'clear_FS_and_transform_cache';
-const dir = path.resolve(__dirname, `../${dirName}`);
+const dir = path.resolve(tmpdir(), 'clear_FS_and_transform_cache');
 const testFileContent = `
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 
-const asboulteTestHelperFile = path.resolve(__dirname, './testHelper.js');
+const absoluteTestHelperFile = path.resolve(__dirname, './testHelper.js');
 
 test('value is 1', () => {
   const value = require('./testHelper');
@@ -16,7 +18,7 @@ test('value is 1', () => {
 });
 
 test('value is 1 after file is changed', () => {
-  fs.writeFileSync(asboulteTestHelperFile, 'module.exports = 2;');
+  fs.writeFileSync(absoluteTestHelperFile, 'module.exports = 2;');
   const value = require('./testHelper');
   expect(value).toBe(1);
 });
@@ -26,9 +28,9 @@ test('value is 2 after calling "jest.resetModules"', () => {
   const value = require('./testHelper');
   expect(value).toBe(2);
 });
-
 `;
 
+beforeAll(() => cleanup(dir));
 afterEach(() => cleanup(dir));
 
 test('clear FS and transform cache', () => {
@@ -37,6 +39,6 @@ test('clear FS and transform cache', () => {
     'test.js': testFileContent,
     'testHelper.js': 'module.exports = 1;',
   });
-  const {exitCode} = runJest(dirName);
+  const {exitCode} = runJest(dir);
   expect(exitCode).toBe(0);
 });
