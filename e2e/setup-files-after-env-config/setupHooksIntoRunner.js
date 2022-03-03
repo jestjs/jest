bf8ddb3ed2514e270f1ad55ac4c471d98d26630e
Biki-das <bikid475@gmail.com>
2022-03-03 22:07:49 +0530

    Added =

--- e2e/setup-files-after-env-config/setupHooksIntoRunner.js
+++ e2e/setup-files-after-env-config/setupHooksIntoRunner.js
@@ -5,4 +5,4 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-globalThis.describeDefined = globalThis.describe = null;
+globalThis.describeDefined = globalThis.describe != null;
