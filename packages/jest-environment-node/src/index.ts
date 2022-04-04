c7b15385a67c4380af9eea83638f8203b8472225
Araxeus <oaeben@gmail.com>
2022-04-04 22:07:47 +0300

    fix(jest-environment-node): Add `structuredClone` to globals (#12631)

--- packages/jest-environment-node/src/index.ts
+++ packages/jest-environment-node/src/index.ts
@@ -95,6 +95,12 @@ export default class NodeEnvironment implements JestEnvironment<Timer> {
       global.atob = atob;
       global.btoa = btoa;
     }
+    // structuredClone is global in Node >= 17
+    // @ts-expect-error type definition for structuredClone is missing
+    if (typeof structuredClone !== 'undefined') {
+      // @ts-expect-error type definition for structuredClone is missing
+      global.structuredClone = structuredClone;
+    }
     installCommonGlobals(global, projectConfig.globals);
 
     this.moduleMocker = new ModuleMocker(global);
