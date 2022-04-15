7d05164d8ab6f552a984aaf67554a251c445a7b6
Rob Hogan <robhogan@fb.com>
2022-04-15 19:31:00 +0100

    chore(checkCopyrightHeaders): Allow "Meta Platforms"

--- scripts/checkCopyrightHeaders.mjs
+++ scripts/checkCopyrightHeaders.mjs
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -117,6 +117,13 @@ const INCLUDED_PATTERNS = [
 ];
 
 const COPYRIGHT_LICENSE = [
+  ' * Copyright (c) Meta Platforms, Inc. and affiliates.',
+  ' *',
+  ' * This source code is licensed under the MIT license found in the',
+  ' * LICENSE file in the root directory of this source tree.',
+].join('\n');
+
+const LEGACY_COPYRIGHT_LICENSE = [
   ' * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.',
   ' *',
   ' * This source code is licensed under the MIT license found in the',
@@ -125,7 +132,11 @@ const COPYRIGHT_LICENSE = [
 
 function needsCopyrightHeader(file) {
   const contents = getFileContents(file);
-  return contents.trim().length > 0 && !contents.includes(COPYRIGHT_LICENSE);
+  return (
+    contents.trim().length > 0 &&
+    !contents.includes(COPYRIGHT_LICENSE) &&
+    !contents.includes(LEGACY_COPYRIGHT_LICENSE)
+  );
 }
 
 function check() {
