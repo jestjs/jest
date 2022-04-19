5183c15a8e14089351d6403f4fdb72a6705aa379
Simen Bekkhus <sbekkhus91@gmail.com>
2022-04-19 15:59:29 +0200

    fix: pass `root` in babel config (#12689)
    Co-authored-by: Bradford Lemley <bradfordlemley@gmail.com>

--- examples/react-native/jest.config.js
+++ examples/react-native/jest.config.js
@@ -3,9 +3,6 @@ const {resolve} = require('path');
 module.exports = {
   preset: 'react-native',
   testEnvironment: 'jsdom',
-  transform: {
-    '\\.(js|ts|tsx)$': require.resolve('react-native/jest/preprocessor.js'),
-  },
   // this is specific to the Jest repo, not generally needed (the files we ignore will be in node_modules which is ignored by default)
   transformIgnorePatterns: [resolve(__dirname, '../../packages')],
 };
