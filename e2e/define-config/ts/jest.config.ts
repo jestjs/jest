f294b3241521e90431acb091fbad6c661a64e32e
Tom Mrazauskas <tom@mrazauskas.de>
2022-05-04 10:17:24 +0300

    from 'jest-config'

--- e2e/define-config/ts/jest.config.ts
+++ e2e/define-config/ts/jest.config.ts
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import {JestConfig, defineConfig} from 'jest';
+import {JestConfig, defineConfig} from 'jest-config';
 
 async function getVerbose() {
   return true;
