9faefc59dae26ebbf2e41bd084889b8dd9987c70
Michał Warda <michal.warda@elpassion.pl>
2022-05-06 11:32:30 +0200

    Throw when trying to use `failing` in jasmine

--- packages/jest-jasmine2/src/jasmineAsyncInstall.ts
+++ packages/jest-jasmine2/src/jasmineAsyncInstall.ts
@@ -224,7 +224,11 @@ function makeConcurrent(
   // each is binded after the function is made concurrent, so for now it is made noop
   concurrentFn.each = () => () => {};
   // "failing" is not implemented in jasmine. It has to be defined because of TS types.
-  concurrentFn.failing = () => () => {};
+  concurrentFn.failing = () => () => {
+    throw new Error(
+      'Jest: `failing` tests are only supported in `jest-circus`.',
+    );
+  };
   return concurrentFn;
 }
 
