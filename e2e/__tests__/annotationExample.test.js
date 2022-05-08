300e7eb00eab4708c6930cbb7d20b62ae35d0ff5
Tom Mrazauskas <tom@mrazauskas.de>
2022-05-08 22:29:41 +0300

    remove examples

--- e2e/__tests__/annotationExample.test.js
+++
@@ -1,9 +0,0 @@
-test('failing snapshot example', () => {
-  expect('nothing').toMatchSnapshot();
-});
-
-describe('nested', () => {
-  test('failing example', () => {
-    expect(10).toBe(1);
-  });
-});
