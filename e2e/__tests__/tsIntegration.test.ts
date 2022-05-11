cdb9f206b3256e06416aa314e503cfce7cbff8b2
Tom Mrazauskas <tom@mrazauskas.de>
2022-05-11 09:29:31 +0300

    fix `` and add type check tests

--- e2e/__tests__/tsIntegration.test.ts
+++ e2e/__tests__/tsIntegration.test.ts
@@ -8,7 +8,7 @@
 import * as path from 'path';
 import {onNodeVersions} from '@jest/test-utils';
 import {cleanup, writeFiles} from '../Utils';
-import {getConfig} from '../runJest';
+import runJest, {getConfig} from '../runJest';
 
 const DIR = path.resolve(__dirname, '../ts-node-integration');
 
@@ -16,13 +16,14 @@ beforeEach(() => cleanup(DIR));
 afterAll(() => cleanup(DIR));
 
 describe('when `Config` type is imported from "@jest/types"', () => {
-  test('works with object config exported from TS file', () => {
+  test('with object config exported from TS file', () => {
     writeFiles(DIR, {
       '__tests__/dummy.test.js': "test('dummy', () => expect(123).toBe(123));",
-      'jest.config.ts':
-        'import type {Config} from "@jest/types";' +
-        'const config: Config.InitialOptions = {displayName: "ts-object-config", verbose: true};' +
-        'export default config;',
+      'jest.config.ts': `
+        import type {Config} from '@jest/types';
+        const config: Config.InitialOptions = {displayName: 'ts-object-config', verbose: true};
+        export default config;
+        `,
       'package.json': '{}',
     });
 
@@ -33,16 +34,17 @@ describe('when `Config` type is imported from "@jest/types"', () => {
     expect(globalConfig.verbose).toBe(true);
   });
 
-  test('works with function config exported from TS file', () => {
+  test('with function config exported from TS file', () => {
     writeFiles(DIR, {
       '__tests__/dummy.test.js': "test('dummy', () => expect(123).toBe(123));",
-      'jest.config.ts':
-        'import type {Config} from "@jest/types";' +
-        'async function getVerbose() {return true;}' +
-        'export default async (): Promise<Config.InitialOptions> => {' +
-        '  const verbose: Config.InitialOptions["verbose"] = await getVerbose();' +
-        '  return {displayName: "ts-async-function-config", verbose};' +
-        '};',
+      'jest.config.ts': `
+        import type {Config} from '@jest/types';
+        async function getVerbose() {return true;}
+        export default async (): Promise<Config.InitialOptions> => {
+          const verbose: Config.InitialOptions['verbose'] = await getVerbose();
+          return {displayName: 'ts-async-function-config', verbose};
+        };
+        `,
       'package.json': '{}',
     });
 
@@ -53,16 +55,35 @@ describe('when `Config` type is imported from "@jest/types"', () => {
     expect(globalConfig.verbose).toBe(true);
   });
 
+  test('type checks the config file', () => {
+    writeFiles(DIR, {
+      '__tests__/dummy.test.js': "test('dummy', () => expect(123).toBe(123));",
+      'jest.config.ts': `
+        import type {Config} from '@jest/types';
+        const config: Config.InitialOptions = {testTimeout: '10000'};
+        export default config;
+        `,
+      'package.json': '{}',
+    });
+
+    const {stderr, exitCode} = runJest(DIR);
+
+    expect(stderr).toMatch(
+      "jest.config.ts(2,40): error TS2322: Type 'string' is not assignable to type 'number'.",
+    );
+    expect(exitCode).toBe(1);
+  });
+
   // The versions where vm.Module exists and commonjs with "exports" is not broken
   onNodeVersions('>=12.16.0', () => {
     test('works with object config exported from TS file when package.json#type=module', () => {
       writeFiles(DIR, {
-        '__tests__/dummy.test.js':
-          "test('dummy', () => expect(123).toBe(123));",
-        'jest.config.ts':
-          'import type {Config} from "@jest/types";' +
-          'const config: Config.InitialOptions = {displayName: "ts-esm-object-config", verbose: true};' +
-          'export default config;',
+        '__tests__/dummy.test.js': "test('dummy', () => expect(12).toBe(12));",
+        'jest.config.ts': `
+          import type {Config} from '@jest/types';
+          const config: Config.InitialOptions = {displayName: 'ts-esm-object-config', verbose: true};
+          export default config;
+          `,
         'package.json': '{"type": "module"}',
       });
 
@@ -75,15 +96,15 @@ describe('when `Config` type is imported from "@jest/types"', () => {
 
     test('works with function config exported from TS file when package.json#type=module', () => {
       writeFiles(DIR, {
-        '__tests__/dummy.test.js':
-          "test('dummy', () => expect(123).toBe(123));",
-        'jest.config.ts':
-          'import type {Config} from "@jest/types";' +
-          'async function getVerbose() {return true;}' +
-          'export default async (): Promise<Config.InitialOptions> => {' +
-          '  const verbose: Config.InitialOptions["verbose"] = await getVerbose();' +
-          '  return {displayName: "ts-esm-async-function-config", verbose};' +
-          '};',
+        '__tests__/dummy.test.js': "test('dummy', () => expect(12).toBe(12));",
+        'jest.config.ts': `
+          import type {Config} from '@jest/types';
+          async function getVerbose() {return true;}
+          export default async (): Promise<Config.InitialOptions> => {
+            const verbose: Config.InitialOptions['verbose'] = await getVerbose();
+            return {displayName: 'ts-esm-async-function-config', verbose};
+          };
+          `,
         'package.json': '{"type": "module"}',
       });
 
@@ -92,6 +113,25 @@ describe('when `Config` type is imported from "@jest/types"', () => {
       expect(configs).toHaveLength(1);
       expect(configs[0].displayName?.name).toBe('ts-esm-async-function-config');
       expect(globalConfig.verbose).toBe(true);
+    });
+
+    test('type checks the config file when package.json#type=module', () => {
+      writeFiles(DIR, {
+        '__tests__/dummy.test.js': "test('dummy', () => expect(12).toBe(12));",
+        'jest.config.ts': `
+          import type {Config} from '@jest/types';
+          const config: Config.InitialOptions = {testTimeout: '10000'};
+          export default config;
+          `,
+        'package.json': '{"type": "module"}',
+      });
+
+      const {stderr, exitCode} = runJest(DIR);
+
+      expect(stderr).toMatch(
+        "jest.config.ts(2,40): error TS2322: Type 'string' is not assignable to type 'number'.",
+      );
+      expect(exitCode).toBe(1);
     });
   });
 });
