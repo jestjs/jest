8c2539397b7c4011430983de0a530c30573d5bab
Tom Mrazauskas <tom@mrazauskas.de>
2022-05-30 18:33:10 +0300

    use docusaurus-remark-plugin-tab-blocks

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -56,7 +56,7 @@ module.exports = {
           sidebarPath: path.resolve(__dirname, './sidebars.json'),
           remarkPlugins: [
             [require('@docusaurus/remark-plugin-npm2yarn'), {sync: true}],
-            require('./src/remark/code-tabs-plugin'),
+            require('docusaurus-remark-plugin-tab-blocks'),
           ],
         },
         blog: {
--- website/src/remark/__tests__/__fixtures__/base.md
+++
@@ -1,7 +0,0 @@
-```js tab
-console.log('this is JS');
-```
-
-```ts tab
-console.log('this is TS');
-```
