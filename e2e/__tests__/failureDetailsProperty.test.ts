1f908e167253c36a7b8b8105ad40590ef8ca2e0b
Simen Bekkhus <sbekkhus91@gmail.com>
2022-07-17 22:14:51 +0200

    jasmine snap

--- e2e/__tests__/failureDetailsProperty.test.ts
+++ e2e/__tests__/failureDetailsProperty.test.ts
@@ -90,7 +90,7 @@ test('that the failureDetails property is set', () => {
             "actual": "",
             "error": Object {
               "matcherResult": Object {
-                "actual": "Object {
+                "actual": "{
         "p1": "hello",
         "p2": "world",
       }",
@@ -105,7 +105,7 @@ test('that the failureDetails property is set', () => {
       - Snapshot  - 1
       + Received  + 1
 
-        Object {
+        {
           "p1": "hello",
       -   "p2": "sunshine",
       +   "p2": "world",
@@ -123,7 +123,7 @@ test('that the failureDetails property is set', () => {
       - Snapshot  - 1
       + Received  + 1
 
-        Object {
+        {
           "p1": "hello",
       -   "p2": "sunshine",
       +   "p2": "world",
@@ -136,7 +136,7 @@ test('that the failureDetails property is set', () => {
       - Snapshot  - 1
       + Received  + 1
 
-        Object {
+        {
           "p1": "hello",
       -   "p2": "sunshine",
       +   "p2": "world",
