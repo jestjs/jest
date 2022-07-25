233477ef8ccadd6d3f4db70a4fab068c5b183b06
Paul Hawxby <phawxby@gmail.com>
2022-07-25 17:42:49 +0100

    chore: add facebook header

--- packages/jest-worker/src/workers/__tests__/ChildProcessWorkerEdgeCases.test.js
+++ packages/jest-worker/src/workers/__tests__/ChildProcessWorkerEdgeCases.test.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import {rm, writeFile} from 'fs/promises';
 import {join} from 'path';
 import {transformFileAsync} from '@babel/core';
