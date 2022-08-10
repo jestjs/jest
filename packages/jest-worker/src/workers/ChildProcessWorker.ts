d6b2db01db595c15d5982003f269d58ad55b681d
Paul Hawxby <phawxby@gmail.com>
2022-08-10 21:18:21 +0100

    chore: try adding more possible detections

--- packages/jest-worker/src/workers/ChildProcessWorker.ts
+++ packages/jest-worker/src/workers/ChildProcessWorker.ts
@@ -69,7 +69,6 @@ export default class ChildProcessWorker
   private _stdout: ReturnType<typeof mergeStream> | null;
   private _stderr: ReturnType<typeof mergeStream> | null;
 
-  private _stdoutBuffer: Array<Buffer> = [];
   private _stderrBuffer: Array<Buffer> = [];
 
   private _memoryUsagePromise: Promise<number> | undefined;
@@ -122,7 +121,6 @@ export default class ChildProcessWorker
       console.warn('Unable to detect out of memory event if silent === false');
     }
 
-    this._stdoutBuffer = [];
     this._stderrBuffer = [];
 
     const options: ForkOptions = {
@@ -150,12 +148,6 @@ export default class ChildProcessWorker
       }
 
       this._stdout.add(this._child.stdout);
-
-      this._child.stdout.on('data', chunk => {
-        if (chunk) {
-          this._stdoutBuffer.push(Buffer.from(chunk));
-        }
-      });
     }
 
     if (this._child.stderr) {
@@ -216,28 +208,58 @@ export default class ChildProcessWorker
     exitCode?: number | null,
     signal?: NodeJS.Signals | null,
   ): void {
-    for (const buffer of [this._stderrBuffer, this._stdoutBuffer]) {
-      try {
-        const bufferStr = Buffer.concat(buffer).toString('utf8');
+    try {
+      const bufferStr = Buffer.concat(this._stderrBuffer).toString('utf8');
 
-        if (bufferStr.includes('heap out of memory')) {
-          if (
-            this.state === WorkerStates.OK ||
-            this.state === WorkerStates.STARTING
-          ) {
-            this.state = WorkerStates.OUT_OF_MEMORY;
-          }
+      if (
+        bufferStr.includes('heap out of memory') ||
+        bufferStr.includes('allocation failure;') ||
+        bufferStr.includes('Last few GCs')
+      ) {
+        if (
+          this.state === WorkerStates.OK ||
+          this.state === WorkerStates.STARTING
+        ) {
+          this.state = WorkerStates.OUT_OF_MEMORY;
         }
-      } catch (err) {
-        console.error('Error looking for out of memory crash', err);
       }
+    } catch (err) {
+      console.error('Error looking for out of memory crash', err);
     }
 
     // This is to try and work around a weird edge case when for some reason Windows doesn't feel
-    // like sending stderr, so we end up with no data to look at.
+    // like sending all of stderr, or even any of it in some cases, so we end up with not much
+    // to go on.
+    // Just to add to the confusion out of memory on windows can occur with either
+    // -> exitCode 134 | signal null
+    // OR (for some unknown reason)
+    // -> exitCode null | signal 'SIGTERM'
+    // Here are some samples
+
+    // exitCode: 134,
+    // signal: null,
+    // stderr: '\r\n' +
+    //   '<--- Last few GCs --->\r\n' +
+    //   '\r\n' +
+    //   '[6996:000001FCB84D40B0]      263 ms: Mark-sweep 60.1 (9',
+    // platform: 'win32'
+
+    // exitCode: null,
+    // signal: 'SIGTERM',
+    // stderr: '2.7) -> 37.7 (70.3) MB, 33.7 / 0.0 ms  (average mu = 0.539, current mu = 0.562) allocation failure; scavenge might not succeed\r\n' +
+    //   '[6996:000001FCB84D40B0]      357 ms: Mark-sweep 88.1 (120.7) -> 54.4 (87.1) MB, 32.3 / 0.0 ms  (average mu = 0.608, current mu = 0.656) allocation failure; scavenge might not succeed\r\n' +
+    //   '\r\n' +
+    //   '\r\n' +
+    //   '<--- JS stacktrace --->\r\n' +
+    //   '\r\n',
+    // platform: 'win32'
+
     if (process.platform === 'win32') {
-      if (!this._stderrBuffer.length && !this._stdoutBuffer.length) {
-        if (exitCode === SIGABRT_EXIT_CODE && signal === null) {
+      if (!this._stderrBuffer.length) {
+        if (
+          (exitCode === SIGABRT_EXIT_CODE && signal === null) ||
+          (exitCode === null && signal === 'SIGTERM')
+        ) {
           if (
             this.state === WorkerStates.OK ||
             this.state === WorkerStates.STARTING
@@ -346,7 +368,6 @@ export default class ChildProcessWorker
       exitCode,
       signal,
       stderr: Buffer.concat(this._stderrBuffer).toString('utf8'),
-      stdout: Buffer.concat(this._stdoutBuffer).toString('utf8'),
       childExitCode: this._child.exitCode,
       childConnected: this._child.connected,
       childKilled: this._child.killed,
