9331bd228a7a455c88629c395b5138ce69354ce6
Paul Hawxby <phawxby@gmail.com>
2022-08-10 14:52:03 +0100

    chore: make noisy

--- packages/jest-worker/src/workers/__tests__/WorkerEdgeCases.test.js
+++ packages/jest-worker/src/workers/__tests__/WorkerEdgeCases.test.js
@@ -175,6 +175,7 @@ describe.each([
             orderOfEvents.push(state);
           },
         },
+        silent: false,
         workerPath: join(__dirname, '__fixtures__', 'EdgeCasesWorker'),
       });
     });
