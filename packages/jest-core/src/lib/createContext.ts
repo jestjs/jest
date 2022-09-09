b8913c271cfc26ab102dd11e4e33b9e2ba6bda08
Biki-das <bikid475@gmail.com>
2022-09-10 01:08:46 +0530

    Test fix

--- packages/jest-core/src/lib/createContext.ts
+++ packages/jest-core/src/lib/createContext.ts
@@ -7,12 +7,13 @@
 
 import type {TestContext} from '@jest/test-result';
 import type {Config} from '@jest/types';
-import type {IHasteFS,ImoduleMap} from 'jest-haste-map';
+import type {IHasteFS,IModuleMap} from 'jest-haste-map';
 import Runtime from 'jest-runtime';
 
 export default function createContext(
   config: Config.ProjectConfig,
-  {hasteFS:IHasteFS, moduleMap:ImoduleMap}:
+  {hasteFS,moduleMap}:
+  {hasteFS: IHasteFS; moduleMap: IModuleMap},
 ): TestContext {
   return {
     config,
