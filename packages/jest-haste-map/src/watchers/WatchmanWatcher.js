6c8998acb1b803db30a0ed3ac9e89966dfa861ed
Yadd <yadd@debian.org>
2022-09-13 15:38:09 +0200

    Replace deprecated call to __proto__ by Object.setPrototypeOf

--- packages/jest-haste-map/src/watchers/WatchmanWatcher.js
+++ packages/jest-haste-map/src/watchers/WatchmanWatcher.js
@@ -35,7 +35,7 @@ export default function WatchmanWatcher(dir, opts) {
 }
 
 // eslint-disable-next-line no-proto
-WatchmanWatcher.prototype.__proto__ = EventEmitter.prototype;
+Object.setPrototypeOf(WatchmanWatcher.prototype, EventEmitter.prototype);
 
 /**
  * Run the watchman `watch` command on the root and subscribe to changes.
