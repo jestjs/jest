9741e7e0f592658c82d130ecaa4226ca0bb9a52e
Simen Bekkhus <sbekkhus91@gmail.com>
2022-09-23 15:01:47 +0200

    no arrow

--- packages/jest-util/src/isPromise.ts
+++ packages/jest-util/src/isPromise.ts
@@ -5,8 +5,12 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const isPromise = (candidate: unknown): candidate is PromiseLike<unknown> =>
-  candidate != null &&
-  (typeof candidate === 'object' || typeof candidate === 'function') &&
-  typeof (candidate as any).then === 'function';
-export default isPromise;
+export default function isPromise<T = unknown>(
+  candidate: unknown,
+): candidate is PromiseLike<T> {
+  return (
+    candidate != null &&
+    (typeof candidate === 'object' || typeof candidate === 'function') &&
+    typeof (candidate as any).then === 'function'
+  );
+}
