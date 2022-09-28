f86074a3f6e38c6c6f3ba476070ad07437fa328d
Simen Bekkhus <sbekkhus91@gmail.com>
2022-09-28 09:17:09 +0200

    copyright headers

--- packages/jest-mock/src/__tests__/class-mocks-single-import.test.ts
+++ packages/jest-mock/src/__tests__/class-mocks-single-import.test.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import SuperTestClass, {TestClass} from './class-mocks-types';
 jest.mock('./class-mocks-types');
 
--- packages/jest-mock/src/__tests__/class-mocks.test.ts
+++ packages/jest-mock/src/__tests__/class-mocks.test.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 describe('Testing the mocking of a class', () => {
   it('can call an instance method', () => {
     class TestClass {
