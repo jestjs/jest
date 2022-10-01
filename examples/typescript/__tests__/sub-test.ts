dcd66d0a3c91db3d1d4dcf756f58aa46071109fe
Tom Mrazauskas <tom@mrazauskas.de>
2022-10-01 10:32:14 +0300

    fix lint

--- examples/typescript/__tests__/sub-test.ts
+++ examples/typescript/__tests__/sub-test.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
 
-import sub from '../sub';
 import {expect, it} from '@jest/globals';
+import sub from '../sub';
 
 it('subtracts 5 - 1 to equal 4 in TypeScript', () => {
   expect(sub(5, 1)).toBe(4);
