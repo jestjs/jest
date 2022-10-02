c2dffe995af845ccc0bd4affc936d8013f90bf19
Nico Jansen <jansennico@gmail.com>
2022-10-02 14:25:31 +0200

    Import from `@jest/types` instead of `jest-runner`
    Co-authored-by: Tom Mrazauskas <tom@mrazauskas.de>

--- packages/jest-config/src/__tests__/readInitialOptions.test.ts
+++ packages/jest-config/src/__tests__/readInitialOptions.test.ts
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import type {Config} from 'jest-runner';
+import type {Config} from '@jest/types';
 import {readInitialOptions} from '../index';
 
 describe(readInitialOptions.name, () => {
