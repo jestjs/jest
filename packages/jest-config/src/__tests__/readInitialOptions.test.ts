b6276fbc57925710e63681cc82293b4f72d74455
Nico Jansen <jansennico@gmail.com>
2022-10-02 13:30:09 +0200

    Add copyright headers

--- packages/jest-config/src/__tests__/readInitialOptions.test.ts
+++ packages/jest-config/src/__tests__/readInitialOptions.test.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import path = require('path');
 import type {Config} from 'jest-runner';
 import {readInitialOptions} from '../index';
