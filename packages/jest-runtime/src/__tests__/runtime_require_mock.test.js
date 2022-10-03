1c6d23e8b48562a38e1a3a3807eb9dc75d03d4ee
Tom Mrazauskas <tom@mrazauskas.de>
2022-10-03 18:06:16 +0300

    chore: fix error message in native module test

--- packages/jest-runtime/src/__tests__/runtime_require_mock.test.js
+++ packages/jest-runtime/src/__tests__/runtime_require_mock.test.js
@@ -93,7 +93,7 @@ describe('Runtime', () => {
         error = e;
       } finally {
         expect(error.message).toMatch(
-          /NativeModule.node: file too short|not a valid Win\d+ application/,
+          /NativeModule.node: file too short|NativeModule.node, 0x0001|not a valid Win\d+ application/,
         );
       }
     });
