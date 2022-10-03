5c54a6ae11b464c2a1d152866df403ccd0cc861c
Nico Jansen <jansennico@gmail.com>
2022-10-03 08:25:11 +0200

    Add copyright header

--- e2e/read-initial-options/readOptions.js
+++ e2e/read-initial-options/readOptions.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 const {readInitialOptions} = require('jest-config');
 async function readConfig() {
   console.log(JSON.stringify(await readInitialOptions(process.argv[2])));
