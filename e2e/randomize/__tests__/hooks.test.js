19cf2b52f7a6fae4c82a1c3691b464136be613ed
Joshua Hwang <jhwang@atlassian.com>
2022-10-04 16:16:28 +1100

    modifed snapshots to not rely on the actual lines

--- e2e/randomize/__tests__/hooks.test.js
+++ e2e/randomize/__tests__/hooks.test.js
@@ -6,93 +6,93 @@
  */
 
 beforeAll(() => {
-  console.log('This is before all');
+  process.stdout.write('This is before all\n');
 });
 
 beforeEach(() => {
-  console.log('This is before each');
+  process.stdout.write('This is before each\n');
 });
 
 afterEach(() => {
-  console.log('This is after each');
+  process.stdout.write('This is after each\n');
 });
 
 afterAll(() => {
-  console.log('This is after all');
+  process.stdout.write('This is after all\n');
 });
 
 it('test1', () => {
-  console.log('test1');
+  process.stdout.write('test1\n');
 });
 
 it('test2', () => {
-  console.log('test2');
+  process.stdout.write('test2\n');
 });
 
 it('test3', () => {
-  console.log('test3');
+  process.stdout.write('test3\n');
 });
 
 describe('describe1', () => {
   it('test4', () => {
-    console.log('test4');
+    process.stdout.write('test4\n');
   });
 
   it('test5', () => {
-    console.log('test5');
+    process.stdout.write('test5\n');
   });
 
   it('test6', () => {
-    console.log('test6');
+    process.stdout.write('test6\n');
   });
 });
 
 describe('describe2', () => {
   afterAll(() => {
-    console.log('This is after all describe2');
+    process.stdout.write('This is after all describe2\n');
   });
 
   it('test7', () => {
-    console.log('test7');
+    process.stdout.write('test7\n');
   });
 
   it('test8', () => {
-    console.log('test8');
+    process.stdout.write('test8\n');
   });
 
   it('test9', () => {
-    console.log('test9');
+    process.stdout.write('test9\n');
   });
 });
 
 describe('describe3', () => {
   beforeEach(() => {
-    console.log('This is before each describe3');
+    process.stdout.write('This is before each describe3\n');
   });
 
   it('test10', () => {
-    console.log('test10');
+    process.stdout.write('test10\n');
   });
 
   it('test11', () => {
-    console.log('test11');
+    process.stdout.write('test11\n');
   });
 
   it('test12', () => {
-    console.log('test12');
+    process.stdout.write('test12\n');
   });
 
   describe('describe4', () => {
     it('test13', () => {
-      console.log('test13');
+      process.stdout.write('test13\n');
     });
 
     it('test14', () => {
-      console.log('test14');
+      process.stdout.write('test14\n');
     });
 
     it('test15', () => {
-      console.log('test15');
+      process.stdout.write('test15\n');
     });
   });
 });
