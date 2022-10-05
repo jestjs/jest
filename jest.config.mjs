3c9787dbf49a5f9e48d1dbbbca75675df94cce7d
Tom Mrazauskas <tom@mrazauskas.de>
2022-10-05 12:38:14 +0300

    add ignore

--- jest.config.mjs
+++ jest.config.mjs
@@ -51,6 +51,7 @@ export default {
     '/e2e/multi-project-babel',
     '\\.snap$',
     '/packages/.*/build',
+    '/packages/.*/src/__tests__/expect.d.ts',
     '/packages/.*/src/__tests__/setPrettyPrint.ts',
     '/packages/jest-core/src/__tests__/test_root',
     '/packages/jest-core/src/__tests__/__fixtures__/',
