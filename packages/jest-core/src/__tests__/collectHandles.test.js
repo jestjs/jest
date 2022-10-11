57323287b776159f38005976ac8ed212f3b476eb
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2022-10-11 10:05:26 +0800

    fix test

--- packages/jest-core/src/__tests__/collectHandles.test.js
+++ packages/jest-core/src/__tests__/collectHandles.test.js
@@ -42,7 +42,6 @@ describe('collectHandles', () => {
     expect(openHandles).not.toContainEqual(
       expect.objectContaining({message: 'PerformanceObserver'}),
     );
-    obs.disconnect();
   });
 
   it('should not collect the DNSCHANNEL open handle', async () => {
