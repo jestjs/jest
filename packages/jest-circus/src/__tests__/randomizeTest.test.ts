604499df05a35ba9ba05933d24aa3099e2970f65
josh <41476440+Joshua-Hwang@users.noreply.github.com>
2022-10-15 00:43:27 +1000

    added unit tests for the shuffling of tests

---
+++ packages/jest-circus/src/__tests__/randomizeTest.test.ts
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import {runTest} from '../__mocks__/testUtils';
+
+test('simple test', () => {
+  const {stdout} = runTest(`
+    describe('describe', () => {
+      beforeEach(() => {});
+      afterEach(() => {});
+      test('one', () => {});
+      test('two', () => {});
+    })
+  `, { randomize: true, seed: 3 });
+
+  expect(stdout).toMatchSnapshot();
+});
+
+test('function descriptors', () => {
+  const {stdout} = runTest(`
+    describe(function describer() {}, () => {
+      test(class One {}, () => {});
+    })
+  `, { randomize: true, seed: 3 });
+
+  expect(stdout).toMatchSnapshot();
+});
+
+test('failures', () => {
+  const {stdout} = runTest(`
+    describe('describe', () => {
+      beforeEach(() => {});
+      afterEach(() => { throw new Error('banana')});
+      test('one', () => { throw new Error('kentucky')});
+      test('two', () => {});
+    })
+  `, { randomize: true, seed: 3 });
+
+  expect(stdout).toMatchSnapshot();
+});
