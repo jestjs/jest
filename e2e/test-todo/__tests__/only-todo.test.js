095e6d636df49b30af68698fff76b73d3c75cd58
Matt Phillips <matt@mattphillips.io>
2022-10-24 10:58:45 +0100

    Fix lint error

--- e2e/test-todo/__tests__/only-todo.test.js
+++ e2e/test-todo/__tests__/only-todo.test.js
@@ -4,6 +4,9 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+/* eslint-disable jest/no-focused-tests */
+
 'use strict';
 
 describe.only("with .only, should show 'passed', 'todo', 'todo'", () => {
