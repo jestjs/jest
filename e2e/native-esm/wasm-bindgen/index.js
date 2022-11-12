942188c53e4caf1b0318c7c860e43934d2bc61cd
Alexander Kachkaev <alexander@kachkaev.ru>
2022-11-12 19:35:52 +0000

    Add failing test

---
+++ e2e/native-esm/wasm-bindgen/index.js
@@ -0,0 +1,5 @@
+// folder source: https://github.com/rustwasm/wasm-bindgen/tree/4f865308afbe8d2463968457711ad356bae63b71/examples/hello_world
+// docs: https://rustwasm.github.io/docs/wasm-bindgen/examples/hello-world.html
+
+import * as wasm from './index_bg.wasm';
+export * from './index_bg.js';
