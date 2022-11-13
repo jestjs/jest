5d4a115227607c15798bee65ef476f7ed9c7fb40
Alexander Kachkaev <alexander@kachkaev.ru>
2022-11-13 12:52:11 +0000

    Remove redundant line

--- e2e/native-esm/__tests__/native-esm-wasm.test.js
+++ e2e/native-esm/__tests__/native-esm-wasm.test.js
@@ -58,7 +58,6 @@ test('imports from "data:application/wasm" URI with invalid encoding fail', asyn
 
 test('supports wasm files that import js resources (wasm-bindgen)', async () => {
   globalThis.alert = jest.fn();
-  jest.spyOn(globalThis, 'alert');
 
   const {greet} = await import('../wasm-bindgen/index.js');
   greet('World');
