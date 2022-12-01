944934fb394a5bf943db97658bf7ceceb951c908
Andre Wiggins <andrewiggins@live.com>
2022-11-30 23:54:46 -0800

    Clean up new tests a bit

--- packages/expect/src/__tests__/customEqualityTesters.test.ts
+++ packages/expect/src/__tests__/customEqualityTesters.test.ts
@@ -14,10 +14,10 @@ const specialObjSymbol = Symbol('special test object type');
 
 interface SpecialObject {
   $$special: symbol;
-  value: number | string;
+  value: string;
 }
 
-function createSpecialObject(value: number | string) {
+function createSpecialObject(value: string) {
   return {
     [specialObjPropName]: specialObjSymbol,
     value,
@@ -55,17 +55,6 @@ function* toIterator<T>(array: Array<T>): Iterator<T> {
   }
 }
 
-const specialArg1 = createSpecialObject('arg1');
-const specialArg2 = createSpecialObject('arg2');
-const specialArg3 = createSpecialObject('arg3');
-const specialArg4 = createSpecialObject('arg4');
-const specialReturn1 = createSpecialObject('return1');
-const specialReturn2 = createSpecialObject('return2');
-
-const testArgs = [specialArg1, specialArg2, [specialArg3, specialArg4]];
-// Swap the order of args to assert customer tester does not affect test
-const expectedArgs = [specialArg2, specialArg1, [specialArg4, specialArg3]];
-
 declare module '../types' {
   interface Matchers<R> {
     toSpecialObjectEqual(expected: SpecialObject): R;
@@ -88,57 +77,82 @@ jestExpect.extend({
   },
 });
 
-it('has no custom testers as default', () => {
-  const special1 = createSpecialObject(1);
-  const special2 = createSpecialObject(2);
+const special1 = createSpecialObject('1');
+const special2 = createSpecialObject('2');
 
-  // Basic matchers passing with default settings
-  expect(special1).toBe(special1);
-  expect(special1).toEqual(special1);
-  expect([special1, special2]).toEqual([special1, special2]);
-  expect(new Set([special1])).toEqual(new Set([special1]));
-  expect(new Map([['key', special1]])).toEqual(new Map([['key', special1]]));
-  expect(toIterator([special1, special2])).toEqual(
-    toIterator([special1, special2]),
-  );
+const specialArg1 = createSpecialObject('arg1');
+const specialArg2 = createSpecialObject('arg2');
+const specialArg3 = createSpecialObject('arg3');
+const specialArg4 = createSpecialObject('arg4');
+const specialReturn1 = createSpecialObject('return1');
+const specialReturn2 = createSpecialObject('return2');
 
-  // Basic matchers not passing with default settings
-  expect(special1).not.toBe(special2);
-  expect(createSpecialObject(1)).not.toEqual(createSpecialObject(2));
-  expect([special1, special2]).not.toEqual([special2, special1]);
-  expect(new Set([special1])).not.toEqual(new Set([special2]));
-  expect(new Map([['key', special1]])).not.toEqual(
-    new Map([['key', special2]]),
-  );
-  expect(toIterator([special1, special2])).not.toEqual(
-    toIterator([special2, special1]),
-  );
-  expect({a: special1, b: undefined}).not.toStrictEqual({
-    a: special2,
-    b: undefined,
+const testArgs = [specialArg1, specialArg2, [specialArg3, specialArg4]];
+// Swap the order of args to assert customer tester does not affect test
+const expectedArgs = [specialArg2, specialArg1, [specialArg4, specialArg3]];
+
+describe('without custom equality testers', () => {
+  it('basic matchers correctly match the same special objects', () => {
+    // Basic matchers passing with default settings
+    expect(special1).toBe(special1);
+    expect(special1).toEqual(special1);
+    expect([special1, special2]).toEqual([special1, special2]);
+    expect(new Set([special1])).toEqual(new Set([special1]));
+    expect(new Map([['key', special1]])).toEqual(new Map([['key', special1]]));
+    expect(toIterator([special1, special2])).toEqual(
+      toIterator([special1, special2]),
+    );
+    expect({a: special1, b: undefined}).toStrictEqual({
+      a: special1,
+      b: undefined,
+    });
+    expect({a: 1, b: {c: special1}}).toMatchObject({a: 1, b: {c: special1}});
   });
-  expect({a: 1, b: {c: special1}}).not.toMatchObject({a: 1, b: {c: special2}});
 
-  // Asymmetric matchers
-  expect([special1]).not.toEqual(expect.arrayContaining([special2]));
-  expect({a: 1, b: {c: special1}}).not.toEqual(
-    expect.objectContaining({b: {c: special2}}),
-  );
+  it('basic matchers do not pass different special objects', () => {
+    expect(special1).not.toBe(special2);
+    expect(special1).not.toEqual(special2);
+    expect([special1, special2]).not.toEqual([special2, special1]);
+    expect(new Set([special1])).not.toEqual(new Set([special2]));
+    expect(new Map([['key', special1]])).not.toEqual(
+      new Map([['key', special2]]),
+    );
+    expect(toIterator([special1, special2])).not.toEqual(
+      toIterator([special2, special1]),
+    );
+    expect({a: special1, b: undefined}).not.toStrictEqual({
+      a: special2,
+      b: undefined,
+    });
+    expect({a: 1, b: {c: special1}}).not.toMatchObject({
+      a: 1,
+      b: {c: special2},
+    });
+  });
 
-  // Spy matchers
-  const mockFn = jest.fn(() => specialReturn1);
-  mockFn(...testArgs);
+  it('asymmetric matchers do not pass different special objects', () => {
+    expect([special1]).not.toEqual(expect.arrayContaining([special2]));
+    expect({a: 1, b: {c: special1}}).not.toEqual(
+      expect.objectContaining({b: {c: special2}}),
+    );
+  });
 
-  expect(mockFn).not.toHaveBeenCalledWith(...expectedArgs);
-  expect(mockFn).not.toHaveBeenLastCalledWith(...expectedArgs);
-  expect(mockFn).not.toHaveBeenNthCalledWith(1, ...expectedArgs);
+  it('spy matchers do not pass different special objects', () => {
+    const mockFn = jest.fn(() => specialReturn1);
+    mockFn(...testArgs);
 
-  expect(mockFn).not.toHaveReturnedWith(specialReturn2);
-  expect(mockFn).not.toHaveLastReturnedWith(specialReturn2);
-  expect(mockFn).not.toHaveNthReturnedWith(1, specialReturn2);
+    expect(mockFn).not.toHaveBeenCalledWith(...expectedArgs);
+    expect(mockFn).not.toHaveBeenLastCalledWith(...expectedArgs);
+    expect(mockFn).not.toHaveBeenNthCalledWith(1, ...expectedArgs);
 
-  // Custom matchers
-  expect(special1).not.toSpecialObjectEqual(special2);
+    expect(mockFn).not.toHaveReturnedWith(specialReturn2);
+    expect(mockFn).not.toHaveLastReturnedWith(specialReturn2);
+    expect(mockFn).not.toHaveNthReturnedWith(1, specialReturn2);
+  });
+
+  it('custom matcher does not pass different special objects', () => {
+    expect(special1).not.toSpecialObjectEqual(special2);
+  });
 });
 
 describe('with custom equality testers', () => {
@@ -153,21 +167,12 @@ describe('with custom equality testers', () => {
     jestExpect.customEqualityTesters = originalTesters;
   });
 
-  it('basic matchers customTesters do not apply to', () => {
-    const special1 = createSpecialObject(1);
-    const special2 = createSpecialObject(2);
-
-    expect(special1).toBe(special1);
-    expect([special1]).toContain(special1);
-
+  it('basic matchers customTesters do not apply to still do not pass different special objects', () => {
     expect(special1).not.toBe(special2);
     expect([special1]).not.toContain(special2);
   });
 
-  it('basic matchers customTesters do apply to', () => {
-    const special1 = createSpecialObject(1);
-    const special2 = createSpecialObject(2);
-
+  it('basic matchers pass different special objects', () => {
     expect(special1).toEqual(special1);
     expect(special1).toEqual(special2);
     expect([special1, special2]).toEqual([special2, special1]);
@@ -188,17 +193,14 @@ describe('with custom equality testers', () => {
     });
   });
 
-  it('asymmetric matchers with custom testers', () => {
-    const special1 = createSpecialObject(1);
-    const special2 = createSpecialObject(2);
-
+  it('asymmetric matchers pass different special objects', () => {
     expect([special1]).toEqual(expect.arrayContaining([special2]));
     expect({a: 1, b: {c: special1}}).toEqual(
       expect.objectContaining({b: {c: special2}}),
     );
   });
 
-  it('spy matchers with custom testers', () => {
+  it('spy matchers pass different special objects', () => {
     const mockFn = jest.fn(() => specialReturn1);
     mockFn(...testArgs);
 
@@ -211,7 +213,7 @@ describe('with custom equality testers', () => {
     expect(mockFn).toHaveNthReturnedWith(1, specialReturn2);
   });
 
-  it('custom matchers with custom testers', () => {
-    expect(createSpecialObject(1)).toSpecialObjectEqual(createSpecialObject(2));
+  it('custom matchers pass different special objects', () => {
+    expect(special1).toSpecialObjectEqual(special2);
   });
 });
