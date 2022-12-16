32d0e389b6a0b5ab15570687ecf800960c774292
Andre Wiggins <andrewiggins@live.com>
2022-12-15 22:09:40 -0800

    Add type tests for new API

--- packages/expect/__typetests__/expect.test.ts
+++ packages/expect/__typetests__/expect.test.ts
@@ -48,6 +48,7 @@ expectType<void>(
       expectType<Array<Error>>(this.suppressedErrors);
       expectType<string | undefined>(this.testPath);
       expectType<MatcherUtils>(this.utils);
+      expectType<Array<Tester>>(this.customTesters);
 
       const pass = actual >= floor && actual <= ceiling;
       if (pass) {
@@ -78,6 +79,17 @@ declare module 'expect' {
 
 expectType<void>(expect(100).toBeWithinRange(90, 110));
 expectType<void>(expect(101).not.toBeWithinRange(0, 100));
+
+expectType<void>(
+  expect.addEqualityTesters([
+    (a, b, customTesters) => {
+      expectType<any>(a);
+      expectType<any>(b);
+      expectType<Array<Tester>>(customTesters);
+      return true;
+    },
+  ]),
+);
 
 expectType<void>(
   expect({apples: 6, bananas: 3}).toEqual({
