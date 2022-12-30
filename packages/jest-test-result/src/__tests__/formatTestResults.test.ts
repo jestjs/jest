ab2ae6d1496e72193d254f91f80ef08d8ba39a7d
Luca Pizzini <lpizzini7@gmail.com>
2022-12-30 09:27:46 +0100

    feature: Added skipped and focused status to FormattedTestResult

--- packages/jest-test-result/src/__tests__/formatTestResults.test.ts
+++ packages/jest-test-result/src/__tests__/formatTestResults.test.ts
@@ -26,10 +26,57 @@ describe('formatTestResults', () => {
     ],
   };
 
-  it('includes test full name', () => {
-    const result = formatTestResults(results, undefined, null);
-    expect(result.testResults[0].assertionResults[0].fullName).toEqual(
-      assertion.fullName,
+  const skippedAssertion = {
+    fullName: 'Pending test',
+    status: 'pending',
+    title: 'is still pending',
+  };
+
+  const skippedResults: AggregatedResult = {
+    testResults: [
+      {
+        numFailingTests: 0,
+        numPassingTests: 0,
+        numPendingTests: 2,
+        numTodoTests: 2,
+        perfStats: {end: 2, runtime: 1, slow: false, start: 1},
+        // @ts-expect-error
+        testResults: [skippedAssertion],
+      },
+    ],
+  };
+
+  it('should mark result status to skipped', () => {
+    const result = formatTestResults(skippedResults, undefined, null);
+    expect(result.testResults[0].assertionResults[0].status).toEqual(
+      skippedAssertion.status,
+    );
+  });
+
+  const focusedAssertion = {
+    fullName: 'Focused test',
+    status: 'focused',
+    title: 'focused test',
+  };
+
+  const focusedResults: AggregatedResult = {
+    testResults: [
+      {
+        numFailingTests: 0,
+        numPassingTests: 1,
+        numPendingTests: 1,
+        numTodoTests: 2,
+        perfStats: {end: 2, runtime: 1, slow: false, start: 1},
+        // @ts-expect-error
+        testResults: [focusedAssertion],
+      },
+    ],
+  };
+
+  it('should mark result status to focused', () => {
+    const result = formatTestResults(focusedResults, undefined, null);
+    expect(result.testResults[0].assertionResults[0].status).toEqual(
+      focusedAssertion.status,
     );
   });
 });
