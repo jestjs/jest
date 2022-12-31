bf6d28f1dc544cdaef7d223e39638248190e4f3a
Simen Bekkhus <sbekkhus91@gmail.com>
2022-12-31 11:21:55 +0100

    Apply suggestions from code review
    Co-authored-by: Tom Mrazauskas <tom@mrazauskas.de>

--- packages/jest-test-result/src/__tests__/formatTestResults.test.ts
+++ packages/jest-test-result/src/__tests__/formatTestResults.test.ts
@@ -6,7 +6,7 @@
  */
 
 import formatTestResults from '../formatTestResults';
-import type {AggregatedResult} from '../types';
+import type {AggregatedResult, AssertionResult} from '../types';
 
 describe('formatTestResults', () => {
   const assertion = {
@@ -36,7 +36,7 @@ describe('formatTestResults', () => {
     fullName: 'Pending test',
     status: 'pending',
     title: 'is still pending',
-  };
+  } as AssertionResult;
 
   const skippedResults = {
     testResults: [
