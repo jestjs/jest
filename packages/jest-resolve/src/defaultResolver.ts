74776d8b581fabeb5c3993b6499643b8c6ffedf6
Simen Bekkhus <sbekkhus91@gmail.com>
2023-01-01 13:32:16 +0100

    feat: support subpath imports (#13705)

--- packages/jest-resolve/src/defaultResolver.ts
+++ packages/jest-resolve/src/defaultResolver.ts
@@ -6,6 +6,7 @@
  */
 
 import {dirname, isAbsolute, resolve as pathResolve} from 'path';
+import {resolve as resolveImports} from '@okikio/resolve.imports';
 import pnpResolver from 'jest-pnp-resolver';
 import {SyncOpts as UpstreamResolveOptions, sync as resolveSync} from 'resolve';
 import {
@@ -83,7 +84,7 @@ export type ResolverOptions = {
 };
 
 type UpstreamResolveOptionsWithConditions = UpstreamResolveOptions &
-  Pick<ResolverOptions, 'basedir' | 'conditions'>;
+  ResolverOptions;
 
 export type SyncResolver = (path: string, options: ResolverOptions) => string;
 export type AsyncResolver = (
@@ -136,12 +137,42 @@ function getPathInModule(
     return path;
   }
 
+  if (path.startsWith('#')) {
+    const closestPackageJson = findClosestPackageJson(options.basedir);
+
+    if (!closestPackageJson) {
+      throw new Error(
+        `Jest: unable to locate closest package.json from ${options.basedir} when resolving import "${path}"`,
+      );
+    }
+
+    const pkg = readPackageCached(closestPackageJson);
+
+    const resolved = resolveImports(
+      pkg,
+      path,
+      createResolveOptions(options.conditions),
+    );
+
+    if (!resolved) {
+      throw new Error(
+        '`imports` exists, but no results - this is a bug in Jest. Please report an issue',
+      );
+    }
+
+    if (resolved.startsWith('.')) {
+      return pathResolve(dirname(closestPackageJson), resolved);
+    }
+
+    // this is an external module, re-resolve it
+    return defaultResolver(resolved, options);
+  }
+
   const segments = path.split('/');
 
   let moduleName = segments.shift();
 
   if (moduleName) {
-    // TODO: handle `#` here: https://github.com/facebook/jest/issues/12270
     if (moduleName.startsWith('@')) {
       moduleName = `${moduleName}/${segments.shift()}`;
     }
@@ -213,6 +244,6 @@ function createResolveOptions(
       {browser: false, require: true};
 }
 
-// if it's a relative import or an absolute path, exports are ignored
+// if it's a relative import or an absolute path, imports/exports are ignored
 const shouldIgnoreRequestForExports = (path: string) =>
   path.startsWith('.') || isAbsolute(path);
