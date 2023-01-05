503d86f82155c9c01ca2724d4917f8b98f98d2cd
Biki-das <bikid475@gmail.com>
2023-01-06 00:59:19 +0530

    Removed transformed files import

--- packages/jest-runtime/src/index.ts
+++ packages/jest-runtime/src/index.ts
@@ -42,7 +42,6 @@ import {
   CallerTransformOptions,
   ScriptTransformer,
   ShouldInstrumentOptions,
-  TransformResult,
   TransformationOptions,
   handlePotentialSyntaxError,
   shouldInstrument,
