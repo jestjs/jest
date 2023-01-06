eace3a1fefe95af3325b2de5374939788bbf6d34
Felipe ou Jimmy <feliperli@hotmail.com>
2023-01-06 13:47:13 -0300

    fix: stop changing the behaviour of spies on mockFn.mockReset (#13692)

--- packages/jest-mock/src/index.ts
+++ packages/jest-mock/src/index.ts
@@ -509,6 +509,7 @@ export class ModuleMocker {
   private _mockConfigRegistry: WeakMap<Function, MockFunctionConfig>;
   private _spyState: Set<() => void>;
   private _invocationCallCounter: number;
+  private _originalFn: WeakMap<Mock, Function>;
 
   /**
    * @see README.md
@@ -521,6 +522,7 @@ export class ModuleMocker {
     this._mockConfigRegistry = new WeakMap();
     this._spyState = new Set();
     this._invocationCallCounter = 1;
+    this._originalFn = new WeakMap();
   }
 
   private _getSlots(object?: Record<string, any>): Array<string> {
@@ -775,7 +777,12 @@ export class ModuleMocker {
 
       f.mockReset = () => {
         f.mockClear();
-        this._mockConfigRegistry.delete(f);
+        const originalFn = this._originalFn.get(f);
+        const originalMockImpl = {
+          ...this._defaultMockConfig(),
+          mockImpl: originalFn,
+        };
+        this._mockConfigRegistry.set(f, originalMockImpl);
         return f;
       };
 
@@ -1215,7 +1222,7 @@ export class ModuleMocker {
         return original.apply(this, arguments);
       });
     }
-
+    this._originalFn.set(object[methodKey] as Mock, original);
     return object[methodKey] as Mock;
   }
 
@@ -1401,6 +1408,7 @@ export class ModuleMocker {
   }
 
   resetAllMocks(): void {
+    this._spyState.forEach(reset => reset());
     this._mockConfigRegistry = new WeakMap();
     this._mockState = new WeakMap();
   }
