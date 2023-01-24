6ca4f3389992674e466c277086d34c7f3f16e1b3
Simen Bekkhus <sbekkhus91@gmail.com>
2023-01-24 11:08:53 +0100

    fix(resolve): remove faulty check for `node:` modules

--- packages/jest-resolve/src/__tests__/resolve.test.ts
+++ packages/jest-resolve/src/__tests__/resolve.test.ts
@@ -91,11 +91,11 @@ describe('isCoreModule', () => {
     expect(isCore).toBe(true);
   });
 
-  it('returns false if using `node:` URLs and `moduleName` is not a core module.', () => {
+  it('returns true if using `node:` URLs and `moduleName` is not a core module.', () => {
     const moduleMap = ModuleMap.create('/');
     const resolver = new Resolver(moduleMap, {} as ResolverConfig);
     const isCore = resolver.isCoreModule('node:not-a-core-module');
-    expect(isCore).toBe(false);
+    expect(isCore).toBe(true);
   });
 });
 
