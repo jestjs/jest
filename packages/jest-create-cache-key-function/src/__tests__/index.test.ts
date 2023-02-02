f849b2c1bedb89f3914e78fde6650c8b1beab6d0
Archimedes Trajano <developer@trajano.net>
2023-02-02 08:47:03 -0500

    Update index.test.ts

--- packages/jest-create-cache-key-function/src/__tests__/index.test.ts
+++ packages/jest-create-cache-key-function/src/__tests__/index.test.ts
@@ -9,17 +9,20 @@ import {interopRequireDefault} from 'jest-util';
 
 let NODE_ENV: string;
 let BABEL_ENV: string;
+let PLATFORM: string;
 
 beforeEach(() => {
   NODE_ENV = process.env.NODE_ENV;
   process.env.NODE_ENV = 'test';
   BABEL_ENV = process.env.BABEL_ENV;
   process.env.BABEL_ENV = 'test';
+  PLATFORM = process.platform;
 });
 
 afterEach(() => {
   process.env.NODE_ENV = NODE_ENV;
   process.env.BABEL_ENV = BABEL_ENV;
+  process.platform = PLATFORM;
 });
 
 test('creation of a cache key', () => {
@@ -41,6 +44,30 @@ test('creation of a cache key', () => {
   });
 
   expect(hashA).toHaveLength(32);
+  expect(hashA).not.toEqual(hashB);
+  expect(hashA).not.toEqual(hashC);
+});
+
+test('creation of a cache key on win32', () => {
+  process.platform = 'win32';
+  const createCacheKeyFunction = interopRequireDefault(
+    require('../index'),
+  ).default;
+  const createCacheKey = createCacheKeyFunction([], ['value']);
+  const hashA = createCacheKey('test', 'test.js', null, {
+    config: {},
+    instrument: false,
+  });
+  const hashB = createCacheKey('test code;', 'test.js', null, {
+    config: {},
+    instrument: false,
+  });
+  const hashC = createCacheKey('test', 'test.js', null, {
+    config: {},
+    instrument: true,
+  });
+
+  expect(hashA).toHaveLength(16);
   expect(hashA).not.toEqual(hashB);
   expect(hashA).not.toEqual(hashC);
 });
