537d2138b81ad8ccaaad9750385f7929971bd3c4
Ben Kraft <benkraft@makenotion.com>
2023-04-06 14:27:58 -0700

    fix(jest-core): always use workers in watch mode
    The comment here already said one good reason to do this; another is
    that if the test hard-crashes (e.g. an async error after it completes)
    then using workers allows us to still watch for changes (perhaps to fix
    that crash). So now we just always use workers in watch mode; this
    probably worsens startup time slightly but for watch mode that's
    hopefully not as much of a problem.
    
    Fixes #13996.

--- packages/jest-core/src/testSchedulerHelper.ts
+++ packages/jest-core/src/testSchedulerHelper.ts
@@ -27,24 +27,24 @@ export function shouldRunInBand(
   }
 
   /*
-   * Run in band if we only have one test or one worker available, unless we
-   * are using the watch mode, in which case the TTY has to be responsive and
-   * we cannot schedule anything in the main thread. Same logic applies to
-   * watchAll.
-   * Also, if we are confident from previous runs that the tests will finish
-   * quickly we also run in band to reduce the overhead of spawning workers.
-   * Finally, the user can provide the runInBand argument in the CLI to
-   * force running in band.
-   * https://github.com/facebook/jest/blob/700e0dadb85f5dc8ff5dac6c7e98956690049734/packages/jest-config/src/getMaxWorkers.js#L14-L17
+   * If we are using watch/watchAll mode, don't schedule anything in the main
+   * thread to keep the TTY responsive and to keep the watcher running even if
+   * the test crashes.
    */
   const isWatchMode = watch || watchAll;
+  if (isWatchMode) {
+    return false;
+  }
+
+  /*
+   * Otherwise, run in band if we only have one test or one worker available.
+   * Also, if we are confident from previous runs that the tests will finish
+   * quickly we also run in band to reduce the overhead of spawning workers.
+   * https://github.com/facebook/jest/blob/700e0dadb85f5dc8ff5dac6c7e98956690049734/packages/jest-config/src/getMaxWorkers.js#L14-L17
+   */
   const areFastTests = timings.every(timing => timing < SLOW_TEST_TIME);
   const oneWorkerOrLess = maxWorkers <= 1;
   const oneTestOrLess = tests.length <= 1;
-
-  if (isWatchMode) {
-    return oneWorkerOrLess || (oneTestOrLess && areFastTests);
-  }
 
   return (
     // When specifying a memory limit, workers should be used
