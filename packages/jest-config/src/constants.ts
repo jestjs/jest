faa4fffad84d3ca3689cddb2e90653f54c167a60
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2023-04-13 12:35:09 +0200

    Allow for jest.config.cts, add fixture and snapshots

--- packages/jest-config/src/constants.ts
+++ packages/jest-config/src/constants.ts
@@ -15,11 +15,13 @@ export const JEST_CONFIG_EXT_CJS = '.cjs';
 export const JEST_CONFIG_EXT_MJS = '.mjs';
 export const JEST_CONFIG_EXT_JS = '.js';
 export const JEST_CONFIG_EXT_TS = '.ts';
+export const JEST_CONFIG_EXT_CTS = '.cts';
 export const JEST_CONFIG_EXT_JSON = '.json';
 export const JEST_CONFIG_EXT_ORDER = Object.freeze([
   JEST_CONFIG_EXT_JS,
   JEST_CONFIG_EXT_TS,
   JEST_CONFIG_EXT_MJS,
   JEST_CONFIG_EXT_CJS,
+  JEST_CONFIG_EXT_CTS,
   JEST_CONFIG_EXT_JSON,
 ]);
