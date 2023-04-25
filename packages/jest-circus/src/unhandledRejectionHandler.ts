9efe383df599a986c2bc1c1ce1255f1a31007c75
Martin Štekl <martin.stekl@gmail.com>
2023-04-25 23:48:08 +0200

    Wait until next event loop turn when there is a reason to wait for rejectionHandled event only
    Awaiting for the next turn is potentially conflicting with fake timers usage.

--- packages/jest-circus/src/unhandledRejectionHandler.ts
+++ packages/jest-circus/src/unhandledRejectionHandler.ts
@@ -19,49 +19,60 @@ const unhandledRejectionHandler: Circus.EventHandler = async (
   state,
 ): Promise<void> => {
   if (event.name === 'hook_success' || event.name === 'hook_failure') {
-    // We need to give event loop the time to actually execute `rejectionHandled` event
-    await untilNextEventLoopTurn();
-
     const {test, describeBlock, hook} = event;
     const {asyncError, type} = hook;
 
     if (type === 'beforeAll') {
       invariant(describeBlock, 'always present for `*All` hooks');
-      for (const error of state.unhandledRejectionErrorByPromise.values()) {
-        addErrorToEachTestUnderDescribe(describeBlock, error, asyncError);
+      if (state.unhandledRejectionErrorByPromise.size > 0) {
+        // We need to give event loop the time to actually execute `rejectionHandled` event
+        await untilNextEventLoopTurn();
+        for (const error of state.unhandledRejectionErrorByPromise.values()) {
+          addErrorToEachTestUnderDescribe(describeBlock, error, asyncError);
+        }
       }
     } else if (type === 'afterAll') {
-      // Attaching `afterAll` errors to each test makes execution flow
-      // too complicated, so we'll consider them to be global.
-      for (const error of state.unhandledRejectionErrorByPromise.values()) {
-        state.unhandledErrors.push([error, asyncError]);
+      if (state.unhandledRejectionErrorByPromise.size > 0) {
+        // We need to give event loop the time to actually execute `rejectionHandled` event
+        await untilNextEventLoopTurn();
+        // Attaching `afterAll` errors to each test makes execution flow
+        // too complicated, so we'll consider them to be global.
+        for (const error of state.unhandledRejectionErrorByPromise.values()) {
+          state.unhandledErrors.push([error, asyncError]);
+        }
       }
     } else {
       invariant(test, 'always present for `*Each` hooks');
-      for (const error of test.unhandledRejectionErrorByPromise.values()) {
-        test.errors.push([error, asyncError]);
+      if (test.unhandledRejectionErrorByPromise.size > 0) {
+        // We need to give event loop the time to actually execute `rejectionHandled` event
+        await untilNextEventLoopTurn();
+        for (const error of test.unhandledRejectionErrorByPromise.values()) {
+          test.errors.push([error, asyncError]);
+        }
       }
     }
   } else if (
     event.name === 'test_fn_success' ||
     event.name === 'test_fn_failure'
   ) {
-    // We need to give event loop the time to actually execute `rejectionHandled` event
-    await untilNextEventLoopTurn();
-
     const {test} = event;
-
     invariant(test, 'always present for `*Each` hooks');
-    for (const error of test.unhandledRejectionErrorByPromise.values()) {
-      test.errors.push([error, event.test.asyncError]);
+
+    if (test.unhandledRejectionErrorByPromise.size > 0) {
+      // We need to give event loop the time to actually execute `rejectionHandled` event
+      await untilNextEventLoopTurn();
+      for (const error of test.unhandledRejectionErrorByPromise.values()) {
+        test.errors.push([error, event.test.asyncError]);
+      }
     }
   } else if (event.name === 'teardown') {
-    // We need to give event loop the time to actually execute `rejectionHandled` event
-    await untilNextEventLoopTurn();
-
-    state.unhandledErrors.push(
-      ...state.unhandledRejectionErrorByPromise.values(),
-    );
+    if (state.unhandledRejectionErrorByPromise.size > 0) {
+      // We need to give event loop the time to actually execute `rejectionHandled` event
+      await untilNextEventLoopTurn();
+      state.unhandledErrors.push(
+        ...state.unhandledRejectionErrorByPromise.values(),
+      );
+    }
   }
 };
 
