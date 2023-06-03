6460335f88cee3dcb9d29c49d55ab02b9d83f994
Christoph Nakazawa <christoph.pojer@gmail.com>
2023-06-03 16:46:42 +0900

    Remove 1-minute video. (#14181)

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -534,32 +534,6 @@ class Index extends React.Component {
           </Container>
           <Container
             padding={['bottom', 'top']}
-            className="section-container bottom-margin docs"
-          >
-            <div className="blockElement imageAlignSide gridBlock video-block">
-              <div className="blockContent">
-                <div className="video">
-                  <LiteYouTubeEmbed id="SyHzgcFefBk" />
-                </div>
-              </div>
-            </div>
-            <div className="blockElement red bottom-margin">
-              <div className="blockContent">
-                <MarkdownBlock>
-                  <Translate>
-                    The Meta Open Source team has put together a short overview
-                    of Jest, where they explained the project in beginner's
-                    terms. You can also find other content about [Meta Open
-                    Source projects](https://opensource.fb.com/) on their
-                    [YouTube
-                    Channel](https://www.youtube.com/channel/UCCQY962PmHabTjaHv2wJzfQ).
-                  </Translate>
-                </MarkdownBlock>
-              </div>
-            </div>
-          </Container>
-          <Container
-            padding={['bottom', 'top']}
             background="light"
             className="section-container community imageAlignSide twoByGridBlock"
           >
