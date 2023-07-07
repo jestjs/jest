a8f45c999748428730bc773c3c067f5c9e161329
Stevepurpose <stephenodogwu83@gmail.com>
2023-07-07 04:13:39 +0100

    more corrections according to lint specs

--- examples/react/App.js
+++ examples/react/App.js
@@ -1,18 +1,20 @@
 //parent component
-import {useRef} from 'react'
-import CheckboxWithLabel from './CheckboxWithLabel'
+import {useRef} from 'react';
+import CheckboxWithLabel from './CheckboxWithLabel';
 
- export default function App(){
-  const labelRef = useRef(null)
-  const inputRef = useRef(null)
-  const labelOn = 'on'
-  const labelOff = 'off'
+export default function App() {
+  const labelRef = useRef(null);
+  const inputRef = useRef(null);
+  const labelOn = 'on';
+  const labelOff = 'off';
 
-  return(
+  return (
     <div>
-<CheckboxWithLabel labelRef={labelRef} inputRef={inputRef} labelOn={labelOn} labelOff={labelOff}/>
+      <CheckboxWithLabel        
+        labelRef={labelRef}        
+        inputRef={inputRef} 
+        labelOn={labelOn} 
+        labelOff={labelOff}/>
     </div>
   );
-
-
 }
