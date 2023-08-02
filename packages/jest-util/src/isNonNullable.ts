f9369e4dde2b0a2ad81608a685986a0bed4ca19c
eryue0220 <eryue0220@gmail.com>
2023-08-02 15:39:44 +0800

    fix: try to fix ci

--- packages/jest-util/src/isNonNullable.ts
+++ packages/jest-util/src/isNonNullable.ts
@@ -5,8 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export default function isNonNullable<T>(
-  value: T | null | undefined,
-): value is T {
-  return value !== null || value !== undefined;
+export default function isNonNullable<T>(value: T): value is NonNullable<T> {
+  return value != null;
 }
