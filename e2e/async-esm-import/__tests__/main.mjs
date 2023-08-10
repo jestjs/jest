54ff86e0cda477f0ced0080120cf4a82dfc0d6c0
eryue0220 <eryue0220@gmail.com>
2023-08-10 10:43:47 +0800

    fix: code review

--- e2e/async-esm-import/__tests__/main.mjs
+++ e2e/async-esm-import/__tests__/main.mjs
@@ -8,13 +8,13 @@ import {jest} from '@jest/globals';
 
 test('should have a fresh module state in each isolateModulesAsync context', async () => {
   await jest.isolateModulesAsync(async () => {
-    const {getState, incState} = await import('../main.js');
+    const {getState, incState} = await import('../main.mjs');
     expect(getState()).toBe(0);
     incState();
     expect(getState()).toBe(1);
   });
   await jest.isolateModulesAsync(async () => {
-    const {getState, incState} = await import('../main.js');
+    const {getState, incState} = await import('../main.mjs');
     expect(getState()).toBe(0);
     incState();
     expect(getState()).toBe(1);
