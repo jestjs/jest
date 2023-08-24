f6f71290bd96b78dd551842b33cd6c44d633c242
Simen Bekkhus <sbekkhus91@gmail.com>
2023-08-24 11:39:21 +0200

    lint

--- e2e/override-globals/__tests__/index.js
+++ e2e/override-globals/__tests__/index.js
@@ -29,7 +29,9 @@ describe('parent', () => {
   });
 
   it('can override atob and btoa', () => {
+    // eslint-disable-next-line no-restricted-globals
     global.atob = () => 'hello';
+    // eslint-disable-next-line no-restricted-globals
     global.btoa = () => 'there';
 
     expect(`${atob()} ${btoa()}`).toBe('hello there');
