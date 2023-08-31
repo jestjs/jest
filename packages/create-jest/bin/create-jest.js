48b3d1d3a685e2df3e31df7955c401a8cd3770b1
Ilya Kuznetsov <ilia.kuznetsov@fingerprint.com>
2023-08-31 11:10:34 +0400

    fix: Remove unnecessary `local` check

--- packages/create-jest/bin/create-jest.js
+++ packages/create-jest/bin/create-jest.js
@@ -5,9 +5,4 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-const importLocal = require('import-local');
-
-if (!importLocal(__filename)) {
-    require('..').runCLI();
-}
+require('..').runCLI();
