46a7ce9c2be83591976e182d8cf4910d3722745c
Nico Jansen <jansennico@gmail.com>
2023-10-31 21:09:15 +0100

    fix: use jest's native esm api in e2e test

--- e2e/run-programmatically/jest.mjs
+++ e2e/run-programmatically/jest.mjs
@@ -4,8 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import jestModule from 'jest';
-const {createJest} = jestModule;
+import {createJest} from 'jest';
 
 const jest = await createJest();
 jest.globalConfig = {
