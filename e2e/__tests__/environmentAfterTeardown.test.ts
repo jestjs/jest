2812a0d9d5b7ec01e9690eefe07ef1c3170a0856
Martin Štekl <martin.stekl@gmail.com>
2023-11-06 13:15:25 +0100

    Fix failing tests

--- e2e/__tests__/environmentAfterTeardown.test.ts
+++ e2e/__tests__/environmentAfterTeardown.test.ts
@@ -5,17 +5,14 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import {skipSuiteOnJasmine} from '@jest/test-utils';
 import runJest from '../runJest';
-
-skipSuiteOnJasmine();
 
 test('prints useful error for environment methods after test is done', () => {
   const {stderr} = runJest('environment-after-teardown');
-  const interestingLines = stderr.split('\n').slice(5, 14).join('\n');
+  const interestingLines = stderr.split('\n').slice(9, 18).join('\n');
 
   expect(interestingLines).toMatchSnapshot();
-  expect(stderr.split('\n')[5]).toMatch(
-    'ReferenceError: You are trying to access a property or method of the Jest environment outside of the scope of the test code.',
+  expect(stderr.split('\n')[9]).toBe(
+    'ReferenceError: You are trying to access a property or method of the Jest environment after it has been torn down. From __tests__/afterTeardown.test.js.',
   );
 });
