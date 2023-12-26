2c3b767e2639399d8f04a88549f33329a6ac2bfa
Simen Bekkhus <sbekkhus91@gmail.com>
2023-12-26 16:16:07 +0100

    chore: use OpenJS Foundation copyright notice and logo

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -281,27 +281,15 @@ const config = {
               },
             ],
           },
-          {
-            title: 'Legal',
-            items: [
-              {
-                label: 'Privacy',
-                href: 'https://opensource.facebook.com/legal/privacy/',
-              },
-              {
-                label: 'Terms',
-                href: 'https://opensource.facebook.com/legal/terms/',
-              },
-            ],
-          },
         ],
         logo: {
-          //         src: 'img/jest-outline.svg',
-          alt: 'Facebook Open Source Logo',
+          alt: 'OpenJS Foundation Logo',
           src: 'img/oss_logo.png',
-          href: 'https://opensource.facebook.com',
+          href: 'https://openjsf.org/',
         },
-        copyright: `Copyright © ${new Date().getFullYear()} Meta Platforms, Inc. and affiliates. Built with Docusaurus.`,
+        copyright:
+          '<p>Copyright <a href="https://openjsf.org">OpenJS Foundation</a> and Jest contributors. All rights reserved. The <a href="https://openjsf.org">OpenJS Foundation</a> has registered trademarks and uses trademarks.  For a list of trademarks of the <a href="https://openjsf.org">OpenJS Foundation</a>, please see our <a href="https://trademark-policy.openjsf.org">Trademark Policy</a> and <a href="https://trademark-list.openjsf.org">Trademark List</a>.  Trademarks and logos not indicated on the <a href="https://trademark-list.openjsf.org">list of OpenJS Foundation trademarks</a> are trademarks&trade; or registered&reg; trademarks of their respective holders. Use of them does not imply any affiliation with or endorsement by them.</p>\n' +
+          '<p><a href="https://openjsf.org">The OpenJS Foundation</a> | <a href="https://terms-of-use.openjsf.org">Terms of Use</a> | <a href="https://privacy-policy.openjsf.org">Privacy Policy</a> | <a href="https://bylaws.openjsf.org">Bylaws</a> | <a href="https://code-of-conduct.openjsf.org">Code of Conduct</a> | <a href="https://trademark-policy.openjsf.org">Trademark Policy</a> | <a href="https://trademark-list.openjsf.org">Trademark List</a> | <a href="https://www.linuxfoundation.org/cookies">Cookie Policy</a></p>',
       },
       algolia: {
         indexName: 'jest-v2',
