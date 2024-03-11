bb4bb87e5dac25b4816fbcc9bc3e0e7ce31ad32d
Simen Bekkhus <sbekkhus91@gmail.com>
2024-03-11 08:15:52 +0100

    do not import `node:test`

--- e2e/__tests__/listTests.test.ts
+++ e2e/__tests__/listTests.test.ts
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import {describe} from 'node:test';
 import * as path from 'path';
 import * as fs from 'graceful-fs';
 import runJest from '../runJest';
