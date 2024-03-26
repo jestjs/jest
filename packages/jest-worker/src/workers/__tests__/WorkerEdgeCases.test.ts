9edb582ad68602c847fa4213d5ab1ee57220c2cf
Simen Bekkhus <sbekkhus91@gmail.com>
2024-03-26 09:10:52 +0100

    chore: skip flaky test on Node 22 as well

--- packages/jest-worker/src/workers/__tests__/WorkerEdgeCases.test.ts
+++ packages/jest-worker/src/workers/__tests__/WorkerEdgeCases.test.ts
@@ -28,7 +28,11 @@ const processChildWorkerPath = join(
 );
 const threadChildWorkerPath = join(writeDestination, 'workers/threadChild.js');
 
-if (process.platform === 'win32' && process.version.startsWith('v21.')) {
+// https://github.com/nodejs/node/issues/51766
+if (
+  process.platform === 'win32' &&
+  (process.version.startsWith('v21.') || process.version.startsWith('v22.'))
+) {
   // eslint-disable-next-line jest/no-focused-tests
   test.only('skipping test on broken platform', () => {
     console.warn('Skipping test on broken platform');
