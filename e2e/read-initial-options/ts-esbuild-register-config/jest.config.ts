a729f80f06e7da998837ebf3258d93c39f1a496f
Rajat v <rd.rajat23@gmail.com>
2024-07-15 09:27:12 +0000

    fix: failing checks

--- e2e/read-initial-options/ts-esbuild-register-config/jest.config.ts
+++ e2e/read-initial-options/ts-esbuild-register-config/jest.config.ts
@@ -1,11 +1,10 @@
 /**
- * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- *
- * @jest-config-loader esbuild-register
  */
+/**@jest-config-loader esbuild-register */
 interface Config {
   jestConfig: string;
 }
--- e2e/read-initial-options/ts-node-config/jest.config.ts
+++ e2e/read-initial-options/ts-node-config/jest.config.ts
@@ -1,11 +1,10 @@
 /**
- * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
- *
- * @jest-config-loader ts-node
  */
+/**@jest-config-loader ts-node */
 interface Config {
   jestConfig: string;
 }
