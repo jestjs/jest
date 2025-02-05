86deb20bf816833e2bcf456d13abe554a6ad25b1
Trung Nguyen <lantrungseo@gmail.com>
2025-02-05 16:29:09 +0700

    fix: add copyright header

--- e2e/resolve-conditions/__tests__/module-name-mapper.test.mjs
+++ e2e/resolve-conditions/__tests__/module-name-mapper.test.mjs
@@ -1,5 +1,10 @@
 /**
- * @jest-environment jsdom
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @jest-environment jest-environment-jsdom
  */
 
 import {fn} from 'fake-dual-dep2';
