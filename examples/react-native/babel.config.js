a94c6d03256764335bddc160f046727e07346761
cpojer <christoph.pojer@gmail.com>
2025-06-04 09:07:30 +0900

    Update deps.

--- examples/react-native/babel.config.js
+++ examples/react-native/babel.config.js
@@ -2,9 +2,6 @@
 
 const {createRequire} = require('module');
 
-const rnRequire = createRequire(require.resolve('react-native'));
-
 module.exports = {
-  // RN bundles this preset, so let's load it instead of depending on it ourselves
-  presets: [rnRequire.resolve('@react-native/babel-preset')],
+  presets: ['@react-native/babel-preset'],
 };
