113b947faf83f46db6421229c2ee3ba0422dc0a3
Tom Mrazauskas <tom@mrazauskas.de>
2025-06-12 11:19:07 +0300

    chore: remove `jest-repl` (#15673)
    Co-authored-by: eryue0220 <eryue0220@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -705,7 +705,6 @@ const config = typescriptEslint.config(
       'scripts/*',
       'packages/*/__benchmarks__/test.js',
       'packages/create-jest/src/runCreate.ts',
-      'packages/jest-repl/src/cli/runtime-cli.ts',
     ],
     rules: {
       'no-console': 'off',
