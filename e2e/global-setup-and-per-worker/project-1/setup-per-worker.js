e2ff74503aa4092f55d96067c59dbfd471ec4512
Eyal Roth <eyalroth1@gmail.com>
2025-06-28 18:56:41 +0300

    #8708 Add e2e for combination of globalSetup and globalSetupPerWorker

---
+++ e2e/global-setup-and-per-worker/project-1/setup-per-worker.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const crypto = require('crypto');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const {createDirectory} = require('jest-util');
+
+const DIR = path.join(
+  os.tmpdir(),
+  'jest-global-setup-and-per-worker-project-1',
+);
+
+module.exports = function () {
+  return new Promise((resolve, reject) => {
+    createDirectory(DIR);
+    const fileId = crypto.randomBytes(20).toString('hex');
+    const data = ['setup-per-worker', process.env.JEST_WORKER_ID].join('\n');
+    fs.writeFileSync(path.join(DIR, fileId), data);
+    resolve();
+  });
+};
