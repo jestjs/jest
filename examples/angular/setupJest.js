26dfb4e295847489fc0e9c60351cc83abb6221e9
Simen Bekkhus <sbekkhus91@gmail.com>
2025-12-12 09:51:49 +0100

    chore: update angular in example

--- examples/angular/setupJest.js
+++ examples/angular/setupJest.js
@@ -1,18 +1,6 @@
 'use strict';
 
-require('core-js/proposals/reflect-metadata');
-require('zone.js/dist/zone.js');
-require('zone.js/dist/proxy.js');
-require('zone.js/dist/sync-test');
-require('zone.js/dist/async-test');
-require('zone.js/dist/fake-async-test');
 // eslint-disable-next-line import-x/no-extraneous-dependencies
-require('jest-zone-patch');
+const {setupZoneTestEnv} = require('jest-preset-angular/setup-env/zone');
 
-const {getTestBed} = require('@angular/core/testing');
-const testingModule = require('@angular/platform-browser-dynamic/testing');
-
-getTestBed().initTestEnvironment(
-  testingModule.BrowserDynamicTestingModule,
-  testingModule.platformBrowserDynamicTesting(),
-);
+setupZoneTestEnv();
