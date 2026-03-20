76043aae767547f2cce9df5404f71a0aeed8c40b
Ashok Narayan <vs.ashoknarayan@gmail.com>
2026-03-21 01:17:06 +0530

    test: fix typing for innerDone

--- packages/jest-circus/src/__tests__/asyncError.test.ts
+++ packages/jest-circus/src/__tests__/asyncError.test.ts
@@ -2,7 +2,7 @@ describe('async error handling', () => {
   it('does not crash when asyncError is undefined and a plain object is thrown', done => {
     const {test} = require('../');
 
-    test('inner test', innerDone => {
+    test('inner test', (innerDone: any) => {
       // simulate async error path (where asyncError can be undefined)
       Promise.resolve().then(() => {
         throw {status: 403, message: 'Forbidden'};
