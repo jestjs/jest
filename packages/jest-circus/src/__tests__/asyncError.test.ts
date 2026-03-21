569688084e686ae1aabd1d49ab6b12780e6a29d0
Ashok Narayan <vs.ashoknarayan@gmail.com>
2026-03-21 11:11:29 +0530

    Add header

--- packages/jest-circus/src/__tests__/asyncError.test.ts
+++ packages/jest-circus/src/__tests__/asyncError.test.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+
 describe('async error handling', () => {
   it('does not crash when asyncError is undefined and a plain object is thrown', done => {
     const {test} = require('../');
