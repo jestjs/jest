f53796ecd403ab71931db500cf1c6dfacbebcca2
Gandor <juraj.muransky@gmail.com>
2026-04-22 10:36:06 +0200

    add tests

---
+++ e2e/native-esm-cjs-require/esm-dep.mjs
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export const answer = 42;
+
+export function double(n) {
+  return n * 2;
+}
