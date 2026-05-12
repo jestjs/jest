e528aed49ad0816d57765f7813480f463ee7a187
Stell Hub <xiaoyaoyunlian@gmail.com>
2026-05-12 12:47:04 +0800

    Normalize node: prefixes in built jest-util bundles

--- packages/jest-util/src/isError.ts
+++ packages/jest-util/src/isError.ts
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import {isNativeError} from 'util/types';
+import {isNativeError} from 'node:util/types';
 
 export const isError =
   typeof Error.isError === 'function' ? Error.isError : isNativeError;
