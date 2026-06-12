e11fa5068da734b23e214f088c040607102aa75c
fhaziz0730 <fhaziz073@gmail.com>
2026-06-12 16:53:06 -0400

    Finally understood issue behind Satisfying generic and have a new working test for Satisfying.

--- packages/expect/src/asymmetricMatchers.ts
+++ packages/expect/src/asymmetricMatchers.ts
@@ -297,15 +297,22 @@ class ObjectContaining extends AsymmetricMatcher<
   }
 }
 
-class Satisfying extends AsymmetricMatcher<(value: unknown) => boolean> {
-  constructor(sample: (value: unknown) => boolean, inverse = false) {
+class Satisfying<T> extends AsymmetricMatcher<(value: T) => boolean> {
+  constructor(sample: (value: T) => boolean, inverse = false) {
     super(sample, inverse);
   }
-  asymmetricMatch(other: unknown): boolean {
-    return true;
+  asymmetricMatch(other: T): boolean {
+    if (typeof this.sample !== 'function') {
+      throw new TypeError(
+        `You must provide a predicate to ${this.toString()}, not '${typeof this.sample}'.`,
+      );
+    }
+    const result = this.sample(other);
+    return this.inverse ? !result : result;
   }
   toString() {
-    return `${this.inverse ? 'Not' : ''}Satisfying[]`;
+    const name = this.sample.name === '' ? '' : `[${this.sample.name}]`;
+    return `${this.inverse ? 'Not' : ''}Satisfying${name}`;
   }
 }
 
@@ -426,6 +433,11 @@ export const objectContaining = (
 export const objectNotContaining = (
   sample: Record<string, unknown>,
 ): ObjectContaining => new ObjectContaining(sample, true);
+export const satisfying = <T>(expected: (value: T) => boolean): Satisfying<T> =>
+  new Satisfying(expected);
+export const notSatisfying = <T>(
+  expected: (value: T) => boolean,
+): Satisfying<T> => new Satisfying(expected, true);
 export const stringContaining = (expected: string): StringContaining =>
   new StringContaining(expected);
 export const stringNotContaining = (expected: string): StringContaining =>
