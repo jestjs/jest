b365abad150f1bcfd89a3c34a35b8638f9d5969f
Simen Bekkhus <sbekkhus91@gmail.com>
2026-08-10 17:15:57 +0200

    update to RNTL

--- examples/react-native/__tests__/intro.test.js
+++ examples/react-native/__tests__/intro.test.js
@@ -4,50 +4,36 @@
  * Sample React Native Snapshot Test
  */
 
+import {render, screen} from '@testing-library/react-native';
 import React from 'react';
 import {ActivityIndicator, FlatList, Text, TextInput} from 'react-native';
-import renderer, {act} from 'react-test-renderer';
 import Intro from '../Intro';
 
 jest.setTimeout(15_000);
 
-it('renders correctly', () => {
-  let tree;
-  act(() => {
-    tree = renderer.create(<Intro />);
-  });
-  expect(tree.toJSON()).toMatchSnapshot();
+it('renders correctly', async () => {
+  await render(<Intro />);
+  expect(screen.toJSON()).toMatchSnapshot();
 });
 
 // These serve as integration tests for the jest-react-native preset.
-it('renders the ActivityIndicator component', () => {
-  let tree;
-  act(() => {
-    tree = renderer.create(<ActivityIndicator animating={true} size="small" />);
-  });
-  expect(tree.toJSON()).toMatchSnapshot();
+it('renders the ActivityIndicator component', async () => {
+  await render(<ActivityIndicator animating={true} size="small" />);
+  expect(screen.toJSON()).toMatchSnapshot();
 });
 
-it('renders the TextInput component', () => {
-  let tree;
-  act(() => {
-    tree = renderer.create(
-      <TextInput autoCorrect={false} value="apple banana kiwi" />,
-    );
-  });
-  expect(tree.toJSON()).toMatchSnapshot();
+it('renders the TextInput component', async () => {
+  await render(<TextInput autoCorrect={false} value="apple banana kiwi" />);
+  expect(screen.toJSON()).toMatchSnapshot();
 });
 
-it('renders the FlatList component', () => {
-  let tree;
-  act(() => {
-    tree = renderer.create(
-      <FlatList
-        data={['apple', 'banana', 'kiwi']}
-        keyExtractor={item => item}
-        renderItem={({item}) => <Text>{item}</Text>}
-      />,
-    );
-  });
-  expect(tree.toJSON()).toMatchSnapshot();
+it('renders the FlatList component', async () => {
+  await render(
+    <FlatList
+      data={['apple', 'banana', 'kiwi']}
+      keyExtractor={item => item}
+      renderItem={({item}) => <Text>{item}</Text>}
+    />,
+  );
+  expect(screen.toJSON()).toMatchSnapshot();
 });
