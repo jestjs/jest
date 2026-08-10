fd51bf965af12e361aa7da4b772a3f0a22d64c08
Simen Bekkhus <sbekkhus91@gmail.com>
2026-08-10 17:05:04 +0200

    wrap in act

--- examples/react-native/__tests__/intro.test.js
+++ examples/react-native/__tests__/intro.test.js
@@ -6,40 +6,48 @@
 
 import React from 'react';
 import {ActivityIndicator, FlatList, Text, TextInput} from 'react-native';
-import renderer from 'react-test-renderer';
+import renderer, {act} from 'react-test-renderer';
 import Intro from '../Intro';
 
 jest.setTimeout(15_000);
 
 it('renders correctly', () => {
-  const tree = renderer.create(<Intro />).toJSON();
-  expect(tree).toMatchSnapshot();
+  let tree;
+  act(() => {
+    tree = renderer.create(<Intro />);
+  });
+  expect(tree.toJSON()).toMatchSnapshot();
 });
 
 // These serve as integration tests for the jest-react-native preset.
 it('renders the ActivityIndicator component', () => {
-  const tree = renderer
-    .create(<ActivityIndicator animating={true} size="small" />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+  let tree;
+  act(() => {
+    tree = renderer.create(<ActivityIndicator animating={true} size="small" />);
+  });
+  expect(tree.toJSON()).toMatchSnapshot();
 });
 
 it('renders the TextInput component', () => {
-  const tree = renderer
-    .create(<TextInput autoCorrect={false} value="apple banana kiwi" />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+  let tree;
+  act(() => {
+    tree = renderer.create(
+      <TextInput autoCorrect={false} value="apple banana kiwi" />,
+    );
+  });
+  expect(tree.toJSON()).toMatchSnapshot();
 });
 
 it('renders the FlatList component', () => {
-  const tree = renderer
-    .create(
+  let tree;
+  act(() => {
+    tree = renderer.create(
       <FlatList
         data={['apple', 'banana', 'kiwi']}
         keyExtractor={item => item}
         renderItem={({item}) => <Text>{item}</Text>}
       />,
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+    );
+  });
+  expect(tree.toJSON()).toMatchSnapshot();
 });
